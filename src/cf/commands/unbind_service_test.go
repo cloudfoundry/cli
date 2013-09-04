5a8bd0d2f913c38afe822d4ce245c8bbaf776083
Casey McTaggart <cmctaggart@pivotallabs.com>
2013-09-04 09:29:16 -0600

    Use requirements in unbind service command
    Signed-off-by: Damien Le Berrigaud <damien@pivotallabs.com>

--- src/cf/commands/unbind_service_test.go
+++ src/cf/commands/unbind_service_test.go
@@ -5,6 +5,7 @@ import (
 	"cf/api"
 	. "cf/commands"
 	"cf/configuration"
+	"cf/requirements"
 	"github.com/stretchr/testify/assert"
 	"testhelpers"
 	"testing"
@@ -13,14 +14,16 @@ import (
 func TestUnbindCommand(t *testing.T) {
 	app := cf.Application{Name: "my-app", Guid: "my-app-guid"}
 	serviceInstance := cf.ServiceInstance{Name: "my-service", Guid: "my-service-guid"}
-
-	appRepo := &testhelpers.FakeApplicationRepository{AppByName: app}
+	reqFactory := &testhelpers.FakeReqFactory{
+		Application:     app,
+		ServiceInstance: serviceInstance,
+	}
 	serviceRepo := &testhelpers.FakeServiceRepo{FindInstanceByNameServiceInstance: serviceInstance}
 	config := &configuration.Configuration{}
-	fakeUI := callUnbindService([]string{"--service", "my-service", "--app", "my-app"}, config, serviceRepo, appRepo)
+	fakeUI := callUnbindService([]string{"--service", "my-service", "--app", "my-app"}, config, reqFactory, serviceRepo)
 
-	assert.Equal(t, serviceRepo.FindInstanceByNameName, "my-service")
-	assert.Equal(t, appRepo.AppName, "my-app")
+	assert.Equal(t, reqFactory.ApplicationName, "my-app")
+	assert.Equal(t, reqFactory.ServiceInstanceName, "my-service")
 
 	assert.Contains(t, fakeUI.Outputs[0], "Unbinding service")
 	assert.Contains(t, fakeUI.Outputs[0], "my-service")
@@ -32,9 +35,13 @@ func TestUnbindCommand(t *testing.T) {
 	assert.Contains(t, fakeUI.Outputs[1], "OK")
 }
 
-func callUnbindService(args []string, config *configuration.Configuration, serviceRepo api.ServiceRepository, appRepo api.ApplicationRepository) (fakeUI *testhelpers.FakeUI) {
+func callUnbindService(args []string, config *configuration.Configuration, reqFactory requirements.Factory, serviceRepo api.ServiceRepository) (fakeUI *testhelpers.FakeUI) {
 	fakeUI = new(testhelpers.FakeUI)
-	target := NewUnbindService(fakeUI, config, serviceRepo, appRepo)
-	target.Run(testhelpers.NewContext("unbind-service", args))
+	ctxt := testhelpers.NewContext("unbind-service", args)
+
+	cmd := NewUnbindService(fakeUI, config, serviceRepo)
+	cmd.GetRequirements(reqFactory, ctxt)
+	cmd.Run(ctxt)
+
 	return
 }
