c5b4c2757c97bae09d3cb0c068f92a12195c0fba
Casey McTaggart <cmctaggart@pivotallabs.com>
2013-09-04 09:56:19 -0600

    Use requirements in delete command
    Signed-off-by: Damien Le Berrigaud <damien@pivotallabs.com>

--- src/cf/commands/delete.go
+++ src/cf/commands/delete.go
@@ -13,27 +13,25 @@ type Delete struct {
 	ui      term.UI
 	config  *configuration.Configuration
 	appRepo api.ApplicationRepository
+	appReq  requirements.ApplicationRequirement
 }
 
-func NewDelete(ui term.UI, config *configuration.Configuration, appRepo api.ApplicationRepository) (d Delete) {
+func NewDelete(ui term.UI, config *configuration.Configuration, appRepo api.ApplicationRepository) (d *Delete) {
+	d = new(Delete)
 	d.ui = ui
 	d.config = config
 	d.appRepo = appRepo
 	return
 }
 
-func (cmd Delete) GetRequirements(reqFactory requirements.Factory, c *cli.Context) (reqs []Requirement) {
-	return
+func (cmd *Delete) GetRequirements(reqFactory requirements.Factory, c *cli.Context) (reqs []Requirement) {
+	cmd.appReq = reqFactory.NewApplicationRequirement(c.Args()[0])
+
+	return []Requirement{&cmd.appReq}
 }
 
-func (d Delete) Run(c *cli.Context) {
-	appName := c.Args()[0]
-
-	app, err := d.appRepo.FindByName(d.config, appName)
-	if err != nil {
-		d.ui.Failed("Error finding app.", err)
-		return
-	}
+func (d *Delete) Run(c *cli.Context) {
+	app := d.appReq.Application
 
 	response := strings.ToLower(d.ui.Ask("Really delete %s?>", app.Name))
 	if response != "y" && response != "yes" {
@@ -41,7 +39,7 @@ func (d Delete) Run(c *cli.Context) {
 	}
 
 	d.ui.Say("Deleting %s", app.Name)
-	err = d.appRepo.Delete(d.config, app)
+	err := d.appRepo.Delete(d.config, app)
 	if err != nil {
 		d.ui.Failed("Error deleting app.", err)
 		return
