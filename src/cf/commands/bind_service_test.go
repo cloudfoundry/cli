538d545b46b0fac4e767385effe0dbf95922e063
Damien Le Berrigaud <damien@pivotallabs.com>
2013-09-05 11:08:03 -0600

    Display custom message when trying to bind already bound service and app
    Signed-off-by: Casey McTaggart <cmctaggart@pivotallabs.com>

--- src/cf/commands/bind_service_test.go
+++ src/cf/commands/bind_service_test.go
@@ -17,7 +17,7 @@ func TestBindCommand(t *testing.T) {
 		Application:     app,
 		ServiceInstance: serviceInstance,
 	}
-	serviceRepo := &testhelpers.FakeServiceRepo{FindInstanceByNameServiceInstance: serviceInstance}
+	serviceRepo := &testhelpers.FakeServiceRepo{}
 	config := &configuration.Configuration{}
 	fakeUI := callBindService([]string{"--service", "my-service", "--app", "my-app"}, config, reqFactory, serviceRepo)
 
@@ -32,6 +32,22 @@ func TestBindCommand(t *testing.T) {
 	assert.Equal(t, serviceRepo.BindServiceApplication, app)
 
 	assert.Contains(t, fakeUI.Outputs[1], "OK")
+}
+
+func TestBindCommandIfServiceIsAlreadyBound(t *testing.T) {
+	app := cf.Application{Name: "my-app", Guid: "my-app-guid"}
+	serviceInstance := cf.ServiceInstance{Name: "my-service", Guid: "my-service-guid"}
+	reqFactory := &testhelpers.FakeReqFactory{
+		Application:     app,
+		ServiceInstance: serviceInstance,
+	}
+	serviceRepo := &testhelpers.FakeServiceRepo{BindServiceErrorCode: 90003}
+	config := &configuration.Configuration{}
+	fakeUI := callBindService([]string{"--service", "my-service", "--app", "my-app"}, config, reqFactory, serviceRepo)
+
+	assert.Contains(t, fakeUI.Outputs[0], "Binding service")
+	assert.Contains(t, fakeUI.Outputs[1], "OK")
+	assert.Contains(t, fakeUI.Outputs[2], "is already bound")
 }
 
 func TestBindCommandFailsWithUsage(t *testing.T) {
