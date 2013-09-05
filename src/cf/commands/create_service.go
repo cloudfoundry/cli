d7b5700a4cb72bf2585e1891d38a50219aaf977b
Damien Le Berrigaud <damien@pivotallabs.com>
2013-09-05 10:39:09 -0600

    Allow creating user provided service instances
    Signed-off-by: Casey McTaggart <cmctaggart@pivotallabs.com>

--- src/cf/commands/create_service.go
+++ src/cf/commands/create_service.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/codegangsta/cli"
+	"strings"
 )
 
 type CreateService struct {
@@ -25,14 +26,52 @@ func NewCreateService(ui term.UI, config *configuration.Configuration, sR api.Se
 }
 
 func (cmd CreateService) GetRequirements(reqFactory requirements.Factory, c *cli.Context) (reqs []Requirement, err error) {
+	offeringName := c.String("offering")
+	parameterList := c.String("parameters")
+
+	if offeringName == "user-provided" && parameterList == "" {
+		err = errors.New("Incorrect Usage")
+		cmd.ui.FailWithUsage(c, "create-service")
+		return
+	}
+
 	return
 }
 
 func (cmd CreateService) Run(c *cli.Context) {
 	name := c.String("name")
 	offeringName := c.String("offering")
-	planName := c.String("plan")
 
+	if offeringName == "user-provided" {
+		params := c.String("parameters")
+		cmd.createUserProvidedService(name, params)
+	} else {
+		planName := c.String("plan")
+		cmd.createService(name, offeringName, planName)
+	}
+}
+
+func (cmd CreateService) createUserProvidedService(name string, params string) {
+	paramsMap := make(map[string]string)
+	params = strings.Trim(params, `"`)
+
+	for _, param := range strings.Split(params, ",") {
+		param = strings.Trim(param, " ")
+		paramsMap[param] = cmd.ui.Ask("%s%s", param, term.Cyan(">"))
+	}
+
+	cmd.ui.Say("Creating service...")
+	err := cmd.serviceRepo.CreateUserProvidedServiceInstance(cmd.config, name, paramsMap)
+	if err != nil {
+		cmd.ui.Failed("Error creating user provided service instance", err)
+		return
+	}
+
+	cmd.ui.Ok()
+	return
+}
+
+func (cmd CreateService) createService(name string, offeringName string, planName string) {
 	offerings, err := cmd.serviceRepo.GetServiceOfferings(cmd.config)
 	if err != nil {
 		cmd.ui.Failed("Error fetching offerings", err)
--- src/cf/commands/create_service_test.go
+++ src/cf/commands/create_service_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-func TestCreateCommand(t *testing.T) {
+func TestCreateService(t *testing.T) {
 	serviceOfferings := []cf.ServiceOffering{
 		cf.ServiceOffering{Label: "cleardb", Plans: []cf.ServicePlan{
 			cf.ServicePlan{Name: "spark", Guid: "cleardb-spark-guid"},
@@ -19,7 +19,12 @@ func TestCreateCommand(t *testing.T) {
 	}
 	serviceRepo := &testhelpers.FakeServiceRepo{ServiceOfferings: serviceOfferings}
 	config := &configuration.Configuration{}
-	fakeUI := callCreateService([]string{"--offering", "cleardb", "--plan", "spark", "--name", "my-cleardb-service"}, config, serviceRepo)
+	fakeUI := callCreateService(
+		[]string{"--offering", "cleardb", "--plan", "spark", "--name", "my-cleardb-service"},
+		[]string{},
+		config,
+		serviceRepo,
+	)
 
 	assert.Contains(t, fakeUI.Outputs[0], "Creating service")
 	assert.Contains(t, fakeUI.Outputs[0], "my-cleardb-service")
@@ -28,9 +33,50 @@ func TestCreateCommand(t *testing.T) {
 	assert.Contains(t, fakeUI.Outputs[1], "OK")
 }
 
-func callCreateService(args []string, config *configuration.Configuration, serviceRepo api.ServiceRepository) (fakeUI *testhelpers.FakeUI) {
-	fakeUI = new(testhelpers.FakeUI)
-	target := NewCreateService(fakeUI, config, serviceRepo)
-	target.Run(testhelpers.NewContext("create-service", args))
+func TestCreateUserProvidedService(t *testing.T) {
+	serviceRepo := &testhelpers.FakeServiceRepo{}
+	config := &configuration.Configuration{}
+	fakeUI := callCreateService(
+		[]string{"--offering", "user-provided", "--name", "my-custom-service", "--parameters", `"foo, bar, baz"`},
+		[]string{"foo value", "bar value", "baz value"},
+		config,
+		serviceRepo,
+	)
+
+	assert.Contains(t, fakeUI.Prompts[0], "foo")
+	assert.Contains(t, fakeUI.Prompts[1], "bar")
+	assert.Contains(t, fakeUI.Prompts[2], "baz")
+
+	assert.Equal(t, serviceRepo.CreateUserProvidedServiceInstanceName, "my-custom-service")
+	assert.Equal(t, serviceRepo.CreateUserProvidedServiceInstanceParameters, map[string]string{
+		"foo": "foo value",
+		"bar": "bar value",
+		"baz": "baz value",
+	})
+
+	assert.Contains(t, fakeUI.Outputs[0], "Creating service")
+	assert.Contains(t, fakeUI.Outputs[1], "OK")
+}
+
+func TestCreateUserProvidedServiceWithNoParameterList(t *testing.T) {
+	serviceRepo := &testhelpers.FakeServiceRepo{}
+	config := &configuration.Configuration{}
+	fakeUI := callCreateService(
+		[]string{"--offering", "user-provided", "--name", "my-custom-service"},
+		[]string{},
+		config,
+		serviceRepo,
+	)
+
+	assert.Contains(t, fakeUI.Outputs[0], "FAILED")
+}
+
+func callCreateService(args []string, inputs []string, config *configuration.Configuration, serviceRepo api.ServiceRepository) (fakeUI *testhelpers.FakeUI) {
+	fakeUI = &testhelpers.FakeUI{Inputs: inputs}
+	ctxt := testhelpers.NewContext("create-service", args)
+	cmd := NewCreateService(fakeUI, config, serviceRepo)
+	reqFactory := &testhelpers.FakeReqFactory{}
+
+	testhelpers.RunCommand(cmd, ctxt, reqFactory)
 	return
 }
