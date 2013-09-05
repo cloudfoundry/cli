76f2e201f5931652603e86c7c2b87012ad6cc365
Ted Young <tyoung@pivotallabs.com>
2013-09-05 16:11:54 -0600

    Fix up password hiding code
    Signed-off-by: Mike Gehard <mgehard@pivotallabs.com>

--- src/cf/terminal/ui_unix.go
+++ src/cf/terminal/ui_unix.go
@@ -28,7 +28,6 @@ var ws syscall.WaitStatus = 0
 
 func (ui TerminalUI) AskForPassword(prompt string, args ...interface{}) (passwd string) {
 	sig := make(chan os.Signal, 10)
-	brk := make(chan bool)
 
 	// Display the prompt.
 	fmt.Println("")
@@ -41,30 +40,33 @@ func (ui TerminalUI) AskForPassword(prompt string, args ...interface{}) (passwd 
 	// watch for these signals so we can turn back on echo if need be.
 	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGKILL, syscall.SIGQUIT,
 		syscall.SIGTERM)
-	go catchSignal(fd, sig, brk)
+	defer signal.Stop(sig)
 
-	// Turn off the terminal echo.
+	go catchSignal(fd, sig)
+
 	pid, err := echoOff(fd)
+	defer echoOn(fd)
 	if err != nil {
 		return
 	}
 
-	// Turn on the terminal echo and stop listening for signals.
-	defer close(brk)
-	defer echoOn(fd)
-
-	rd := bufio.NewReader(os.Stdin)
-	syscall.Wait4(pid, &ws, 0, nil)
-
-	line, err := rd.ReadString('\n')
-	if err == nil {
-		passwd = strings.TrimSpace(line)
-	}
+	passwd = readPassword(pid)
 
 	// Carraige return after the user input.
 	fmt.Println("")
 
 	return
+}
+
+func readPassword(pid int) string {
+	rd := bufio.NewReader(os.Stdin)
+	syscall.Wait4(pid, &ws, 0, nil)
+
+	line, err := rd.ReadString('\n')
+	if err == nil {
+		return strings.TrimSpace(line)
+	}
+	return ""
 }
 
 func echoOff(fd []uintptr) (int, error) {
@@ -90,12 +92,10 @@ func echoOn(fd []uintptr) {
 // catchSignal tries to catch SIGKILL, SIGQUIT and SIGINT so that we can turn terminal
 // echo back on before the program ends.  Otherwise the user is left with echo off on
 // their terminal.
-func catchSignal(fd []uintptr, sig chan os.Signal, brk chan bool) {
+func catchSignal(fd []uintptr, sig chan os.Signal) {
 	select {
 	case <-sig:
 		echoOn(fd)
-		os.Exit(-1)
-	case <-brk:
-		signal.Stop(sig)
+		os.Exit(2)
 	}
 }
