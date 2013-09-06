689d75e2c4d54292188a88d80a6856f0acd88bdf
Casey McTaggart <cmctaggart@pivotallabs.com>
2013-09-06 14:31:58 -0600

    Change help text when app or space is not targeted
    Signed-off-by: Damien Le Berrigaud <damien@pivotallabs.com>

--- src/cf/requirements/space.go
+++ src/cf/requirements/space.go
@@ -18,7 +18,8 @@ func NewSpaceRequirement(ui terminal.UI, config *configuration.Configuration) Sp
 
 func (req SpaceRequirement) Execute() (err error) {
 	if !req.config.HasOrganization() {
-		message := fmt.Sprintf("No org and space targeted. Use '%s' to target an org and space.", terminal.Yellow("cf target -o"))
+		message := fmt.Sprintf("No org and space targeted. See '%s' to target an org and space.",
+			terminal.Yellow("cf target --o ORGNAME --s SPACENAME"))
 		req.ui.Failed(message, nil)
 		err = errors.New("No org and space targeted")
 		return
