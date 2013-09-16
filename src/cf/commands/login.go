a7dd5be051d1a72c3f8b25ac9d65acf887da759a
Mike Gehard <mgehard@pivotallabs.com>
2013-09-16 09:48:26 -0600

    Removed unused code
    Signed-off-by: Damien Le Berrigaud <damien@pivotallabs.com>

--- src/cf/commands/login.go
+++ src/cf/commands/login.go
@@ -1,13 +1,11 @@
 package commands
 
 import (
-	"cf"
 	"cf/api"
 	"cf/configuration"
 	"cf/requirements"
 	term "cf/terminal"
 	"github.com/codegangsta/cli"
-	"strconv"
 )
 
 const maxLoginTries = 3
@@ -70,89 +68,4 @@ func (l Login) Run(c *cli.Context) {
 
 		return
 	}
-}
-
-func (l Login) targetOrganization(config *configuration.Configuration, organizations []cf.Organization) {
-	var selectedOrg cf.Organization
-
-	if len(organizations) == 1 {
-		selectedOrg = organizations[0]
-	} else {
-		selectedOrg = l.chooseOrg(organizations)
-	}
-
-	l.ui.Say("Targeting org %s...", term.EntityNameColor(selectedOrg.Name))
-	err := l.saveOrg(config, selectedOrg)
-
-	if err == nil {
-		l.ui.Ok()
-	}
-}
-
-func (l Login) chooseOrg(orgs []cf.Organization) (org cf.Organization) {
-	for i, org := range orgs {
-		l.ui.Say("%s: %s", term.SuccessColor(strconv.Itoa(i+1)), org.Name)
-	}
-
-	index, err := strconv.Atoi(l.ui.Ask("Organization%s", term.PromptColor(">")))
-
-	if err != nil || index > len(orgs) {
-		l.ui.Failed("Invalid number")
-		return l.chooseOrg(orgs)
-	}
-
-	return orgs[index-1]
-}
-
-func (l Login) saveOrg(config *configuration.Configuration, org cf.Organization) (err error) {
-	config.Organization = org
-	err = l.configRepo.Save()
-
-	if err != nil {
-		l.ui.Failed(err.Error())
-		return
-	}
-
-	return
-}
-
-func (l Login) targetSpace(config *configuration.Configuration, spaces []cf.Space) {
-	if len(spaces) == 1 {
-		l.saveSpace(config, spaces[0])
-	} else {
-		selectedSpace := l.chooseSpace(spaces)
-		l.ui.Say("Targeting space %s...", term.EntityNameColor(selectedSpace.Name))
-		err := l.saveSpace(config, selectedSpace)
-
-		if err == nil {
-			l.ui.Ok()
-		}
-	}
-}
-
-func (l Login) chooseSpace(spaces []cf.Space) (space cf.Space) {
-	for i, space := range spaces {
-		l.ui.Say("%s: %s", term.SuccessColor(strconv.Itoa(i+1)), space.Name)
-	}
-
-	index, err := strconv.Atoi(l.ui.Ask("Space%s", term.PromptColor(">")))
-
-	if err != nil || index > len(spaces) {
-		l.ui.Failed("Invalid number")
-		return l.chooseSpace(spaces)
-	}
-
-	return spaces[index-1]
-}
-
-func (l Login) saveSpace(config *configuration.Configuration, space cf.Space) (err error) {
-	config.Space = space
-	err = l.configRepo.Save()
-
-	if err != nil {
-		l.ui.Failed(err.Error())
-		return
-	}
-
-	return
 }
