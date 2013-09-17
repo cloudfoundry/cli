8a9623f7dd55ae29a14e4cfe5026533c493099e6
Will and Ted <will+ted@gopivotal.com>
2013-09-16 17:20:19 -0700

    Assert not colorized on Windows

--- src/cf/terminal/color_test.go
+++ src/cf/terminal/color_test.go
@@ -3,11 +3,16 @@ package terminal
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"runtime"
 )
 
 func TestColorize(t *testing.T) {
 	text := "Hello World"
 	colorizedText := colorize(text, red, true)
 
-	assert.Equal(t, colorizedText, "\033[1;31mHello World\033[0m")
+	if runtime.GOOS == "windows" {
+        assert.Equal(t, colorizedText, "Hello World")
+    } else {
+	    assert.Equal(t, colorizedText, "\033[1;31mHello World\033[0m")
+	}
 }
