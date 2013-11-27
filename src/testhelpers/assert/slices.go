8b01f47af3563077cda17104a92879235adb62a6
Sheel Choksi <schoksi@pivotallabs.com>
2013-11-26 16:41:14 -0800

    Remove 'Staging...' output because we weren't always staging
    [fixes #60901098]

--- src/testhelpers/assert/slices.go
+++ src/testhelpers/assert/slices.go
@@ -6,15 +6,27 @@ import (
 	"strings"
 )
 
-func SliceContains(t *testing.T, actual []string, expected []string, msgAndArgs ...interface{}) bool{
+type Line []string
+func (line Line)String()string{
+	return strings.Join(line,", ")
+}
+
+type Lines []Line
+
+func SliceContains(t *testing.T, actual []string, expected Lines, msgAndArgs ...interface{}) bool {
 	expectedIndex := 0
 	for _, actualValue := range actual {
-		if strings.Contains(actualValue,expected[expectedIndex]) {
+		allStringsFound := true
+		for _, expectedValue := range expected[expectedIndex] {
+			allStringsFound = allStringsFound && strings.Contains(actualValue, expectedValue)
+		}
+
+		if allStringsFound {
 			expectedIndex++
 			if expectedIndex == len(expected) {
 				return true
 			}
 		}
 	}
-	return Fail(t, fmt.Sprintf("\"%s\" not found",expected[expectedIndex]), msgAndArgs...)
+	return Fail(t, fmt.Sprintf("\"%s\" not found", expected[expectedIndex]), msgAndArgs...)
 }
