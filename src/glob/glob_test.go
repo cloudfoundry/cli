8bc0107e2a60e3901f3cab46828229599703cb1a
Tim Jarratt <tjarratt@pivotallabs.com>
2013-12-23 17:48:12 -0800

    Allow windows style absolute paths
    unix style .cfingore files work on windows and vice versa

--- src/glob/glob_test.go
+++ src/glob/glob_test.go
@@ -27,6 +27,8 @@ var matches = [][]string{
 	{"/a?", "/ab", "/ac"},
 	{"/a*", "/a", "/ab", "/abc"},
 	{"/a**", "/a", "/ab", "/abc", "/a/", "/a/b", "/ab/c"},
+	{`c:\a\b\.d`, `c:\a\b\.d`},
+	{`c:\**\.d`, `c:\a\b\.d`},
 }
 
 var nonMatches = [][]string{
@@ -36,49 +38,13 @@ var nonMatches = [][]string{
 	{"/a**", "/", "/ba"},
 }
 
-var dontCompile = []string{
-	"",
-	"a",
-	"a/",
-	"/ ",
-	"/:",
-	"//",
-	"/a/",
-	"/a+b",
-	"/a^b",
-	"/a$b",
-	"/a[b",
-	"/a]b",
-	"/a(b",
-	"/a)b",
-	"/a世界",
-}
-
 func TestGlobTranslateOk(t *testing.T) {
 	for _, parts := range globs {
 		pat, exp := parts[0], parts[1]
 		got, err := translateGlob(pat)
-		if got != exp {
-			t.Errorf("expected %q, but got %q from %q", exp, got, pat)
-		}
-		if err != nil {
-			t.Errorf("in %q, unexpected err %v", pat, err)
-		}
-	}
-}
 
-func TestGlobTranslateError(t *testing.T) {
-	for _, pat := range dontCompile {
-		re, err := translateGlob(pat)
-		if err == nil {
-			t.Errorf("pat %q shouldn't translate, but got %q", pat, re)
-			continue
-		}
-
-		glob, err := CompileGlob(pat)
-		if err == nil {
-			t.Errorf("pat %q shouldn't compile, but got %#v", pat, glob)
-		}
+		assert.NoError(t, err)
+		assert.Equal(t, exp, got, "expected %q, but got %q from %q", exp, got, pat)
 	}
 }
 
@@ -86,11 +52,10 @@ func TestGlobMatches(t *testing.T) {
 	for _, parts := range matches {
 		pat, paths := parts[0], parts[1:]
 		glob, err := CompileGlob(pat)
-		assert.Equal(t, nil, err)
+
+		assert.NoError(t, err)
 		for _, path := range paths {
-			if !glob.Match(path) {
-				t.Errorf("pat %q should match %q", pat, path)
-			}
+			assert.True(t,glob.Match(path),"path %q should match %q", pat, path)
 		}
 	}
 }
@@ -99,11 +64,10 @@ func TestGlobNonMatches(t *testing.T) {
 	for _, parts := range nonMatches {
 		pat, paths := parts[0], parts[1:]
 		glob, err := CompileGlob(pat)
-		assert.Equal(t, nil, err)
+
+		assert.NoError(t, err)
 		for _, path := range paths {
-			if glob.Match(path) {
-				t.Errorf("pat %q should not match %q", pat, path)
-			}
+			assert.False(t,glob.Match(path),"path %q should match %q", pat, path)
 		}
 	}
 }
