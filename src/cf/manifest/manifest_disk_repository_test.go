fb0a81ad7a8e12abf7346e45be423ccf10ed530a
Max Brunsfeld <mbrunsfeld@pivotallabs.com>
2014-01-09 11:39:25 -0800

    Consolidate logic for finding manifest file into ManifestDiskRepository

--- src/cf/manifest/manifest_disk_repository_test.go
+++ src/cf/manifest/manifest_disk_repository_test.go
@@ -6,10 +6,14 @@ import (
 	"testing"
 )
 
-func TestReadManifestWithName(t *testing.T) {
+func assertFeatureFlag(t *testing.T) {
 	if os.Getenv("CF_MANIFEST") != "true" {
 		t.Fatal("CF_MANIFEST must be set to 'true' to run manifest tests")
 	}
+}
+
+func TestReadManifestWithDirectoryName(t *testing.T) {
+	assertFeatureFlag(t)
 	repo := NewManifestDiskRepository()
 	manifest, err := repo.ReadManifest("../../fixtures/example-app")
 
@@ -18,12 +22,30 @@ func TestReadManifestWithName(t *testing.T) {
 	assert.Equal(t, manifest.Applications[0].Get("name").(string), "hello")
 }
 
-func TestReadManifestWithBadPath(t *testing.T) {
-	if os.Getenv("CF_MANIFEST") != "true" {
-		t.Fatal("CF_MANIFEST must be set to 'true' to run manifest tests")
-	}
+func TestReadDifferentManifestFilename(t *testing.T) {
+	assertFeatureFlag(t)
 	repo := NewManifestDiskRepository()
-	_, err := repo.ReadManifest("some/path/that/doesnt/exist")
+	manifest, err := repo.ReadManifest("../../fixtures/different-manifest.yml")
 
-	assert.Error(t, err)
+	assert.NoError(t, err)
+	assert.Equal(t, len(manifest.Applications), 1)
+	assert.Equal(t, manifest.Applications[0].Get("name").(string), "goodbyte")
+}
+
+func TestReadManifestWithBadPath(t *testing.T) {
+	assertFeatureFlag(t)
+	repo := NewManifestDiskRepository()
+	m, err := repo.ReadManifest("some/path/that/doesnt/exist")
+
+	assert.NoError(t, err)
+	assert.Equal(t, m, NewEmptyManifest())
+}
+
+func TestReadManifestWithValidPathAndNoManifest(t *testing.T) {
+	assertFeatureFlag(t)
+	repo := NewManifestDiskRepository()
+	m, err := repo.ReadManifest(".")
+
+	assert.NoError(t, err)
+	assert.Equal(t, m, NewEmptyManifest())
 }
