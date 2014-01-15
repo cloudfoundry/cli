eb7c1dfa6addbcc6352c96bf8e38d8d3a6291ad0
Stephen Levine <slevine@pivotallabs.com>
2014-01-15 15:27:26 -0800

    Added replace-sha script for CI and windows CI

---
+++ bin/replace-sha.ps1
@@ -0,0 +1,5 @@
+$APP_CONST_FILE = $(split-path $MyInvocation.MyCommand.Definition) + "\..\src\cf\app_constants.go"
+$APP_CONST_FILE_TMP = $APP_CONST_FILE + ".tmp"
+
+get-content $APP_CONST_FILE | %{$_ -replace "SHA", ${git rev-parse --short HEAD}} > $APP_CONST_FILE_TMP
+mv -Force $APP_CONST_FILE_TMP $APP_CONST_FILE
