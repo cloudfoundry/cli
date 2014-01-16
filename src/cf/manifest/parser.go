bada422624645d7e6bcda01317f749241e957c13
Tim Jarratt <tjarratt@pivotallabs.com>
2014-01-16 11:14:33 -0800

    Remove some dead code
    Signed-off-by: Max Brunsfeld <mbrunsfeld@pivotallabs.com>

--- src/cf/manifest/parser.go
+++ src/cf/manifest/parser.go
@@ -21,13 +21,3 @@ func Parse(reader io.Reader) (yamlMap generic.Map, err error) {
 	yamlMap = generic.NewMap(document)
 	return
 }
-
-func ParseToManifest(reader io.Reader) (m *Manifest, errs ManifestErrors) {
-	mapp, err := Parse(reader)
-	if err != nil {
-		errs = append(errs, err)
-		return
-	}
-
-	return NewManifest(mapp)
-}
