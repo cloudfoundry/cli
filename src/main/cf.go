d5e093b2f2df34c67b08c42d9a934df547c1874b
Max Brunsfeld <mbrunsfeld@pivotallabs.com>
2014-01-17 17:40:33 -0800

    Display helpful message when cf crashes
    [finishes #64011004]
    
    Signed-off-by: Tim Jarratt <tjarratt@pivotallabs.com>

--- src/main/cf.go
+++ src/main/cf.go
@@ -13,9 +13,19 @@ import (
 	"fileutils"
 	"fmt"
 	"os"
+	"runtime/debug"
+	"strings"
 )
 
 func main() {
+	defer func() {
+		maybeSomething := recover()
+
+		if maybeSomething != nil {
+			displayCrashDialog()
+		}
+	}()
+
 	fileutils.SetTmpPathPrefix("cf")
 
 	if os.Getenv("CF_COLOR") == "" {
@@ -56,4 +66,33 @@ func loadConfig(termUI terminal.UI, configRepo configuration.ConfigurationReposi
 		return
 	}
 	return
+}
+
+func displayCrashDialog() {
+	formattedString := `
+
+       d8888 888       888 888       888        .d8888b.  888    888 888     888  .d8888b.  888    d8P   .d8888b.
+      d88888 888   o   888 888   o   888       d88P  Y88b 888    888 888     888 d88P  Y88b 888   d8P   d88P  Y88b
+     d88P888 888  d8b  888 888  d8b  888       Y88b.      888    888 888     888 888    888 888  d8P    Y88b.
+    d88P 888 888 d888b 888 888 d888b 888        "Y888b.   8888888888 888     888 888        888d88K      "Y888b.
+   d88P  888 888d88888b888 888d88888b888           "Y88b. 888    888 888     888 888        8888888b        "Y88b.
+  d88P   888 88888P Y88888 88888P Y88888             "888 888    888 888     888 888    888 888  Y88b         "888
+ d8888888888 8888P   Y8888 8888P   Y8888       Y88b  d88P 888    888 Y88b. .d88P Y88b  d88P 888   Y88b  Y88b  d88P d8b
+d88P     888 888P     Y888 888P     Y888        "Y8888P"  888    888  "Y88888P"   "Y8888P"  888    Y88b  "Y8888P"  Y8P
+
+
+Something completely unexpected happened. This is a bug in %s.
+Please file this bug : https://github.com/cloudfoundry/cli/issues
+Tell us that you ran this command:
+
+	%s
+
+and got this stack trace:
+
+%s
+	`
+
+	stackTrace := "\t" + strings.Replace(string(debug.Stack()), "\n", "\n\t", -1)
+	println(fmt.Sprintf(formattedString, cf.Name(), strings.Join(os.Args, " "), stackTrace))
+	os.Exit(1)
 }
