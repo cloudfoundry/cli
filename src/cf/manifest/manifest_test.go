0b841773a853b0f9a83757605eebed253e0f286a
Tim Jarratt <tjarratt@pivotallabs.com>
2014-01-22 10:26:03 -0800

    Validation of manifest keys is more consistent
    If you parse a manifest key, you now have to validate that it is not
    nil.
    
    [Finishes #64207168]
    
    Signed-off-by: Stephen Levine <slevine@pivotallabs.com>

--- src/cf/manifest/manifest_test.go
+++ src/cf/manifest/manifest_test.go
@@ -5,6 +5,8 @@ import (
 	"generic"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	testassert "testhelpers/assert"
+	"strings"
 )
 
 func TestManifestWithGlobalAndAppSpecificProperties(t *testing.T) {
@@ -72,6 +74,37 @@ func TestParsingManifestWithEmptyEnvVarIsInvalid(t *testing.T) {
 	assert.Contains(t, err.Error(), "env var 'bar' should not be null")
 }
 
+func TestParsingManifestWithNulls(t *testing.T) {
+	_, errs := manifest.NewManifest(generic.NewMap(map[string]interface{}{
+		"applications": []interface{}{
+			map[string]interface{}{
+				"buildpack": nil,
+				"disk_quota": nil,
+				"domain": nil,
+				"host": nil,
+				"name": nil,
+				"path": nil,
+				"stack": nil,
+				"memory": nil,
+				"instances": nil,
+				"timeout": nil,
+				"no-route": nil,
+				"services": nil,
+				"env": nil,
+			},
+		},
+	}))
+
+	assert.Error(t, errs)
+	errorSlice  := strings.Split(errs.Error(), "\n")
+	manifestKeys := []string{"buildpack", "disk_quota", "domain", "host", "name", "path", "stack",
+							 "memory", "instances", "timeout", "no-route", "services", "env"}
+
+	for _, key := range manifestKeys {
+		testassert.SliceContains(t, errorSlice, testassert.Lines{{key, "not be null"}})
+	}
+}
+
 func TestParsingManifestWithPropertiesReturnsErrors(t *testing.T) {
 	_, err := manifest.NewManifest(generic.NewMap(map[string]interface{}{
 		"applications": []interface{}{
@@ -79,7 +112,6 @@ func TestParsingManifestWithPropertiesReturnsErrors(t *testing.T) {
 				"env": map[string]interface{}{
 					"bar": "many-${foo}-are-cool",
 				},
-				"instances": nil,
 			},
 		},
 	}))
