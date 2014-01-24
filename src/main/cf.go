9db2798c82ab80ae26cb9b06d3b9e7cf6066427b
Tim Jarratt <tjarratt@pivotallabs.com>
2014-01-24 09:20:33 -0800

    Revert "Aww shucks"
    This reverts commit 7861ba62e1e108bcb1c87bb15e300f2ba15c6c28.

--- src/main/cf.go
+++ src/main/cf.go
@@ -16,7 +16,6 @@ import (
 	"os"
 	"runtime/debug"
 	"strings"
-	"github.com/olekukonko/ts"
 )
 
 func main() {
@@ -111,13 +110,13 @@ func loadConfig(termUI terminal.UI, configRepo configuration.ConfigurationReposi
 func displayCrashDialog() {
 	formattedString := `
 
-%s
+Aww shucks.
 
 Something completely unexpected happened. This is a bug in %s.
 Please file this bug : https://github.com/cloudfoundry/cli/issues
 Tell us that you ran this command:
 
-%s
+	%s
 
 and got this stack trace:
 
@@ -125,26 +124,6 @@ and got this stack trace:
 	`
 
 	stackTrace := "\t" + strings.Replace(string(debug.Stack()), "\n", "\n\t", -1)
-	println(fmt.Sprintf(formattedString, awwShucks(), cf.Name(), strings.Join(os.Args, " "), stackTrace))
+	println(fmt.Sprintf(formattedString, cf.Name(), strings.Join(os.Args, " "), stackTrace))
 	os.Exit(1)
-}
-
-func awwShucks() string {
-	size, err := ts.GetSize()
-	if err != nil {
-		return "Aww shucks."
-	} else if size.Col() < 80 {
-		return "Aww shucks."
-	} else {
-		return `
-                               _____ _                _
-     /\                       / ____| |              | |
-    /  \__      ____      __ | (___ | |__  _   _  ___| | _____
-   / /\ \ \ /\ / /\ \ /\ / /  \___ \| '_ \| | | |/ __| |/ / __|
-  / ____ \ V  V /  \ V  V /   ____) | | | | |_| | (__|   <\__ \_
- /_/    \_\_/\_/    \_/\_/   |_____/|_| |_|\__,_|\___|_|\_\___(_)
-
-
-		`
-	}
 }
