0426e3f4bc2a7debee93a17c3bc38d963c537fd0
Tim Jarratt <tjarratt@pivotallabs.com>
2014-01-28 10:15:27 -0800

    Remove create-domain TIP referencing map-domain

--- src/cf/commands/domain/create_domain.go
+++ src/cf/commands/domain/create_domain.go
@@ -1,7 +1,6 @@
 package domain
 
 import (
-	"cf"
 	"cf/api"
 	"cf/configuration"
 	"cf/requirements"
@@ -57,6 +56,4 @@ func (cmd *CreateDomain) Run(c *cli.Context) {
 	}
 
 	cmd.ui.Ok()
-
-	cmd.ui.Say("TIP: Use '%s' to assign it to a space", terminal.CommandColor(cf.Name()+" map-domain"))
 }
