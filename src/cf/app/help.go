e15d27979ac876aafce9f62bb94896b93a5dd956
Ted Young <tyoung@pivotallabs.com>
2014-01-27 16:40:06 -0800

    alphabetize env vars

--- src/cf/app/help.go
+++ src/cf/app/help.go
@@ -27,10 +27,10 @@ var appHelpTemplate = `{{.Title "NAME:"}}
    {{range .Flags}}{{.}}
    {{end}}
 {{.Title "ENVIRONMENT VARIABLES:"}}
+   CF_COLOR=false - will not colorize output
    CF_HOME=path/to/config/ override default config directory
    CF_STAGING_TIMEOUT=15 max wait time for buildpack staging, in minutes
    CF_STARTUP_TIMEOUT=5 max wait time for app instance startup, in minutes
-   CF_COLOR=false - will not colorize output
    CF_TRACE=true - print API request diagnostics to stdout
    CF_TRACE=path/to/trace.log - append API request diagnostics to a log file
    HTTP_PROXY=http://proxy.example.com:8080 - enable HTTP proxying for API requests
