66034623e6a1ded237e3b80e3d0faad6a27abfc7
Chris Jobst and Tim Jarratt <cjobst@pivotallabs.com>
2014-02-03 17:49:16 -0800

    Update generic package to use Ginkgo
    Also included these tests in our test runner

--- src/generic/map_test.go
+++ src/generic/map_test.go
@@ -2,48 +2,54 @@ package generic
 
 import (
 	"fmt"
+	. "github.com/onsi/ginkgo"
 	"github.com/stretchr/testify/assert"
-	"testing"
+	mr "github.com/tjarratt/mr_t"
 )
 
-func TestDeepMerge(t *testing.T) {
-	map1 := NewMap(map[interface{}]interface{}{
-		"key1": "val1",
-		"key2": "val2",
-		"nest1": map[interface{}]interface{}{
-			"nestKey1": "nest1Val1",
-			"nestKey2": "nest1Val2",
-		},
-		"nest2": []interface{}{
-			"nest2Val1",
-		},
-	})
+func init() {
+	Describe("Testing with ginkgo", func() {
+		It("TestDeepMerge", func() {
 
-	map2 := NewMap(map[interface{}]interface{}{
-		"key1": "newVal1",
-		"nest1": map[interface{}]interface{}{
-			"nestKey1": "newNest1Val1",
-		},
-		"nest2": []interface{}{
-			"something",
-		},
-	})
+			map1 := NewMap(map[interface{}]interface{}{
+				"key1": "val1",
+				"key2": "val2",
+				"nest1": map[interface{}]interface{}{
+					"nestKey1": "nest1Val1",
+					"nestKey2": "nest1Val2",
+				},
+				"nest2": []interface{}{
+					"nest2Val1",
+				},
+			})
 
-	expectedMap := NewMap(map[interface{}]interface{}{
-		"key1": "newVal1",
-		"key2": "val2",
-		"nest1": NewMap(map[interface{}]interface{}{
-			"nestKey1": "newNest1Val1",
-			"nestKey2": "nest1Val2",
-		}),
-		"nest2": []interface{}{
-			"nest2Val1",
-			"something",
-		},
-	})
+			map2 := NewMap(map[interface{}]interface{}{
+				"key1": "newVal1",
+				"nest1": map[interface{}]interface{}{
+					"nestKey1": "newNest1Val1",
+				},
+				"nest2": []interface{}{
+					"something",
+				},
+			})
 
-	mergedMap := DeepMerge(map1, map2)
-	fmt.Printf("Merged Map:\n%s\n", mergedMap)
-	fmt.Printf("Expected Map:\n%s\n", expectedMap)
-	assert.Equal(t, mergedMap, expectedMap)
+			expectedMap := NewMap(map[interface{}]interface{}{
+				"key1": "newVal1",
+				"key2": "val2",
+				"nest1": NewMap(map[interface{}]interface{}{
+					"nestKey1": "newNest1Val1",
+					"nestKey2": "nest1Val2",
+				}),
+				"nest2": []interface{}{
+					"nest2Val1",
+					"something",
+				},
+			})
+
+			mergedMap := DeepMerge(map1, map2)
+			fmt.Printf("Merged Map:\n%s\n", mergedMap)
+			fmt.Printf("Expected Map:\n%s\n", expectedMap)
+			assert.Equal(mr.T(), mergedMap, expectedMap)
+		})
+	})
 }
