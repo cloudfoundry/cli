55bd67e39ecaff96f0337b31251a2e048e32a97e
Andy Bravery <andrewjf_bravery@uk.ibm.com>
2014-02-05 14:59:52 +0000

    Change the word UserFields to User in the messages seen in the console

--- src/testhelpers/api/fake_user_repo.go
+++ src/testhelpers/api/fake_user_repo.go
@@ -43,7 +43,7 @@ func (repo *FakeUserRepository) FindByUsername(username string) (user cf.UserFie
 	user = repo.FindByUsernameUserFields
 
 	if repo.FindByUsernameNotFound {
-		apiResponse = net.NewNotFoundApiResponse("UserFields not found")
+		apiResponse = net.NewNotFoundApiResponse("User not found")
 	}
 
 	return
@@ -92,7 +92,7 @@ func (repo *FakeUserRepository) Create(username, password string) (apiResponse n
 	repo.CreateUserPassword = password
 
 	if repo.CreateUserExists {
-		apiResponse = net.NewApiResponse("UserFields already exists", cf.USER_EXISTS, 400)
+		apiResponse = net.NewApiResponse("User already exists", cf.USER_EXISTS, 400)
 	}
 
 	return
