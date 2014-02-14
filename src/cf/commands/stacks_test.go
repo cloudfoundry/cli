7d18dc53c6fa956c52c17edc79757cfdeefba837
Stephen Levine and Tim Jarratt <slevine@pivotallabs.com>
2014-02-14 14:20:48 -0800

    'stacks' command should require the user to be logged in

--- src/cf/commands/stacks_test.go
+++ src/cf/commands/stacks_test.go
@@ -4,38 +4,54 @@ import (
 	. "cf/commands"
 	"cf/models"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	testapi "testhelpers/api"
+	testreq "testhelpers/requirements"
 	testassert "testhelpers/assert"
 	testcmd "testhelpers/commands"
 	testconfig "testhelpers/configuration"
 	testterm "testhelpers/terminal"
 )
 
-func callStacks(stackRepo *testapi.FakeStackRepository) (ui *testterm.FakeUI) {
-	ui = &testterm.FakeUI{}
-	ctxt := testcmd.NewContext("stacks", []string{})
-	configRepo := testconfig.NewRepositoryWithDefaults()
-	cmd := NewStacks(ui, configRepo, stackRepo)
-	testcmd.RunCommand(cmd, ctxt, nil)
-
-	return
-}
-
 var _ = Describe("stacks command", func() {
+	var (
+		ui         *testterm.FakeUI
+		cmd        Stacks
+		repo       *testapi.FakeStackRepository
+		reqFactory *testreq.FakeReqFactory
+	)
+
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		config := testconfig.NewRepositoryWithDefaults()
+		reqFactory = &testreq.FakeReqFactory{LoginSuccess: true}
+		repo = &testapi.FakeStackRepository{}
+		cmd = NewStacks(ui, config, repo)
+	})
+
+	Describe("login requirements", func() {
+		It("fails if the user is not logged in", func() {
+			reqFactory.LoginSuccess = false
+			context := testcmd.NewContext("stacks", []string{})
+			testcmd.RunCommand(cmd, context, reqFactory)
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
+	})
+
 	It("lists the stacks", func() {
-		stack1 := models.Stack{}
-		stack1.Name = "Stack-1"
-		stack1.Description = "Stack 1 Description"
-
-		stack2 := models.Stack{}
-		stack2.Name = "Stack-2"
-		stack2.Description = "Stack 2 Description"
-
-		stackRepo := &testapi.FakeStackRepository{
-			FindAllStacks: []models.Stack{stack1, stack2},
+		stack1 := models.Stack{
+			Name: "Stack-1",
+			Description: "Stack 1 Description",
+		}
+		stack2 := models.Stack{
+			Name: "Stack-2",
+			Description: "Stack 2 Description",
 		}
 
-		ui := callStacks(stackRepo)
+		repo.FindAllStacks = []models.Stack{stack1, stack2}
+		context := testcmd.NewContext("stacks", []string{})
+		testcmd.RunCommand(cmd, context, reqFactory)
+
 		testassert.SliceContains(ui.Outputs, testassert.Lines{
 			{"Getting stacks in org", "my-org", "my-space", "my-user"},
 			{"OK"},
