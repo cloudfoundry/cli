c7793b9c2eb329bb2ff7ca0b98151ad9dc275a99
Stephen Levine <slevine@pivotallabs.com>
2014-02-13 16:47:42 -0800

    Add API endpoint requirement as part of login req
    [Finishes #65764594]

--- src/cf/requirements/login_test.go
+++ src/cf/requirements/login_test.go
@@ -1,6 +1,7 @@
 package requirements_test
 
 import (
+	"cf/configuration"
 	. "cf/requirements"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -9,21 +10,38 @@ import (
 	testterm "testhelpers/terminal"
 )
 
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestLoginRequirement", func() {
+var _ = Describe("LoginRequirement", func() {
+	var ui *testterm.FakeUI
 
-		ui := new(testterm.FakeUI)
-		config := testconfig.NewRepositoryWithDefaults()
+	BeforeEach(func() {
+		ui = new(testterm.FakeUI)
+	})
 
+	It("succeeds when given a config with an API endpoint and authentication", func() {
+		config := testconfig.NewRepositoryWithAccessToken(configuration.TokenInfo{Username: "my-user"})
+		config.SetApiEndpoint("api.example.com")
 		req := NewLoginRequirement(ui, config)
 		success := req.Execute()
 		Expect(success).To(BeTrue())
+	})
 
-		config.SetAccessToken("")
-		req = NewLoginRequirement(ui, config)
-		success = req.Execute()
+	It("fails when given a config with only an API endpoint", func() {
+		config := testconfig.NewRepository()
+		config.SetApiEndpoint("api.example.com")
+		req := NewLoginRequirement(ui, config)
+		success := req.Execute()
 		Expect(success).To(BeFalse())
 
 		testassert.SliceContains(ui.Outputs, testassert.Lines{{"Not logged in."}})
+	})
+
+	It("fails when given a config with neither an API endpoint nor authentication", func() {
+		config := testconfig.NewRepository()
+		req := NewLoginRequirement(ui, config)
+		success := req.Execute()
+		Expect(success).To(BeFalse())
+
+		testassert.SliceContains(ui.Outputs, testassert.Lines{{"No API endpoint"}})
+		testassert.SliceDoesNotContain(ui.Outputs, testassert.Lines{{"Not logged in."}})
 	})
 })
