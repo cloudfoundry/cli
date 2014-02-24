ed56a0402613fdb0053b03155bcbdb71394f5fdd
Max Brunsfeld and Tim Jarratt <tjarratt@pivotallabs.com>
2014-02-24 14:33:21 -0800

    Use std library for tempfile, tempdir
    This removes the cf prefix for tempfiles
    [Fixes #66221626] (#84)

--- src/fileutils/tmp_utils.go
+++ src/fileutils/tmp_utils.go
@@ -1,35 +1,13 @@
 package fileutils
 
 import (
-	"crypto/rand"
-	"fmt"
-	"math"
-	"math/big"
 	"os"
-	"path/filepath"
-	"time"
+	"io/ioutil"
 )
 
-const dirMask = os.ModeDir | os.ModeTemporary | os.ModePerm
-
-var tmpPathPrefix = ""
-
-func SetTmpPathPrefix(path string) {
-	tmpPathPrefix = path
-}
-
-func TmpPathPrefix() string {
-	return tmpPathPrefix
-}
-
 func TempDir(namePrefix string, cb func(tmpDir string, err error)) {
-	baseDir, err := baseTempDir()
-	if err != nil {
-		return
-	}
+	tmpDir, err := ioutil.TempDir("", namePrefix)
 
-	tmpDir := filepath.Join(baseDir, uniqueKey(namePrefix))
-	err = os.MkdirAll(tmpDir, dirMask)
 	defer func() {
 		os.RemoveAll(tmpDir)
 	}()
@@ -38,40 +16,12 @@ func TempDir(namePrefix string, cb func(tmpDir string, err error)) {
 }
 
 func TempFile(namePrefix string, cb func(tmpFile *os.File, err error)) {
-	var (
-		tmpFile     *os.File
-		tmpFilepath string
-		err         error
-		tmpDir      string
-	)
+	tmpFile, err := ioutil.TempFile("", namePrefix)
 
-	tmpDir, err = baseTempDir()
-	if err != nil {
-		cb(tmpFile, err)
-		return
-	}
-
-	tmpFilepath = filepath.Join(tmpDir, uniqueKey(namePrefix))
-	tmpFile, err = os.Create(tmpFilepath)
 	defer func() {
 		tmpFile.Close()
 		os.Remove(tmpFile.Name())
 	}()
 
 	cb(tmpFile, err)
-}
-
-func baseTempDir() (dir string, err error) {
-	dir = filepath.Join(os.TempDir(), TmpPathPrefix())
-	err = os.MkdirAll(dir, dirMask)
-	return
-}
-
-func uniqueKey(namePrefix string) string {
-	salt, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt32))
-	if err != nil {
-		salt = big.NewInt(1)
-	}
-
-	return fmt.Sprintf("%s_%d_%d", namePrefix, time.Now().Unix(), salt)
 }
