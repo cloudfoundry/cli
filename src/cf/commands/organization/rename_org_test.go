df6b8749a4a352f40c0d4288e62ff314fd253f4c
Max Brunsfeld and Tim Jarratt <mbrunsfeld@pivotallabs.com>
2014-03-06 11:39:57 -0800

    When renaming targetted org, update org name in config file
    [Finishes #63087464]

--- src/cf/commands/organization/rename_org_test.go
+++ src/cf/commands/organization/rename_org_test.go
@@ -2,6 +2,7 @@ package organization_test
 
 import (
 	"cf/commands/organization"
+	"cf/configuration"
 	"cf/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -14,61 +15,75 @@ import (
 )
 
 var _ = Describe("rename-org command", func() {
-	var reqFactory *testreq.FakeReqFactory
-	var orgRepo *testapi.FakeOrgRepository
+	var (
+		requirementsFactory *testreq.FakeReqFactory
+		orgRepo             *testapi.FakeOrgRepository
+		ui                  *testterm.FakeUI
+		configRepo          configuration.ReadWriter
+	)
 
 	BeforeEach(func() {
-		reqFactory = &testreq.FakeReqFactory{}
+		requirementsFactory = &testreq.FakeReqFactory{}
 		orgRepo = &testapi.FakeOrgRepository{}
+		ui = new(testterm.FakeUI)
+		configRepo = testconfig.NewRepositoryWithDefaults()
 	})
 
+	var callRenameOrg = func(args []string) {
+		cmd := organization.NewRenameOrg(ui, configRepo, orgRepo)
+		testcmd.RunCommand(cmd, testcmd.NewContext("rename-org", args), requirementsFactory)
+	}
+
 	It("fails with usage when given less than two args", func() {
-		ui := callRenameOrg([]string{}, reqFactory, orgRepo)
+		callRenameOrg([]string{})
 		Expect(ui.FailedWithUsage).To(BeTrue())
 
-		ui = callRenameOrg([]string{"foo"}, reqFactory, orgRepo)
+		callRenameOrg([]string{"foo"})
 		Expect(ui.FailedWithUsage).To(BeTrue())
 	})
 
 	It("fails requirements when not logged in", func() {
-		callRenameOrg([]string{"my-org", "my-new-org"}, reqFactory, orgRepo)
+		callRenameOrg([]string{"my-org", "my-new-org"})
 		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
 	})
 
 	Context("when logged in and given an org to rename", func() {
-		var ui *testterm.FakeUI
-
 		BeforeEach(func() {
 			org := models.Organization{}
-			org.Name = "my-org"
-			org.Guid = "my-org-guid"
-			reqFactory.Organization = org
-			reqFactory.LoginSuccess = true
-			ui = callRenameOrg([]string{"my-org", "my-new-org"}, reqFactory, orgRepo)
+			org.Name = "the-old-org-name"
+			org.Guid = "the-old-org-guid"
+			requirementsFactory.Organization = org
+			requirementsFactory.LoginSuccess = true
 		})
 
-		It("pass requirements", func() {
+		It("passes requirements", func() {
+			callRenameOrg([]string{"the-old-org-name", "the-new-org-name"})
 			Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
 		})
 
 		It("renames an organization", func() {
-			Expect(reqFactory.OrganizationName).To(Equal("my-org"))
+			targetedOrgName := configRepo.OrganizationFields().Name
+			callRenameOrg([]string{"the-old-org-name", "the-new-org-name"})
 			testassert.SliceContains(ui.Outputs, testassert.Lines{
-				{"Renaming org", "my-org", "my-new-org", "my-user"},
+				{"Renaming org", "the-old-org-name", "the-new-org-name", "my-user"},
 				{"OK"},
 			})
 
-			Expect(orgRepo.RenameOrganizationGuid).To(Equal("my-org-guid"))
-			Expect(orgRepo.RenameNewName).To(Equal("my-new-org"))
+			Expect(requirementsFactory.OrganizationName).To(Equal("the-old-org-name"))
+			Expect(orgRepo.RenameOrganizationGuid).To(Equal("the-old-org-guid"))
+			Expect(orgRepo.RenameNewName).To(Equal("the-new-org-name"))
+			Expect(configRepo.OrganizationFields().Name).To(Equal(targetedOrgName))
+		})
+
+		Describe("when the organization is currently targetted", func() {
+			It("updates the name of the org in the config", func() {
+				configRepo.SetOrganizationFields(models.OrganizationFields{
+					Guid: "the-old-org-guid",
+					Name: "the-old-org-name",
+				})
+				callRenameOrg([]string{"the-old-org-name", "the-new-org-name"})
+				Expect(configRepo.OrganizationFields().Name).To(Equal("the-new-org-name"))
+			})
 		})
 	})
 })
-
-func callRenameOrg(args []string, reqFactory *testreq.FakeReqFactory, orgRepo *testapi.FakeOrgRepository) (ui *testterm.FakeUI) {
-	ui = new(testterm.FakeUI)
-	ctxt := testcmd.NewContext("rename-org", args)
-	configRepo := testconfig.NewRepositoryWithDefaults()
-	cmd := organization.NewRenameOrg(ui, configRepo, orgRepo)
-	testcmd.RunCommand(cmd, ctxt, reqFactory)
-	return
-}
