22105f3e0b12f5960be2724591c8d6c22f3495b3
Max Brunsfeld and Tim Jarratt <mbrunsfeld@pivotallabs.com>
2014-03-06 17:23:35 -0800

    Remove unnecessary logic from fake endpoint repo

--- src/testhelpers/api/fake_endpoint_repo.go
+++ src/testhelpers/api/fake_endpoint_repo.go
@@ -1,42 +1,13 @@
 package api
 
-import (
-	"cf/configuration"
-	"cf/errors"
-)
+import "cf/errors"
 
 type FakeEndpointRepo struct {
-	Config configuration.ReadWriter
-
 	UpdateEndpointReceived string
 	UpdateEndpointError    errors.Error
-
-	LoggregatorEndpointReturns struct {
-		Endpoint string
-		Error    errors.Error
-	}
-
-	UAAEndpointReturns struct {
-		Endpoint string
-		Error    errors.Error
-	}
 }
 
-func (repo *FakeEndpointRepo) UpdateEndpoint(endpoint string) (finalEndpoint string, apiErr errors.Error) {
+func (repo *FakeEndpointRepo) UpdateEndpoint(endpoint string) (string, errors.Error) {
 	repo.UpdateEndpointReceived = endpoint
-	apiErr = repo.UpdateEndpointError
-
-	if apiErr != nil {
-		return
-	}
-
-	repo.Config.SetApiEndpoint(endpoint)
-	finalEndpoint = endpoint
-	return
-}
-
-func (repo *FakeEndpointRepo) GetLoggregatorEndpoint() (endpoint string, apiErr errors.Error) {
-	endpoint = repo.LoggregatorEndpointReturns.Endpoint
-	apiErr = repo.LoggregatorEndpointReturns.Error
-	return
+	return endpoint, repo.UpdateEndpointError
 }
