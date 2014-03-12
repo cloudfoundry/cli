7779f4db8934a03e7397c3f9cac4a4a434db0ae9
Karen Wang and Max Brunsfeld <mbrunsfeld@pivotallabs.com>
2014-03-11 17:35:42 -0700

    Add tests for .cfignore inclusions
    [#67147682]

--- src/cf/app_files_test.go
+++ src/cf/app_files_test.go
@@ -8,16 +8,32 @@ import (
 )
 
 var _ = Describe("AppFiles", func() {
+	fixturePath := filepath.Join("..", "fixtures", "applications")
 
-	Context("AppFilesInDir", func() {
+	Describe("AppFilesInDir", func() {
 		It("all files have '/' path separators", func() {
-			files, err := AppFilesInDir(filepath.Join("..", "fixtures", "applications"))
+			files, err := AppFilesInDir(fixturePath)
 			Expect(err).ShouldNot(HaveOccurred())
 
 			for _, afile := range files {
 				Expect(afile.Path).Should(Equal(filepath.ToSlash(afile.Path)))
 			}
 		})
-	})
 
+		It("excludes files based on the .cfignore file", func() {
+			appPath := filepath.Join(fixturePath, "app-with-cfignore")
+			files, err := AppFilesInDir(appPath)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			paths := []string{}
+			for _, file := range files {
+				paths = append(paths, file.Path)
+			}
+
+			Expect(paths).To(Equal([]string{
+				filepath.Join("dir1", "child-dir", "file3.txt"),
+				filepath.Join("dir1", "file1.txt"),
+			}))
+		})
+	})
 })
