bbeadfa46f2aefbb0bc68f19cb05b585919ee44b
Karen Wang and Max Brunsfeld <khwang+mbrunsfeld@pivotallabs.com>
2014-03-13 16:13:03 -0700

    Fix create-user when UAA is being directly used as auth server
    If the authorization server doesn't return an UAA endpoint link, assume that
    the auth server *is* the UAA, and use it for user management.
    
    [Finishes #67477014]

--- src/cf/api/authentication_test.go
+++ src/cf/api/authentication_test.go
@@ -21,122 +21,149 @@ var _ = Describe("AuthenticationRepository", func() {
 		testServer *httptest.Server
 		handler    *testnet.TestHandler
 		config     configuration.ReadWriter
+		auth       AuthenticationRepository
 	)
 
 	BeforeEach(func() {
-		config = testconfig.NewRepositoryWithDefaults()
+		config = testconfig.NewRepository()
 		gateway = net.NewUAAGateway(config)
+		auth = NewUAAAuthenticationRepository(gateway, config)
 	})
 
 	AfterEach(func() {
 		testServer.Close()
 	})
 
-	It("logs in", func() {
-		testServer, handler, config = setupAuthDependencies(successfulLoginRequest)
+	var setupTestServer = func(request testnet.TestRequest) {
+		testServer, handler = testnet.NewServer([]testnet.TestRequest{request})
+		config.SetAuthenticationEndpoint(testServer.URL)
+	}
 
-		auth := NewUAAAuthenticationRepository(gateway, config)
-		apiErr := auth.Authenticate(map[string]string{
-			"username": "foo@example.com",
-			"password": "bar",
+	Describe("authenticating", func() {
+		var apiErr error
+
+		JustBeforeEach(func() {
+			apiErr = auth.Authenticate(map[string]string{
+				"username": "foo@example.com",
+				"password": "bar",
+			})
 		})
 
-		Expect(handler).To(testnet.HaveAllRequestsCalled())
-		Expect(apiErr).NotTo(HaveOccurred())
-		Expect(config.AuthenticationEndpoint()).To(Equal(testServer.URL))
-		Expect(config.AccessToken()).To(Equal("BEARER my_access_token"))
-		Expect(config.RefreshToken()).To(Equal("my_refresh_token"))
-	})
+		Describe("when login succeeds", func() {
+			BeforeEach(func() {
+				setupTestServer(successfulLoginRequest)
+			})
 
-	It("returns a failure response when login fails", func() {
-		testServer, handler, config = setupAuthDependencies(unsuccessfulLoginRequest)
-
-		auth := NewUAAAuthenticationRepository(gateway, config)
-		apiErr := auth.Authenticate(map[string]string{
-			"username": "foo@example.com",
-			"password": "bar",
+			It("stores the access and refresh tokens in the config", func() {
+				Expect(handler).To(testnet.HaveAllRequestsCalled())
+				Expect(apiErr).NotTo(HaveOccurred())
+				Expect(config.AuthenticationEndpoint()).To(Equal(testServer.URL))
+				Expect(config.AccessToken()).To(Equal("BEARER my_access_token"))
+				Expect(config.RefreshToken()).To(Equal("my_refresh_token"))
+			})
 		})
 
-		Expect(handler).To(testnet.HaveAllRequestsCalled())
-		Expect(apiErr).NotTo(BeNil())
-		Expect(apiErr.Error()).To(Equal("Password is incorrect, please try again."))
-		Expect(config.AccessToken()).To(BeEmpty())
-	})
+		Describe("when login fails", func() {
+			BeforeEach(func() {
+				setupTestServer(unsuccessfulLoginRequest)
+			})
 
-	It("returns a failure response when an error occurs during login", func() {
-		testServer, handler, config = setupAuthDependencies(errorLoginRequest)
-
-		auth := NewUAAAuthenticationRepository(gateway, config)
-		apiErr := auth.Authenticate(map[string]string{
-			"username": "foo@example.com",
-			"password": "bar",
+			It("returns an error", func() {
+				Expect(handler).To(testnet.HaveAllRequestsCalled())
+				Expect(apiErr).NotTo(BeNil())
+				Expect(apiErr.Error()).To(Equal("Password is incorrect, please try again."))
+				Expect(config.AccessToken()).To(BeEmpty())
+				Expect(config.RefreshToken()).To(BeEmpty())
+			})
 		})
 
-		Expect(handler).To(testnet.HaveAllRequestsCalled())
-		Expect(apiErr).To(HaveOccurred())
-		Expect(apiErr.Error()).To(Equal("Server error, status code: 500, error code: , message: "))
-		Expect(config.AccessToken()).To(BeEmpty())
-	})
+		Describe("when an error occurs during login", func() {
+			BeforeEach(func() {
+				setupTestServer(errorLoginRequest)
+			})
 
-	It("returns an error response when the UAA has an error but still returns a 200", func() {
-		testServer, handler, config = setupAuthDependencies(errorMaskedAsSuccessLoginRequest)
-
-		auth := NewUAAAuthenticationRepository(gateway, config)
-		apiErr := auth.Authenticate(map[string]string{
-			"username": "foo@example.com",
-			"password": "bar",
+			It("returns a failure response", func() {
+				Expect(handler).To(testnet.HaveAllRequestsCalled())
+				Expect(apiErr).To(HaveOccurred())
+				Expect(apiErr.Error()).To(Equal("Server error, status code: 500, error code: , message: "))
+				Expect(config.AccessToken()).To(BeEmpty())
+			})
 		})
 
-		Expect(handler).To(testnet.HaveAllRequestsCalled())
-		Expect(apiErr).To(HaveOccurred())
-		Expect(apiErr.Error()).To(Equal("Authentication Server error: I/O error: uaa.10.244.0.22.xip.io; nested exception is java.net.UnknownHostException: uaa.10.244.0.22.xip.io"))
-		Expect(config.AccessToken()).To(BeEmpty())
+		Describe("when the UAA server has an error but still returns a 200", func() {
+			BeforeEach(func() {
+				setupTestServer(errorMaskedAsSuccessLoginRequest)
+			})
+
+			It("returns an error", func() {
+				Expect(handler).To(testnet.HaveAllRequestsCalled())
+				Expect(apiErr).To(HaveOccurred())
+				Expect(apiErr.Error()).To(Equal("Authentication Server error: I/O error: uaa.10.244.0.22.xip.io; nested exception is java.net.UnknownHostException: uaa.10.244.0.22.xip.io"))
+				Expect(config.AccessToken()).To(BeEmpty())
+			})
+		})
 	})
 
-	Describe("getting login resources", func() {
+	Describe("getting login info", func() {
 		var (
 			apiErr  errors.Error
 			prompts map[string]configuration.AuthPrompt
 		)
 
-		BeforeEach(func() {
-			testServer, handler, config = setupAuthDependencies(loginInfoRequest)
-			defer testServer.Close()
-			auth := NewUAAAuthenticationRepository(gateway, config)
+		JustBeforeEach(func() {
 			prompts, apiErr = auth.GetLoginPromptsAndSaveUAAServerURL()
 		})
 
-		It("does not return an error", func() {
-			Expect(apiErr).NotTo(HaveOccurred())
+		Describe("when the login info API succeeds", func() {
+			BeforeEach(func() {
+				setupTestServer(loginServerLoginRequest)
+			})
+
+			It("does not return an error", func() {
+				Expect(apiErr).NotTo(HaveOccurred())
+			})
+
+			It("gets the login prompts", func() {
+				Expect(prompts).To(Equal(map[string]configuration.AuthPrompt{
+					"username": configuration.AuthPrompt{
+						DisplayName: "Email",
+						Type:        configuration.AuthPromptTypeText,
+					},
+					"pin": configuration.AuthPrompt{
+						DisplayName: "PIN Number",
+						Type:        configuration.AuthPromptTypePassword,
+					},
+				}))
+			})
+
+			It("saves the UAA server to the config", func() {
+				Expect(config.UaaEndpoint()).To(Equal("https://uaa.run.pivotal.io"))
+			})
 		})
 
-		It("gets the login prompts", func() {
-			Expect(prompts).To(Equal(map[string]configuration.AuthPrompt{
-				"username": configuration.AuthPrompt{
-					DisplayName: "Email",
-					Type:        configuration.AuthPromptTypeText,
-				},
-				"pin": configuration.AuthPrompt{
-					DisplayName: "PIN Number",
-					Type:        configuration.AuthPromptTypePassword,
-				},
-			}))
+		Describe("when the login info API fails", func() {
+			BeforeEach(func() {
+				setupTestServer(loginServerLoginFailureRequest)
+			})
+
+			It("returns a failure response when the login info API fails", func() {
+				Expect(handler).To(testnet.HaveAllRequestsCalled())
+				Expect(apiErr).To(HaveOccurred())
+				Expect(prompts).To(BeEmpty())
+			})
 		})
 
-		It("saves the UAA server to the config", func() {
-			Expect(config.UaaEndpoint()).To(Equal("https://uaa.run.pivotal.io"))
+		Context("when the response does not contain links", func() {
+			BeforeEach(func() {
+				setupTestServer(uaaServerLoginRequest)
+			})
+
+			It("presumes that the authorization server is the UAA", func() {
+				Expect(config.UaaEndpoint()).To(Equal(config.AuthenticationEndpoint()))
+			})
 		})
 	})
 
-	It("returns a failure response when the login info API fails", func() {
-		testServer, handler, config = setupAuthDependencies(loginInfoFailureRequest)
-		auth := NewUAAAuthenticationRepository(gateway, config)
-
-		prompts, apiErr := auth.GetLoginPromptsAndSaveUAAServerURL()
-		Expect(handler).To(testnet.HaveAllRequestsCalled())
-		Expect(apiErr).To(HaveOccurred())
-		Expect(prompts).To(BeEmpty())
-	})
 })
 
 var authHeaders = http.Header{
@@ -206,7 +233,7 @@ var errorMaskedAsSuccessLoginRequest = testnet.TestRequest{
 `},
 }
 
-var loginInfoRequest = testnet.TestRequest{
+var loginServerLoginRequest = testnet.TestRequest{
 	Method: "GET",
 	Path:   "/login",
 	Response: testnet.TestResponse{
@@ -237,7 +264,7 @@ var loginInfoRequest = testnet.TestRequest{
 	},
 }
 
-var loginInfoFailureRequest = testnet.TestRequest{
+var loginServerLoginFailureRequest = testnet.TestRequest{
 	Method: "GET",
 	Path:   "/login",
 	Response: testnet.TestResponse{
@@ -245,10 +272,25 @@ var loginInfoFailureRequest = testnet.TestRequest{
 	},
 }
 
-func setupAuthDependencies(request testnet.TestRequest) (*httptest.Server, *testnet.TestHandler, configuration.ReadWriter) {
-	ts, handler := testnet.NewServer([]testnet.TestRequest{request})
-	config := testconfig.NewRepository()
-	config.SetAuthenticationEndpoint(ts.URL)
-
-	return ts, handler, config
+var uaaServerLoginRequest = testnet.TestRequest{
+	Method: "GET",
+	Path:   "/login",
+	Response: testnet.TestResponse{
+		Status: http.StatusOK,
+		Body: `
+{
+	"timestamp":"2013-12-18T11:26:53-0700",
+	"app":{
+		"artifact":"cloudfoundry-identity-uaa",
+		"description":"User Account and Authentication Service",
+		"name":"UAA",
+		"version":"1.4.7"
+	},
+	"commit_id":"2701cc8",
+	"prompts":{
+		"username": ["text","Email"],
+		"pin": ["password", "PIN Number"]
+	}
+}`,
+	},
 }
