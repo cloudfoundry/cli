a6a651cbf998ab820b7ba52c66a5d38388c67183
Mark Rushakoff and Tim Jarratt <tjarratt@pivotallabs.com>
2014-03-17 15:34:57 -0700

    Add batch files for windows CI

---
+++ ci/scripts/windows-32-test.bat
@@ -0,0 +1,11 @@
+git fetch
+git checkout %GIT_COMMIT%
+git submodule update --init
+
+powershell -command set-executionpolicy remotesigned
+powershell .\bin\replace-sha.ps1
+
+SET GOPATH=c:\jenkins\workspace\go-cli-tests-windows32Bit
+c:\Go\bin\go build -v -o cf-windows-386.exe main
+c:\Go\bin\go test -i ./src/cf/...
+c:\Go\bin\go test -v ./src/cf/...
