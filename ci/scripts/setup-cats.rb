0e804744a49a80a5da762256b3274a39d524c1a0
John Foley and Max Brunsfeld <jfoley@pivotallabs.com>
2014-03-21 15:42:54 -0700

    Disable host key checking in litterbox script

--- ci/scripts/setup-cats.rb
+++ ci/scripts/setup-cats.rb
@@ -12,12 +12,13 @@ def cf(cmd)
 end
 
 def run_or_die(cmd)
+  puts "Running #{cmd}"
   out, err, status = Open3.capture3(cmd)
   puts out
-  raise "cf failed:\n#{err}" unless status == 0
+  raise "Command failed:\n#{err}" unless status == 0
 end
 
-run_or_die("ssh #{BOSH_LITE_HOSTNAME} \"rm -rf $(find /opt/warden/disks/bind_mount_points -name '*cc-droplets*' 2> /dev/null)\"")
+run_or_die("ssh -o \"StrictHostKeyChecking no\" #{BOSH_LITE_HOSTNAME} \"rm -rf $(find /opt/warden/disks/bind_mount_points -name '*cc-droplets*' 2> /dev/null)\"")
 
 cf "api #{CC_HOSTNAME} --skip-ssl-validation"
 cf "login -u #{CF_ADMIN_USER} -p #{CF_ADMIN_PASSWORD}"
