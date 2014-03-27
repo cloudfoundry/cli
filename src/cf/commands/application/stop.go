4753798da2bb275a2db6675597cdb659d631a835
John Foley and Tim Jarratt <tjarratt@pivotallabs.com>
2014-03-27 12:20:02 -0700

    Cleanup tests for stop command
    Yadda yadda yadda explicit login requirements
    
    [#66911426]

--- src/cf/commands/application/stop.go
+++ src/cf/commands/application/stop.go
@@ -30,16 +30,16 @@ func NewStop(ui terminal.UI, config configuration.Reader, appRepo api.Applicatio
 	return
 }
 
-func (cmd *Stop) GetRequirements(reqFactory requirements.Factory, c *cli.Context) (reqs []requirements.Requirement, err error) {
+func (cmd *Stop) GetRequirements(requirementsFactory requirements.Factory, c *cli.Context) (reqs []requirements.Requirement, err error) {
 	if len(c.Args()) == 0 {
 		err = errors.New("Incorrect Usage")
 		cmd.ui.FailWithUsage(c, "stop")
 		return
 	}
 
-	cmd.appReq = reqFactory.NewApplicationRequirement(c.Args()[0])
+	cmd.appReq = requirementsFactory.NewApplicationRequirement(c.Args()[0])
 
-	reqs = []requirements.Requirement{cmd.appReq}
+	reqs = []requirements.Requirement{requirementsFactory.NewLoginRequirement(), cmd.appReq}
 	return
 }
 
--- src/cf/commands/application/stop_test.go
+++ src/cf/commands/application/stop_test.go
@@ -14,112 +14,132 @@ import (
 	testterm "testhelpers/terminal"
 )
 
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestStopCommandFailsWithUsage", func() {
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		appRepo := &testapi.FakeApplicationRepository{ReadApp: app}
-		reqFactory := &testreq.FakeReqFactory{Application: app}
+var _ = Describe("stop command", func() {
+	var (
+		requirementsFactory *testreq.FakeReqFactory
+	)
 
-		ui := callStop([]string{}, reqFactory, appRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
-
-		ui = callStop([]string{"my-app"}, reqFactory, appRepo)
-		Expect(ui.FailedWithUsage).To(BeFalse())
+	BeforeEach(func() {
+		requirementsFactory = &testreq.FakeReqFactory{}
 	})
-	It("TestStopApplication", func() {
 
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		appRepo := &testapi.FakeApplicationRepository{ReadApp: app}
-		args := []string{"my-app"}
-		reqFactory := &testreq.FakeReqFactory{Application: app}
-		ui := callStop(args, reqFactory, appRepo)
-
-		testassert.SliceContains(ui.Outputs, testassert.Lines{
-			{"Stopping app", "my-app", "my-org", "my-space", "my-user"},
-			{"OK"},
-		})
-
-		Expect(reqFactory.ApplicationName).To(Equal("my-app"))
-		Expect(appRepo.UpdateAppGuid).To(Equal("my-app-guid"))
-	})
-	It("TestStopApplicationWhenStopFails", func() {
-
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		appRepo := &testapi.FakeApplicationRepository{ReadApp: app, UpdateErr: true}
-		args := []string{"my-app"}
-		reqFactory := &testreq.FakeReqFactory{Application: app}
-		ui := callStop(args, reqFactory, appRepo)
-
-		testassert.SliceContains(ui.Outputs, testassert.Lines{
-			{"Stopping", "my-app"},
-			{"FAILED"},
-			{"Error updating app."},
-		})
-		Expect(appRepo.UpdateAppGuid).To(Equal("my-app-guid"))
-	})
-	It("TestStopApplicationIsAlreadyStopped", func() {
-
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		app.State = "stopped"
-		appRepo := &testapi.FakeApplicationRepository{ReadApp: app}
-		args := []string{"my-app"}
-		reqFactory := &testreq.FakeReqFactory{Application: app}
-		ui := callStop(args, reqFactory, appRepo)
-
-		testassert.SliceContains(ui.Outputs, testassert.Lines{
-			{"my-app", "is already stopped"},
-		})
-		Expect(appRepo.UpdateAppGuid).To(Equal(""))
-	})
-	It("TestApplicationStopReturnsUpdatedApp", func() {
-
-		appToStop := models.Application{}
-		appToStop.Name = "my-app"
-		appToStop.Guid = "my-app-guid"
-		appToStop.State = "started"
-		expectedStoppedApp := models.Application{}
-		expectedStoppedApp.Name = "my-stopped-app"
-		expectedStoppedApp.Guid = "my-stopped-app-guid"
-		expectedStoppedApp.State = "stopped"
-
-		appRepo := &testapi.FakeApplicationRepository{UpdateAppResult: expectedStoppedApp}
-		config := testconfig.NewRepository()
-		stopper := NewStop(new(testterm.FakeUI), config, appRepo)
-		actualStoppedApp, err := stopper.ApplicationStop(appToStop)
-
-		Expect(err).NotTo(HaveOccurred())
-		Expect(expectedStoppedApp).To(Equal(actualStoppedApp))
-	})
-	It("TestApplicationStopReturnsUpdatedAppWhenAppIsAlreadyStopped", func() {
-
-		appToStop := models.Application{}
-		appToStop.Name = "my-app"
-		appToStop.Guid = "my-app-guid"
-		appToStop.State = "stopped"
+	It("fails requirements when not logged in", func() {
+		requirementsFactory.LoginSuccess = false
 		appRepo := &testapi.FakeApplicationRepository{}
-		config := testconfig.NewRepository()
-		stopper := NewStop(new(testterm.FakeUI), config, appRepo)
-		updatedApp, err := stopper.ApplicationStop(appToStop)
+		cmd := NewStop(new(testterm.FakeUI), testconfig.NewRepository(), appRepo)
+		testcmd.RunCommand(cmd, testcmd.NewContext("stop", []string{"some-app-name"}), requirementsFactory)
 
-		Expect(err).NotTo(HaveOccurred())
-		Expect(appToStop).To(Equal(updatedApp))
+		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+
+	})
+
+	Context("when logged in", func() {
+		BeforeEach(func() {
+			requirementsFactory.LoginSuccess = true
+		})
+
+		It("fails with usage when the app name is not given", func() {
+			app := models.Application{}
+			app.Name = "my-app"
+			app.Guid = "my-app-guid"
+			appRepo := &testapi.FakeApplicationRepository{ReadApp: app}
+
+			ui := callStop([]string{}, requirementsFactory, appRepo)
+			Expect(ui.FailedWithUsage).To(BeTrue())
+		})
+
+		It("stops the app with the given name", func() {
+			app := models.Application{}
+			app.Name = "my-app"
+			app.Guid = "my-app-guid"
+			appRepo := &testapi.FakeApplicationRepository{ReadApp: app}
+			args := []string{"my-app"}
+			requirementsFactory.Application = app
+			ui := callStop(args, requirementsFactory, appRepo)
+
+			testassert.SliceContains(ui.Outputs, testassert.Lines{
+				{"Stopping app", "my-app", "my-org", "my-space", "my-user"},
+				{"OK"},
+			})
+
+			Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
+			Expect(appRepo.UpdateAppGuid).To(Equal("my-app-guid"))
+		})
+
+		It("warns the user when stopping the app fails", func() {
+			app := models.Application{}
+			app.Name = "my-app"
+			app.Guid = "my-app-guid"
+			appRepo := &testapi.FakeApplicationRepository{ReadApp: app, UpdateErr: true}
+			args := []string{"my-app"}
+			requirementsFactory.Application = app
+			ui := callStop(args, requirementsFactory, appRepo)
+
+			testassert.SliceContains(ui.Outputs, testassert.Lines{
+				{"Stopping", "my-app"},
+				{"FAILED"},
+				{"Error updating app."},
+			})
+			Expect(appRepo.UpdateAppGuid).To(Equal("my-app-guid"))
+		})
+
+		It("warns the user when the app is already stopped", func() {
+			app := models.Application{}
+			app.Name = "my-app"
+			app.Guid = "my-app-guid"
+			app.State = "stopped"
+			appRepo := &testapi.FakeApplicationRepository{ReadApp: app}
+			args := []string{"my-app"}
+			requirementsFactory.Application = app
+			ui := callStop(args, requirementsFactory, appRepo)
+
+			testassert.SliceContains(ui.Outputs, testassert.Lines{
+				{"my-app", "is already stopped"},
+			})
+			Expect(appRepo.UpdateAppGuid).To(Equal(""))
+		})
+
+		It("returns the updated app model from ApplicationStop()", func() {
+			appToStop := models.Application{}
+			appToStop.Name = "my-app"
+			appToStop.Guid = "my-app-guid"
+			appToStop.State = "started"
+			expectedStoppedApp := models.Application{}
+			expectedStoppedApp.Name = "my-stopped-app"
+			expectedStoppedApp.Guid = "my-stopped-app-guid"
+			expectedStoppedApp.State = "stopped"
+
+			appRepo := &testapi.FakeApplicationRepository{UpdateAppResult: expectedStoppedApp}
+			config := testconfig.NewRepository()
+			stopper := NewStop(new(testterm.FakeUI), config, appRepo)
+			actualStoppedApp, err := stopper.ApplicationStop(appToStop)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(expectedStoppedApp).To(Equal(actualStoppedApp))
+		})
+
+		It("TestApplicationStopReturnsUpdatedAppWhenAppIsAlreadyStopped", func() {
+			appToStop := models.Application{}
+			appToStop.Name = "my-app"
+			appToStop.Guid = "my-app-guid"
+			appToStop.State = "stopped"
+			appRepo := &testapi.FakeApplicationRepository{}
+			config := testconfig.NewRepository()
+			stopper := NewStop(new(testterm.FakeUI), config, appRepo)
+			updatedApp, err := stopper.ApplicationStop(appToStop)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(appToStop).To(Equal(updatedApp))
+		})
 	})
 })
 
-func callStop(args []string, reqFactory *testreq.FakeReqFactory, appRepo api.ApplicationRepository) (ui *testterm.FakeUI) {
+func callStop(args []string, requirementsFactory *testreq.FakeReqFactory, appRepo api.ApplicationRepository) (ui *testterm.FakeUI) {
 	ui = new(testterm.FakeUI)
 	ctxt := testcmd.NewContext("stop", args)
 
 	configRepo := testconfig.NewRepositoryWithDefaults()
 	cmd := NewStop(ui, configRepo, appRepo)
-	testcmd.RunCommand(cmd, ctxt, reqFactory)
+	testcmd.RunCommand(cmd, ctxt, requirementsFactory)
 	return
 }
