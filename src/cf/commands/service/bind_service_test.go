25c335904b95c5622870aa332877b710884679e2
John Foley and Tim Jarratt <tjarratt@pivotallabs.com>
2014-03-27 12:37:39 -0700

    Cleanup bind-service tests
    [#66911426]

--- src/cf/commands/service/bind_service_test.go
+++ src/cf/commands/service/bind_service_test.go
@@ -14,78 +14,93 @@ import (
 	testterm "testhelpers/terminal"
 )
 
-func callBindService(args []string, reqFactory *testreq.FakeReqFactory, serviceBindingRepo api.ServiceBindingRepository) (fakeUI *testterm.FakeUI) {
+var _ = Describe("bind-service command", func() {
+	var (
+		requirementsFactory *testreq.FakeReqFactory
+	)
+
+	BeforeEach(func() {
+		requirementsFactory = &testreq.FakeReqFactory{}
+	})
+
+	It("fails requirements when not logged in", func() {
+		context := testcmd.NewContext("bind-service", []string{"service", "app"})
+		cmd := NewBindService(&testterm.FakeUI{}, testconfig.NewRepository(), &testapi.FakeServiceBindingRepo{})
+		testcmd.RunCommand(cmd, context, requirementsFactory)
+
+		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+	})
+
+	Context("when logged in", func() {
+		BeforeEach(func() {
+			requirementsFactory.LoginSuccess = true
+		})
+
+		It("binds a service instance to an app", func() {
+			app := models.Application{}
+			app.Name = "my-app"
+			app.Guid = "my-app-guid"
+			serviceInstance := models.ServiceInstance{}
+			serviceInstance.Name = "my-service"
+			serviceInstance.Guid = "my-service-guid"
+			requirementsFactory.Application = app
+			requirementsFactory.ServiceInstance = serviceInstance
+			serviceBindingRepo := &testapi.FakeServiceBindingRepo{}
+			ui := callBindService([]string{"my-app", "my-service"}, requirementsFactory, serviceBindingRepo)
+
+			Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
+			Expect(requirementsFactory.ServiceInstanceName).To(Equal("my-service"))
+
+			testassert.SliceContains(ui.Outputs, testassert.Lines{
+				{"Binding service", "my-service", "my-app", "my-org", "my-space", "my-user"},
+				{"OK"},
+				{"TIP"},
+			})
+			Expect(serviceBindingRepo.CreateServiceInstanceGuid).To(Equal("my-service-guid"))
+			Expect(serviceBindingRepo.CreateApplicationGuid).To(Equal("my-app-guid"))
+		})
+
+		It("warns the user when the service instance is already bound to the given app", func() {
+			app := models.Application{}
+			app.Name = "my-app"
+			app.Guid = "my-app-guid"
+			serviceInstance := models.ServiceInstance{}
+			serviceInstance.Name = "my-service"
+			serviceInstance.Guid = "my-service-guid"
+			requirementsFactory.Application = app
+			requirementsFactory.ServiceInstance = serviceInstance
+			serviceBindingRepo := &testapi.FakeServiceBindingRepo{CreateErrorCode: "90003"}
+			ui := callBindService([]string{"my-app", "my-service"}, requirementsFactory, serviceBindingRepo)
+
+			testassert.SliceContains(ui.Outputs, testassert.Lines{
+				{"Binding service"},
+				{"OK"},
+				{"my-app", "is already bound", "my-service"},
+			})
+		})
+
+		It("fails with usage when called without a service instance and app", func() {
+			serviceBindingRepo := &testapi.FakeServiceBindingRepo{}
+
+			ui := callBindService([]string{"my-service"}, requirementsFactory, serviceBindingRepo)
+			Expect(ui.FailedWithUsage).To(BeTrue())
+
+			ui = callBindService([]string{"my-app"}, requirementsFactory, serviceBindingRepo)
+			Expect(ui.FailedWithUsage).To(BeTrue())
+
+			ui = callBindService([]string{"my-app", "my-service"}, requirementsFactory, serviceBindingRepo)
+			Expect(ui.FailedWithUsage).To(BeFalse())
+		})
+	})
+})
+
+func callBindService(args []string, requirementsFactory *testreq.FakeReqFactory, serviceBindingRepo api.ServiceBindingRepository) (fakeUI *testterm.FakeUI) {
 	fakeUI = new(testterm.FakeUI)
 	ctxt := testcmd.NewContext("bind-service", args)
 
 	config := testconfig.NewRepositoryWithDefaults()
 
 	cmd := NewBindService(fakeUI, config, serviceBindingRepo)
-	testcmd.RunCommand(cmd, ctxt, reqFactory)
+	testcmd.RunCommand(cmd, ctxt, requirementsFactory)
 	return
 }
-
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestBindCommand", func() {
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		serviceInstance := models.ServiceInstance{}
-		serviceInstance.Name = "my-service"
-		serviceInstance.Guid = "my-service-guid"
-		reqFactory := &testreq.FakeReqFactory{
-			Application:     app,
-			ServiceInstance: serviceInstance,
-		}
-		serviceBindingRepo := &testapi.FakeServiceBindingRepo{}
-		ui := callBindService([]string{"my-app", "my-service"}, reqFactory, serviceBindingRepo)
-
-		Expect(reqFactory.ApplicationName).To(Equal("my-app"))
-		Expect(reqFactory.ServiceInstanceName).To(Equal("my-service"))
-
-		Expect(len(ui.Outputs)).To(Equal(3))
-		testassert.SliceContains(ui.Outputs, testassert.Lines{
-			{"Binding service", "my-service", "my-app", "my-org", "my-space", "my-user"},
-			{"OK"},
-			{"TIP"},
-		})
-		Expect(serviceBindingRepo.CreateServiceInstanceGuid).To(Equal("my-service-guid"))
-		Expect(serviceBindingRepo.CreateApplicationGuid).To(Equal("my-app-guid"))
-	})
-	It("TestBindCommandIfServiceIsAlreadyBound", func() {
-
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		serviceInstance := models.ServiceInstance{}
-		serviceInstance.Name = "my-service"
-		serviceInstance.Guid = "my-service-guid"
-		reqFactory := &testreq.FakeReqFactory{
-			Application:     app,
-			ServiceInstance: serviceInstance,
-		}
-		serviceBindingRepo := &testapi.FakeServiceBindingRepo{CreateErrorCode: "90003"}
-		ui := callBindService([]string{"my-app", "my-service"}, reqFactory, serviceBindingRepo)
-
-		Expect(len(ui.Outputs)).To(Equal(3))
-		testassert.SliceContains(ui.Outputs, testassert.Lines{
-			{"Binding service"},
-			{"OK"},
-			{"my-app", "is already bound", "my-service"},
-		})
-	})
-	It("TestBindCommandFailsWithUsage", func() {
-
-		reqFactory := &testreq.FakeReqFactory{}
-		serviceBindingRepo := &testapi.FakeServiceBindingRepo{}
-
-		ui := callBindService([]string{"my-service"}, reqFactory, serviceBindingRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
-
-		ui = callBindService([]string{"my-app"}, reqFactory, serviceBindingRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
-
-		ui = callBindService([]string{"my-app", "my-service"}, reqFactory, serviceBindingRepo)
-		Expect(ui.FailedWithUsage).To(BeFalse())
-	})
-})
