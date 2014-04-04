4a5bfd21abecd3c12267c8b99d855d70ca168807
Tim Jarratt <tjarratt@pivotallabs.com>
2014-04-04 14:37:51 -0700

    Add a warning to tests that require cleanup
    We have known for some time that our tests were in a bad condition, but
    it was not clear to us until we grepped around the source code to see how
    many tests had not been cleaned up yet.
    
    Our intention here is to provide contributors some better examples of how
    we think Ginkgo tests should be. Hopefully this clears up some of the
    confusion

--- src/cf/configuration/config_disk_persistor_test.go
+++ src/cf/configuration/config_disk_persistor_test.go
@@ -1,3 +1,28 @@
+/*
+                       WARNING WARNING WARNING
+
+                Attention all potential contributors
+
+   This testfile is not in the best state. We've been slowly transitioning
+   from the built in "testing" package to using Ginkgo. As you can see, we've
+   changed the format, but a lot of the setup, test body, descriptions, etc
+   are either hardcoded, completely lacking, or misleading.
+
+   For example:
+
+   Describe("Testing with ginkgo"...)      // This is not a great description
+   It("TestDoesSoemthing"...)              // This is a horrible description
+
+   Describe("create-user command"...       // Describe the actual object under test
+   It("creates a user when provided ..."   // this is more descriptive
+
+   For good examples of writing Ginkgo tests for the cli, refer to
+
+   src/cf/commands/application/delete_app_test.go
+   src/cf/terminal/ui_test.go
+   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
+*/
+
 package configuration_test
 
 import (
