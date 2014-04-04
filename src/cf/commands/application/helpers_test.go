4745bfa5f71a7b517c8cc5497070e5f6f6ab21f9
Tim Jarratt <tjarratt@pivotallabs.com>
2014-04-04 12:12:56 -0700

    go fmt

--- src/cf/commands/application/helpers_test.go
+++ src/cf/commands/application/helpers_test.go
@@ -25,21 +25,21 @@ var _ = Describe("Helpers", func() {
 	date := time.Date(2014, 4, 4, 11, 39, 20, 5, time.UTC)
 
 	Context("when the message comes from an app", func() {
-	    It("includes the instance index", func() {
+		It("includes the instance index", func() {
 			msg := createMessage("4", "App", logmessage.LogMessage_OUT, date)
 			Expect(LogMessageOutput(msg, time.UTC)).To(Equal("2014-04-04T11:39:20.00+0000 [App/4]   OUT Hello World!"))
 		})
 	})
 
 	Context("when the message comes from a cloudfoundry component", func() {
-	    It("doesn't include the instance index", func() {
+		It("doesn't include the instance index", func() {
 			msg := createMessage("4", "DEA", logmessage.LogMessage_OUT, date)
 			Expect(LogMessageOutput(msg, time.UTC)).To(Equal("2014-04-04T11:39:20.00+0000 [DEA]     OUT Hello World!"))
-	    })
+		})
 	})
 
 	Context("when the message was written to stderr", func() {
-	    It("shows the log type as 'ERR'", func() {
+		It("shows the log type as 'ERR'", func() {
 			msg := createMessage("4", "DEA", logmessage.LogMessage_ERR, date)
 			Expect(LogMessageOutput(msg, time.UTC)).To(Equal("2014-04-04T11:39:20.00+0000 [DEA]     ERR Hello World!"))
 		})
@@ -47,6 +47,6 @@ var _ = Describe("Helpers", func() {
 
 	It("formats the time in the given time zone", func() {
 		msg := createMessage("4", "DEA", logmessage.LogMessage_ERR, date)
-		Expect(LogMessageOutput(msg, time.FixedZone("the-zone", 3 * 60 * 60))).To(Equal("2014-04-04T14:39:20.00+0300 [DEA]     ERR Hello World!"))
+		Expect(LogMessageOutput(msg, time.FixedZone("the-zone", 3*60*60))).To(Equal("2014-04-04T14:39:20.00+0300 [DEA]     ERR Hello World!"))
 	})
 })
