08b124c8992ac04b326e1cdcb9c3ec5dc0f4192b
Tim Jarratt <tjarratt@pivotallabs.com>
2014-04-04 12:05:32 -0700

    Add a datetime for when the binary was built
    Part of our CI pipeline for now; apologies for having such an opaque process
    into how this works. FWIW the changes to replace-sha are included here as well,
    so if you're interested in duplicating this, you shouldn't have much trouble
    
    [Finishes #68515588]

--- src/cf/app_constants.go
+++ src/cf/app_constants.go
@@ -6,8 +6,9 @@ import (
 )
 
 const (
-	Version = "BUILT_FROM_SOURCE"
-	Usage   = "A command line tool to interact with Cloud Foundry"
+	Version     = "BUILT_FROM_SOURCE"
+	BuiltOnDate = "BUILT_AT_UNKNOWN_TIME"
+	Usage       = "A command line tool to interact with Cloud Foundry"
 )
 
 func Name() string {
