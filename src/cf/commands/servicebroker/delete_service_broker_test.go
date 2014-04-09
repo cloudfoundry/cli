2c2f6a53e6b0c25e43b3b6a87f7ab5bf37949d7e
John Foley and Tim Jarratt <jfoley@pivotallabs.com>
2014-04-09 10:24:50 -0700

    Clean up delete-service-broker tests

--- src/cf/commands/servicebroker/delete_service_broker_test.go
+++ src/cf/commands/servicebroker/delete_service_broker_test.go
@@ -1,32 +1,8 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/cf/commands/application/delete_app_test.go
-   src/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package servicebroker_test
 
 import (
 	. "cf/commands/servicebroker"
+	"cf/configuration"
 	"cf/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -38,117 +14,95 @@ import (
 	testterm "testhelpers/terminal"
 )
 
-func callDeleteServiceBroker(args []string, reqFactory *testreq.FakeReqFactory, repo *testapi.FakeServiceBrokerRepo) (ui *testterm.FakeUI) {
-	ui = &testterm.FakeUI{}
-	ctxt := testcmd.NewContext("delete-service-broker", args)
-	config := testconfig.NewRepositoryWithDefaults()
+var _ = Describe("delete-service-broker command", func() {
+	var (
+		ui                  *testterm.FakeUI
+		configRepo          configuration.ReadWriter
+		brokerRepo          *testapi.FakeServiceBrokerRepo
+		requirementsFactory *testreq.FakeReqFactory
+	)
 
-	cmd := NewDeleteServiceBroker(ui, config, repo)
-	testcmd.RunCommand(cmd, ctxt, reqFactory)
-	return
-}
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{Inputs: []string{"y"}}
+		brokerRepo = &testapi.FakeServiceBrokerRepo{}
+		configRepo = testconfig.NewRepositoryWithDefaults()
+		requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: true}
+	})
 
-func deleteServiceBroker(confirmation string, args []string) (ui *testterm.FakeUI, reqFactory *testreq.FakeReqFactory, repo *testapi.FakeServiceBrokerRepo) {
-	serviceBroker := models.ServiceBroker{}
-	serviceBroker.Name = "service-broker-to-delete"
-	serviceBroker.Guid = "service-broker-to-delete-guid"
-
-	reqFactory = &testreq.FakeReqFactory{LoginSuccess: true}
-	repo = &testapi.FakeServiceBrokerRepo{FindByNameServiceBroker: serviceBroker}
-	ui = &testterm.FakeUI{
-		Inputs: []string{confirmation},
+	runCommand := func(args ...string) {
+		cmd := NewDeleteServiceBroker(ui, configRepo, brokerRepo)
+		testcmd.RunCommand(cmd, testcmd.NewContext("delete-service-broker", args), requirementsFactory)
 	}
-	config := testconfig.NewRepositoryWithDefaults()
 
-	ctxt := testcmd.NewContext("delete-service-broker", args)
-	cmd := NewDeleteServiceBroker(ui, config, repo)
-	testcmd.RunCommand(cmd, ctxt, reqFactory)
-	return
-}
-
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestDeleteServiceBrokerFailsWithUsage", func() {
-		ui, _, _ := deleteServiceBroker("y", []string{})
-		Expect(ui.FailedWithUsage).To(BeTrue())
-
-		ui, _, _ = deleteServiceBroker("y", []string{"my-broker"})
-		Expect(ui.FailedWithUsage).To(BeFalse())
-	})
-	It("TestDeleteServiceBrokerRequirements", func() {
-
-		reqFactory := &testreq.FakeReqFactory{}
-		repo := &testapi.FakeServiceBrokerRepo{}
-
-		reqFactory.LoginSuccess = false
-		callDeleteServiceBroker([]string{"-f", "my-broker"}, reqFactory, repo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
-
-		reqFactory.LoginSuccess = true
-		callDeleteServiceBroker([]string{"-f", "my-broker"}, reqFactory, repo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
-	})
-	It("TestDeleteConfirmingWithY", func() {
-
-		ui, _, repo := deleteServiceBroker("y", []string{"service-broker-to-delete"})
-
-		Expect(repo.FindByNameName).To(Equal("service-broker-to-delete"))
-		Expect(repo.DeletedServiceBrokerGuid).To(Equal("service-broker-to-delete-guid"))
-		Expect(len(ui.Outputs)).To(Equal(2))
-		testassert.SliceContains(ui.Prompts, testassert.Lines{
-			{"Really delete", "service-broker-to-delete"},
-		})
-		testassert.SliceContains(ui.Outputs, testassert.Lines{
-			{"Deleting service broker", "service-broker-to-delete", "my-user"},
-			{"OK"},
-		})
-	})
-	It("TestDeleteConfirmingWithYes", func() {
-
-		ui, _, repo := deleteServiceBroker("Yes", []string{"service-broker-to-delete"})
-
-		Expect(repo.FindByNameName).To(Equal("service-broker-to-delete"))
-		Expect(repo.DeletedServiceBrokerGuid).To(Equal("service-broker-to-delete-guid"))
-		Expect(len(ui.Outputs)).To(Equal(2))
-		testassert.SliceContains(ui.Prompts, testassert.Lines{
-			{"Really delete", "service-broker-to-delete"},
+	Describe("requirements", func() {
+		It("fails with usage when called without a broker's name", func() {
+			runCommand()
+			Expect(ui.FailedWithUsage).To(BeTrue())
 		})
 
-		testassert.SliceContains(ui.Outputs, testassert.Lines{
-			{"Deleting service broker", "service-broker-to-delete", "my-user"},
-			{"OK"},
+		It("fails requirements when not logged in", func() {
+			requirementsFactory.LoginSuccess = false
+			runCommand("-f", "my-broker")
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
 		})
 	})
-	It("TestDeleteWithForceOption", func() {
 
-		serviceBroker := models.ServiceBroker{}
-		serviceBroker.Name = "service-broker-to-delete"
-		serviceBroker.Guid = "service-broker-to-delete-guid"
+	Context("when the service broker exists", func() {
+		BeforeEach(func() {
+			brokerRepo.FindByNameServiceBroker = models.ServiceBroker{
+				Name: "service-broker-to-delete",
+				Guid: "service-broker-to-delete-guid",
+			}
+		})
 
-		reqFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		repo := &testapi.FakeServiceBrokerRepo{FindByNameServiceBroker: serviceBroker}
-		ui := callDeleteServiceBroker([]string{"-f", "service-broker-to-delete"}, reqFactory, repo)
+		It("deletes the service broker with the given name", func() {
+			runCommand("service-broker-to-delete")
 
-		Expect(repo.FindByNameName).To(Equal("service-broker-to-delete"))
-		Expect(repo.DeletedServiceBrokerGuid).To(Equal("service-broker-to-delete-guid"))
-		Expect(len(ui.Prompts)).To(Equal(0))
-		Expect(len(ui.Outputs)).To(Equal(2))
-		testassert.SliceContains(ui.Outputs, testassert.Lines{
-			{"Deleting service broker", "service-broker-to-delete", "my-user"},
-			{"OK"},
+			Expect(brokerRepo.FindByNameName).To(Equal("service-broker-to-delete"))
+			Expect(brokerRepo.DeletedServiceBrokerGuid).To(Equal("service-broker-to-delete-guid"))
+			Expect(len(ui.Outputs)).To(Equal(2))
+			testassert.SliceContains(ui.Prompts, testassert.Lines{
+				{"Really delete", "service-broker-to-delete"},
+			})
+
+			testassert.SliceContains(ui.Outputs, testassert.Lines{
+				{"Deleting service broker", "service-broker-to-delete", "my-user"},
+				{"OK"},
+			})
+		})
+
+		It("does not prompt when the -f flag is provided", func() {
+			runCommand("-f", "service-broker-to-delete")
+
+			Expect(brokerRepo.FindByNameName).To(Equal("service-broker-to-delete"))
+			Expect(brokerRepo.DeletedServiceBrokerGuid).To(Equal("service-broker-to-delete-guid"))
+
+			Expect(ui.Prompts).To(BeEmpty())
+			testassert.SliceContains(ui.Outputs, testassert.Lines{
+				{"Deleting service broker", "service-broker-to-delete", "my-user"},
+				{"OK"},
+			})
 		})
 	})
-	It("TestDeleteAppThatDoesNotExist", func() {
 
-		reqFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		repo := &testapi.FakeServiceBrokerRepo{FindByNameNotFound: true}
-		ui := callDeleteServiceBroker([]string{"-f", "service-broker-to-delete"}, reqFactory, repo)
+	Context("when the service broker does not exist", func() {
+		BeforeEach(func() {
+			brokerRepo.FindByNameNotFound = true
+		})
 
-		Expect(repo.FindByNameName).To(Equal("service-broker-to-delete"))
-		Expect(repo.DeletedServiceBrokerGuid).To(Equal(""))
-		testassert.SliceContains(ui.Outputs, testassert.Lines{
-			{"Deleting service broker", "service-broker-to-delete"},
-			{"OK"},
-			{"service-broker-to-delete", "does not exist"},
+		It("warns the user", func() {
+			runCommand("-f", "service-broker-to-delete")
+
+			Expect(brokerRepo.FindByNameName).To(Equal("service-broker-to-delete"))
+			Expect(brokerRepo.DeletedServiceBrokerGuid).To(Equal(""))
+			testassert.SliceContains(ui.Outputs, testassert.Lines{
+				{"Deleting service broker", "service-broker-to-delete"},
+				{"OK"},
+			})
+
+			testassert.SliceContains(ui.WarnOutputs, testassert.Lines{
+				{"service-broker-to-delete", "does not exist"},
+			})
 		})
 	})
 })
