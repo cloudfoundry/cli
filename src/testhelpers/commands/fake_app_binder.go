0b8328f702002cbeb538ba2e5299a8c26e238d2f
John Foley and Tim Jarratt <jfoley@pivotallabs.com>
2014-04-08 17:56:35 -0700

    fix bug with binding services in manifests
    [Fixes #68768046]

--- src/testhelpers/commands/fake_app_binder.go
+++ src/testhelpers/commands/fake_app_binder.go
@@ -5,11 +5,15 @@ import "cf/models"
 type FakeAppBinder struct {
 	AppsToBind        []models.Application
 	InstancesToBindTo []models.ServiceInstance
+
+	BindApplicationReturns struct {
+		Error error
+	}
 }
 
-func (binder *FakeAppBinder) BindApplication(app models.Application, service models.ServiceInstance) (apiErr error) {
+func (binder *FakeAppBinder) BindApplication(app models.Application, service models.ServiceInstance) error {
 	binder.AppsToBind = append(binder.AppsToBind, app)
 	binder.InstancesToBindTo = append(binder.InstancesToBindTo, service)
 
-	return
+	return binder.BindApplicationReturns.Error
 }
