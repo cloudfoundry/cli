fdc82dae5a0df3b5c4b8b10beb98afc2c8f6015a
Karen Wang and Tim Jarratt <khwang@pivotallabs.com>
2014-04-17 12:29:29 -0700

    Refactor 'cf quota' to use the formatter.

--- src/cf/commands/quota/quota.go
+++ src/cf/commands/quota/quota.go
@@ -54,16 +54,11 @@ func (cmd *showQuota) Run(context *cli.Context) {
 
 	cmd.ui.Ok()
 
-	paidServices := "disallowed"
-	if quota.NonBasicServicesAllowed {
-		paidServices = "allowed"
-	}
-
 	table := terminal.NewTable(cmd.ui, []string{"", ""})
 	table.Print([][]string{
 		[]string{"Memory", formatters.ByteSize(quota.MemoryLimit * formatters.MEGABYTE)},
 		[]string{"Routes", fmt.Sprintf("%d", quota.RoutesLimit)},
 		[]string{"Services", fmt.Sprintf("%d", quota.ServicesLimit)},
-		[]string{"Paid service plans", paidServices},
+		[]string{"Paid service plans", formatters.Allowed(quota.NonBasicServicesAllowed)},
 	})
 }
