3e9079d82e202d4f8e65478ab52e4f23e5744ca5
Anand Gaitonde and Tim Jarratt <agaitonde@pivotallabs.com>
2014-04-28 15:05:45 -0700

    Actually add a suite test for cf/models package
    Unsure how we got these domain fields tests, but here's a sweet suite
    test so this behavior can be tested.

--- src/cf/models/domain_test.go
+++ src/cf/models/domain_test.go
@@ -1,28 +1,3 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/cf/commands/application/delete_app_test.go
-   src/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package models_test
 
 import (
@@ -31,8 +6,9 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Testing with ginkgo", func() {
+var _ = Describe("DomainFields", func() {
 	var route Route
+
 	BeforeEach(func() {
 		route = Route{}
 
@@ -41,13 +17,12 @@ var _ = Describe("Testing with ginkgo", func() {
 		route.Domain = domain
 	})
 
-	It("TestRouteURL", func() {
+	It("uses the hostname as part of the URL", func() {
 		route.Host = "foo"
 		Expect(route.URL()).To(Equal("foo.example.com"))
 	})
 
-	It("TestRouteURLWithoutHost", func() {
-		route.Host = ""
+	It("omits the hostname when none is given", func() {
 		Expect(route.URL()).To(Equal("example.com"))
 	})
 })
