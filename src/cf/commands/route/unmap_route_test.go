e6b74bfb31f0324a3339332584a8d67745b2c2db
Tim Jarratt <tjarratt@pivotallabs.com>
2014-04-29 18:38:02 -0700

    Cleanup unmap-route test

--- src/cf/commands/route/unmap_route_test.go
+++ src/cf/commands/route/unmap_route_test.go
@@ -1,34 +1,9 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/cf/commands/application/delete_app_test.go
-   src/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package route_test
 
 import (
 	. "cf/commands/route"
+	"cf/configuration"
 	"cf/models"
-	"github.com/codegangsta/cli"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	testapi "testhelpers/api"
@@ -40,68 +15,82 @@ import (
 	. "testhelpers/matchers"
 )
 
-var _ = Describe("Unmap Route Command", func() {
-	It("TestUnmapRouteFailsWithUsage", func() {
-		requirementsFactory := &testreq.FakeReqFactory{}
-		routeRepo := &testapi.FakeRouteRepository{}
+var _ = Describe("unmap-route command", func() {
+	var (
+		ui                  *testterm.FakeUI
+		configRepo          configuration.ReadWriter
+		routeRepo           *testapi.FakeRouteRepository
+		requirementsFactory *testreq.FakeReqFactory
+	)
 
-		ui := callUnmapRoute([]string{}, requirementsFactory, routeRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
-
-		ui = callUnmapRoute([]string{"foo"}, requirementsFactory, routeRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
-
-		ui = callUnmapRoute([]string{"foo", "bar"}, requirementsFactory, routeRepo)
-		Expect(ui.FailedWithUsage).To(BeFalse())
+	BeforeEach(func() {
+		ui = new(testterm.FakeUI)
+		configRepo = testconfig.NewRepositoryWithDefaults()
+		routeRepo = new(testapi.FakeRouteRepository)
+		requirementsFactory = new(testreq.FakeReqFactory)
 	})
 
-	It("TestUnmapRouteRequirements", func() {
-		routeRepo := &testapi.FakeRouteRepository{}
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
+	runCommand := func(args ...string) {
+		cmd := NewUnmapRoute(ui, configRepo, routeRepo)
+		testcmd.RunCommand(cmd, testcmd.NewContext("unmap-route", args), requirementsFactory)
+	}
 
-		callUnmapRoute([]string{"-n", "my-host", "my-app", "my-domain.com"}, requirementsFactory, routeRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
-		Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
-		Expect(requirementsFactory.DomainName).To(Equal("my-domain.com"))
+	Context("when the user is not logged in", func() {
+		It("fails requirements", func() {
+			runCommand("my-app", "some-domain.com")
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
 	})
 
-	It("TestUnmapRouteWhenUnbinding", func() {
-		domain := models.DomainFields{
-			Guid: "my-domain-guid",
-			Name: "example.com",
-		}
-		route := models.Route{
-			Domain: domain,
-			Guid:   "my-route-guid",
-			Host:   "foo",
-		}
-		app := models.Application{ApplicationFields: models.ApplicationFields{
-			Guid: "my-app-guid",
-			Name: "my-app",
-		}}
+	Context("when the user is logged in", func() {
+		BeforeEach(func() {
+			requirementsFactory.LoginSuccess = true
+		})
 
-		routeRepo := &testapi.FakeRouteRepository{}
-		routeRepo.FindByHostAndDomainReturns.Route = route
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true, Application: app, Domain: domain}
+		Context("when the user does not provide two args", func() {
+			It("fails with usage", func() {
+				runCommand()
+				Expect(ui.FailedWithUsage).To(BeTrue())
+			})
+		})
 
-		ui := callUnmapRoute([]string{"-n", "my-host", "my-app", "my-domain.com"}, requirementsFactory, routeRepo)
+		Context("when the user provides an app and a domain", func() {
+			BeforeEach(func() {
+				requirementsFactory.Application = models.Application{ApplicationFields: models.ApplicationFields{
+					Guid: "my-app-guid",
+					Name: "my-app",
+				}}
+				requirementsFactory.Domain = models.DomainFields{
+					Guid: "my-domain-guid",
+					Name: "example.com",
+				}
+				routeRepo.FindByHostAndDomainReturns.Route = models.Route{
+					Domain: requirementsFactory.Domain,
+					Guid:   "my-route-guid",
+					Host:   "foo",
+				}
+				runCommand("-n", "my-host", "my-app", "my-domain.com")
+			})
 
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Removing route", "foo.example.com", "my-app", "my-org", "my-space", "my-user"},
-			[]string{"OK"},
-		))
+			It("passes requirements", func() {
+				Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
 
-		Expect(routeRepo.UnboundRouteGuid).To(Equal("my-route-guid"))
-		Expect(routeRepo.UnboundAppGuid).To(Equal("my-app-guid"))
+			})
+
+			It("reads the app and domain from its requirements", func() {
+				Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
+				Expect(requirementsFactory.DomainName).To(Equal("my-domain.com"))
+			})
+
+			It("unmaps the route", func() {
+				Expect(ui.Outputs).To(ContainSubstrings(
+					[]string{"Removing route", "foo.example.com", "my-app", "my-org", "my-space", "my-user"},
+					[]string{"OK"},
+				))
+
+				Expect(routeRepo.UnboundRouteGuid).To(Equal("my-route-guid"))
+				Expect(routeRepo.UnboundAppGuid).To(Equal("my-app-guid"))
+			})
+		})
 	})
 })
-
-func callUnmapRoute(args []string, requirementsFactory *testreq.FakeReqFactory, routeRepo *testapi.FakeRouteRepository) (ui *testterm.FakeUI) {
-	ui = new(testterm.FakeUI)
-	var ctxt *cli.Context = testcmd.NewContext("unmap-route", args)
-
-	configRepo := testconfig.NewRepositoryWithDefaults()
-	cmd := NewUnmapRoute(ui, configRepo, routeRepo)
-	testcmd.RunCommand(cmd, ctxt, requirementsFactory)
-	return
-}
