aa1da95fe6ddf6007308c4830c68ea22f2c6ab31
Tim Jarratt <tjarratt@pivotallabs.com>
2014-05-02 10:26:14 -0700

    Start removing more global state
    It's been more than a little disappointing that we've had assertions in
    our tests that rely on a global bool in a testhelper package. Changing
    this for one test now so that we have a pattern to abide by later. My
    hope is that we can root out the rest of these one by one.
    
    [Finishes #68922816]

---
+++ src/testhelpers/matchers/passed_requirements.go
@@ -0,0 +1,29 @@
+package matchers
+
+import (
+	"cf/errors"
+	"github.com/onsi/gomega"
+)
+
+type havePassedRequirementsMatcher struct{}
+
+func HavePassedRequirements() gomega.OmegaMatcher {
+	return havePassedRequirementsMatcher{}
+}
+
+func (matcher havePassedRequirementsMatcher) Match(actual interface{}) (bool, error) {
+	asBool, ok := actual.(bool)
+	if !ok {
+		return false, errors.NewWithFmt("Expected actual value to be a bool, but it was a %T", actual)
+	}
+
+	return asBool == true, nil
+}
+
+func (matcher havePassedRequirementsMatcher) FailureMessage(_ interface{}) string {
+	return "Expected command to pass requirements but it did not"
+}
+
+func (matcher havePassedRequirementsMatcher) NegatedFailureMessage(_ interface{}) string {
+	return "Expected command to have not passed requirements but it did"
+}
