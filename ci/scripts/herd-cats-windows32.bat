a8228984d844281c5a8ad50214a97d541a9660ab
Anand Gaitonde and Tim Jarratt <agaitonde@pivotallabs.com>
2014-05-05 10:22:59 -0700

    Refer to main package locally for windows cats tests

--- ci/scripts/herd-cats-windows32.bat
+++ ci/scripts/herd-cats-windows32.bat
@@ -2,7 +2,7 @@ git submodule update --init
 
 SET CLIPATH=c:\jenkins\workspace\go-cli-tests-windows32Bit
 SET GOPATH=%CLIPATH%
-c:\Go\bin\go build -v -o cf-windows-386.exe main
+c:\Go\bin\go build -v -o cf-windows-386.exe .\main
 
 SET GOPATH=c:\Users\Administrator\go
 SET CATSPATH=%GOPATH%\src\github.com\cloudfoundry\cf-acceptance-tests
