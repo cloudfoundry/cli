763cae93bfff1074825604fe5e4fb122339afd40
Anand Gaitonde and Tim Jarratt <tjarratt@pivotallabs.com>
2014-05-05 11:53:21 -0700

    Run windows CATS with last built binary

--- scripts/herd-cats-windows64.bat
+++ scripts/herd-cats-windows64.bat
@@ -2,11 +2,10 @@ git submodule update --init
 
 SET CLIPATH=c:\jenkins\workspace\go-cli-tests-windows64Bit
 SET GOPATH=%CLIPATH%
-c:\Go\bin\go build -v -o cf-windows-amd64.exe .\main
 
 SET GOPATH=c:\Users\Administrator\go
 SET CATSPATH=%GOPATH%\src\github.com\cloudfoundry\cf-acceptance-tests
-copy %CLIPATH%\cf-windows-amd64.exe %CATSPATH%\gcf.exe /Y
+bitsadmin.exe /transfer "DownloadStableCLI" https://s3.amazonaws.com/go-cli/builds/cf-windows-amd64.exe c:\Users\Administrator\go\src\github.com\cloudfoundry\cf-acceptance-tests\gcf.exe
 
 SET PATH=%PATH%;%CATSPATH%;C:\Program Files\cURL\bin
 
