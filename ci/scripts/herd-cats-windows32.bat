41da2e1dc16870d3b8c5c0811b814d2a7ee26823
Anand Gaitonde and Tim Jarratt <tjarratt@pivotallabs.com>
2014-05-05 12:20:51 -0700

    Explicitly checkout latest CATS tests
    Still on @jfoley's fork, but at least this is consistent now.

--- ci/scripts/herd-cats-windows32.bat
+++ ci/scripts/herd-cats-windows32.bat
@@ -3,10 +3,14 @@ git submodule update --init
 SET CLIPATH=c:\jenkins\workspace\go-cli-tests-windows32Bit
 SET GOPATH=%CLIPATH%
 
-
 SET GOPATH=c:\Users\Administrator\go
-SET CATSPATH=%GOPATH%\src\github.com\cloudfoundry\cf-acceptance-tests
-bitsadmin.exe /transfer "DownloadStableCLI" https://s3.amazonaws.com/go-cli/builds/cf-windows-386.exe c:\Users\Administrator\go\src\github.com\cloudfoundry\cf-acceptance-tests\gcf.exe
+go get -d github.com/jfoley/cf-acceptance-tests/
+
+cd %GOPATH%\src/github.com\jfoley\cf-acceptance-tests
+git pull
+
+SET CATSPATH=%GOPATH%\src\github.com\jfoley\cf-acceptance-tests
+bitsadmin.exe /transfer "DownloadStableCLI" https://s3.amazonaws.com/go-cli/builds/cf-windows-386.exe c:\Users\Administrator\go\src\github.com\jfoley\cf-acceptance-tests\gcf.exe
 
 SET PATH=%PATH%;%CATSPATH%;C:\Program Files\cURL\bin
 
