ce88c5ea2938e70cf664ad20f5113d9f80d6eec9
Anand Gaitonde and Michael Maximilien <agaitonde@pivotallabs.com>
2014-05-15 12:24:17 -0700

    formatting locale and tests
    [#71347218]

--- cf/i18n/init.go
+++ cf/i18n/init.go
@@ -4,24 +4,76 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	go_i18n "github.com/nicksnyder/go-i18n/i18n"
 )
 
+const (
+	DEFAULT_LOCAL = "en_US"
+)
+
 func Init(packageName string, i18nDirname string) (go_i18n.TranslateFunc, error) {
-	osLocale := os.Getenv("LANG")
+	userLocale := getUserLocale()
+	loadTranslationFiles(packageName, i18nDirname, userLocale, DEFAULT_LOCAL)
 
-	userLocale := osLocale[:len(".UTF8")] //Might need to make this generic
-	defaultLocale := "en"
-
-	go_i18n.MustLoadTranslationFile(filepath.Join(i18nDirname, packageName, defaultLocale) + ".all.json")
-	go_i18n.MustLoadTranslationFile(filepath.Join(i18nDirname, packageName, userLocale) + ".all.json")
-
-	T, err := go_i18n.Tfunc(userLocale, defaultLocale)
+	T, err := go_i18n.Tfunc(userLocale, DEFAULT_LOCAL)
 	if err != nil {
-		fmt.Printf("Could not initialize i18n strings")
+		fmt.Printf("Could not initialize i18n strings") //TODO: Better Error Handling
 		return nil, err
 	}
 
 	return T, nil
+}
+
+func ValidateLocale(locale string) bool {
+	return ValidateLanguage(locale) && ValidateTerritory(locale)
+}
+
+func ValidateLanguage(language string) bool {
+	//TODO: complete me!
+	return false
+}
+
+func ValidateTerritory(territory string) bool {
+	//TODO: complete me!
+	return false
+}
+
+func getOSLocale() string {
+	if os.Getenv("LC_ALL") != "" {
+		return os.Getenv("LC_ALL")
+	} else if os.Getenv("LANG") != "" {
+		return os.Getenv("LANG")
+	} else {
+		return DEFAULT_LOCAL
+	}
+}
+
+func formatLocale(locale string) string {
+	formattedLocale := strings.Split(locale, ".")[0]
+	formattedLocale = strings.Replace(formattedLocale, "-", "_", -1)
+	language := strings.Split(formattedLocale, "_")[0]
+	territory := strings.Split(formattedLocale, "_")[1]
+	return strings.ToLower(language) + "_" + strings.ToUpper(territory)
+}
+
+func getUserLocale() string {
+	osLocale := getOSLocale()
+	osLocale = formatLocale(osLocale)
+
+	//TODO: add when ValidateLocale() is completed
+	// if !ValidateLocale(osLocale) {
+	// 	osLocale = DEFAULT_LOCAL
+	// }
+
+	return osLocale
+}
+
+func loadTranslationFiles(packageName, i18nDirname, userLocale, defaultLocale string) error {
+	pwd := os.Getenv("PWD")
+	go_i18n.MustLoadTranslationFile(filepath.Join(pwd, i18nDirname, packageName, defaultLocale) + ".all.json")
+	go_i18n.MustLoadTranslationFile(filepath.Join(pwd, i18nDirname, packageName, userLocale) + ".all.json")
+
+	return nil
 }
--- cf/i18n/test_fixtures/main/hello.go
+++ cf/i18n/test_fixtures/main/hello.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world!")
+  fmt.Println("LOL")
 }
