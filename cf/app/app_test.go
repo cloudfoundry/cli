e885c96b2bef2222898d50b6988c5cbb97e263c4
Simon Leung and Tim Jarratt <simonleung@pivotallabs.com>
2014-05-21 09:48:29 -0700

    Cleanup and organize app tests

--- cf/app/app_test.go
+++ cf/app/app_test.go
@@ -2,18 +2,18 @@ package app_test
 
 import (
 	"bytes"
+	"strings"
+	"time"
+
 	"github.com/cloudfoundry/cli/cf"
 	"github.com/cloudfoundry/cli/cf/api"
 	"github.com/cloudfoundry/cli/cf/command_factory"
-	"github.com/cloudfoundry/cli/cf/command_metadata"
 	"github.com/cloudfoundry/cli/cf/net"
 	"github.com/cloudfoundry/cli/cf/trace"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testmanifest "github.com/cloudfoundry/cli/testhelpers/manifest"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
 	"github.com/codegangsta/cli"
-	"strings"
-	"time"
 
 	. "github.com/cloudfoundry/cli/cf/app"
 	. "github.com/cloudfoundry/cli/testhelpers/matchers"
@@ -38,7 +38,12 @@ var expectedCommandNames = []string{
 }
 
 var _ = Describe("App", func() {
-	It("#NewApp", func() {
+	var (
+		app       *cli.App
+		cmdRunner *FakeRunner
+	)
+
+	JustBeforeEach(func() {
 		ui := &testterm.FakeUI{}
 		config := testconfig.NewRepository()
 		manifestRepo := &testmanifest.FakeManifestRepository{}
@@ -50,27 +55,40 @@ var _ = Describe("App", func() {
 		})
 
 		cmdFactory := command_factory.NewFactory(ui, config, manifestRepo, repoLocator)
+		cmdRunner = &FakeRunner{cmdFactory: cmdFactory}
+		app = NewApp(cmdRunner, cmdFactory.CommandMetadatas()...)
+	})
 
-		metadatas := []command_metadata.CommandMetadata{}
-		for _, cmdName := range expectedCommandNames {
-			metadatas = append(metadatas, command_metadata.CommandMetadata{Name: cmdName})
-		}
+	Describe("trace file integration", func() {
+		var (
+			output *bytes.Buffer
+		)
 
-		for _, cmdName := range expectedCommandNames {
-			cmdRunner := &FakeRunner{cmdFactory: cmdFactory}
-			output := bytes.NewBuffer(make([]byte, 1024))
+		BeforeEach(func() {
+			output = bytes.NewBuffer(make([]byte, 1024))
 			trace.SetStdout(output)
 			trace.EnableTrace()
+		})
 
-			app := NewApp(cmdRunner, cmdFactory.CommandMetadatas()...)
+		It("prints its version number to the trace output when constructed", func() {
 			Expect(strings.Split(output.String(), "\n")).To(ContainSubstrings(
 				[]string{"VERSION:"},
 				[]string{cf.Version},
 			))
+		})
+	})
 
-			app.Run([]string{"", cmdName})
-			Expect(cmdRunner.cmdName).To(Equal(cmdName))
-		}
+	Context("when given a command name to run", func() {
+		It("runs the command with that name", func() {
+			for _, cmdName := range expectedCommandNames {
+				app.Run([]string{"", cmdName})
+				Expect(cmdRunner.cmdName).To(Equal(cmdName))
+			}
+		})
+	})
+
+	XIt("includes the built on date in its version string", func() {
+
 	})
 })
 
