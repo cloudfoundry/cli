144f8adc72da76d256ec59b4a395e6f1073e06f8
Tim Jarratt <tjarratt@pivotallabs.com>
2014-05-23 09:58:00 -0700

    FakeAppInstancesRepo should be easier to use
    The behavior before was that each time you called GetInstances, it would slice off
    one of the responses and errors from its slick of mock responses. Eventually there
    would be nothing left and it would just return you the zero-value for the
    response and error, which made it fairly difficult to use this in practice
    because we poll this repo over and over again in practice.

--- testhelpers/api/fake_app_instances_repo.go
+++ testhelpers/api/fake_app_instances_repo.go
@@ -1,9 +1,10 @@
 package api
 
 import (
+	"time"
+
 	"github.com/cloudfoundry/cli/cf/errors"
 	"github.com/cloudfoundry/cli/cf/models"
-	"time"
 )
 
 type FakeAppInstancesRepo struct {
@@ -18,12 +19,20 @@ func (repo *FakeAppInstancesRepo) GetInstances(appGuid string) (instances []mode
 
 	if len(repo.GetInstancesResponses) > 0 {
 		instances = repo.GetInstancesResponses[0]
-		repo.GetInstancesResponses = repo.GetInstancesResponses[1:]
+
+		if len(repo.GetInstancesResponses) > 1 {
+			repo.GetInstancesResponses = repo.GetInstancesResponses[1:]
+		}
 	}
 
 	if len(repo.GetInstancesErrorCodes) > 0 {
 		errorCode := repo.GetInstancesErrorCodes[0]
-		repo.GetInstancesErrorCodes = repo.GetInstancesErrorCodes[1:]
+
+		// don't slice away the last one if this is all we have
+		if len(repo.GetInstancesErrorCodes) > 1 {
+			repo.GetInstancesErrorCodes = repo.GetInstancesErrorCodes[1:]
+		}
+
 		if errorCode != "" {
 			apiErr = errors.NewHttpError(400, errorCode, "Error staging app")
 		}
