20f4fa6c522f32761038ca495b826c0dff2d65f2
Tim Jarratt <tjarratt@pivotallabs.com>
2014-05-28 11:52:12 -0700

    cleanup org-users test
    [#68475822]

--- cf/commands/user/org_users_test.go
+++ cf/commands/user/org_users_test.go
@@ -1,32 +1,8 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package user_test
 
 import (
 	. "github.com/cloudfoundry/cli/cf/commands/user"
+	"github.com/cloudfoundry/cli/cf/configuration"
 	"github.com/cloudfoundry/cli/cf/models"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
@@ -39,108 +15,101 @@ import (
 	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 )
 
-func callOrgUsers(args []string, requirementsFactory *testreq.FakeReqFactory, userRepo *testapi.FakeUserRepository) (ui *testterm.FakeUI) {
-	ui = &testterm.FakeUI{}
-	config := testconfig.NewRepositoryWithDefaults()
-	cmd := NewOrgUsers(ui, config, userRepo)
-	testcmd.RunCommand(cmd, args, requirementsFactory)
-	return
-}
+var _ = Describe("org-users command", func() {
+	var (
+		ui                  *testterm.FakeUI
+		requirementsFactory *testreq.FakeReqFactory
+		configRepo          configuration.ReadWriter
+		userRepo            *testapi.FakeUserRepository
+	)
 
-var _ = Describe("Listing users in an org", func() {
-	It("TestOrgUsersFailsWithUsage", func() {
-		requirementsFactory := &testreq.FakeReqFactory{}
-		userRepo := &testapi.FakeUserRepository{}
-		ui := callOrgUsers([]string{}, requirementsFactory, userRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
-
-		ui = callOrgUsers([]string{"Org1"}, requirementsFactory, userRepo)
-		Expect(ui.FailedWithUsage).To(BeFalse())
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		userRepo = &testapi.FakeUserRepository{}
+		configRepo = testconfig.NewRepositoryWithDefaults()
+		requirementsFactory = &testreq.FakeReqFactory{}
 	})
 
-	It("TestOrgUsersRequirements", func() {
-		requirementsFactory := &testreq.FakeReqFactory{}
-		userRepo := &testapi.FakeUserRepository{}
-		args := []string{"Org1"}
+	runCommand := func(args ...string) {
+		testcmd.RunCommand(NewOrgUsers(ui, configRepo, userRepo), args, requirementsFactory)
+	}
 
-		requirementsFactory.LoginSuccess = false
-		callOrgUsers(args, requirementsFactory, userRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+	Describe("requirements", func() {
+		It("fails with usage when invoked without an org name", func() {
+			requirementsFactory.LoginSuccess = true
 
-		requirementsFactory.LoginSuccess = true
-		callOrgUsers(args, requirementsFactory, userRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
+			runCommand()
+			Expect(ui.FailedWithUsage).To(BeTrue())
+		})
 
-		Expect("Org1").To(Equal(requirementsFactory.OrganizationName))
+		It("fails when not logged in", func() {
+			runCommand("say-hello-to-my-little-org")
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
 	})
 
-	It("TestOrgUsers", func() {
-		org := models.Organization{}
-		org.Name = "Found Org"
-		org.Guid = "found-org-guid"
+	Context("when logged in and given an org with users", func() {
+		BeforeEach(func() {
+			org := models.Organization{}
+			org.Name = "the-org"
+			org.Guid = "the-org-guid"
 
-		userRepo := &testapi.FakeUserRepository{}
-		user := models.UserFields{}
-		user.Username = "user1"
-		user2 := models.UserFields{}
-		user2.Username = "user2"
-		user3 := models.UserFields{}
-		user3.Username = "user3"
-		user4 := models.UserFields{}
-		user4.Username = "user4"
-		userRepo.ListUsersByRole = map[string][]models.UserFields{
-			models.ORG_MANAGER:     []models.UserFields{user, user2},
-			models.BILLING_MANAGER: []models.UserFields{user4},
-			models.ORG_AUDITOR:     []models.UserFields{user3},
-		}
+			user := models.UserFields{}
+			user.Username = "user1"
+			user2 := models.UserFields{}
+			user2.Username = "user2"
+			user3 := models.UserFields{}
+			user3.Username = "user3"
+			user4 := models.UserFields{}
+			user4.Username = "user4"
+			userRepo.ListUsersByRole = map[string][]models.UserFields{
+				models.ORG_MANAGER:     []models.UserFields{user, user2},
+				models.BILLING_MANAGER: []models.UserFields{user4},
+				models.ORG_AUDITOR:     []models.UserFields{user3},
+			}
 
-		requirementsFactory := &testreq.FakeReqFactory{
-			LoginSuccess: true,
-			Organization: org,
-		}
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.Organization = org
+		})
 
-		ui := callOrgUsers([]string{"Org1"}, requirementsFactory, userRepo)
+		It("shows the special users in the given org", func() {
+			runCommand("the-org")
 
-		Expect(userRepo.ListUsersOrganizationGuid).To(Equal("found-org-guid"))
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Getting users in org", "Found Org", "my-user"},
-			[]string{"ORG MANAGER"},
-			[]string{"user1"},
-			[]string{"user2"},
-			[]string{"BILLING MANAGER"},
-			[]string{"user4"},
-			[]string{"ORG AUDITOR"},
-			[]string{"user3"},
-		))
-	})
+			Expect(userRepo.ListUsersOrganizationGuid).To(Equal("the-org-guid"))
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Getting users in org", "the-org", "my-user"},
+				[]string{"ORG MANAGER"},
+				[]string{"user1"},
+				[]string{"user2"},
+				[]string{"BILLING MANAGER"},
+				[]string{"user4"},
+				[]string{"ORG AUDITOR"},
+				[]string{"user3"},
+			))
+		})
 
-	It("lists all org users", func() {
-		org := models.Organization{}
-		org.Name = "Found Org"
-		org.Guid = "found-org-guid"
+		Context("when the -a flag is provided", func() {
+			BeforeEach(func() {
+				user := models.UserFields{}
+				user.Username = "user1"
+				user2 := models.UserFields{}
+				user2.Username = "user2"
+				userRepo.ListUsersByRole = map[string][]models.UserFields{
+					models.ORG_USER: []models.UserFields{user, user2},
+				}
+			})
 
-		userRepo := &testapi.FakeUserRepository{}
-		user := models.UserFields{}
-		user.Username = "user1"
-		user2 := models.UserFields{}
-		user2.Username = "user2"
-		userRepo.ListUsersByRole = map[string][]models.UserFields{
-			models.ORG_USER: []models.UserFields{user, user2},
-		}
+			It("lists all org users, regardless of role", func() {
+				runCommand("-a", "the-org")
 
-		requirementsFactory := &testreq.FakeReqFactory{
-			LoginSuccess: true,
-			Organization: org,
-		}
-
-		ui := callOrgUsers([]string{"-a", "Org1"}, requirementsFactory, userRepo)
-
-		Expect(userRepo.ListUsersOrganizationGuid).To(Equal("found-org-guid"))
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Getting users in org", "Found Org", "my-user"},
-			[]string{"USERS"},
-			[]string{"user1"},
-			[]string{"user2"},
-		))
+				Expect(userRepo.ListUsersOrganizationGuid).To(Equal("the-org-guid"))
+				Expect(ui.Outputs).To(ContainSubstrings(
+					[]string{"Getting users in org", "the-org", "my-user"},
+					[]string{"USERS"},
+					[]string{"user1"},
+					[]string{"user2"},
+				))
+			})
+		})
 	})
 })
