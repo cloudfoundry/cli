8b9cd6b966a0b15a02889ae8d6549f57d5c0624c
Anand Gaitonde and Tim Jarratt <agaitonde@pivotallabs.com>
2014-05-29 11:35:40 -0700

    Cleanup rename-service tests
    [#68475822]

--- cf/commands/service/rename_service_test.go
+++ cf/commands/service/rename_service_test.go
@@ -1,94 +1,82 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package service_test
 
 import (
-	. "github.com/cloudfoundry/cli/cf/commands/service"
+	"github.com/cloudfoundry/cli/cf/configuration"
 	"github.com/cloudfoundry/cli/cf/models"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/service"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 )
 
-func callRenameService(args []string, requirementsFactory *testreq.FakeReqFactory) (ui *testterm.FakeUI, serviceRepo *testapi.FakeServiceRepo) {
-	ui = &testterm.FakeUI{}
-	serviceRepo = &testapi.FakeServiceRepo{}
+var _ = Describe("rename-service command", func() {
+	var (
+		ui                  *testterm.FakeUI
+		config              configuration.ReadWriter
+		serviceRepo         *testapi.FakeServiceRepo
+		requirementsFactory *testreq.FakeReqFactory
+	)
 
-	config := testconfig.NewRepositoryWithDefaults()
-
-	cmd := NewRenameService(ui, config, serviceRepo)
-	testcmd.RunCommand(cmd, args, requirementsFactory)
-	return
-}
-
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestRenameServiceFailsWithUsage", func() {
-		requirementsFactory := &testreq.FakeReqFactory{}
-
-		fakeUI, _ := callRenameService([]string{}, requirementsFactory)
-		Expect(fakeUI.FailedWithUsage).To(BeTrue())
-
-		fakeUI, _ = callRenameService([]string{"my-service"}, requirementsFactory)
-		Expect(fakeUI.FailedWithUsage).To(BeTrue())
-
-		fakeUI, _ = callRenameService([]string{"my-service", "new-name", "extra"}, requirementsFactory)
-		Expect(fakeUI.FailedWithUsage).To(BeTrue())
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		config = testconfig.NewRepositoryWithDefaults()
+		serviceRepo = &testapi.FakeServiceRepo{}
+		requirementsFactory = &testreq.FakeReqFactory{}
 	})
-	It("TestRenameServiceRequirements", func() {
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: false, TargetedSpaceSuccess: true}
-		callRenameService([]string{"my-service", "new-name"}, requirementsFactory)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+	runCommand := func(args ...string) {
+		testcmd.RunCommand(NewRenameService(ui, config, serviceRepo), args, requirementsFactory)
+	}
 
-		requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: true, TargetedSpaceSuccess: false}
-		callRenameService([]string{"my-service", "new-name"}, requirementsFactory)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+	Describe("requirements", func() {
+		It("Fails with usage when exactly two parameters not passed", func() {
+			runCommand("whatever")
+			Expect(ui.FailedWithUsage).To(BeTrue())
+		})
 
-		Expect(requirementsFactory.ServiceInstanceName).To(Equal("my-service"))
+		It("fails when not logged in", func() {
+			requirementsFactory.TargetedSpaceSuccess = true
+			runCommand("banana", "fppants")
+
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
+
+		It("fails when a space is not targeted", func() {
+			requirementsFactory.LoginSuccess = true
+			runCommand("banana", "faaaaasdf")
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
 	})
-	It("TestRenameService", func() {
 
-		serviceInstance := models.ServiceInstance{}
-		serviceInstance.Name = "different-name"
-		serviceInstance.Guid = "different-name-guid"
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true, TargetedSpaceSuccess: true, ServiceInstance: serviceInstance}
-		ui, fakeServiceRepo := callRenameService([]string{"my-service", "new-name"}, requirementsFactory)
+	Context("when logged in and a space is targeted", func() {
+		var serviceInstance models.ServiceInstance
 
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Renaming service", "different-name", "new-name", "my-org", "my-space", "my-user"},
-			[]string{"OK"},
-		))
+		BeforeEach(func() {
+			serviceInstance = models.ServiceInstance{}
+			serviceInstance.Name = "different-name"
+			serviceInstance.Guid = "different-name-guid"
 
-		Expect(fakeServiceRepo.RenameServiceServiceInstance).To(Equal(serviceInstance))
-		Expect(fakeServiceRepo.RenameServiceNewName).To(Equal("new-name"))
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.TargetedSpaceSuccess = true
+			requirementsFactory.ServiceInstance = serviceInstance
+		})
+
+		It("renames the service, obviously", func() {
+			runCommand("my-service", "new-name")
+
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Renaming service", "different-name", "new-name", "my-org", "my-space", "my-user"},
+				[]string{"OK"},
+			))
+
+			Expect(serviceRepo.RenameServiceServiceInstance).To(Equal(serviceInstance))
+			Expect(serviceRepo.RenameServiceNewName).To(Equal("new-name"))
+		})
 	})
 })
