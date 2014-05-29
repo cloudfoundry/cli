822cf31b117dd8d50fe86b2e2a27514799bfd82c
Anand Gaitonde and Tim Jarratt <tjarratt@pivotallabs.com>
2014-05-29 15:06:57 -0700

    Add jibber_jabber as a dependency

---
+++ Godeps/_workspace/src/github.com/XenoPhex/jibber_jabber/jibber_jabber_unix_test.go
@@ -0,0 +1,94 @@
+// +build darwin freebsd linux netbsd openbsd
+
+package jibber_jabber_test
+
+import (
+	. "github.com/XenoPhex/jibber_jabber"
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Unix", func() {
+
+	//os.Setenv("LC_ALL", "fr_FR.UTF-8")
+	//os.Setenv("LANG", "fr_FR.UTF-8")
+	AfterEach(func() {
+		os.Setenv("LC_ALL", "")
+		os.Setenv("LANG", "en_US.UTF-8")
+	})
+
+	Describe("#DetectIETF", func() {
+		Context("Returns IETF encoded locale", func() {
+			It("should return the locale set to LC_ALL", func() {
+				os.Setenv("LC_ALL", "fr_FR.UTF-8")
+				result, _ := DetectIETF()
+				Ω(result).Should(Equal("fr-FR"))
+			})
+
+			It("should return the locale set to LANG if LC_ALL isn't set", func() {
+				os.Setenv("LANG", "fr_FR.UTF-8")
+
+				result, _ := DetectIETF()
+				Ω(result).Should(Equal("fr-FR"))
+			})
+
+			It("should return an error if it cannot detect a locale", func() {
+				os.Setenv("LANG", "")
+
+				_, err := DetectIETF()
+				Ω(err.Error()).Should(Equal(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE))
+			})
+		})
+	})
+
+	Describe("#DetectLanguage", func() {
+		Context("Returns encoded language", func() {
+			It("should return the language set to LC_ALL", func() {
+				os.Setenv("LC_ALL", "fr_FR.UTF-8")
+				result, _ := DetectLanguage()
+				Ω(result).Should(Equal("fr"))
+			})
+
+			It("should return the language set to LANG if LC_ALL isn't set", func() {
+				os.Setenv("LANG", "fr_FR.UTF-8")
+
+				result, _ := DetectLanguage()
+				Ω(result).Should(Equal("fr"))
+			})
+
+			It("should return an error if it cannot detect a language", func() {
+				os.Setenv("LANG", "")
+
+				_, err := DetectLanguage()
+				Ω(err.Error()).Should(Equal(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE))
+			})
+		})
+	})
+
+	Describe("#DetectTerritory", func() {
+		Context("Returns encoded territory", func() {
+			It("should return the territory set to LC_ALL", func() {
+				os.Setenv("LC_ALL", "fr_FR.UTF-8")
+				result, _ := DetectTerritory()
+				Ω(result).Should(Equal("FR"))
+			})
+
+			It("should return the territory set to LANG if LC_ALL isn't set", func() {
+				os.Setenv("LANG", "fr_FR.UTF-8")
+
+				result, _ := DetectTerritory()
+				Ω(result).Should(Equal("FR"))
+			})
+
+			It("should return an error if it cannot detect a territory", func() {
+				os.Setenv("LANG", "")
+
+				_, err := DetectTerritory()
+				Ω(err.Error()).Should(Equal(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE))
+			})
+		})
+	})
+
+})
---
+++ Godeps/_workspace/src/github.com/XenoPhex/jibber_jabber/jibber_jabber_windows.go
@@ -0,0 +1,50 @@
+// +build windows
+
+package jibber_jabber
+
+import (
+	"errors"
+	"syscall"
+	"unsafe"
+)
+
+const LOCALE_NAME_MAX_LENGTH uint32 = 85
+
+func getWindowsLocale() (locale string, err error) {
+	buffer := make([]uint16, LOCALE_NAME_MAX_LENGTH)
+
+	dll := syscall.MustLoadDLL("kernel32")
+	proc := dll.MustFindProc("GetSystemDefaultLocaleName")
+	r, _, dllError := proc.Call(uintptr(unsafe.Pointer(&buffer[0])), uintptr(LOCALE_NAME_MAX_LENGTH))
+	if r == 0 {
+		err = errors.New(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE + ":\n" + dllError.Error())
+		return
+	}
+
+	locale = syscall.UTF16ToString(buffer)
+
+	return
+}
+
+func DetectIETF() (locale string, err error) {
+	locale, err = getWindowsLocale()
+	return
+}
+
+func DetectLanguage() (language string, err error) {
+	windows_locale, err := getWindowsLocale()
+	if err == nil {
+		language, _ = splitLocale(windows_locale)
+	}
+
+	return
+}
+
+func DetectTerritory() (territory string, err error) {
+	windows_locale, err := getWindowsLocale()
+	if err == nil {
+		_, territory = splitLocale(windows_locale)
+	}
+
+	return
+}
