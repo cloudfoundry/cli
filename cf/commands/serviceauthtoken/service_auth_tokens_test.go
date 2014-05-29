a0bb18fa4269be61512578045157936b800a0150
Tim Jarratt <tjarratt@pivotallabs.com>
2014-05-29 10:38:27 -0700

    Cleanup service-auth-tokens test
    [#68475822]

--- cf/commands/serviceauthtoken/service_auth_tokens_test.go
+++ cf/commands/serviceauthtoken/service_auth_tokens_test.go
@@ -1,42 +1,18 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package serviceauthtoken_test
 
 import (
-	. "github.com/cloudfoundry/cli/cf/commands/serviceauthtoken"
+	"github.com/cloudfoundry/cli/cf/configuration"
 	"github.com/cloudfoundry/cli/cf/models"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/serviceauthtoken"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 )
 
 func callListServiceAuthTokens(requirementsFactory *testreq.FakeReqFactory, authTokenRepo *testapi.FakeAuthTokenRepo) (ui *testterm.FakeUI) {
@@ -50,38 +26,52 @@ func callListServiceAuthTokens(requirementsFactory *testreq.FakeReqFactory, auth
 	return
 }
 
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestListServiceAuthTokensRequirements", func() {
-		authTokenRepo := &testapi.FakeAuthTokenRepo{}
-		requirementsFactory := &testreq.FakeReqFactory{}
+var _ = Describe("service-auth-tokens command", func() {
+	var (
+		ui                  *testterm.FakeUI
+		configRepo          configuration.ReadWriter
+		authTokenRepo       *testapi.FakeAuthTokenRepo
+		requirementsFactory *testreq.FakeReqFactory
+	)
 
-		requirementsFactory.LoginSuccess = false
-		callListServiceAuthTokens(requirementsFactory, authTokenRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
-
-		requirementsFactory.LoginSuccess = true
-		callListServiceAuthTokens(requirementsFactory, authTokenRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{Inputs: []string{"y"}}
+		authTokenRepo = &testapi.FakeAuthTokenRepo{}
+		configRepo = testconfig.NewRepositoryWithDefaults()
+		requirementsFactory = &testreq.FakeReqFactory{}
 	})
-	It("TestListServiceAuthTokens", func() {
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		authTokenRepo := &testapi.FakeAuthTokenRepo{}
-		authToken := models.ServiceAuthTokenFields{}
-		authToken.Label = "a label"
-		authToken.Provider = "a provider"
-		authToken2 := models.ServiceAuthTokenFields{}
-		authToken2.Label = "a second label"
-		authToken2.Provider = "a second provider"
-		authTokenRepo.FindAllAuthTokens = []models.ServiceAuthTokenFields{authToken, authToken2}
+	runCommand := func(args ...string) {
+		testcmd.RunCommand(NewListServiceAuthTokens(ui, configRepo, authTokenRepo), args, requirementsFactory)
+	}
 
-		ui := callListServiceAuthTokens(requirementsFactory, authTokenRepo)
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Getting service auth tokens as", "my-user"},
-			[]string{"OK"},
-			[]string{"label", "provider"},
-			[]string{"a label", "a provider"},
-			[]string{"a second label", "a second provider"},
-		))
+	Describe("requirements", func() {
+		It("fails when not logged in", func() {
+			runCommand()
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
+	})
+
+	Context("when logged in and some service auth tokens exist", func() {
+		BeforeEach(func() {
+			requirementsFactory.LoginSuccess = true
+
+			authTokenRepo.FindAllAuthTokens = []models.ServiceAuthTokenFields{
+				models.ServiceAuthTokenFields{Label: "a label", Provider: "a provider"},
+				models.ServiceAuthTokenFields{Label: "a second label", Provider: "a second provider"},
+			}
+		})
+
+		It("shows you the service auth tokens", func() {
+			runCommand()
+
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Getting service auth tokens as", "my-user"},
+				[]string{"OK"},
+				[]string{"label", "provider"},
+				[]string{"a label", "a provider"},
+				[]string{"a second label", "a second provider"},
+			))
+		})
 	})
 })
