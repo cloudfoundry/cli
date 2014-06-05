3d2827b2fbaadc910adf7366bf415375d9413d85
Daniel Lavine and Michael Maximilien <danlavine@pivotallabs.com>
2014-06-05 15:40:42 -0700

    Completed i18n enablement for cf/api/resources
    Also added fr_FR JSON
    
    [Finishes #72633038]

---
+++ cf/api/resources/i18n_init.go
@@ -0,0 +1,12 @@
+package resources
+
+import (
+	"github.com/cloudfoundry/cli/cf/i18n"
+	goi18n "github.com/nicksnyder/go-i18n/i18n"
+)
+
+var T goi18n.TranslateFunc
+
+func init() {
+	T = i18n.Init("cf/api/resources", i18n.GetResourcesPath())
+}
