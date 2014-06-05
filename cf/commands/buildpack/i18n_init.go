db35177c63dc5da4b78d9f2881a7281a9790aa46
Anand Gaitonde and Dan Lavine <agaitonde@pivotallabs.com>
2014-06-04 17:28:41 -0700

    Restructuring the translation files.
    [#72633090]

--- cf/commands/buildpack/i18n_init.go
+++ cf/commands/buildpack/i18n_init.go
@@ -3,10 +3,11 @@ package buildpack
 import (
 	"github.com/cloudfoundry/cli/cf/i18n"
 	goi18n "github.com/nicksnyder/go-i18n/i18n"
+	"path/filepath"
 )
 
 var T goi18n.TranslateFunc
 
 func init() {
-	T = i18n.Init("buildpack", i18n.GetResourcesPath())
+	T = i18n.Init(filepath.Join("cf", "commands", "buildpack"), i18n.GetResourcesPath())
 }
