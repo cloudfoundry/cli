81cd5917c77edcd6b797a2ba8b510fc0ade25e22
Tim Jarratt <tjarratt@pivotallabs.com>
2014-06-06 17:36:23 -0700

    Cleanup cf files tests
    [#68475822]

--- cf/commands/application/files_test.go
+++ cf/commands/application/files_test.go
@@ -1,114 +1,92 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package application_test
 
 import (
-	. "github.com/cloudfoundry/cli/cf/commands/application"
+	"github.com/cloudfoundry/cli/cf/configuration"
 	"github.com/cloudfoundry/cli/cf/models"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/application"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 )
 
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestFilesRequirements", func() {
-		args := []string{"my-app", "/foo"}
-		appFilesRepo := &testapi.FakeAppFilesRepo{}
+var _ = Describe("files command", func() {
+	var (
+		ui                  *testterm.FakeUI
+		configRepo          configuration.ReadWriter
+		requirementsFactory *testreq.FakeReqFactory
+		appFilesRepo        *testapi.FakeAppFilesRepo
+	)
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: false, TargetedSpaceSuccess: true, Application: models.Application{}}
-		callFiles(args, requirementsFactory, appFilesRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
-
-		requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: true, TargetedSpaceSuccess: false, Application: models.Application{}}
-		callFiles(args, requirementsFactory, appFilesRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
-
-		requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: true, TargetedSpaceSuccess: true, Application: models.Application{}}
-		callFiles(args, requirementsFactory, appFilesRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
-		Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
-	})
-	It("TestFilesFailsWithUsage", func() {
-
-		appFilesRepo := &testapi.FakeAppFilesRepo{}
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true, TargetedSpaceSuccess: true, Application: models.Application{}}
-		ui := callFiles([]string{}, requirementsFactory, appFilesRepo)
-
-		Expect(ui.FailedWithUsage).To(BeTrue())
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
-	})
-	It("TestListingDirectoryEntries", func() {
-
-		app := models.Application{}
-		app.Name = "my-found-app"
-		app.Guid = "my-app-guid"
-
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true, TargetedSpaceSuccess: true, Application: app}
-		appFilesRepo := &testapi.FakeAppFilesRepo{FileList: "file 1\nfile 2"}
-
-		ui := callFiles([]string{"my-app", "/foo"}, requirementsFactory, appFilesRepo)
-
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Getting files for app", "my-found-app", "my-org", "my-space", "my-user"},
-			[]string{"OK"},
-			[]string{"file 1"},
-			[]string{"file 2"},
-		))
-
-		Expect(appFilesRepo.AppGuid).To(Equal("my-app-guid"))
-		Expect(appFilesRepo.Path).To(Equal("/foo"))
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		configRepo = testconfig.NewRepositoryWithDefaults()
+		appFilesRepo = &testapi.FakeAppFilesRepo{}
+		requirementsFactory = &testreq.FakeReqFactory{}
 	})
 
-	It("TestListingFilesWithTemplateTokens", func() {
+	runCommand := func(args ...string) {
+		testcmd.RunCommand(NewFiles(ui, configRepo, appFilesRepo), args, requirementsFactory)
+	}
 
-		app := models.Application{}
-		app.Name = "my-found-app"
-		app.Guid = "my-app-guid"
+	Describe("requirements", func() {
+		It("fails when not logged in", func() {
+			requirementsFactory.TargetedSpaceSuccess = true
+			runCommand("my-app", "/foo")
+		})
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true, TargetedSpaceSuccess: true, Application: app}
-		appFilesRepo := &testapi.FakeAppFilesRepo{FileList: "%s %d %i"}
+		It("fails when a space is not targeted", func() {
+			requirementsFactory.LoginSuccess = true
+			runCommand("my-app", "/foo")
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
 
-		ui := callFiles([]string{"my-app", "/foo"}, requirementsFactory, appFilesRepo)
+		It("fails with usage when not provided an app name", func() {
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.TargetedSpaceSuccess = true
+			runCommand()
 
-		Expect(ui.Outputs).To(ContainSubstrings([]string{"%s %d %i"}))
+			Expect(ui.FailedWithUsage).To(BeTrue())
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
+	})
+
+	Context("when logged in, a space is targeted and a valid app name is provided", func() {
+		BeforeEach(func() {
+			app := models.Application{}
+			app.Name = "my-found-app"
+			app.Guid = "my-app-guid"
+
+			requirementsFactory.Application = app
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.TargetedSpaceSuccess = true
+			appFilesRepo.FileList = "file 1\nfile 2"
+		})
+
+		It("it lists files in a directory", func() {
+			runCommand("my-app", "/foo")
+
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Getting files for app", "my-found-app", "my-org", "my-space", "my-user"},
+				[]string{"OK"},
+				[]string{"file 1"},
+				[]string{"file 2"},
+			))
+
+			Expect(appFilesRepo.AppGuid).To(Equal("my-app-guid"))
+			Expect(appFilesRepo.Path).To(Equal("/foo"))
+		})
+
+		It("does not interpolate or interpret special format characters as though it should be a format string", func() {
+			appFilesRepo.FileList = "%s %d %i"
+			runCommand("my-app", "/foo")
+
+			Expect(ui.Outputs).To(ContainSubstrings([]string{"%s %d %i"}))
+		})
 	})
 })
-
-func callFiles(args []string, requirementsFactory *testreq.FakeReqFactory, appFilesRepo *testapi.FakeAppFilesRepo) (ui *testterm.FakeUI) {
-	ui = &testterm.FakeUI{}
-
-	configRepo := testconfig.NewRepositoryWithDefaults()
-	cmd := NewFiles(ui, configRepo, appFilesRepo)
-	testcmd.RunCommand(cmd, args, requirementsFactory)
-
-	return
-}
