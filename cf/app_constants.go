c3d3e99d80b87af8150cad8c05d00aa5b4cc73b6
Michael Maximilien and Tim Jarratt <tjarratt@pivotallabs.com>
2014-06-09 16:30:01 -0700

    Translate the cf package
    Basically the last one.
    
    [Finishes #72633130]

--- cf/app_constants.go
+++ cf/app_constants.go
@@ -3,12 +3,18 @@ package cf
 import (
 	"os"
 	"path/filepath"
+
+	"github.com/cloudfoundry/cli/cf/i18n"
 )
 
 const (
 	Version     = "BUILT_FROM_SOURCE"
 	BuiltOnDate = "BUILT_AT_UNKNOWN_TIME"
-	Usage       = "A command line tool to interact with Cloud Foundry"
+)
+
+var (
+	t     = i18n.Init("cf", i18n.GetResourcesPath())
+	Usage = t("A command line tool to interact with Cloud Foundry")
 )
 
 func Name() string {
