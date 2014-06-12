cce054ab3b98e56f5b60fc90cd1669e1cd52434b
Daniel Lavine and Tim Jarratt <tjarratt@pivotallabs.com>
2014-06-12 12:34:09 -0700

    Cleanup 'cf restart' tests
    [#68475822]

--- cf/commands/application/restart_test.go
+++ cf/commands/application/restart_test.go
@@ -1,90 +1,75 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package application_test
 
 import (
-	. "github.com/cloudfoundry/cli/cf/commands/application"
 	"github.com/cloudfoundry/cli/cf/models"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/application"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-func callRestart(args []string, requirementsFactory *testreq.FakeReqFactory, starter ApplicationStarter, stopper ApplicationStopper) (ui *testterm.FakeUI) {
-	ui = new(testterm.FakeUI)
+var _ = Describe("restart command", func() {
+	var (
+		ui                  *testterm.FakeUI
+		requirementsFactory *testreq.FakeReqFactory
+		starter             *testcmd.FakeAppStarter
+		stopper             *testcmd.FakeAppStopper
+		app                 models.Application
+	)
 
-	cmd := NewRestart(ui, starter, stopper)
-	testcmd.RunCommand(cmd, args, requirementsFactory)
-	return
-}
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		requirementsFactory = &testreq.FakeReqFactory{}
+		starter = &testcmd.FakeAppStarter{}
+		stopper = &testcmd.FakeAppStopper{}
 
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestRestartCommandFailsWithUsage", func() {
-		requirementsFactory := &testreq.FakeReqFactory{}
-		starter := &testcmd.FakeAppStarter{}
-		stopper := &testcmd.FakeAppStopper{}
-		ui := callRestart([]string{}, requirementsFactory, starter, stopper)
-		Expect(ui.FailedWithUsage).To(BeTrue())
-
-		ui = callRestart([]string{"my-app"}, requirementsFactory, starter, stopper)
-		Expect(ui.FailedWithUsage).To(BeFalse())
-	})
-	It("TestRestartRequirements", func() {
-
-		app := models.Application{}
+		app = models.Application{}
 		app.Name = "my-app"
 		app.Guid = "my-app-guid"
-		starter := &testcmd.FakeAppStarter{}
-		stopper := &testcmd.FakeAppStopper{}
-
-		requirementsFactory := &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: true}
-		callRestart([]string{"my-app"}, requirementsFactory, starter, stopper)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
-
-		requirementsFactory = &testreq.FakeReqFactory{Application: app, LoginSuccess: false, TargetedSpaceSuccess: true}
-		callRestart([]string{"my-app"}, requirementsFactory, starter, stopper)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
-
-		requirementsFactory = &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: false}
-		callRestart([]string{"my-app"}, requirementsFactory, starter, stopper)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
 	})
-	It("TestRestartApplication", func() {
 
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		requirementsFactory := &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: true}
-		starter := &testcmd.FakeAppStarter{}
-		stopper := &testcmd.FakeAppStopper{}
-		callRestart([]string{"my-app"}, requirementsFactory, starter, stopper)
+	runCommand := func(args ...string) {
+		testcmd.RunCommand(NewRestart(ui, starter, stopper), args, requirementsFactory)
+	}
 
-		Expect(stopper.AppToStop).To(Equal(app))
-		Expect(starter.AppToStart).To(Equal(app))
+	Describe("requirements", func() {
+		It("fails with usage when an app name is not given", func() {
+			requirementsFactory.LoginSuccess = true
+			runCommand()
+			Expect(ui.FailedWithUsage).To(BeTrue())
+		})
+
+		It("fails when not logged in", func() {
+			requirementsFactory.Application = app
+			requirementsFactory.TargetedSpaceSuccess = true
+			runCommand()
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
+
+		It("fails when a space is not targeted", func() {
+			requirementsFactory.Application = app
+			requirementsFactory.LoginSuccess = true
+			runCommand()
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
+	})
+
+	Context("when logged in, targeting a space, and an app name is provided", func() {
+		BeforeEach(func() {
+			requirementsFactory.Application = app
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.TargetedSpaceSuccess = true
+		})
+
+		It("restarts the given app", func() {
+			runCommand("my-app")
+
+			Expect(stopper.AppToStop).To(Equal(app))
+			Expect(starter.AppToStart).To(Equal(app))
+			Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
+		})
 	})
 })
