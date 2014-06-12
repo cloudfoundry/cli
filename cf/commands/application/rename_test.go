936472d6ba005f12ecc826004c657d6a41a2b5e8
Daniel Lavine and Tim Jarratt <danlavine@pivotallabs.com>
2014-06-12 11:23:25 -0700

    cleanup 'cf rename' tests
    [#68475822]

--- cf/commands/application/rename_test.go
+++ cf/commands/application/rename_test.go
@@ -1,72 +1,59 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package application_test
 
 import (
-	. "github.com/cloudfoundry/cli/cf/commands/application"
+	"github.com/cloudfoundry/cli/cf/configuration"
 	"github.com/cloudfoundry/cli/cf/models"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/application"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 )
 
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestRenameAppFailsWithUsage", func() {
-		requirementsFactory := &testreq.FakeReqFactory{}
-		appRepo := &testapi.FakeApplicationRepository{}
+var _ = Describe("Rename command", func() {
+	var (
+		ui                  *testterm.FakeUI
+		requirementsFactory *testreq.FakeReqFactory
+		configRepo          configuration.ReadWriter
+		appRepo             *testapi.FakeApplicationRepository
+	)
 
-		ui := callRename([]string{}, requirementsFactory, appRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
-
-		ui = callRename([]string{"foo"}, requirementsFactory, appRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		configRepo = testconfig.NewRepositoryWithDefaults()
+		requirementsFactory = &testreq.FakeReqFactory{}
+		appRepo = &testapi.FakeApplicationRepository{}
 	})
-	It("TestRenameRequirements", func() {
 
-		appRepo := &testapi.FakeApplicationRepository{}
+	runCommand := func(args ...string) {
+		testcmd.RunCommand(NewRenameApp(ui, configRepo, appRepo), args, requirementsFactory)
+	}
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		callRename([]string{"my-app", "my-new-app"}, requirementsFactory, appRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
-		Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
+	Describe("requirements", func() {
+		It("fails with usage when not invoked with an old name and a new name", func() {
+			requirementsFactory.LoginSuccess = true
+			runCommand("foo")
+			Expect(ui.FailedWithUsage).To(BeTrue())
+		})
+
+		It("fails when not logged in", func() {
+			runCommand("my-app", "my-new-app")
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
 	})
-	It("TestRenameRun", func() {
 
-		appRepo := &testapi.FakeApplicationRepository{}
+	It("renames an application", func() {
 		app := models.Application{}
 		app.Name = "my-app"
 		app.Guid = "my-app-guid"
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true, Application: app}
-		ui := callRename([]string{"my-app", "my-new-app"}, requirementsFactory, appRepo)
+		requirementsFactory.LoginSuccess = true
+		requirementsFactory.Application = app
+		runCommand("my-app", "my-new-app")
 
 		Expect(appRepo.UpdateAppGuid).To(Equal(app.Guid))
 		Expect(*appRepo.UpdateParams.Name).To(Equal("my-new-app"))
@@ -76,12 +63,3 @@ var _ = Describe("Testing with ginkgo", func() {
 		))
 	})
 })
-
-func callRename(args []string, requirementsFactory *testreq.FakeReqFactory, appRepo *testapi.FakeApplicationRepository) (ui *testterm.FakeUI) {
-	ui = new(testterm.FakeUI)
-
-	configRepo := testconfig.NewRepositoryWithDefaults()
-	cmd := NewRenameApp(ui, configRepo, appRepo)
-	testcmd.RunCommand(cmd, args, requirementsFactory)
-	return
-}
