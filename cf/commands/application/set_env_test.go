139a07f0cc89dacd41711761b06218ab404b3a9c
Tim Jarratt <tjarratt@pivotallabs.com>
2014-06-16 11:52:26 -0700

    Cleanup set_env tests
    [#68475822]

--- cf/commands/application/set_env_test.go
+++ cf/commands/application/set_env_test.go
@@ -1,7 +1,7 @@
 package application_test
 
 import (
-	"github.com/cloudfoundry/cli/cf/api"
+	"github.com/cloudfoundry/cli/cf/configuration"
 	"github.com/cloudfoundry/cli/cf/models"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
@@ -17,58 +17,66 @@ import (
 
 var _ = Describe("set-env command", func() {
 	var (
+		ui                  *testterm.FakeUI
+		configRepo          configuration.ReadWriter
 		app                 models.Application
 		appRepo             *testapi.FakeApplicationRepository
-		args                []string
 		requirementsFactory *testreq.FakeReqFactory
 	)
 
 	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
 		app = models.Application{}
 		app.Name = "my-app"
 		app.Guid = "my-app-guid"
 		appRepo = &testapi.FakeApplicationRepository{}
+		requirementsFactory = &testreq.FakeReqFactory{}
+		configRepo = testconfig.NewRepositoryWithDefaults()
 	})
 
-	JustBeforeEach(func() {
-		requirementsFactory = &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: true}
-	})
+	runCommand := func(args ...string) {
+		testcmd.RunCommand(NewSetEnv(ui, configRepo, appRepo), args, requirementsFactory)
+	}
 
 	Describe("requirements", func() {
-		BeforeEach(func() {
-			args = []string{"my-app", "DATABASE_URL", "mysql://example.com/my-db"}
-		})
-
-		It("passes when all requirements are met", func() {
-			requirementsFactory = &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: true}
-			callSetEnv(args, requirementsFactory, appRepo)
-			Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
-		})
-
 		It("fails when login is not successful", func() {
-			requirementsFactory = &testreq.FakeReqFactory{Application: app, LoginSuccess: false, TargetedSpaceSuccess: true}
-			callSetEnv(args, requirementsFactory, appRepo)
+			requirementsFactory.Application = app
+			requirementsFactory.TargetedSpaceSuccess = true
+
+			runCommand("hey", "gabba", "gabba")
 			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
 		})
 
 		It("fails when a space is not targeted", func() {
-			requirementsFactory = &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: false}
-			callSetEnv(args, requirementsFactory, appRepo)
+			requirementsFactory.Application = app
+			requirementsFactory.LoginSuccess = true
+
+			runCommand("hey", "gabba", "gabba")
 			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
+
+		It("fails with usage when not provided with exactly three args", func() {
+			requirementsFactory.Application = app
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.TargetedSpaceSuccess = true
+
+			runCommand("zomg", "too", "many", "args")
+			Expect(ui.FailedWithUsage).To(BeTrue())
 		})
 	})
 
-	Context("setting an environment variable", func() {
+	Context("when logged in, a space is targeted and given enough args", func() {
 		BeforeEach(func() {
 			app.EnvironmentVars = map[string]string{"foo": "bar"}
-			args = []string{"my-app", "DATABASE_URL", "mysql://new-example.com/my-db"}
+			requirementsFactory.Application = app
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.TargetedSpaceSuccess = true
 		})
 
 		Context("when it is new", func() {
 			It("is created", func() {
-				ui := callSetEnv(args, requirementsFactory, appRepo)
+				runCommand("my-app", "DATABASE_URL", "mysql://new-example.com/my-db")
 
-				Expect(len(ui.Outputs)).To(Equal(3))
 				Expect(ui.Outputs).To(ContainSubstrings(
 					[]string{
 						"Setting env variable",
@@ -98,7 +106,7 @@ var _ = Describe("set-env command", func() {
 			})
 
 			It("is updated", func() {
-				ui := callSetEnv(args, requirementsFactory, appRepo)
+				runCommand("my-app", "DATABASE_URL", "mysql://new-example.com/my-db")
 
 				Expect(ui.Outputs).To(ContainSubstrings(
 					[]string{
@@ -113,19 +121,11 @@ var _ = Describe("set-env command", func() {
 					[]string{"OK"},
 					[]string{"TIP"},
 				))
-
-				Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
-				Expect(appRepo.UpdateAppGuid).To(Equal(app.Guid))
-				Expect(*appRepo.UpdateParams.EnvironmentVars).To(Equal(map[string]string{
-					"DATABASE_URL": "mysql://new-example.com/my-db",
-					"foo":          "bar",
-				}))
 			})
 		})
 
 		It("allows the variable value to begin with a hyphen", func() {
-			args = []string{"my-app", "MY_VAR", "--has-a-cool-value"}
-			ui := callSetEnv(args, requirementsFactory, appRepo)
+			runCommand("my-app", "MY_VAR", "--has-a-cool-value")
 
 			Expect(ui.Outputs).To(ContainSubstrings(
 				[]string{
@@ -136,8 +136,6 @@ var _ = Describe("set-env command", func() {
 				[]string{"OK"},
 				[]string{"TIP"},
 			))
-
-			Expect(appRepo.UpdateAppGuid).To(Equal(app.Guid))
 			Expect(*appRepo.UpdateParams.EnvironmentVars).To(Equal(map[string]string{
 				"MY_VAR": "--has-a-cool-value",
 				"foo":    "bar",
@@ -150,7 +148,7 @@ var _ = Describe("set-env command", func() {
 			})
 
 			It("tells the user", func() {
-				ui := callSetEnv(args, requirementsFactory, appRepo)
+				runCommand("please", "dont", "fail")
 
 				Expect(ui.Outputs).To(ContainSubstrings(
 					[]string{"Setting env variable"},
@@ -160,58 +158,4 @@ var _ = Describe("set-env command", func() {
 			})
 		})
 	})
-
-	Describe("usage requirements", func() {
-		Context("when an app, key, and value are all present", func() {
-			BeforeEach(func() {
-				args = []string{"my-app", "DATABASE_URL", "..."}
-			})
-
-			It("does not fail with usage", func() {
-				ui := callSetEnv(args, requirementsFactory, appRepo)
-				Expect(ui.FailedWithUsage).To(BeFalse())
-			})
-		})
-
-		Context("when the value is missing", func() {
-			BeforeEach(func() {
-				args = []string{"my-app", "DATABASE_URL"}
-			})
-
-			It("fails with usage", func() {
-				ui := callSetEnv(args, requirementsFactory, appRepo)
-				Expect(ui.FailedWithUsage).To(BeTrue())
-			})
-		})
-
-		Context("when the key and value are missing", func() {
-			BeforeEach(func() {
-				args = []string{"my-app"}
-			})
-
-			It("fails with usage", func() {
-				ui := callSetEnv(args, requirementsFactory, appRepo)
-				Expect(ui.FailedWithUsage).To(BeTrue())
-			})
-		})
-
-		Context("when all parameters are missing", func() {
-			BeforeEach(func() {
-				args = []string{}
-			})
-
-			It("fails with usage", func() {
-				ui := callSetEnv(args, requirementsFactory, appRepo)
-				Expect(ui.FailedWithUsage).To(BeTrue())
-			})
-		})
-	})
 })
-
-func callSetEnv(args []string, requirementsFactory *testreq.FakeReqFactory, appRepo api.ApplicationRepository) (ui *testterm.FakeUI) {
-	ui = new(testterm.FakeUI)
-	configRepo := testconfig.NewRepositoryWithDefaults()
-	cmd := NewSetEnv(ui, configRepo, appRepo)
-	testcmd.RunCommand(cmd, args, requirementsFactory)
-	return
-}
