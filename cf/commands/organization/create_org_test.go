7943bf528157b8cdedfe331d7377599a2f836052
Tim Jarratt <tjarratt@pivotallabs.com>
2014-06-16 16:53:23 -0700

    Cleanup create-org tests
    [#68475822]

--- cf/commands/organization/create_org_test.go
+++ cf/commands/organization/create_org_test.go
@@ -1,110 +1,75 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package organization_test
 
 import (
-	. "github.com/cloudfoundry/cli/cf/commands/organization"
 	"github.com/cloudfoundry/cli/cf/configuration"
-	"github.com/cloudfoundry/cli/cf/models"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/organization"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 )
 
-func callCreateOrg(args []string, requirementsFactory *testreq.FakeReqFactory, orgRepo *testapi.FakeOrgRepository) (fakeUI *testterm.FakeUI) {
-	fakeUI = new(testterm.FakeUI)
+var _ = Describe("create-org command", func() {
+	var (
+		config              configuration.ReadWriter
+		ui                  *testterm.FakeUI
+		requirementsFactory *testreq.FakeReqFactory
+		orgRepo             *testapi.FakeOrgRepository
+	)
 
-	space := models.SpaceFields{}
-	space.Name = "my-space"
-
-	organization := models.OrganizationFields{}
-	organization.Name = "my-org"
-
-	token := configuration.TokenInfo{Username: "my-user"}
-	config := testconfig.NewRepositoryWithAccessToken(token)
-	config.SetSpaceFields(space)
-	config.SetOrganizationFields(organization)
-
-	cmd := NewCreateOrg(fakeUI, config, orgRepo)
-
-	testcmd.RunCommand(cmd, args, requirementsFactory)
-	return
-}
-
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestCreateOrgFailsWithUsage", func() {
-		orgRepo := &testapi.FakeOrgRepository{}
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-
-		ui := callCreateOrg([]string{}, requirementsFactory, orgRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
-
-		ui = callCreateOrg([]string{"my-org"}, requirementsFactory, orgRepo)
-		Expect(ui.FailedWithUsage).To(BeFalse())
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		config = testconfig.NewRepositoryWithDefaults()
+		requirementsFactory = &testreq.FakeReqFactory{}
+		orgRepo = &testapi.FakeOrgRepository{}
 	})
 
-	It("TestCreateOrgRequirements", func() {
-		orgRepo := &testapi.FakeOrgRepository{}
+	runCommand := func(args ...string) {
+		testcmd.RunCommand(NewCreateOrg(ui, config, orgRepo), args, requirementsFactory)
+	}
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		callCreateOrg([]string{"my-org"}, requirementsFactory, orgRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
+	Describe("requirements", func() {
+		It("fails with usage when not provided exactly one arg", func() {
+			requirementsFactory.LoginSuccess = true
+			runCommand()
+			Expect(ui.FailedWithUsage).To(BeTrue())
+		})
 
-		requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: false}
-		callCreateOrg([]string{"my-org"}, requirementsFactory, orgRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		It("fails when not logged in", func() {
+			runCommand("my-org")
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
 	})
 
-	It("TestCreateOrg", func() {
-		orgRepo := &testapi.FakeOrgRepository{}
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		ui := callCreateOrg([]string{"my-org"}, requirementsFactory, orgRepo)
+	Context("when logged in and provided the name of an org to create", func() {
+		BeforeEach(func() {
+			requirementsFactory.LoginSuccess = true
+		})
 
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Creating org", "my-org", "my-user"},
-			[]string{"OK"},
-		))
-		Expect(orgRepo.CreateName).To(Equal("my-org"))
-	})
+		It("creates an org", func() {
+			runCommand("my-org")
 
-	It("TestCreateOrgWhenAlreadyExists", func() {
-		orgRepo := &testapi.FakeOrgRepository{CreateOrgExists: true}
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		ui := callCreateOrg([]string{"my-org"}, requirementsFactory, orgRepo)
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Creating org", "my-org", "my-user"},
+				[]string{"OK"},
+			))
+			Expect(orgRepo.CreateName).To(Equal("my-org"))
+		})
 
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Creating org", "my-org"},
-			[]string{"OK"},
-			[]string{"my-org", "already exists"},
-		))
+		It("fails and warns the user when the org already exists", func() {
+			orgRepo.CreateOrgExists = true
+			runCommand("my-org")
+
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Creating org", "my-org"},
+				[]string{"OK"},
+				[]string{"my-org", "already exists"},
+			))
+		})
 	})
 })
