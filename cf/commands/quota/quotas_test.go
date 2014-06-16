ff685a5867b57ff3f0cbbc8e03329ef8503b96dd
Tim Jarratt <tjarratt@pivotallabs.com>
2014-06-16 16:49:06 -0700

    Cleanup imports in quotas test
    [#68475822]

--- cf/commands/quota/quotas_test.go
+++ cf/commands/quota/quotas_test.go
@@ -1,18 +1,18 @@
 package quota_test
 
 import (
-	. "github.com/cloudfoundry/cli/cf/commands/quota"
-	"github.com/cloudfoundry/cli/cf/models"
-	. "github.com/cloudfoundry/cli/testhelpers/matchers"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
 	"github.com/cloudfoundry/cli/cf/errors"
+	"github.com/cloudfoundry/cli/cf/models"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/quota"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("quotas command", func() {
