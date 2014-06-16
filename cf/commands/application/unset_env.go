bb22921ac29db43d3f8acf97f1220bbbec967e2d
Tim Jarratt <tjarratt@pivotallabs.com>
2014-06-16 11:31:20 -0700

    Cleanup unset-env tests
    [#68475822]

--- cf/commands/application/unset_env.go
+++ cf/commands/application/unset_env.go
@@ -35,7 +35,7 @@ func (cmd *UnsetEnv) Metadata() command_metadata.CommandMetadata {
 }
 
 func (cmd *UnsetEnv) GetRequirements(requirementsFactory requirements.Factory, c *cli.Context) (reqs []requirements.Requirement, err error) {
-	if len(c.Args()) < 2 {
+	if len(c.Args()) != 2 {
 		cmd.ui.FailWithUsage(c)
 	}
 
--- cf/commands/application/unset_env_test.go
+++ cf/commands/application/unset_env_test.go
@@ -1,154 +1,119 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package application_test
 
 import (
-	"github.com/cloudfoundry/cli/cf/api"
-	. "github.com/cloudfoundry/cli/cf/commands/application"
+	"github.com/cloudfoundry/cli/cf/configuration"
 	"github.com/cloudfoundry/cli/cf/models"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/application"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 )
 
-var _ = Describe("Testing with ginkgo", func() {
-	It("TestUnsetEnvRequirements", func() {
-		app := models.Application{}
+var _ = Describe("unset-env command", func() {
+	var (
+		ui                  *testterm.FakeUI
+		app                 models.Application
+		appRepo             *testapi.FakeApplicationRepository
+		configRepo          configuration.ReadWriter
+		requirementsFactory *testreq.FakeReqFactory
+	)
+
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		app = models.Application{}
 		app.Name = "my-app"
 		app.Guid = "my-app-guid"
-		appRepo := &testapi.FakeApplicationRepository{}
-		args := []string{"my-app", "DATABASE_URL"}
-
-		requirementsFactory := &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: true}
-		callUnsetEnv(args, requirementsFactory, appRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
-
-		requirementsFactory = &testreq.FakeReqFactory{Application: app, LoginSuccess: false, TargetedSpaceSuccess: true}
-		callUnsetEnv(args, requirementsFactory, appRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
-
-		requirementsFactory = &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: false}
-		callUnsetEnv(args, requirementsFactory, appRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
-	})
-	It("TestUnsetEnvWhenApplicationExists", func() {
-
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		app.EnvironmentVars = map[string]string{"foo": "bar", "DATABASE_URL": "mysql://example.com/my-db"}
-		requirementsFactory := &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: true}
-		appRepo := &testapi.FakeApplicationRepository{}
-
-		args := []string{"my-app", "DATABASE_URL"}
-		ui := callUnsetEnv(args, requirementsFactory, appRepo)
-
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Removing env variable", "DATABASE_URL", "my-app", "my-org", "my-space", "my-user"},
-			[]string{"OK"},
-		))
-
-		Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
-		Expect(appRepo.UpdateAppGuid).To(Equal("my-app-guid"))
-		Expect(*appRepo.UpdateParams.EnvironmentVars).To(Equal(map[string]string{
-			"foo": "bar",
-		}))
+		appRepo = &testapi.FakeApplicationRepository{}
+		requirementsFactory = &testreq.FakeReqFactory{}
+		configRepo = testconfig.NewRepositoryWithDefaults()
 	})
 
-	It("TestUnsetEnvWhenUnsettingTheEnvFails", func() {
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		app.EnvironmentVars = map[string]string{"DATABASE_URL": "mysql://example.com/my-db"}
-		requirementsFactory := &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: true}
-		appRepo := &testapi.FakeApplicationRepository{
-			UpdateErr: true,
-		}
-		appRepo.ReadReturns.App = app
+	runCommand := func(args ...string) {
+		testcmd.RunCommand(NewUnsetEnv(ui, configRepo, appRepo), args, requirementsFactory)
+	}
 
-		args := []string{"does-not-exist", "DATABASE_URL"}
-		ui := callUnsetEnv(args, requirementsFactory, appRepo)
+	Describe("requirements", func() {
+		It("fails when not logged in", func() {
+			requirementsFactory.TargetedSpaceSuccess = true
+			requirementsFactory.Application = app
+			runCommand("foo", "bar")
 
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Removing env variable"},
-			[]string{"FAILED"},
-			[]string{"Error updating app."},
-		))
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
+
+		It("fails when a space is not targeted", func() {
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.Application = app
+			runCommand("foo", "bar")
+
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
+
+		It("fails with usage when not provided with exactly 2 args", func() {
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.TargetedSpaceSuccess = true
+			requirementsFactory.Application = app
+
+			runCommand("too", "many", "args")
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
 	})
 
-	It("TestUnsetEnvWhenEnvVarDoesNotExist", func() {
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		requirementsFactory := &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: true}
-		appRepo := &testapi.FakeApplicationRepository{}
+	Context("when logged in, a space is targeted and provided enough args", func() {
+		BeforeEach(func() {
+			app.EnvironmentVars = map[string]string{"foo": "bar", "DATABASE_URL": "mysql://example.com/my-db"}
 
-		args := []string{"my-app", "DATABASE_URL"}
-		ui := callUnsetEnv(args, requirementsFactory, appRepo)
+			requirementsFactory.Application = app
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.TargetedSpaceSuccess = true
+		})
 
-		Expect(len(ui.Outputs)).To(Equal(3))
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Removing env variable"},
-			[]string{"OK"},
-			[]string{"DATABASE_URL", "was not set."},
-		))
-	})
+		It("updates the app and tells the user what happened", func() {
+			runCommand("my-app", "DATABASE_URL")
 
-	It("TestUnsetEnvFailsWithUsage", func() {
-		app := models.Application{}
-		app.Name = "my-app"
-		app.Guid = "my-app-guid"
-		requirementsFactory := &testreq.FakeReqFactory{Application: app, LoginSuccess: true, TargetedSpaceSuccess: true}
-		appRepo := &testapi.FakeApplicationRepository{}
-		appRepo.ReadReturns.App = app
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Removing env variable", "DATABASE_URL", "my-app", "my-org", "my-space", "my-user"},
+				[]string{"OK"},
+			))
 
-		args := []string{"my-app", "DATABASE_URL"}
-		ui := callUnsetEnv(args, requirementsFactory, appRepo)
-		Expect(ui.FailedWithUsage).To(BeFalse())
+			Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
+			Expect(appRepo.UpdateAppGuid).To(Equal("my-app-guid"))
+			Expect(*appRepo.UpdateParams.EnvironmentVars).To(Equal(map[string]string{
+				"foo": "bar",
+			}))
+		})
 
-		args = []string{"my-app"}
-		ui = callUnsetEnv(args, requirementsFactory, appRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
+		Context("when updating the app fails", func() {
+			BeforeEach(func() {
+				appRepo.UpdateErr = true
+				appRepo.ReadReturns.App = app
+			})
 
-		args = []string{}
-		ui = callUnsetEnv(args, requirementsFactory, appRepo)
-		Expect(ui.FailedWithUsage).To(BeTrue())
+			It("fails and alerts the user", func() {
+				runCommand("does-not-exist", "DATABASE_URL")
+
+				Expect(ui.Outputs).To(ContainSubstrings(
+					[]string{"Removing env variable"},
+					[]string{"FAILED"},
+					[]string{"Error updating app."},
+				))
+			})
+		})
+
+		It("tells the user if the specified env var was not set", func() {
+			runCommand("my-app", "CANT_STOP_WONT_STOP_UNSETTIN_THIS_ENV")
+
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Removing env variable"},
+				[]string{"OK"},
+				[]string{"CANT_STOP_WONT_STOP_UNSETTIN_THIS_ENV", "was not set."},
+			))
+		})
 	})
 })
-
-func callUnsetEnv(args []string, requirementsFactory *testreq.FakeReqFactory, appRepo api.ApplicationRepository) (ui *testterm.FakeUI) {
-	ui = new(testterm.FakeUI)
-	configRepo := testconfig.NewRepositoryWithDefaults()
-	cmd := NewUnsetEnv(ui, configRepo, appRepo)
-	testcmd.RunCommand(cmd, args, requirementsFactory)
-	return
-}
