617acd62a39a7bf3c4ebf30564add9dd34292a3e
Anand Gaitonde <agaitonde@pivotallabs.com>
2014-06-17 16:17:25 -0700

    Cleanup test create shared domain
    [#68475822]

--- cf/commands/domain/create_shared_domain_test.go
+++ cf/commands/domain/create_shared_domain_test.go
@@ -1,71 +1,58 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package domain_test
 
 import (
-	. "github.com/cloudfoundry/cli/cf/commands/domain"
 	"github.com/cloudfoundry/cli/cf/configuration"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/domain"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 )
 
 var _ = Describe("Testing with ginkgo", func() {
-	It("TestShareDomainRequirements", func() {
-		domainRepo := &testapi.FakeDomainRepository{}
+	var (
+		requirementsFactory *testreq.FakeReqFactory
+		ui                  *testterm.FakeUI
+		domainRepo          *testapi.FakeDomainRepository
+		configRepo          configuration.ReadWriter
+	)
+	BeforeEach(func() {
+		requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: true}
+		domainRepo = &testapi.FakeDomainRepository{}
+		configRepo = testconfig.NewRepositoryWithAccessToken(configuration.TokenInfo{Username: "my-user"})
+	})
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		callShareDomain([]string{"example.com"}, requirementsFactory, domainRepo)
+	runCommand := func(args ...string) {
+		ui = new(testterm.FakeUI)
+		cmd := NewCreateSharedDomain(ui, configRepo, domainRepo)
+		testcmd.RunCommand(cmd, args, requirementsFactory)
+		return
+	}
+
+	It("TestShareDomainRequirements", func() {
+		runCommand("example.com")
 		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
 
 		requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: false}
-		callShareDomain([]string{"example.com"}, requirementsFactory, domainRepo)
+		runCommand("example.com")
 		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
 	})
-	It("TestShareDomainFailsWithUsage", func() {
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		domainRepo := &testapi.FakeDomainRepository{}
-		ui := callShareDomain([]string{}, requirementsFactory, domainRepo)
+	It("TestShareDomainFailsWithUsage", func() {
+		runCommand()
 		Expect(ui.FailedWithUsage).To(BeTrue())
 
-		ui = callShareDomain([]string{"example.com"}, requirementsFactory, domainRepo)
+		runCommand("example.com")
 		Expect(ui.FailedWithUsage).To(BeFalse())
 	})
-	It("TestShareDomain", func() {
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		domainRepo := &testapi.FakeDomainRepository{}
-		ui := callShareDomain([]string{"example.com"}, requirementsFactory, domainRepo)
+	It("TestShareDomain", func() {
+		runCommand("example.com")
 
 		Expect(domainRepo.CreateSharedDomainName).To(Equal("example.com"))
 		Expect(ui.Outputs).To(ContainSubstrings(
@@ -74,11 +61,3 @@ var _ = Describe("Testing with ginkgo", func() {
 		))
 	})
 })
-
-func callShareDomain(args []string, requirementsFactory *testreq.FakeReqFactory, domainRepo *testapi.FakeDomainRepository) (fakeUI *testterm.FakeUI) {
-	fakeUI = new(testterm.FakeUI)
-	configRepo := testconfig.NewRepositoryWithAccessToken(configuration.TokenInfo{Username: "my-user"})
-	cmd := NewCreateSharedDomain(fakeUI, configRepo, domainRepo)
-	testcmd.RunCommand(cmd, args, requirementsFactory)
-	return
-}
