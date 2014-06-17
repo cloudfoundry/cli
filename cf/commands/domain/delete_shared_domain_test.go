7f193abaa31ea0dc65abe8f1ddcad3adef5f6bcb
Daniel Lavine and Tim Jarratt <danlavine@pivotallabs.com>
2014-06-17 16:18:25 -0700

    Clean up delete shared domain tests
    [Finishes #68475822]

--- cf/commands/domain/delete_shared_domain_test.go
+++ cf/commands/domain/delete_shared_domain_test.go
@@ -1,32 +1,6 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package domain_test
 
 import (
-	"github.com/cloudfoundry/cli/cf/commands/domain"
 	"github.com/cloudfoundry/cli/cf/configuration"
 	"github.com/cloudfoundry/cli/cf/errors"
 	"github.com/cloudfoundry/cli/cf/models"
@@ -35,134 +9,120 @@ import (
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/domain"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 )
 
 var _ = Describe("delete-shared-domain command", func() {
-	It("TestGetDeleteSharedDomainRequirements", func() {
-		deps := getDeleteSharedDomainDeps()
-		deps.requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: true, TargetedOrgSuccess: true}
+	var (
+		ui                  *testterm.FakeUI
+		domainRepo          *testapi.FakeDomainRepository
+		requirementsFactory *testreq.FakeReqFactory
+		configRepo          configuration.ReadWriter
+	)
 
-		callDeleteSharedDomain([]string{"foo.com"}, []string{"y"}, deps)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
-
-		deps.requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: true, TargetedOrgSuccess: false}
-		callDeleteSharedDomain([]string{"foo.com"}, []string{"y"}, deps)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
-
-		deps.requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: false, TargetedOrgSuccess: true}
-		callDeleteSharedDomain([]string{"foo.com"}, []string{"y"}, deps)
-		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		domainRepo = &testapi.FakeDomainRepository{}
+		requirementsFactory = &testreq.FakeReqFactory{}
+		configRepo = testconfig.NewRepositoryWithDefaults()
 	})
 
-	It("TestDeleteSharedDomainNotFound", func() {
-		deps := getDeleteSharedDomainDeps()
-		deps.domainRepo.FindByNameInOrgApiResponse = errors.NewModelNotFoundError("Domain", "foo.com")
-		ui := callDeleteSharedDomain([]string{"foo.com"}, []string{"y"}, deps)
+	RunCommand := func(args ...string) {
+		testcmd.RunCommand(NewDeleteSharedDomain(ui, configRepo, domainRepo), args, requirementsFactory)
+	}
 
-		Expect(deps.domainRepo.DeleteDomainGuid).To(Equal(""))
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Deleting domain", "foo.com"},
-			[]string{"OK"},
-			[]string{"foo.com", "not found"},
-		))
+	Describe("requirements", func() {
+		It("fails if you are not logged in", func() {
+			requirementsFactory.TargetedOrgSuccess = true
+			RunCommand("foo.com")
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
+
+		It("fails if an organiztion is not targeted", func() {
+			requirementsFactory.LoginSuccess = true
+			RunCommand("foo.com")
+			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
+		})
 	})
 
-	It("TestDeleteSharedDomainFindError", func() {
-		deps := getDeleteSharedDomainDeps()
-		deps.domainRepo.FindByNameInOrgApiResponse = errors.New("couldn't find the droids you're lookin for")
-		ui := callDeleteSharedDomain([]string{"foo.com"}, []string{"y"}, deps)
+	Context("when logged in and targeted an organiztion", func() {
+		BeforeEach(func() {
+			requirementsFactory.LoginSuccess = true
+			requirementsFactory.TargetedOrgSuccess = true
+			domainRepo.FindByNameInOrgDomain = models.DomainFields{
+				Name:   "foo.com",
+				Guid:   "foo-guid",
+				Shared: true,
+			}
+		})
 
-		Expect(deps.domainRepo.DeleteDomainGuid).To(Equal(""))
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Deleting domain", "foo.com"},
-			[]string{"FAILED"},
-			[]string{"foo.com"},
-			[]string{"couldn't find the droids you're lookin for"},
-		))
-	})
+		Describe("and the command is invoked interactively", func() {
+			BeforeEach(func() {
+				ui.Inputs = []string{"y"}
+			})
 
-	It("TestDeleteSharedDomainDeleteError", func() {
-		deps := getDeleteSharedDomainDeps()
-		deps.domainRepo.DeleteSharedApiResponse = errors.New("failed badly")
-		ui := callDeleteSharedDomain([]string{"foo.com"}, []string{"y"}, deps)
+			It("when the domain is not found it tells the user", func() {
+				domainRepo.FindByNameInOrgApiResponse = errors.NewModelNotFoundError("Domain", "foo.com")
+				RunCommand("foo.com")
 
-		Expect(deps.domainRepo.DeleteSharedDomainGuid).To(Equal("foo-guid"))
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Deleting domain", "foo.com"},
-			[]string{"FAILED"},
-			[]string{"foo.com"},
-			[]string{"failed badly"},
-		))
-	})
+				Expect(ui.Outputs).To(ContainSubstrings(
+					[]string{"Deleting domain", "foo.com"},
+					[]string{"OK"},
+					[]string{"foo.com", "not found"},
+				))
+			})
 
-	It("TestDeleteSharedDomainHasConfirmation", func() {
-		deps := getDeleteSharedDomainDeps()
-		ui := callDeleteSharedDomain([]string{"foo.com"}, []string{"y"}, deps)
+			It("fails when the api returns an error", func() {
+				domainRepo.FindByNameInOrgApiResponse = errors.New("couldn't find the droids you're lookin for")
+				RunCommand("foo.com")
+				println(ui.DumpOutputs())
 
-		Expect(deps.domainRepo.DeleteSharedDomainGuid).To(Equal("foo-guid"))
-		Expect(ui.Prompts).To(ContainSubstrings([]string{"shared", "foo.com"}))
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Deleting domain", "foo.com"},
-			[]string{"OK"},
-		))
-	})
+				Expect(ui.Outputs).To(ContainSubstrings(
+					[]string{"Deleting domain", "foo.com"},
+					[]string{"FAILED"},
+					[]string{"foo.com"},
+					[]string{"couldn't find the droids you're lookin for"},
+				))
+			})
 
-	It("TestDeleteSharedDomainForceFlagSkipsConfirmation", func() {
-		deps := getDeleteSharedDomainDeps()
-		ui := callDeleteSharedDomain([]string{"-f", "foo.com"}, []string{}, deps)
+			It("fails when deleting the domain encounters an error", func() {
+				domainRepo.DeleteSharedApiResponse = errors.New("failed badly")
+				RunCommand("foo.com")
 
-		Expect(deps.domainRepo.DeleteSharedDomainGuid).To(Equal("foo-guid"))
-		Expect(len(ui.Prompts)).To(Equal(0))
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Deleting domain", "foo.com"},
-			[]string{"OK"},
-		))
+				Expect(domainRepo.DeleteSharedDomainGuid).To(Equal("foo-guid"))
+				Expect(ui.Outputs).To(ContainSubstrings(
+					[]string{"Deleting domain", "foo.com"},
+					[]string{"FAILED"},
+					[]string{"foo.com"},
+					[]string{"failed badly"},
+				))
+			})
+
+			It("Prompts a user to delete the shared domain", func() {
+				RunCommand("foo.com")
+
+				Expect(domainRepo.DeleteSharedDomainGuid).To(Equal("foo-guid"))
+				Expect(ui.Prompts).To(ContainSubstrings([]string{"delete", "domain", "foo.com"}))
+				Expect(ui.Outputs).To(ContainSubstrings(
+					[]string{"Deleting domain", "foo.com"},
+					[]string{"OK"},
+				))
+			})
+		})
+
+		It("skips confirmation if the force flag is passed", func() {
+			RunCommand("-f", "foo.com")
+
+			Expect(domainRepo.DeleteSharedDomainGuid).To(Equal("foo-guid"))
+			Expect(ui.Prompts).To(BeEmpty())
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Deleting domain", "foo.com"},
+				[]string{"OK"},
+			))
+		})
 	})
 })
-
-func fakeDomainRepo() *testapi.FakeDomainRepository {
-	domain := models.DomainFields{}
-	domain.Name = "foo.com"
-	domain.Guid = "foo-guid"
-	domain.Shared = true
-
-	return &testapi.FakeDomainRepository{
-		FindByNameInOrgDomain: domain,
-	}
-}
-
-type deleteSharedDomainDependencies struct {
-	requirementsFactory *testreq.FakeReqFactory
-	domainRepo          *testapi.FakeDomainRepository
-}
-
-func getDeleteSharedDomainDeps() deleteSharedDomainDependencies {
-	return deleteSharedDomainDependencies{
-		requirementsFactory: &testreq.FakeReqFactory{LoginSuccess: true, TargetedOrgSuccess: true},
-		domainRepo:          fakeDomainRepo(),
-	}
-}
-
-func callDeleteSharedDomain(args []string, inputs []string, deps deleteSharedDomainDependencies) (ui *testterm.FakeUI) {
-	ui = &testterm.FakeUI{
-		Inputs: inputs,
-	}
-
-	configRepo := testconfig.NewRepositoryWithAccessToken(configuration.TokenInfo{Username: "my-user"})
-
-	spaceFields := models.SpaceFields{}
-	spaceFields.Name = "my-space"
-
-	orgFields := models.OrganizationFields{}
-	orgFields.Name = "my-org"
-	configRepo.SetSpaceFields(spaceFields)
-	configRepo.SetOrganizationFields(orgFields)
-
-	cmd := domain.NewDeleteSharedDomain(ui, configRepo, deps.domainRepo)
-	testcmd.RunCommand(cmd, args, deps.requirementsFactory)
-	return
-}
