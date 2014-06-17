3fa692a95d80cb355173f5895731abc955f3ca59
Daniel Lavine and Tim Jarratt <tjarratt@pivotallabs.com>
2014-06-17 10:55:33 -0700

    Cleanup buildpack tests
    [#68475822]

--- cf/commands/buildpack/buildpacks_test.go
+++ cf/commands/buildpack/buildpacks_test.go
@@ -1,28 +1,3 @@
-/*
-                       WARNING WARNING WARNING
-
-                Attention all potential contributors
-
-   This testfile is not in the best state. We've been slowly transitioning
-   from the built in "testing" package to using Ginkgo. As you can see, we've
-   changed the format, but a lot of the setup, test body, descriptions, etc
-   are either hardcoded, completely lacking, or misleading.
-
-   For example:
-
-   Describe("Testing with ginkgo"...)      // This is not a great description
-   It("TestDoesSoemthing"...)              // This is a horrible description
-
-   Describe("create-user command"...       // Describe the actual object under test
-   It("creates a user when provided ..."   // this is more descriptive
-
-   For good examples of writing Ginkgo tests for the cli, refer to
-
-   src/github.com/cloudfoundry/cli/cf/commands/application/delete_app_test.go
-   src/github.com/cloudfoundry/cli/cf/terminal/ui_test.go
-   src/github.com/cloudfoundry/loggregator_consumer/consumer_test.go
-*/
-
 package buildpack_test
 
 import (
@@ -32,72 +7,71 @@ import (
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
 	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 
 	. "github.com/cloudfoundry/cli/testhelpers/matchers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func callListBuildpacks(requirementsFactory *testreq.FakeReqFactory, buildpackRepo *testapi.FakeBuildpackRepository) (ui *testterm.FakeUI) {
-	ui = &testterm.FakeUI{}
-	cmd := buildpack.NewListBuildpacks(ui, buildpackRepo)
-	testcmd.RunCommand(cmd, []string{}, requirementsFactory)
-	return
-}
-
 var _ = Describe("ListBuildpacks", func() {
-	It("has the right requirements", func() {
-		buildpackRepo := &testapi.FakeBuildpackRepository{}
+	var (
+		ui                  *testterm.FakeUI
+		buildpackRepo       *testapi.FakeBuildpackRepository
+		requirementsFactory *testreq.FakeReqFactory
+	)
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-		callListBuildpacks(requirementsFactory, buildpackRepo)
-		Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		buildpackRepo = &testapi.FakeBuildpackRepository{}
+		requirementsFactory = &testreq.FakeReqFactory{}
+	})
 
-		requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: false}
-		callListBuildpacks(requirementsFactory, buildpackRepo)
+	RunCommand := func(args ...string) {
+		cmd := buildpack.NewListBuildpacks(ui, buildpackRepo)
+		testcmd.RunCommand(cmd, args, requirementsFactory)
+	}
+
+	It("fails requirements when login fails", func() {
+		RunCommand()
 		Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
 	})
 
-	It("lists buildpacks", func() {
-		p1 := 5
-		p2 := 10
-		p3 := 15
-		t := true
-		f := false
+	Context("when logged in", func() {
+		BeforeEach(func() {
+			requirementsFactory.LoginSuccess = true
+		})
 
-		buildpackRepo := &testapi.FakeBuildpackRepository{
-			Buildpacks: []models.Buildpack{
+		It("lists buildpacks", func() {
+			p1 := 5
+			p2 := 10
+			p3 := 15
+			t := true
+			f := false
+
+			buildpackRepo.Buildpacks = []models.Buildpack{
 				models.Buildpack{Name: "Buildpack-1", Position: &p1, Enabled: &t, Locked: &f},
 				models.Buildpack{Name: "Buildpack-2", Position: &p2, Enabled: &f, Locked: &t},
 				models.Buildpack{Name: "Buildpack-3", Position: &p3, Enabled: &t, Locked: &f},
-			},
-		}
+			}
 
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
+			RunCommand()
 
-		ui := callListBuildpacks(requirementsFactory, buildpackRepo)
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Getting buildpacks"},
+				[]string{"buildpack", "position", "enabled"},
+				[]string{"Buildpack-1", "5", "true", "false"},
+				[]string{"Buildpack-2", "10", "false", "true"},
+				[]string{"Buildpack-3", "15", "true", "false"},
+			))
+		})
 
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Getting buildpacks"},
-			[]string{"buildpack", "position", "enabled"},
-			[]string{"Buildpack-1", "5", "true", "false"},
-			[]string{"Buildpack-2", "10", "false", "true"},
-			[]string{"Buildpack-3", "15", "true", "false"},
-		))
+		It("tells the user if no build packs exist", func() {
+			RunCommand()
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Getting buildpacks"},
+				[]string{"No buildpacks found"},
+			))
+		})
 	})
 
-	It("TestListingBuildpacksWhenNoneExist", func() {
-		buildpackRepo := &testapi.FakeBuildpackRepository{
-			Buildpacks: []models.Buildpack{},
-		}
-
-		requirementsFactory := &testreq.FakeReqFactory{LoginSuccess: true}
-
-		ui := callListBuildpacks(requirementsFactory, buildpackRepo)
-
-		Expect(ui.Outputs).To(ContainSubstrings(
-			[]string{"Getting buildpacks"},
-			[]string{"No buildpacks found"},
-		))
-	})
 })
