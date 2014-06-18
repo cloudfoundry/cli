66032f1cff2589e84210ed15792be1b1244e9446
Tim Jarratt and Yang Jiang <yjiang@pivotallabs.com>
2014-06-18 15:17:41 -0700

    Add 'name' as a required argument to create-application-security-group
    [#73494074]

--- cf/commands/appsecuritygroup/create_app_security_group_test.go
+++ cf/commands/appsecuritygroup/create_app_security_group_test.go
@@ -3,6 +3,7 @@ package appsecuritygroup_test
 import (
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
+	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
 
 	. "github.com/cloudfoundry/cli/cf/commands/appsecuritygroup"
 	. "github.com/onsi/ginkgo"
@@ -11,28 +12,36 @@ import (
 
 var _ = Describe("create-app-security-group", func() {
 	var (
+		ui                  *testterm.FakeUI
 		requirementsFactory *testreq.FakeReqFactory
 	)
 
 	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
 		requirementsFactory = &testreq.FakeReqFactory{}
 	})
 
 	runCommand := func(args ...string) {
-		cmd := NewCreateAppSecurityGroup()
+		cmd := NewCreateAppSecurityGroup(ui)
 		testcmd.RunCommand(cmd, args, requirementsFactory)
 	}
 
 	Describe("requirements", func() {
 		It("fails when the user is not logged in", func() {
-			runCommand()
+			runCommand("the-security-group")
 			Expect(testcmd.CommandDidPassRequirements).To(BeFalse())
 		})
 
 		It("succeeds when the user is logged in", func() {
 			requirementsFactory.LoginSuccess = true
-			runCommand()
+			runCommand("the-security-group")
 			Expect(testcmd.CommandDidPassRequirements).To(BeTrue())
+		})
+
+		It("fails with usage when a name is not provided", func() {
+			requirementsFactory.LoginSuccess = true
+			runCommand()
+			Expect(ui.FailedWithUsage).To(BeTrue())
 		})
 	})
 })
--- cf/commands/appsecuritygroup/create_application_security_group.go
+++ cf/commands/appsecuritygroup/create_application_security_group.go
@@ -3,13 +3,18 @@ package appsecuritygroup
 import (
 	"github.com/cloudfoundry/cli/cf/command_metadata"
 	"github.com/cloudfoundry/cli/cf/requirements"
+	"github.com/cloudfoundry/cli/cf/terminal"
 	"github.com/codegangsta/cli"
 )
 
-type CreateAppSecurityGroup struct{}
+type CreateAppSecurityGroup struct {
+	ui terminal.UI
+}
 
-func NewCreateAppSecurityGroup() CreateAppSecurityGroup {
-	return CreateAppSecurityGroup{}
+func NewCreateAppSecurityGroup(ui terminal.UI) CreateAppSecurityGroup {
+	return CreateAppSecurityGroup{
+		ui: ui,
+	}
 }
 
 func (cmd CreateAppSecurityGroup) Metadata() command_metadata.CommandMetadata {
@@ -21,10 +26,13 @@ func (cmd CreateAppSecurityGroup) Metadata() command_metadata.CommandMetadata {
 }
 
 func (cmd CreateAppSecurityGroup) GetRequirements(requirementsFactory requirements.Factory, context *cli.Context) ([]requirements.Requirement, error) {
+	if len(context.Args()) != 1 {
+		cmd.ui.FailWithUsage(context)
+	}
+
 	requirements := []requirements.Requirement{requirementsFactory.NewLoginRequirement()}
 	return requirements, nil
 }
 
 func (cmd CreateAppSecurityGroup) Run(context *cli.Context) {
-
 }
