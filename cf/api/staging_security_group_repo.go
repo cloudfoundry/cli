b11f998fe0f0f8fe226c9aa5a3bda7808c4109e0
Daniel Lavine and Tim Jarratt <danlavine@pivotallabs.com>
2014-06-25 15:35:53 -0700

    Add List function to staging defaults repo
    [#73577720]

--- cf/api/staging_security_group_repo.go
+++ cf/api/staging_security_group_repo.go
@@ -3,12 +3,15 @@ package api
 import (
 	"fmt"
 
+	"github.com/cloudfoundry/cli/cf/api/resources"
 	"github.com/cloudfoundry/cli/cf/configuration"
+	"github.com/cloudfoundry/cli/cf/models"
 	"github.com/cloudfoundry/cli/cf/net"
 )
 
 type StagingSecurityGroupsRepo interface {
 	AddToDefaultStagingSet(string) error
+	List() ([]models.SecurityGroupFields, error)
 }
 
 type cloudControllerStagingSecurityGroupRepo struct {
@@ -26,4 +29,23 @@ func NewStagingSecurityGroupsRepo(configRepo configuration.Reader, gateway net.G
 func (repo *cloudControllerStagingSecurityGroupRepo) AddToDefaultStagingSet(groupGuid string) error {
 	path := fmt.Sprintf("%s/v2/config/staging_security_groups/%s", repo.configRepo.ApiEndpoint(), groupGuid)
 	return repo.gateway.CreateResourceFromStruct(path, "")
+}
+
+func (repo *cloudControllerStagingSecurityGroupRepo) List() ([]models.SecurityGroupFields, error) {
+	groups := []models.SecurityGroupFields{}
+
+	err := repo.gateway.ListPaginatedResources(
+		repo.configRepo.ApiEndpoint(),
+		"/v2/config/staging_security_groups",
+		resources.SecurityGroupResource{},
+		func(resource interface{}) bool {
+			if securityGroupResource, ok := resource.(resources.SecurityGroupResource); ok {
+				groups = append(groups, securityGroupResource.ToFields())
+			}
+
+			return true
+		},
+	)
+
+	return groups, err
 }
--- cf/api/staging_security_group_repo_test.go
+++ cf/api/staging_security_group_repo_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/cloudfoundry/cli/cf/configuration"
+	"github.com/cloudfoundry/cli/cf/models"
 	"github.com/cloudfoundry/cli/cf/net"
 	testapi "github.com/cloudfoundry/cli/testhelpers/api"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
@@ -35,16 +36,77 @@ var _ = Describe("StagingSecurityGroupsRepo", func() {
 		testServer.Close()
 	})
 
+	setupTestServer := func(reqs ...testnet.TestRequest) {
+		testServer, testHandler = testnet.NewServer(reqs)
+		configRepo.SetApiEndpoint(testServer.URL)
+	}
+
 	Describe("AddToDefaultStagingSet", func() {
 		It("makes a correct request", func() {
-			testServer, testHandler = testnet.NewServer([]testnet.TestRequest{
+			setupTestServer(
 				testapi.NewCloudControllerTestRequest(testnet.TestRequest{
 					Method: "POST",
 					Path:   "/v2/config/staging_security_groups/a-real-guid",
 					Response: testnet.TestResponse{
 						Status: http.StatusCreated,
-						Body: `
-{
+						Body:   addStagingResponse,
+					},
+				}),
+			)
+
+			err := repo.AddToDefaultStagingSet("a-real-guid")
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(testHandler).To(HaveAllRequestsCalled())
+		})
+	})
+
+	Describe(".List", func() {
+		It("returns a list of security groups that are the defaults for staging", func() {
+			setupTestServer(
+				testapi.NewCloudControllerTestRequest(testnet.TestRequest{
+					Method: "GET",
+					Path:   "/v2/config/staging_security_groups",
+					Response: testnet.TestResponse{
+						Status: http.StatusOK,
+						Body:   firstStagingListItem,
+					},
+				}),
+				testapi.NewCloudControllerTestRequest(testnet.TestRequest{
+					Method: "GET",
+					Path:   "/v2/config/staging_security_groups",
+					Response: testnet.TestResponse{
+						Status: http.StatusOK,
+						Body:   secondStagingListItem,
+					},
+				}),
+			)
+
+			defaults, err := repo.List()
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(testHandler).To(HaveAllRequestsCalled())
+			Expect(defaults).To(Equal([]models.SecurityGroupFields{
+				{
+					Name: "name-71",
+					Guid: "cd186158-b356-474d-9861-724f34f48502",
+					Rules: []map[string]string{{
+						"protocol": "udp",
+					}},
+				},
+				{
+					Name: "name-72",
+					Guid: "d3374b62-7eac-4823-afbd-460d2bf44c67",
+					Rules: []map[string]string{{
+						"destination": "198.41.191.47/1",
+					}},
+				},
+			}))
+		})
+	})
+})
+
+var addStagingResponse string = `{
   "metadata": {
     "guid": "897341eb-ef31-406f-b57b-414f51583a3a",
     "url": "/v2/config/staging_security_groups/897341eb-ef31-406f-b57b-414f51583a3a",
@@ -61,17 +123,50 @@ var _ = Describe("StagingSecurityGroupsRepo", func() {
       }
     ]
   }
-}
-`,
-					},
-				}),
-			})
+}`
 
-			configRepo.SetApiEndpoint(testServer.URL)
-			err := repo.AddToDefaultStagingSet("a-real-guid")
+var firstStagingListItem string = `{
+  "next_url": "/v2/config/staging_security_groups?page=2",
+  "resources": [
+    {
+      "metadata": {
+        "guid": "cd186158-b356-474d-9861-724f34f48502",
+        "url": "/v2/security_groups/cd186158-b356-474d-9861-724f34f48502",
+        "created_at": "2014-06-23T22:55:30+00:00",
+        "updated_at": null
+      },
+      "entity": {
+        "name": "name-71",
+        "rules": [
+          {
+            "protocol": "udp"
+          }
+        ],
+        "spaces_url": "/v2/security_groups/d3374b62-7eac-4823-afbd-460d2bf44c67/spaces"
+      }
+    }
+  ]
+}`
 
-			Expect(err).ToNot(HaveOccurred())
-			Expect(testHandler).To(HaveAllRequestsCalled())
-		})
-	})
-})
+var secondStagingListItem string = `{
+  "next_url": null,
+  "resources": [
+    {
+      "metadata": {
+        "guid": "d3374b62-7eac-4823-afbd-460d2bf44c67",
+        "url": "/v2/config/staging_security_groups/d3374b62-7eac-4823-afbd-460d2bf44c67",
+        "created_at": "2014-06-23T22:55:30+00:00",
+        "updated_at": null
+      },
+      "entity": {
+        "name": "name-72",
+        "rules": [
+          {
+            "destination": "198.41.191.47/1"
+          }
+        ],
+        "spaces_url": "/v2/security_groups/d3374b62-7eac-4823-afbd-460d2bf44c67/spaces"
+      }
+    }
+  ]
+}`
