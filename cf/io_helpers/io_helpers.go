2feb1dddff8a676de09fa25fe3c8cb79b9f35e08
Daniel Lavine and Tim Jarratt <tjarratt@pivotallabs.com>
2014-06-25 10:22:49 -0700

    Better comments

--- cf/io_helpers/io_helpers.go
+++ cf/io_helpers/io_helpers.go
@@ -41,9 +41,15 @@ func CaptureOutput(block func()) []string {
 	return <-result
 }
 
+/*
+ The reason we're doing is that you can't write an infinite amount of bytes into a pipe.
+ On some platforms, the limit is fairly high; on other platforms, the limit is infuriatingly small
+ (looking at you, Windows). To counteract this, we need to read in a goroutine from one end of
+ the pipe and return the result across a channel.
+*/
 func captureOutputAsyncronously(doneWriting <-chan bool, result chan<- []string, reader io.Reader) {
 	var readingString string
-	
+
 	for {
 		var buf bytes.Buffer
 		io.Copy(&buf, reader)
@@ -51,9 +57,9 @@ func captureOutputAsyncronously(doneWriting <-chan bool, result chan<- []string,
 
 		_, ok := <-doneWriting
 		if ok {
-			// there is no guarantee that the writer did not 
+			// there is no guarantee that the writer did not
 			// write more in between the read above and reading from this channel
-			// so we must absolutely read again
+			// so we absolute must read once more if we want all the bytes
 			var buf bytes.Buffer
 			io.Copy(&buf, reader)
 			readingString += buf.String()
