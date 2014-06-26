3e1237f86ac6458ff70d4de82a0f6efb1bb4bafb
Daniel Lavine and David Varvel <dvarvel@pivotallabs.com>
2014-06-26 15:13:35 -0700

    Add API for adding a security group to the running defaults.
    [#73577806]

--- cf/api/security_groups/defaults/running/running.go
+++ cf/api/security_groups/defaults/running/running.go
@@ -1,1 +1,31 @@
 package running
+
+import (
+	"github.com/cloudfoundry/cli/cf/configuration"
+	"github.com/cloudfoundry/cli/cf/net"
+
+	. "github.com/cloudfoundry/cli/cf/api/security_groups/defaults"
+)
+
+const urlPath = "/v2/config/running_security_groups"
+
+type RunningSecurityGroupsRepo interface {
+	AddToDefaultRunningSet(string) error
+}
+
+type cloudControllerRunningSecurityGroupRepo struct {
+	repoBase SecurityGroupsRepoBase
+}
+
+func NewRunningSecurityGroupsRepo(configRepo configuration.Reader, gateway net.Gateway) RunningSecurityGroupsRepo {
+	return &cloudControllerRunningSecurityGroupRepo{
+		repoBase: SecurityGroupsRepoBase{
+			ConfigRepo: configRepo,
+			Gateway:    gateway,
+		},
+	}
+}
+
+func (repo *cloudControllerRunningSecurityGroupRepo) AddToDefaultRunningSet(groupGuid string) error {
+	return repo.repoBase.Add(groupGuid, urlPath)
+}
