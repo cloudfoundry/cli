2ceb03289bbd8b9e6a771250546a2b9f085a406d
Daniel Lavine and David Varvel <danlavine@pivotallabs.com>
2014-06-27 12:25:42 -0700

    Fix import path for running security group test.
    Case sensitivity is a thing on Linux.

--- cf/commands/securitygroup/running_security_groups_test.go
+++ cf/commands/securitygroup/running_security_groups_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/cloudfoundry/cli/cf/errors"
 	"github.com/cloudfoundry/cli/cf/models"
 
-	testapi "github.com/cloudfoundry/cli/cf/api/security_groups/defaults/Running/fakes"
+	testapi "github.com/cloudfoundry/cli/cf/api/security_groups/defaults/running/fakes"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
 	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
