8dfdec1a965b0aebb60db9c3a1f5190d0fc2b2eb
Devin Fallak and Tim Jarratt <tjarratt@pivotallabs.com>
2014-06-30 15:25:05 -0700

    Add simple API object to bind spaces to sec. groups
    [#74078880]

---
+++ cf/api/security_groups/spaces/space_binder_test.go
@@ -0,0 +1,64 @@
+package spaces_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	testapi "github.com/cloudfoundry/cli/cf/api/fakes"
+	"github.com/cloudfoundry/cli/cf/configuration"
+	"github.com/cloudfoundry/cli/cf/net"
+	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
+	testnet "github.com/cloudfoundry/cli/testhelpers/net"
+
+	. "github.com/cloudfoundry/cli/cf/api/security_groups/spaces"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("SecurityGroupSpaceBinder", func() {
+	var (
+		repo        SecurityGroupSpaceBinder
+		gateway     net.Gateway
+		testServer  *httptest.Server
+		testHandler *testnet.TestHandler
+		configRepo  configuration.ReadWriter
+	)
+
+	BeforeEach(func() {
+		configRepo = testconfig.NewRepositoryWithDefaults()
+		gateway = net.NewCloudControllerGateway((configRepo), time.Now)
+		repo = NewSecurityGroupSpaceBinder(configRepo, gateway)
+	})
+
+	AfterEach(func() { testServer.Close() })
+
+	setupTestServer := func(reqs ...testnet.TestRequest) {
+		testServer, testHandler = testnet.NewServer(reqs)
+		configRepo.SetApiEndpoint(testServer.URL)
+	}
+
+	Describe(".BindSpace", func() {
+		It("associates the security group with the space", func() {
+			setupTestServer(
+				testapi.NewCloudControllerTestRequest(testnet.TestRequest{
+					Method: "PUT",
+					Path:   "/v2/security_groups/this-is-a-security-group-guid/spaces/yes-its-a-space-guid",
+					Response: testnet.TestResponse{
+						Status: http.StatusCreated,
+						Body: `
+{
+  "metadata": {"guid": "fb6fdf81-ce1b-448f-ada9-09bbb8807812"},
+  "entity": {"name": "dummy1", "rules": [] }
+}`,
+					},
+				}))
+
+			err := repo.BindSpace("this-is-a-security-group-guid", "yes-its-a-space-guid")
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(testHandler).To(HaveAllRequestsCalled())
+		})
+	})
+})
