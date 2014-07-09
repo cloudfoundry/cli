2f83794892ea67ff66c532aa03fe770f5b036d41
Anand Gaitonde <agaitonde@pivotallabs.com>
2014-07-08 17:36:28 -0700

    Updating Jibber Jabber.
    [Finishes #74656770]

--- Godeps/_workspace/src/github.com/pivotal-cf-experimental/jibber_jabber/jibber_jabber_unix_test.go
+++ Godeps/_workspace/src/github.com/pivotal-cf-experimental/jibber_jabber/jibber_jabber_unix_test.go
@@ -3,8 +3,8 @@
 package jibber_jabber_test
 
 import (
-	"os"
 	. "github.com/pivotal-cf-experimental/jibber_jabber"
+	"os"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
--- Godeps/_workspace/src/github.com/pivotal-cf-experimental/jibber_jabber/jibber_jabber_windows.go
+++ Godeps/_workspace/src/github.com/pivotal-cf-experimental/jibber_jabber/jibber_jabber_windows.go
@@ -10,11 +10,11 @@ import (
 
 const LOCALE_NAME_MAX_LENGTH uint32 = 85
 
-func getWindowsLocale() (locale string, err error) {
+func getWindowsLocaleFrom(sysCall string) (locale string, err error) {
 	buffer := make([]uint16, LOCALE_NAME_MAX_LENGTH)
 
 	dll := syscall.MustLoadDLL("kernel32")
-	proc := dll.MustFindProc("GetSystemDefaultLocaleName")
+	proc := dll.MustFindProc(sysCall)
 	r, _, dllError := proc.Call(uintptr(unsafe.Pointer(&buffer[0])), uintptr(LOCALE_NAME_MAX_LENGTH))
 	if r == 0 {
 		err = errors.New(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE + ":\n" + dllError.Error())
@@ -26,6 +26,13 @@ func getWindowsLocale() (locale string, err error) {
 	return
 }
 
+func getWindowsLocale() (locale string, err error) {
+	locale, err = getWindowsLocaleFrom("GetUserDefaultLocaleName")
+	if err != nil {
+		locale, err = getWindowsLocaleFrom("GetSystemDefaultLocaleName")
+	}
+	return
+}
 func DetectIETF() (locale string, err error) {
 	locale, err = getWindowsLocale()
 	return
