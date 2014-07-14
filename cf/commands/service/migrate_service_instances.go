2a5c5fde372dd3464b99e0d7c8bec3fecbf4faaf
Christopher Piraino and Tim Jarratt <tjarratt@pivotallabs.com>
2014-07-14 10:05:48 -0700

    Ensure service commands translations are correct
    Embarassing that we managed to commit this string as it was. We were
    double translating the service instance warning as part of the usage.
    
    [#74818356]

--- cf/commands/service/migrate_service_instances.go
+++ cf/commands/service/migrate_service_instances.go
@@ -28,14 +28,14 @@ func NewMigrateServiceInstances(ui terminal.UI, configRepo configuration.Reader,
 }
 
 func migrateServiceInstanceWarning() string {
-	return T(`WARNING: This operation is internal to Cloud Foundry; service brokers will not be contacted and resources for service instances will not be altered. The primary use case for this operation is to replace a service broker which implements the v1 Service Broker API with a broker which implements the v2 API by remapping service instances from v1 plans to v2 plans. We recommend making the v1 plan private or shutting down the v1 broker to prevent additional instances from being created. Once service instances have been migrated, the v1 services and plans can be removed from Cloud Foundry.`)
+	return T("WARNING: This operation is internal to Cloud Foundry; service brokers will not be contacted and resources for service instances will not be altered. The primary use case for this operation is to replace a service broker which implements the v1 Service Broker API with a broker which implements the v2 API by remapping service instances from v1 plans to v2 plans.  We recommend making the v1 plan private or shutting down the v1 broker to prevent additional instances from being created. Once service instances have been migrated, the v1 services and plans can be removed from Cloud Foundry.")
 }
 
 func (cmd *MigrateServiceInstances) Metadata() command_metadata.CommandMetadata {
 	return command_metadata.CommandMetadata{
 		Name:        "migrate-service-instances",
 		Description: T("Migrate service instances from one service plan to another"),
-		Usage:       T(`CF_NAME migrate-service-instances v1_SERVICE v1_PROVIDER v1_PLAN v2_SERVICE v2_PLAN\n\n` + migrateServiceInstanceWarning()),
+		Usage:       T("CF_NAME migrate-service-instances v1_SERVICE v1_PROVIDER v1_PLAN v2_SERVICE v2_PLAN\n\n") + migrateServiceInstanceWarning(),
 		Flags: []cli.Flag{
 			cli.BoolFlag{Name: "f", Usage: T("Force migration without confirmation")},
 		},
