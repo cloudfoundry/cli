4ced62ebd032f64fc268798b0c89aa7996ba1068
Dan Lavine and David Varvel <dvarvel@pivotallabs.com>
2014-07-31 11:37:49 -0700

    rename serviceplan package to serviceaccess

--- cf/commands/serviceaccess/serviceaccess_suite_test.go
+++ cf/commands/serviceaccess/serviceaccess_suite_test.go
@@ -1,4 +1,4 @@
-package serviceplan_test
+package serviceaccess_test
 
 import (
 	. "github.com/onsi/ginkgo"
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestServicePlan(t *testing.T) {
+func TestServiceAccess(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Service Plan Suite")
+	RunSpecs(t, "Service Access Suite")
 }
