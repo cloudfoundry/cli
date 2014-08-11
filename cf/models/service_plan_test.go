768a67df8c2741cff066892cf59afa6d81b4821c
Christopher Piraino and Jonathan Berkhahn <cpiraino@pivotallabs.com>
2014-08-11 09:33:37 -0700

    Refactor of the services_plans actor.

---
+++ cf/models/service_plan_test.go
@@ -0,0 +1,47 @@
+package models_test
+
+import (
+	. "github.com/cloudfoundry/cli/cf/models"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ServicePlanFields", func() {
+	var servicePlanFields ServicePlanFields
+
+	BeforeEach(func() {
+		servicePlanFields = ServicePlanFields{
+			Guid:                "I-am-a-guid",
+			Name:                "BestServicePlanEver",
+			Free:                false,
+			Public:              true,
+			Description:         "A Plan For Service",
+			Active:              true,
+			ServiceOfferingGuid: "service-offering-guid",
+			OrgNames:            []string{"org1", "org2"},
+		}
+	})
+
+	Describe(".OrgHasVisibility", func() {
+		Context("when the service plan is public", func() {
+			It("returns true", func() {
+				Expect(servicePlanFields.OrgHasVisibility("anyOrg")).To(BeTrue())
+			})
+		})
+
+		Context("when the service plan is not public", func() {
+			BeforeEach(func() {
+				servicePlanFields.Public = false
+			})
+
+			It("returns true if the orgname is in the list of orgs that have visibility", func() {
+				Expect(servicePlanFields.OrgHasVisibility("org1")).To(BeTrue())
+			})
+
+			It("returns false if the orgname is not in the list of orgs that have visibility", func() {
+				Expect(servicePlanFields.OrgHasVisibility("org-that-has-no-visibility")).To(BeFalse())
+			})
+		})
+
+	})
+})
