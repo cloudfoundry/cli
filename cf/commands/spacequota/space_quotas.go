b95e94782d3fe2ec09d60a7b93b20d3cb5a690db
David Varvel and Jonathan Berkhahn <dvarvel@pivotallabs.com>
2014-08-15 15:42:59 -0700

    Preliminary implementation of space-quotas command
    - Doesn't do anything with orgs yet.
    
    [#76026790]

---
+++ cf/commands/spacequota/space_quotas.go
@@ -0,0 +1,72 @@
+package spacequota
+
+import (
+	"fmt"
+
+	"github.com/cloudfoundry/cli/cf/api/space_quotas"
+	"github.com/cloudfoundry/cli/cf/command_metadata"
+	"github.com/cloudfoundry/cli/cf/configuration"
+	"github.com/cloudfoundry/cli/cf/formatters"
+	. "github.com/cloudfoundry/cli/cf/i18n"
+	"github.com/cloudfoundry/cli/cf/requirements"
+	"github.com/cloudfoundry/cli/cf/terminal"
+	"github.com/codegangsta/cli"
+)
+
+type ListSpaceQuotas struct {
+	ui             terminal.UI
+	config         configuration.Reader
+	spaceQuotaRepo space_quotas.SpaceQuotaRepository
+}
+
+func NewListSpaceQuotas(ui terminal.UI, config configuration.Reader, spaceQuotaRepo space_quotas.SpaceQuotaRepository) (cmd *ListSpaceQuotas) {
+	return &ListSpaceQuotas{
+		ui:             ui,
+		config:         config,
+		spaceQuotaRepo: spaceQuotaRepo,
+	}
+}
+
+func (cmd *ListSpaceQuotas) Metadata() command_metadata.CommandMetadata {
+	return command_metadata.CommandMetadata{
+		Name:        "quotas",
+		Description: T("List available usage quotas"),
+		Usage:       T("CF_NAME quotas"),
+	}
+}
+
+func (cmd *ListSpaceQuotas) GetRequirements(requirementsFactory requirements.Factory, c *cli.Context) (reqs []requirements.Requirement, err error) {
+	reqs = []requirements.Requirement{
+		requirementsFactory.NewLoginRequirement(),
+	}
+	return
+}
+
+func (cmd *ListSpaceQuotas) Run(c *cli.Context) {
+	cmd.ui.Say(T("Getting space quotas as {{.Username}}...", map[string]interface{}{"Username": terminal.EntityNameColor(cmd.config.Username())}))
+
+	quotas, apiErr := cmd.spaceQuotaRepo.FindAll()
+
+	if apiErr != nil {
+		cmd.ui.Failed(apiErr.Error())
+		return
+	}
+	cmd.ui.Ok()
+	cmd.ui.Say("")
+
+	table := terminal.NewTable(cmd.ui, []string{T("name"), T("total memory limit"), T("instance memory limit"), T("routes"), T("service instances"), T("paid service plans")})
+
+	for _, quota := range quotas {
+		table.Add(
+			quota.Name,
+			formatters.ByteSize(quota.MemoryLimit*formatters.MEGABYTE),
+			formatters.ByteSize(quota.InstanceMemoryLimit*formatters.MEGABYTE),
+			fmt.Sprintf("%d", quota.RoutesLimit),
+			fmt.Sprintf("%d", quota.ServicesLimit),
+			formatters.Allowed(quota.NonBasicServicesAllowed),
+		)
+	}
+
+	table.Print()
+
+}
---
+++ cf/commands/spacequota/space_quotas_test.go
@@ -0,0 +1,93 @@
+package spacequota_test
+
+import (
+	"github.com/cloudfoundry/cli/cf/api/space_quotas/fakes"
+	"github.com/cloudfoundry/cli/cf/errors"
+	"github.com/cloudfoundry/cli/cf/models"
+	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
+	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
+	testreq "github.com/cloudfoundry/cli/testhelpers/requirements"
+	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+
+	. "github.com/cloudfoundry/cli/cf/commands/spacequota"
+	. "github.com/cloudfoundry/cli/testhelpers/matchers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("quotas command", func() {
+	var (
+		ui                  *testterm.FakeUI
+		quotaRepo           *fakes.FakeSpaceQuotaRepository
+		requirementsFactory *testreq.FakeReqFactory
+	)
+
+	BeforeEach(func() {
+		ui = &testterm.FakeUI{}
+		quotaRepo = &fakes.FakeSpaceQuotaRepository{}
+		requirementsFactory = &testreq.FakeReqFactory{LoginSuccess: true}
+	})
+
+	runCommand := func() bool {
+		cmd := NewListSpaceQuotas(ui, testconfig.NewRepositoryWithDefaults(), quotaRepo)
+		return testcmd.RunCommand(cmd, []string{}, requirementsFactory)
+	}
+
+	Describe("requirements", func() {
+		It("requires the user to be logged in", func() {
+			requirementsFactory.LoginSuccess = false
+			Expect(runCommand()).ToNot(HavePassedRequirements())
+		})
+	})
+
+	Context("when quotas exist", func() {
+		BeforeEach(func() {
+			quotaRepo.FindAllReturns([]models.SpaceQuota{
+				models.SpaceQuota{
+					Name:                    "quota-name",
+					MemoryLimit:             1024,
+					InstanceMemoryLimit:     512,
+					RoutesLimit:             111,
+					ServicesLimit:           222,
+					NonBasicServicesAllowed: true,
+					//					OrgGuid:                 "my-org",
+				},
+				models.SpaceQuota{
+					Name:                    "quota-non-basic-not-allowed",
+					MemoryLimit:             434,
+					InstanceMemoryLimit:     3,
+					RoutesLimit:             1,
+					ServicesLimit:           2,
+					NonBasicServicesAllowed: false,
+					//					OrgGuid:                 "your-org",
+				},
+			}, nil)
+		})
+
+		It("lists quotas", func() {
+			Expect(Expect(runCommand()).To(HavePassedRequirements())).To(HavePassedRequirements())
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Getting space quotas as", "my-user"},
+				[]string{"OK"},
+				[]string{"name", "total memory limit", "instance memory limit", "routes", "service instances", "paid service plans"},
+				[]string{"quota-name", "1G", "512M", "111", "222", "allowed"},
+				[]string{"quota-non-basic-not-allowed", "434M", "3M", "1", "2", "disallowed"},
+			))
+		})
+	})
+
+	Context("when an error occurs fetching quotas", func() {
+		BeforeEach(func() {
+			quotaRepo.FindAllReturns([]models.SpaceQuota{}, errors.New("I haz a borken!"))
+		})
+
+		It("prints an error", func() {
+			Expect(runCommand()).To(HavePassedRequirements())
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"Getting space quotas as", "my-user"},
+				[]string{"FAILED"},
+			))
+		})
+	})
+
+})
