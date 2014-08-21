b66d6ae5921675a65bc0cb42ab90734dde6c0f86
Dan Lavine, dlavine and David Varvel, dvarvel <dvarvel@pivotal.io>
2014-08-21 15:29:02 -0700

    Print warning messages that come back from CC
    - We now deduplicate the warning messages.
    
    [Finishes #77395800]

--- cf/net/warnings_collector.go
+++ cf/net/warnings_collector.go
@@ -23,16 +23,37 @@ func NewWarningsCollector(ui terminal.UI, warning_producers ...WarningProducer) 
 }
 
 func (warnings_collector WarningsCollector) PrintWarnings() {
-	if os.Getenv("CF_RAISE_ERROR_ON_WARNINGS") != "" {
-		warnings := []string{}
-		for _, warning_producer := range warnings_collector.warning_producers {
-			for _, warning := range warning_producer.Warnings() {
-				warnings = append(warnings, warning)
-			}
+	warnings := []string{}
+	for _, warning_producer := range warnings_collector.warning_producers {
+		for _, warning := range warning_producer.Warnings() {
+			warnings = append(warnings, warning)
 		}
+	}
 
+	if os.Getenv("CF_RAISE_ERROR_ON_WARNINGS") != "" {
 		if len(warnings) > 0 {
 			panic(strings.Join(warnings, "\n"))
 		}
 	}
+
+	warnings = warnings_collector.removeDuplicates(warnings)
+
+	for _, warning := range warnings {
+		warnings_collector.ui.Warn(warning)
+	}
+}
+
+func (warnings_collector WarningsCollector) removeDuplicates(stringArray []string) []string {
+	length := len(stringArray) - 1
+	for i := 0; i < length; i++ {
+		for j := i + 1; j <= length; j++ {
+			if stringArray[i] == stringArray[j] {
+				stringArray[j] = stringArray[length]
+				stringArray = stringArray[0:length]
+				length--
+				j--
+			}
+		}
+	}
+	return stringArray
 }
