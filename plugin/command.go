3b305422b567d18b90141120f0365fcdcbe640a9
Dan Lavine, dlavine & Simon Leung, leungs <pair+dan+simon@us.ibm.com>
2014-09-22 09:42:03 -0700

    First pass at rpc model
    - have hardcoded port 20001
    - sleep for 3 seconds waiting for rpc server
    
    [Finishes #78397654]

---
+++ plugin/command.go
@@ -0,0 +1,46 @@
+package plugin
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+	"os"
+)
+
+/**
+	Command interface needs to be implementd for a runnable sub-command of `cf`
+**/
+type Command interface {
+	//run is passed in all the command line parameter arguments and
+	//an object containing all of the cli commands available to them
+	Run(args []string, cmds *string) error
+}
+
+/**
+	This function is called by the plugin to setup their server. This allows us to call Run on the plugin
+**/
+func ServeCommand(cmd Command) {
+	fmt.Println("trying to listen")
+
+	//register command
+	rpc.Register(cmd)
+
+	listener, err := net.Listen("tcp", ":20001")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("listining ding dong")
+
+	//listen for the run command
+	for {
+		if conn, err := listener.Accept(); err != nil {
+			log.Fatal("accept error: " + err.Error())
+		} else {
+			log.Printf("new connection established\n")
+			go rpc.ServeConn(conn)
+		}
+	}
+}
