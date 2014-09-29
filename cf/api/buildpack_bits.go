49beccf7726887211cfb05a20f6bbc175ec5847e
Derek Richard and Tyler Schultz <tschultz@pivotal.io>
2014-09-29 16:32:41 -0700

    Use filepath instead of path where possible
    -Path does not always work well with windows
    
    [#79748230]

--- cf/api/buildpack_bits.go
+++ cf/api/buildpack_bits.go
@@ -160,7 +160,7 @@ func findBuildpackPath(zipFiles []*zip.File) (parentPath string, foundBuildpack 
 	for _, file := range zipFiles {
 		if strings.HasSuffix(file.Name, needle) {
 			foundBuildpack = true
-			parentPath = path.Join(file.Name, "..", "..")
+			parentPath = filepath.Join(file.Name, "..", "..")
 			if parentPath == "." {
 				parentPath = ""
 			}
