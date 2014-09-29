04a975f885d5b9c0a6e5f9e33460dd7537b4c8da
Derek Richard and Tyler Schultz <drichard@pivotal.io>
2014-09-29 10:51:36 -0700

    Update GOPATH var in windows bat scripts

--- scripts/windows-64-test.bat
+++ scripts/windows-64-test.bat
@@ -7,7 +7,9 @@ go-bindata -pkg resources -ignore ".go" -o cf/resources/i18n_resources.go cf/i18
 powershell -command set-executionpolicy remotesigned
 powershell .\bin\replace-sha.ps1
 
-SET GOPATH=%CD%\Godeps\_workspace;c:\Users\Administrator\go
+$env:GOPATH = "%CD%\Godeps\_workspace;c:\Users\Administrator\go"
+Get-ChildItem Env:GOPATH
+
 c:\Go\bin\go build -v -o cf-windows-amd64.exe ./main
 c:\Go\bin\go test -i ./cf/... ./generic/... ./testhelpers/... ./main/...
 c:\Go\bin\go test -cover -v ./cf/... ./generic/... ./testhelpers/... ./main/...
