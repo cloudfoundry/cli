c33a72c7b080c2ef054034b3dcb1f07ee4aa9f71
Dan Lavine <dlavine@us.ibm.com>
2014-10-08 11:06:17 -0700

    Plugin pings cf when it is ready to accept commands.
    - removes sleep from cf.
    
    [#79964866]
    
    Signed-off-by: Simon Leung <leungs@us.ibm.com>

--- plugin/command.go
+++ plugin/command.go
@@ -2,10 +2,10 @@ package plugin
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"net/rpc"
 	"os"
+	"time"
 )
 
 type Command struct {
@@ -40,12 +40,26 @@ func ServeCommand(cmd RpcPlugin) {
 		os.Exit(1)
 	}
 
+	//call back to cf saying we have been setup
+	for i := 0; i < 5; i++ {
+		conn, err := net.Dial("tcp", "127.0.0.1:"+os.Args[2])
+		defer conn.Close()
+		if err != nil {
+			time.Sleep(200 * time.Millisecond)
+		} else {
+			break
+		}
+	}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	//listen for the run command
 	for {
 		if conn, err := listener.Accept(); err != nil {
-			log.Fatal("accept error: " + err.Error())
+			fmt.Println("accept error: " + err.Error())
 		} else {
-			log.Printf("new connection established\n")
 			go rpc.ServeConn(conn)
 		}
 	}
