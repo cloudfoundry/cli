5d253ef482b70dec34dc9f86612f9740285a8ad8
Tyler Schultz <tschultz@pivotal.io>
2014-10-17 09:53:07 -0700

    Compile test plugins to one directory
    Signed-off-by: Derek Richard <drichard@pivotal.io>

--- cf/app/app_suite_test.go
+++ cf/app/app_suite_test.go
@@ -24,13 +24,13 @@ func TestApp(t *testing.T) {
 		panic(err)
 	}
 
-	cmd := exec.Command("go", "build", "-o", filepath.Join(dir, "..", "..", "fixtures", "config", "help-plugin-test-config", ".cf", "plugins", "test_1.exe"), filepath.Join(dir, "..", "..", "fixtures", "plugins", "test_1.go"))
+	cmd := exec.Command("go", "build", "-o", filepath.Join(dir, "..", "..", "fixtures", "plugins", "test_1.exe"), filepath.Join(dir, "..", "..", "fixtures", "plugins", "test_1.go"))
 	err = cmd.Run()
 	if err != nil {
 		panic(err)
 	}
 
-	cmd = exec.Command("go", "build", "-o", filepath.Join(dir, "..", "..", "fixtures", "config", "help-plugin-test-config", ".cf", "plugins", "test_2.exe"), filepath.Join(dir, "..", "..", "fixtures", "plugins", "test_2.go"))
+	cmd = exec.Command("go", "build", "-o", filepath.Join(dir, "..", "..", "fixtures", "plugins", "test_2.exe"), filepath.Join(dir, "..", "..", "fixtures", "plugins", "test_2.go"))
 	err = cmd.Run()
 	if err != nil {
 		panic(err)
--- main/main_suite_test.go
+++ main/main_suite_test.go
@@ -19,26 +19,26 @@ func TestMain(t *testing.T) {
 		panic(err)
 	}
 
-	cmd := exec.Command("go", "build", "-o", filepath.Join(dir, "..", "fixtures", "config", "main-plugin-test-config", ".cf", "plugins", "test_1.exe"), filepath.Join(dir, "..", "fixtures", "plugins", "test_1.go"))
+	cmd := exec.Command("go", "build", "-o", filepath.Join(dir, "..", "fixtures", "plugins", "test_1.exe"), filepath.Join(dir, "..", "fixtures", "plugins", "test_1.go"))
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println(err)
 	}
 	Expect(err).NotTo(HaveOccurred())
 
-	cmd = exec.Command("go", "build", "-o", filepath.Join(dir, "..", "fixtures", "config", "main-plugin-test-config", ".cf", "plugins", "test_2.exe"), filepath.Join(dir, "..", "fixtures", "plugins", "test_2.go"))
+	cmd = exec.Command("go", "build", "-o", filepath.Join(dir, "..", "fixtures", "plugins", "test_2.exe"), filepath.Join(dir, "..", "fixtures", "plugins", "test_2.go"))
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	cmd = exec.Command("go", "build", "-o", filepath.Join(dir, "..", "fixtures", "config", "main-plugin-test-config", ".cf", "plugins", "test_with_push.exe"), filepath.Join(dir, "..", "fixtures", "plugins", "test_with_push.go"))
+	cmd = exec.Command("go", "build", "-o", filepath.Join(dir, "..", "fixtures", "plugins", "test_with_push.exe"), filepath.Join(dir, "..", "fixtures", "plugins", "test_with_push.go"))
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	cmd = exec.Command("go", "build", "-o", filepath.Join(dir, "..", "fixtures", "config", "main-plugin-test-config", ".cf", "plugins", "test_with_help.exe"), filepath.Join(dir, "..", "fixtures", "plugins", "test_with_help.go"))
+	cmd = exec.Command("go", "build", "-o", filepath.Join(dir, "..", "fixtures", "plugins", "test_with_help.exe"), filepath.Join(dir, "..", "fixtures", "plugins", "test_with_help.go"))
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println(err)
