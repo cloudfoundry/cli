183ae2f1d1cf42d3deea8d54b6c3933cfb76e52f
Dan Lavine <dlavine@us.ibm.com>
2014-10-23 17:41:06 -0700

    Output is tee'd to buffer in preparation for
    sending to plugins
    
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

---
+++ cf/terminal/tee_printer.go
@@ -0,0 +1,45 @@
+package terminal
+
+import (
+	"fmt"
+)
+
+type Printer interface {
+	Print(a ...interface{}) (n int, err error)
+	Printf(format string, a ...interface{}) (n int, err error)
+	Println(a ...interface{}) (n int, err error)
+}
+
+type TeePrinter struct {
+	output []string
+}
+
+func NewTeePrinter() *TeePrinter {
+	return &TeePrinter{
+		output: []string{},
+	}
+}
+
+func (t *TeePrinter) GetOutputAndReset() []string {
+	currentOutput := t.output
+	t.output = []string{}
+	return currentOutput
+}
+
+func (t *TeePrinter) Print(values ...interface{}) (n int, err error) {
+	str := fmt.Sprint(values...)
+	t.output = append(t.output, str)
+	return fmt.Print(str)
+}
+
+func (t *TeePrinter) Printf(format string, a ...interface{}) (n int, err error) {
+	str := fmt.Sprintf(format, a...)
+	t.output = append(t.output, str)
+	return fmt.Print(str)
+}
+
+func (t *TeePrinter) Println(values ...interface{}) (n int, err error) {
+	str := fmt.Sprint(values...)
+	t.output = append(t.output, str)
+	return fmt.Println(str)
+}
