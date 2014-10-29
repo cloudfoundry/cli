7d44814d596a1f976224d83f6a48b7ca8051ae61
Tyler Schultz <tschultz@pivotal.io>
2014-10-29 15:31:41 -0700

    `cf copy-app` has api implemented.
    Signed-off-by: Dan Lavine <dlavine@us.ibm.com>

---
+++ cf/api/copy_application_source/copy_application_source_suite_test.go
@@ -0,0 +1,18 @@
+package copy_application_source_test
+
+import (
+	"github.com/cloudfoundry/cli/cf/i18n"
+	"github.com/cloudfoundry/cli/testhelpers/configuration"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestCopyApplicationSource(t *testing.T) {
+	config := configuration.NewRepositoryWithDefaults()
+	i18n.T = i18n.Init(config)
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "CopyApplicationSource Suite")
+}
---
+++ cf/api/copy_application_source/copy_application_source_test.go
@@ -0,0 +1,61 @@
+package copy_application_source_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	. "github.com/cloudfoundry/cli/cf/api/copy_application_source"
+	testapi "github.com/cloudfoundry/cli/cf/api/fakes"
+	"github.com/cloudfoundry/cli/cf/configuration/core_config"
+	"github.com/cloudfoundry/cli/cf/net"
+	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
+	testnet "github.com/cloudfoundry/cli/testhelpers/net"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CopyApplicationSource", func() {
+	var (
+		repo        CopyApplicationSourceRepository
+		testServer  *httptest.Server
+		testHandler *testnet.TestHandler
+		configRepo  core_config.ReadWriter
+	)
+
+	setupTestServer := func(reqs ...testnet.TestRequest) {
+		testServer, testHandler = testnet.NewServer(reqs)
+		configRepo.SetApiEndpoint(testServer.URL)
+	}
+
+	BeforeEach(func() {
+		configRepo = testconfig.NewRepositoryWithDefaults()
+		gateway := net.NewCloudControllerGateway(configRepo, time.Now)
+		repo = NewCloudControllerCopyApplicationSourceRepository(configRepo, gateway)
+	})
+
+	AfterEach(func() {
+		testServer.Close()
+	})
+
+	Describe(".CopyApplication", func() {
+		BeforeEach(func() {
+			setupTestServer(testapi.NewCloudControllerTestRequest(testnet.TestRequest{
+				Method: "POST",
+				Path:   "/v2/apps/target-app-guid/copy_bits",
+				Matcher: testnet.RequestBodyMatcher(`{
+					"source_app_guid": "source-app-guid"
+				}`),
+				Response: testnet.TestResponse{
+					Status: http.StatusCreated,
+				},
+			}))
+		})
+
+		It("should return a CopyApplicationModel", func() {
+			err := repo.CopyApplication("source-app-guid", "target-app-guid")
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+})
