c34e05e9c4da20ec902379b010d887fd16c316ce
Dan Lavine <dlavine@us.ibm.com>
2014-11-03 17:06:21 -0800

    Add plugin_examples and README
    [finishes #78236438]
    
    Signed-off-by: Derek Richard <drichard@pivotal.io>

---
+++ plugin_examples/interactive.go
@@ -0,0 +1,43 @@
+/**
+* This is an example of an interactive plugin. The plugin is invoked with
+* `cf interactive` after which the user is prompted to enter a word. This word is
+* then echoed back to the user.
+ */
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/cloudfoundry/cli/plugin"
+)
+
+type Interactive struct{}
+
+func (c *Interactive) Run(args []string) {
+	if args[0] == "interactive" {
+		var Echo string
+		fmt.Printf("Enter word: ")
+
+		// Simple scan to wait for interactive from stdin
+		fmt.Scanf("%s", &Echo)
+
+		fmt.Println("Your word was:", Echo)
+	}
+}
+
+func (c *Interactive) GetMetadata() plugin.PluginMetadata {
+	return plugin.PluginMetadata{
+		Name: "Interactive",
+		Commands: []plugin.Command{
+			{
+				Name:     "interactive",
+				HelpText: "help text for interactive",
+			},
+		},
+	}
+}
+
+func main() {
+	plugin.Start(new(Interactive))
+}
