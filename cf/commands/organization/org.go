56e5bae5ff9e32ed3c3e5e9eef7cfe2ae8f929bd
Derek Richard <drichard@pivotal.io>
2014-12-01 10:49:24 -0800

    Add guid flag to `cf org`
    [#83435546]
    
    Signed-off-by: Derek Richard <drichard@pivotal.io>

--- cf/commands/organization/org.go
+++ cf/commands/organization/org.go
@@ -31,6 +31,9 @@ func (cmd *ShowOrg) Metadata() command_metadata.CommandMetadata {
 		Name:        "org",
 		Description: T("Show org info"),
 		Usage:       T("CF_NAME org ORG"),
+		Flags: []cli.Flag{
+			cli.BoolFlag{Name: "guid", Usage: T("Retrieve and display the given org's guid.  All other output for the org is suppressed.")},
+		},
 	}
 }
 
@@ -50,43 +53,48 @@ func (cmd *ShowOrg) GetRequirements(requirementsFactory requirements.Factory, c 
 
 func (cmd *ShowOrg) Run(c *cli.Context) {
 	org := cmd.orgReq.GetOrganization()
-	cmd.ui.Say(T("Getting info for org {{.OrgName}} as {{.Username}}...",
-		map[string]interface{}{
-			"OrgName":  terminal.EntityNameColor(org.Name),
-			"Username": terminal.EntityNameColor(cmd.config.Username())}))
-	cmd.ui.Ok()
-	cmd.ui.Say("")
 
-	table := terminal.NewTable(cmd.ui, []string{terminal.EntityNameColor(org.Name) + ":", "", ""})
+	if c.Bool("guid") {
+		cmd.ui.Say(org.Guid)
+	} else {
+		cmd.ui.Say(T("Getting info for org {{.OrgName}} as {{.Username}}...",
+			map[string]interface{}{
+				"OrgName":  terminal.EntityNameColor(org.Name),
+				"Username": terminal.EntityNameColor(cmd.config.Username())}))
+		cmd.ui.Ok()
+		cmd.ui.Say("")
 
-	domains := []string{}
-	for _, domain := range org.Domains {
-		domains = append(domains, domain.Name)
+		table := terminal.NewTable(cmd.ui, []string{terminal.EntityNameColor(org.Name) + ":", "", ""})
+
+		domains := []string{}
+		for _, domain := range org.Domains {
+			domains = append(domains, domain.Name)
+		}
+
+		spaces := []string{}
+		for _, space := range org.Spaces {
+			spaces = append(spaces, space.Name)
+		}
+
+		spaceQuotas := []string{}
+		for _, spaceQuota := range org.SpaceQuotas {
+			spaceQuotas = append(spaceQuotas, spaceQuota.Name)
+		}
+
+		quota := org.QuotaDefinition
+		orgQuota := fmt.Sprintf(T("{{.QuotaName}} ({{.MemoryLimit}}M memory limit, {{.RoutesLimit}} routes, {{.ServicesLimit}} services, paid services {{.NonBasicServicesAllowed}})",
+			map[string]interface{}{
+				"QuotaName":               quota.Name,
+				"MemoryLimit":             quota.MemoryLimit,
+				"RoutesLimit":             quota.RoutesLimit,
+				"ServicesLimit":           quota.ServicesLimit,
+				"NonBasicServicesAllowed": formatters.Allowed(quota.NonBasicServicesAllowed)}))
+
+		table.Add("", T("domains:"), terminal.EntityNameColor(strings.Join(domains, ", ")))
+		table.Add("", T("quota:"), terminal.EntityNameColor(orgQuota))
+		table.Add("", T("spaces:"), terminal.EntityNameColor(strings.Join(spaces, ", ")))
+		table.Add("", T("space quotas:"), terminal.EntityNameColor(strings.Join(spaceQuotas, ", ")))
+
+		table.Print()
 	}
-
-	spaces := []string{}
-	for _, space := range org.Spaces {
-		spaces = append(spaces, space.Name)
-	}
-
-	spaceQuotas := []string{}
-	for _, spaceQuota := range org.SpaceQuotas {
-		spaceQuotas = append(spaceQuotas, spaceQuota.Name)
-	}
-
-	quota := org.QuotaDefinition
-	orgQuota := fmt.Sprintf(T("{{.QuotaName}} ({{.MemoryLimit}}M memory limit, {{.RoutesLimit}} routes, {{.ServicesLimit}} services, paid services {{.NonBasicServicesAllowed}})",
-		map[string]interface{}{
-			"QuotaName":               quota.Name,
-			"MemoryLimit":             quota.MemoryLimit,
-			"RoutesLimit":             quota.RoutesLimit,
-			"ServicesLimit":           quota.ServicesLimit,
-			"NonBasicServicesAllowed": formatters.Allowed(quota.NonBasicServicesAllowed)}))
-
-	table.Add("", T("domains:"), terminal.EntityNameColor(strings.Join(domains, ", ")))
-	table.Add("", T("quota:"), terminal.EntityNameColor(orgQuota))
-	table.Add("", T("spaces:"), terminal.EntityNameColor(strings.Join(spaces, ", ")))
-	table.Add("", T("space quotas:"), terminal.EntityNameColor(strings.Join(spaceQuotas, ", ")))
-
-	table.Print()
 }
