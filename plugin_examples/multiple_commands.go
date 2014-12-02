238a4e9b5cb6a54c97e410793bdeea1ac921e788
Simon Leung <leungs@us.ibm.com>
2014-12-01 16:40:55 -0800

    code example for plugin with multiple commands
    [#83690584]
    
    Signed-off-by: Jonathan Berkhahn <jaberkha@us.ibm.com>

---
+++ plugin_examples/multiple_commands.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cloudfoundry/cli/plugin"
+)
+
+type MultiCmd struct{}
+
+func (c *MultiCmd) GetMetadata() plugin.PluginMetadata {
+	return plugin.PluginMetadata{
+		Name: "MultiCmd",
+		Commands: []plugin.Command{
+			{
+				Name:     "command-1",
+				HelpText: "Help text for command-1",
+			},
+			{
+				Name:     "command-2",
+				HelpText: "Help text for command-2",
+			},
+			{
+				Name:     "command-3",
+				HelpText: "Help text for command-3",
+			},
+		},
+	}
+}
+
+func main() {
+	plugin.Start(new(MultiCmd))
+}
+
+func (c *MultiCmd) Run(cliConnection plugin.CliConnection, args []string) {
+	if args[0] == "command-1" {
+		c.Command1()
+	} else if args[0] == "command-2" {
+		c.Command2()
+	} else if args[0] == "command-3" {
+		c.Command3()
+	}
+}
+
+func (c *MultiCmd) Command1() {
+	fmt.Println("Function command-1 in plugin 'MultiCmd' is called.")
+}
+
+func (c *MultiCmd) Command2() {
+	fmt.Println("Function command-2 in plugin 'MultiCmd' is called.")
+}
+
+func (c *MultiCmd) Command3() {
+	fmt.Println("Function command-3 in plugin 'MultiCmd' is called.")
+}
