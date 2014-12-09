f01939851accac1740cb78ec853d32680e066fe9
Simon Leung <leungs@us.ibm.com>
2014-12-08 23:58:56 -0800

    New utils for download single file from url

---
+++ fileutils/file_download.go
@@ -0,0 +1,113 @@
+package fileutils
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Downloader interface {
+	DownloadFile(string) (int64, string, error)
+	RemoveFile() error
+}
+
+type downloader struct {
+	saveDir  string
+	filename string
+}
+
+func NewDownloader(saveDir string) Downloader {
+	return &downloader{
+		saveDir: saveDir,
+	}
+}
+
+//this func returns byte written, filename and error
+func (d *downloader) DownloadFile(url string) (int64, string, error) {
+	c := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			r.URL.Opaque = r.URL.Path
+
+			//some redirect return '/' as url
+			if strings.Trim(r.URL.Opaque, "/") != "" {
+				url = r.URL.Opaque
+			}
+
+			return nil
+		},
+	}
+
+	r, err := c.Get(url)
+
+	if err != nil {
+		fmt.Printf("Error while downloading %q: %v", url, err)
+		return 0, "", err
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode == 200 {
+		d.filename = get_filename_from_header(r.Header.Get("Content-Disposition"))
+
+		if d.filename == "" {
+			d.filename = get_filename_from_url(url)
+		}
+
+		f, err := os.Create(filepath.Join(d.saveDir, d.filename))
+		if err != nil {
+			return 0, "", err
+		}
+		defer f.Close()
+
+		size, err := io.Copy(f, r.Body)
+		if err != nil {
+			return 0, "", err
+		}
+
+		return size, d.filename, nil
+
+	} else {
+		return 0, "", fmt.Errorf("Error downloading file from %s", url)
+	}
+}
+
+func (d *downloader) RemoveFile() error {
+	return os.Remove(filepath.Join(d.saveDir, d.filename))
+}
+
+func get_filename_from_header(h string) string {
+	if h == "" {
+		return ""
+	}
+
+	contents := strings.Split(h, ";")
+	for _, content := range contents {
+		if strings.Contains(content, "filename=") {
+			name := strings.TrimLeft(content, "filename=")
+			return strings.Trim(name, `"`)
+		}
+	}
+
+	return ""
+}
+
+func get_filename_from_url(url string) string {
+	tmp := strings.Split(url, "/")
+	token := tmp[len(tmp)-1]
+
+	if i := strings.LastIndex(token, "?"); i != -1 {
+		token = token[i+1:]
+	}
+
+	if i := strings.LastIndex(token, "&"); i != -1 {
+		token = token[i+1:]
+	}
+
+	if i := strings.LastIndex(token, "="); i != -1 {
+		return token[i+1:]
+	}
+
+	return token
+}
---
+++ fileutils/file_download_test.go
@@ -0,0 +1,139 @@
+package fileutils_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+
+	. "github.com/cloudfoundry/cli/fileutils"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Downloader", func() {
+	var (
+		ts         *httptest.Server
+		downloader Downloader
+	)
+
+	Describe("DownloadFile", func() {
+		Context("URL contains filename", func() {
+			BeforeEach(func() {
+				ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					fmt.Fprintln(w, "abc123")
+				}))
+				downloader = NewDownloader("./")
+			})
+
+			AfterEach(func() {
+				ts.Close()
+
+				err := downloader.RemoveFile()
+				Ω(err).ToNot(HaveOccurred())
+			})
+
+			It("saves file with name found in URL in provided dir", func() {
+				_, name, err := downloader.DownloadFile(ts.URL + "/abc.zip")
+				Ω(err).ToNot(HaveOccurred())
+				Ω(name).To(Equal("abc.zip"))
+
+				_, err = os.Stat("./abc.zip")
+				Ω(err).ToNot(HaveOccurred())
+			})
+
+			It("returns the number of bytes written to the file", func() {
+				n, _, err := downloader.DownloadFile(ts.URL + "/abc.zip")
+				Ω(err).ToNot(HaveOccurred())
+				Ω(n).To(Equal(int64(len("abc123") + 1)))
+			})
+		})
+
+		Context("header contains filename", func() {
+			BeforeEach(func() {
+				ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.Header().Set("Content-Disposition", "attachment;filename=header.zip")
+					fmt.Fprintln(w, "abc123")
+				}))
+			})
+
+			AfterEach(func() {
+				ts.Close()
+
+				err := downloader.RemoveFile()
+				Ω(err).ToNot(HaveOccurred())
+			})
+
+			It("saves file with name found in header, ignore filename in url", func() {
+				_, name, err := downloader.DownloadFile(ts.URL + "/abc.zip")
+				Ω(err).ToNot(HaveOccurred())
+				Ω(name).To(Equal("header.zip"))
+
+				_, err = os.Stat("./abc.zip")
+				Ω(err).To(HaveOccurred())
+
+				_, err = os.Stat("./header.zip")
+				Ω(err).ToNot(HaveOccurred())
+			})
+
+		})
+
+		Context("When URL redirects", func() {
+			BeforeEach(func() {
+				ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+					if !strings.Contains(r.URL.String(), "redirect.zip") {
+						http.Redirect(w, r, ts.URL+"/redirect.zip", http.StatusMovedPermanently)
+					} else {
+						fmt.Fprintln(w, "abc123")
+					}
+				}))
+			})
+
+			AfterEach(func() {
+				ts.Close()
+
+				err := downloader.RemoveFile()
+				Ω(err).ToNot(HaveOccurred())
+			})
+
+			It("follows redirects and download file", func() {
+				_, _, err := downloader.DownloadFile(ts.URL)
+				Ω(err).ToNot(HaveOccurred())
+
+				_, err = os.Stat("./redirect.zip")
+				Ω(err).ToNot(HaveOccurred())
+			})
+
+		})
+	})
+
+	Describe("RemoveFile", func() {
+		BeforeEach(func() {
+			ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintln(w, "abc123")
+			}))
+		})
+
+		AfterEach(func() {
+			ts.Close()
+		})
+
+		It("removes the downloaded file", func() {
+			_, _, err := downloader.DownloadFile(ts.URL + "/abc.zip")
+			Ω(err).ToNot(HaveOccurred())
+
+			_, err = os.Stat("./abc.zip")
+			Ω(err).ToNot(HaveOccurred())
+
+			err = downloader.RemoveFile()
+			Ω(err).ToNot(HaveOccurred())
+
+			_, err = os.Stat("./abc.zip")
+			Ω(err).To(HaveOccurred())
+		})
+	})
+
+})
