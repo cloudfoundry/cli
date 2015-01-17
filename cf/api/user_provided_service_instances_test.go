65a12df01712e5f534ba56099b43dba2cf4d712b
Simon Leung <leungs@us.ibm.com>
2015-01-16 16:24:12 -0800

    fix usage of test http server
    [#79188196]
    
    Signed-off-by: Jonathan Berkhahn <jaberkha@us.ibm.com>

--- cf/api/user_provided_service_instances_test.go
+++ cf/api/user_provided_service_instances_test.go
@@ -19,75 +19,83 @@ import (
 )
 
 var _ = Describe("UserProvidedServiceRepository", func() {
-	It("creates a user provided service with a name and credentials", func() {
-		req := testapi.NewCloudControllerTestRequest(testnet.TestRequest{
-			Method:   "POST",
-			Path:     "/v2/user_provided_service_instances",
-			Matcher:  testnet.RequestBodyMatcher(`{"name":"my-custom-service","credentials":{"host":"example.com","password":"secret","user":"me"},"space_guid":"my-space-guid","syslog_drain_url":""}`),
-			Response: testnet.TestResponse{Status: http.StatusCreated},
+
+	Context("Create()", func() {
+		It("creates a user provided service with a name and credentials", func() {
+			req := testapi.NewCloudControllerTestRequest(testnet.TestRequest{
+				Method:   "POST",
+				Path:     "/v2/user_provided_service_instances",
+				Matcher:  testnet.RequestBodyMatcher(`{"name":"my-custom-service","credentials":{"host":"example.com","password":"secret","user":"me"},"space_guid":"my-space-guid","syslog_drain_url":""}`),
+				Response: testnet.TestResponse{Status: http.StatusCreated},
+			})
+
+			ts, handler, repo := createUserProvidedServiceInstanceRepo([]testnet.TestRequest{req})
+			defer ts.Close()
+
+			apiErr := repo.Create("my-custom-service", "", map[string]interface{}{
+				"host":     "example.com",
+				"user":     "me",
+				"password": "secret",
+			})
+			Expect(handler).To(HaveAllRequestsCalled())
+			Expect(apiErr).NotTo(HaveOccurred())
 		})
 
-		ts, handler, repo := createUserProvidedServiceInstanceRepo(req)
-		defer ts.Close()
+		It("creates user provided service instances with syslog drains", func() {
+			req := testapi.NewCloudControllerTestRequest(testnet.TestRequest{
+				Method:   "POST",
+				Path:     "/v2/user_provided_service_instances",
+				Matcher:  testnet.RequestBodyMatcher(`{"name":"my-custom-service","credentials":{"host":"example.com","password":"secret","user":"me"},"space_guid":"my-space-guid","syslog_drain_url":"syslog://example.com"}`),
+				Response: testnet.TestResponse{Status: http.StatusCreated},
+			})
 
-		apiErr := repo.Create("my-custom-service", "", map[string]interface{}{
-			"host":     "example.com",
-			"user":     "me",
-			"password": "secret",
+			ts, handler, repo := createUserProvidedServiceInstanceRepo([]testnet.TestRequest{req})
+			defer ts.Close()
+
+			apiErr := repo.Create("my-custom-service", "syslog://example.com", map[string]interface{}{
+				"host":     "example.com",
+				"user":     "me",
+				"password": "secret",
+			})
+			Expect(handler).To(HaveAllRequestsCalled())
+			Expect(apiErr).NotTo(HaveOccurred())
 		})
-		Expect(handler).To(HaveAllRequestsCalled())
-		Expect(apiErr).NotTo(HaveOccurred())
 	})
 
-	It("creates user provided service instances with syslog drains", func() {
-		req := testapi.NewCloudControllerTestRequest(testnet.TestRequest{
-			Method:   "POST",
-			Path:     "/v2/user_provided_service_instances",
-			Matcher:  testnet.RequestBodyMatcher(`{"name":"my-custom-service","credentials":{"host":"example.com","password":"secret","user":"me"},"space_guid":"my-space-guid","syslog_drain_url":"syslog://example.com"}`),
-			Response: testnet.TestResponse{Status: http.StatusCreated},
-		})
+	Context("Update()", func() {
+		It("can update a user provided service, given a service instance with a guid", func() {
+			req := testapi.NewCloudControllerTestRequest(testnet.TestRequest{
+				Method:   "PUT",
+				Path:     "/v2/user_provided_service_instances/my-instance-guid",
+				Matcher:  testnet.RequestBodyMatcher(`{"credentials":{"host":"example.com","password":"secret","user":"me"},"syslog_drain_url":"syslog://example.com"}`),
+				Response: testnet.TestResponse{Status: http.StatusCreated},
+			})
 
-		ts, handler, repo := createUserProvidedServiceInstanceRepo(req)
-		defer ts.Close()
+			ts, handler, repo := createUserProvidedServiceInstanceRepo([]testnet.TestRequest{req})
+			defer ts.Close()
 
-		apiErr := repo.Create("my-custom-service", "syslog://example.com", map[string]interface{}{
-			"host":     "example.com",
-			"user":     "me",
-			"password": "secret",
+			params := map[string]interface{}{
+				"host":     "example.com",
+				"user":     "me",
+				"password": "secret",
+			}
+			serviceInstance := models.ServiceInstanceFields{}
+			serviceInstance.Guid = "my-instance-guid"
+			serviceInstance.Params = params
+			serviceInstance.SysLogDrainUrl = "syslog://example.com"
+
+			apiErr := repo.Update(serviceInstance)
+			Expect(handler).To(HaveAllRequestsCalled())
+			Expect(apiErr).NotTo(HaveOccurred())
 		})
-		Expect(handler).To(HaveAllRequestsCalled())
-		Expect(apiErr).NotTo(HaveOccurred())
 	})
 
-	It("can update a user provided service, given a service instance with a guid", func() {
-		req := testapi.NewCloudControllerTestRequest(testnet.TestRequest{
-			Method:   "PUT",
-			Path:     "/v2/user_provided_service_instances/my-instance-guid",
-			Matcher:  testnet.RequestBodyMatcher(`{"credentials":{"host":"example.com","password":"secret","user":"me"},"syslog_drain_url":"syslog://example.com"}`),
-			Response: testnet.TestResponse{Status: http.StatusCreated},
-		})
 
-		ts, handler, repo := createUserProvidedServiceInstanceRepo(req)
-		defer ts.Close()
-
-		params := map[string]interface{}{
-			"host":     "example.com",
-			"user":     "me",
-			"password": "secret",
-		}
-		serviceInstance := models.ServiceInstanceFields{}
-		serviceInstance.Guid = "my-instance-guid"
-		serviceInstance.Params = params
-		serviceInstance.SysLogDrainUrl = "syslog://example.com"
-
-		apiErr := repo.Update(serviceInstance)
-		Expect(handler).To(HaveAllRequestsCalled())
-		Expect(apiErr).NotTo(HaveOccurred())
 	})
 })
 
-func createUserProvidedServiceInstanceRepo(req testnet.TestRequest) (ts *httptest.Server, handler *testnet.TestHandler, repo UserProvidedServiceInstanceRepository) {
-	ts, handler = testnet.NewServer([]testnet.TestRequest{req})
+func createUserProvidedServiceInstanceRepo(req []testnet.TestRequest) (ts *httptest.Server, handler *testnet.TestHandler, repo UserProvidedServiceInstanceRepository) {
+	ts, handler = testnet.NewServer(req)
 	configRepo := testconfig.NewRepositoryWithDefaults()
 	configRepo.SetApiEndpoint(ts.URL)
 	gateway := net.NewCloudControllerGateway(configRepo, time.Now, &testterm.FakeUI{})
