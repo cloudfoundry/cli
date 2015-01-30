c15e70ef446b93e44e0a2ada43b33d119060418f
Simon Leung <leungs@us.ibm.com>
2015-01-29 17:06:48 -0800

    remove commented code
    Signed-off-by: Jonathan Berkhahn <jaberkha@us.ibm.com>

--- testhelpers/matchers/contain_substrings.go
+++ testhelpers/matchers/contain_substrings.go
@@ -17,31 +17,6 @@ func ContainSubstrings(substrings ...[]string) gomega.OmegaMatcher {
 	return &SliceMatcher{expected: substrings}
 }
 
-//func (matcher *SliceMatcher) Match(actual interface{}) (success bool, err error) {
-//	actualStrings, ok := actual.([]string)
-//	if !ok {
-//		return false, nil
-//	}
-//
-//	matcher.failedAtIndex = 0
-//	for _, actualValue := range actualStrings {
-//		allStringsFound := true
-//		for _, expectedValue := range matcher.expected[matcher.failedAtIndex] {
-//			allStringsFound = allStringsFound && strings.Contains(terminal.Decolorize(actualValue), expectedValue)
-//		}
-//
-//		if allStringsFound {
-//			matcher.failedAtIndex++
-//			if matcher.failedAtIndex == len(matcher.expected) {
-//				matcher.failedAtIndex--
-//				return true, nil
-//			}
-//		}
-//	}
-//
-//	return false, nil
-//}
-
 func (matcher *SliceMatcher) Match(actual interface{}) (success bool, err error) {
 	actualStrings, ok := actual.([]string)
 	if !ok {
