e51c040cb4d300db8189600bf566bacaf00ef410
AmitRoushan <amit.roushan@huawei.com>
2015-03-09 14:53:48 +0530

    Add a flag to force a restage after bind/unbind service instance
    As of now user has to restage an app with 'cf restage SOME_APP' command after
    binding or unbinding to ensure bind/unbind env variable changes take effect.
    With --force-restage flag , User has option to restage an app with bind/unbind itself.

--- cf/command_factory/factory.go
+++ cf/command_factory/factory.go
@@ -156,7 +156,6 @@ func NewFactory(ui terminal.UI, config core_config.ReadWriter, manifestRepo mani
 	factory.cmdsByName["stacks"] = commands.NewListStacks(ui, config, repoLocator.GetStackRepository())
 	factory.cmdsByName["stack"] = commands.NewListStack(ui, config, repoLocator.GetStackRepository())
 	factory.cmdsByName["target"] = commands.NewTarget(ui, config, repoLocator.GetOrganizationRepository(), repoLocator.GetSpaceRepository())
-	factory.cmdsByName["unbind-service"] = service.NewUnbindService(ui, config, repoLocator.GetServiceBindingRepository())
 	factory.cmdsByName["unset-env"] = application.NewUnsetEnv(ui, config, repoLocator.GetApplicationRepository())
 	factory.cmdsByName["unset-org-role"] = user.NewUnsetOrgRole(ui, config, repoLocator.GetUserRepository())
 	factory.cmdsByName["unset-space-role"] = user.NewUnsetSpaceRole(ui, config, repoLocator.GetSpaceRepository(), repoLocator.GetUserRepository())
@@ -215,10 +214,12 @@ func NewFactory(ui terminal.UI, config core_config.ReadWriter, manifestRepo mani
 	stop := application.NewStop(ui, config, repoLocator.GetApplicationRepository())
 	restart := application.NewRestart(ui, config, start, stop)
 	restage := application.NewRestage(ui, config, repoLocator.GetApplicationRepository(), start)
-	bind := service.NewBindService(ui, config, repoLocator.GetServiceBindingRepository())
+	bind := service.NewBindService(ui, config, repoLocator.GetServiceBindingRepository(), repoLocator.GetApplicationRepository(), start)
+	unbind := service.NewUnbindService(ui, config, repoLocator.GetServiceBindingRepository(), repoLocator.GetApplicationRepository(), start)
 
 	factory.cmdsByName["app"] = displayApp
 	factory.cmdsByName["bind-service"] = bind
+	factory.cmdsByName["unbind-service"] = unbind
 	factory.cmdsByName["start"] = start
 	factory.cmdsByName["stop"] = stop
 	factory.cmdsByName["restart"] = restart
--- cf/commands/service/unbind_service_test.go
+++ cf/commands/service/unbind_service_test.go
@@ -2,8 +2,11 @@ package service_test
 
 import (
 	"github.com/cloudfoundry/cli/cf/api"
+	"github.com/cloudfoundry/cli/cf/api/applications"
+	testApplication "github.com/cloudfoundry/cli/cf/api/applications/fakes"
 	testapi "github.com/cloudfoundry/cli/cf/api/fakes"
 	. "github.com/cloudfoundry/cli/cf/commands/service"
+	"github.com/cloudfoundry/cli/cf/configuration/core_config"
 	"github.com/cloudfoundry/cli/cf/models"
 	testcmd "github.com/cloudfoundry/cli/testhelpers/commands"
 	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
@@ -21,6 +24,9 @@ var _ = Describe("unbind-service command", func() {
 		serviceInstance     models.ServiceInstance
 		requirementsFactory *testreq.FakeReqFactory
 		serviceBindingRepo  *testapi.FakeServiceBindingRepo
+		stagingWatcher      *fakeStagingWatcher
+		appRepo             *testApplication.FakeApplicationRepository
+		configRepo          core_config.ReadWriter
 	)
 
 	BeforeEach(func() {
@@ -34,12 +40,15 @@ var _ = Describe("unbind-service command", func() {
 		requirementsFactory.Application = app
 		requirementsFactory.ServiceInstance = serviceInstance
 
+		appRepo = &testApplication.FakeApplicationRepository{}
+		stagingWatcher = &fakeStagingWatcher{}
+
 		serviceBindingRepo = &testapi.FakeServiceBindingRepo{}
 	})
 
 	Context("when not logged in", func() {
 		It("fails requirements when not logged in", func() {
-			cmd := NewUnbindService(&testterm.FakeUI{}, testconfig.NewRepository(), serviceBindingRepo)
+			cmd := NewUnbindService(&testterm.FakeUI{}, testconfig.NewRepository(), serviceBindingRepo, appRepo, stagingWatcher)
 			Expect(testcmd.RunCommand(cmd, []string{"my-service", "my-app"}, requirementsFactory)).To(BeFalse())
 		})
 	})
@@ -47,11 +56,12 @@ var _ = Describe("unbind-service command", func() {
 	Context("when logged in", func() {
 		BeforeEach(func() {
 			requirementsFactory.LoginSuccess = true
+			configRepo = testconfig.NewRepositoryWithDefaults()
 		})
 
 		Context("when the service instance exists", func() {
 			It("unbinds a service from an app", func() {
-				ui := callUnbindService([]string{"my-app", "my-service"}, requirementsFactory, serviceBindingRepo)
+				ui := callUnbindService([]string{"my-app", "my-service"}, requirementsFactory, serviceBindingRepo, appRepo, stagingWatcher)
 
 				Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
 				Expect(requirementsFactory.ServiceInstanceName).To(Equal("my-service"))
@@ -63,6 +73,41 @@ var _ = Describe("unbind-service command", func() {
 				Expect(serviceBindingRepo.DeleteServiceInstance).To(Equal(serviceInstance))
 				Expect(serviceBindingRepo.DeleteApplicationGuid).To(Equal("my-app-guid"))
 			})
+
+			It("restage app after unbinds a service when force restage", func() {
+				ui := callUnbindService([]string{"my-app", "my-service", "--force-restage"}, requirementsFactory, serviceBindingRepo, appRepo, stagingWatcher)
+
+				Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
+				Expect(requirementsFactory.ServiceInstanceName).To(Equal("my-service"))
+
+				Expect(ui.Outputs).To(ContainSubstrings(
+					[]string{"Unbinding app", "my-service", "my-app", "my-org", "my-space", "my-user"},
+					[]string{"OK"},
+					[]string{"Restaging app", "my-app", "my-org", "my-space", "my-user"},
+				))
+				Expect(serviceBindingRepo.DeleteServiceInstance).To(Equal(serviceInstance))
+				Expect(serviceBindingRepo.DeleteApplicationGuid).To(Equal("my-app-guid"))
+				Expect(stagingWatcher.watched).To(Equal(app))
+				Expect(stagingWatcher.orgName).To(Equal(configRepo.OrganizationFields().Name))
+				Expect(stagingWatcher.spaceName).To(Equal(configRepo.SpaceFields().Name))
+			})
+
+			It("do not restage app after unbinds a service when not force restage", func() {
+				ui := callUnbindService([]string{"my-app", "my-service"}, requirementsFactory, serviceBindingRepo, appRepo, stagingWatcher)
+
+				Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
+				Expect(requirementsFactory.ServiceInstanceName).To(Equal("my-service"))
+
+				Expect(ui.Outputs).To(ContainSubstrings(
+					[]string{"Unbinding app", "my-service", "my-app", "my-org", "my-space", "my-user"},
+					[]string{"OK"},
+				))
+				Expect(serviceBindingRepo.DeleteServiceInstance).To(Equal(serviceInstance))
+				Expect(serviceBindingRepo.DeleteApplicationGuid).To(Equal("my-app-guid"))
+				Expect(stagingWatcher.watched).To(Equal(models.Application{}))
+				Expect(stagingWatcher.orgName).To(Equal(""))
+				Expect(stagingWatcher.spaceName).To(Equal(""))
+			})
 		})
 
 		Context("when the service instance does not exist", func() {
@@ -71,7 +116,7 @@ var _ = Describe("unbind-service command", func() {
 			})
 
 			It("warns the user the the service instance does not exist", func() {
-				ui := callUnbindService([]string{"my-app", "my-service"}, requirementsFactory, serviceBindingRepo)
+				ui := callUnbindService([]string{"my-app", "my-service"}, requirementsFactory, serviceBindingRepo, appRepo, stagingWatcher)
 
 				Expect(requirementsFactory.ApplicationName).To(Equal("my-app"))
 				Expect(requirementsFactory.ServiceInstanceName).To(Equal("my-service"))
@@ -87,24 +132,24 @@ var _ = Describe("unbind-service command", func() {
 		})
 
 		It("when no parameters are given the command fails with usage", func() {
-			ui := callUnbindService([]string{"my-service"}, requirementsFactory, serviceBindingRepo)
+			ui := callUnbindService([]string{"my-service"}, requirementsFactory, serviceBindingRepo, appRepo, stagingWatcher)
 			Expect(ui.FailedWithUsage).To(BeTrue())
 
-			ui = callUnbindService([]string{"my-app"}, requirementsFactory, serviceBindingRepo)
+			ui = callUnbindService([]string{"my-app"}, requirementsFactory, serviceBindingRepo, appRepo, stagingWatcher)
 			Expect(ui.FailedWithUsage).To(BeTrue())
 
-			ui = callUnbindService([]string{"my-app", "my-service"}, requirementsFactory, serviceBindingRepo)
+			ui = callUnbindService([]string{"my-app", "my-service"}, requirementsFactory, serviceBindingRepo, appRepo, stagingWatcher)
 			Expect(ui.FailedWithUsage).To(BeFalse())
 		})
 	})
 })
 
-func callUnbindService(args []string, requirementsFactory *testreq.FakeReqFactory, serviceBindingRepo api.ServiceBindingRepository) (fakeUI *testterm.FakeUI) {
+func callUnbindService(args []string, requirementsFactory *testreq.FakeReqFactory, serviceBindingRepo api.ServiceBindingRepository, appRepo applications.ApplicationRepository, stagingWatcher ApplicationStagingWatcher) (fakeUI *testterm.FakeUI) {
 	fakeUI = &testterm.FakeUI{}
 
 	config := testconfig.NewRepositoryWithDefaults()
 
-	cmd := NewUnbindService(fakeUI, config, serviceBindingRepo)
+	cmd := NewUnbindService(fakeUI, config, serviceBindingRepo, appRepo, stagingWatcher)
 	testcmd.RunCommand(cmd, args, requirementsFactory)
 	return
 }
