dd5eed9ce3d8e482fb549c5cfa6ccdcf9eacd9cd
Greg Cobb and Liz Dahlstrom <edahlstrom@pivotal.io>
2015-05-28 17:51:15 -0700

    Surface error when json from file is invalid
    - When parsing arbitrary parameters from a file path
    - Only read file contents if we know it's a file
    
    [#88670540]

--- json/json_parser_test.go
+++ json/json_parser_test.go
@@ -47,54 +47,21 @@ var _ = Describe("JSON Parser", func() {
 
 			It("tries to convert the json file but fails because it was given something it didn't like", func() {
 				_, err := json.ParseJsonArray(filename)
-				Expect(err).ToNot(BeNil())
-			})
-		})
-	})
-
-	Describe("ParseJsonHash", func() {
-		var filename string
-		var tmpFile *os.File
-
-		Context("when everything is proper", func() {
-			BeforeEach(func() {
-				tmpFile, _ = ioutil.TempFile("", "")
-				filename = tmpFile.Name()
-				ioutil.WriteFile(filename, []byte("{\"akey\": \"avalue\"}"), 0644)
-			})
-
-			AfterEach(func() {
-				tmpFile.Close()
-				os.Remove(tmpFile.Name())
-			})
-
-			It("converts a json file into an unmarshalled slice of string->string map objects", func() {
-				stringMap, err := json.ParseJsonHash(filename)
-				Expect(err).To(BeNil())
-				Expect(stringMap["akey"]).To(Equal("avalue"))
-			})
-		})
-
-		Context("when the JSON is invalid", func() {
-			BeforeEach(func() {
-				tmpFile, _ = ioutil.TempFile("", "")
-				filename = tmpFile.Name()
-				ioutil.WriteFile(filename, []byte("SCARY NOISES}"), 0644)
-			})
-
-			AfterEach(func() {
-				tmpFile.Close()
-				os.Remove(tmpFile.Name())
-			})
-
-			It("tries to convert the json file but fails because it was given something it didn't like", func() {
-				_, err := json.ParseJsonHash(filename)
-				Expect(err).ToNot(BeNil())
+				Expect(err).To(MatchError("Incorrect json format: invalid character 'S' looking for beginning of value"))
 			})
 		})
 	})
 
 	Describe("ParseJsonFromFileOrString", func() {
+		Context("when the input is empty", func() {
+			It("returns nil", func() {
+				result, err := json.ParseJsonFromFileOrString("")
+
+				Expect(result).To(BeNil())
+				Expect(err).To(BeNil())
+			})
+		})
+
 		Context("when the input is a file", func() {
 			var jsonFile *os.File
 			var fileContent string
@@ -133,12 +100,12 @@ var _ = Describe("JSON Parser", func() {
 
 				It("returns an error", func() {
 					_, err := json.ParseJsonFromFileOrString(jsonFile.Name())
-					Expect(err).To(HaveOccurred())
+					Expect(err).To(MatchError("Incorrect json format: invalid character 'b' looking for beginning of value"))
 				})
 			})
 		})
 
-		Context("when the input is a json string", func() {
+		Context("when the input is considered a json string (when it is not a file path)", func() {
 			var jsonString string
 
 			BeforeEach(func() {
@@ -150,6 +117,17 @@ var _ = Describe("JSON Parser", func() {
 				Expect(err).NotTo(HaveOccurred())
 
 				Expect(result).To(Equal(map[string]interface{}{"foo": "bar"}))
+			})
+
+			Context("when the JSON is invalid", func() {
+				BeforeEach(func() {
+					jsonString = "SOMETHING IS WRONG"
+				})
+
+				It("returns a json parse error", func() {
+					_, err := json.ParseJsonFromFileOrString(jsonString)
+					Expect(err).To(MatchError("Incorrect json format: invalid character 'S' looking for beginning of value"))
+				})
 			})
 		})
 
