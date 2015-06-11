2506fd81ff3323125f2b02046275eca9ba84e5f4
Mark St.Godard <markstgodard@gmail.com>
2015-06-11 14:16:41 -0700

    Fix logic to handle graceful timeout if we cannot talk to log server. Also make log server connection timeout internally configurable.
    [Finished #96626036]
    
    Signed-off-by: Simon Leung <leungs@us.ibm.com>

--- cf/commands/application/start_test.go
+++ cf/commands/application/start_test.go
@@ -128,6 +128,20 @@ var _ = Describe("start command", func() {
 		return
 	}
 
+	callStartWithTimeout := func(args []string, config core_config.Reader, requirementsFactory *testreq.FakeReqFactory, displayApp ApplicationDisplayer, appRepo applications.ApplicationRepository, appInstancesRepo app_instances.AppInstancesRepository, logRepo api.LogsNoaaRepository, oldLogsRepo api.OldLogsRepository) (ui *testterm.FakeUI) {
+		oldLogsRepoWithTimeout := &testapi.FakeOldLogsRepositoryWithTimeout{}
+		ui = new(testterm.FakeUI)
+
+		cmd := NewStart(ui, config, displayApp, appRepo, appInstancesRepo, logRepo, oldLogsRepoWithTimeout)
+		cmd.LogServerConnectionTimeout = 100 * time.Millisecond
+		cmd.StagingTimeout = 100 * time.Millisecond
+		cmd.StartupTimeout = 200 * time.Millisecond
+		cmd.PingerThrottle = 50 * time.Millisecond
+
+		testcmd.RunCommand(cmd, args, requirementsFactory)
+		return
+	}
+
 	startAppWithInstancesAndErrors := func(displayApp ApplicationDisplayer, app models.Application, requirementsFactory *testreq.FakeReqFactory) (ui *testterm.FakeUI, appRepo *testApplication.FakeApplicationRepository, appInstancesRepo *testAppInstanaces.FakeAppInstancesRepository) {
 		configRepo := testconfig.NewRepositoryWithDefaults()
 		appRepo = &testApplication.FakeApplicationRepository{
@@ -303,10 +317,27 @@ var _ = Describe("start command", func() {
 			displayApp := &testcmd.FakeAppDisplayer{}
 			ui, appRepo, _ := startAppWithInstancesAndErrors(displayApp, defaultAppForStart, requirementsFactory)
 
-			Expect(appRepo.ReadCalls).To(Equal(1))
+			Eventually(appRepo.ReadCalls).Should(Equal(1))
 			Expect(ui.Outputs).To(ContainSubstrings(
 				[]string{"App my-app was started using this command `detected start command`"},
 			))
+		})
+
+		It("handles timeouts gracefully", func() {
+			displayApp := &testcmd.FakeAppDisplayer{}
+			requirementsFactory.Application = defaultAppForStart
+			appRepo := &testApplication.FakeApplicationRepository{
+				UpdateAppResult: defaultAppForStart,
+			}
+			appRepo.ReadReturns.App = defaultAppForStart
+
+			appInstancesRepo := &testAppInstanaces.FakeAppInstancesRepository{}
+
+			ui := callStartWithTimeout([]string{"my-app"}, testconfig.NewRepository(), requirementsFactory, displayApp, appRepo, appInstancesRepo, logRepo, oldLogsRepo)
+			Expect(ui.Outputs).To(ContainSubstrings(
+				[]string{"timeout connecting to log server"},
+			))
+
 		})
 
 		It("only displays staging logs when an app is starting", func() {
@@ -527,7 +558,7 @@ var _ = Describe("start command", func() {
 
 				ui, _, _ := startAppWithInstancesAndErrors(displayApp, defaultAppForStart, requirementsFactory)
 
-				Expect(ui.Outputs).To(ContainSubstrings(
+				Eventually(ui.Outputs).Should(ContainSubstrings(
 					[]string{"my-app"},
 					[]string{"0 of 2 instances running", "2 starting"},
 					[]string{"0 of 2 instances running", "1 starting (no compatible cell)", "1 down"},
