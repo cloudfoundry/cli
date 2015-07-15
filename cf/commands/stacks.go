b3b38376b641c4f83e995979497a80b826c6dfd4
Simon Leung <leungs@us.ibm.com>
2015-07-15 10:21:21 -0700

    convert stacks to non-codegangsta [#97061558]
    Signed-off-by: Jonathan Berkhahn <jaberkha@us.ibm.com>

--- cf/commands/stacks.go
+++ cf/commands/stacks.go
@@ -2,12 +2,12 @@ package commands
 
 import (
 	"github.com/cloudfoundry/cli/cf/api/stacks"
-	"github.com/cloudfoundry/cli/cf/command_metadata"
+	"github.com/cloudfoundry/cli/cf/command_registry"
 	"github.com/cloudfoundry/cli/cf/configuration/core_config"
 	. "github.com/cloudfoundry/cli/cf/i18n"
 	"github.com/cloudfoundry/cli/cf/requirements"
 	"github.com/cloudfoundry/cli/cf/terminal"
-	"github.com/codegangsta/cli"
+	"github.com/cloudfoundry/cli/flags"
 )
 
 type ListStacks struct {
@@ -16,31 +16,35 @@ type ListStacks struct {
 	stacksRepo stacks.StackRepository
 }
 
-func NewListStacks(ui terminal.UI, config core_config.Reader, stacksRepo stacks.StackRepository) (cmd ListStacks) {
-	cmd.ui = ui
-	cmd.config = config
-	cmd.stacksRepo = stacksRepo
-	return
+func init() {
+	command_registry.Register(&ListStacks{})
 }
 
-func (cmd ListStacks) Metadata() command_metadata.CommandMetadata {
-	return command_metadata.CommandMetadata{
+func (cmd *ListStacks) MetaData() command_registry.CommandMetadata {
+	return command_registry.CommandMetadata{
 		Name:        "stacks",
 		Description: T("List all stacks (a stack is a pre-built file system, including an operating system, that can run apps)"),
 		Usage:       T("CF_NAME stacks"),
 	}
 }
 
-func (cmd ListStacks) GetRequirements(requirementsFactory requirements.Factory, c *cli.Context) (reqs []requirements.Requirement, err error) {
-	if len(c.Args()) != 0 {
-		cmd.ui.FailWithUsage(c)
+func (cmd *ListStacks) Requirements(requirementsFactory requirements.Factory, fc flags.FlagContext) (reqs []requirements.Requirement, err error) {
+	if len(fc.Args()) != 0 {
+		cmd.ui.Failed(T("Incorrect Usage. Requires app name as argument\n\n") + command_registry.Commands.CommandUsage("stacks"))
 	}
 
 	reqs = append(reqs, requirementsFactory.NewLoginRequirement())
 	return
 }
 
-func (cmd ListStacks) Run(c *cli.Context) {
+func (cmd *ListStacks) SetDependency(deps command_registry.Dependency, pluginCall bool) command_registry.Command {
+	cmd.ui = deps.Ui
+	cmd.config = deps.Config
+	cmd.stacksRepo = deps.RepoLocator.GetStackRepository()
+	return cmd
+}
+
+func (cmd *ListStacks) Execute(c flags.FlagContext) {
 	cmd.ui.Say(T("Getting stacks in org {{.OrganizationName}} / space {{.SpaceName}} as {{.Username}}...",
 		map[string]interface{}{"OrganizationName": terminal.EntityNameColor(cmd.config.OrganizationFields().Name),
 			"SpaceName": terminal.EntityNameColor(cmd.config.SpaceFields().Name),
