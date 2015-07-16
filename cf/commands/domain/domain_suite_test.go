af886ccfb7a6caa064444556b378f926a9d6765f
Simon Leung <leungs@us.ibm.com>
2015-07-16 15:31:37 -0700

    make reference to domain test suite for commands to self registered
    - Godep golang 1.4

--- cf/commands/domain/domain_suite_test.go
+++ cf/commands/domain/domain_suite_test.go
@@ -1,6 +1,7 @@
 package domain_test
 
 import (
+	"github.com/cloudfoundry/cli/cf/commands/domain"
 	"github.com/cloudfoundry/cli/cf/i18n"
 	"github.com/cloudfoundry/cli/cf/i18n/detection"
 	"github.com/cloudfoundry/cli/testhelpers/configuration"
@@ -14,6 +15,9 @@ import (
 func TestDomain(t *testing.T) {
 	config := configuration.NewRepositoryWithDefaults()
 	i18n.T = i18n.Init(config, &detection.JibberJabberDetector{})
+
+	//make a reference to something in cf/commands/domain, so all init() in the directory will run
+	_ = domain.CreateDomain{}
 
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Domain Suite")
