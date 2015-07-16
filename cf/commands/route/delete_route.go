52b8ddd5546827e849d6db5daf61e4fa6318443f
Simon Leung <leungs@us.ibm.com>
2015-07-16 11:57:34 -0700

    convert all check-route, delete-route and delete-orphaned-routes to non-codegangsta [#97061558]
    Signed-off-by: Jonathan Berkhahn <jaberkha@us.ibm.com>

--- cf/commands/route/delete_route.go
+++ cf/commands/route/delete_route.go
@@ -2,14 +2,14 @@ package route
 
 import (
 	"github.com/cloudfoundry/cli/cf/api"
-	"github.com/cloudfoundry/cli/cf/command_metadata"
+	"github.com/cloudfoundry/cli/cf/command_registry"
 	"github.com/cloudfoundry/cli/cf/configuration/core_config"
 	"github.com/cloudfoundry/cli/cf/errors"
-	"github.com/cloudfoundry/cli/cf/flag_helpers"
 	. "github.com/cloudfoundry/cli/cf/i18n"
 	"github.com/cloudfoundry/cli/cf/requirements"
 	"github.com/cloudfoundry/cli/cf/terminal"
-	"github.com/codegangsta/cli"
+	"github.com/cloudfoundry/cli/flags"
+	"github.com/cloudfoundry/cli/flags/flag"
 )
 
 type DeleteRoute struct {
@@ -19,32 +19,29 @@ type DeleteRoute struct {
 	domainReq requirements.DomainRequirement
 }
 
-func NewDeleteRoute(ui terminal.UI, config core_config.Reader, routeRepo api.RouteRepository) (cmd *DeleteRoute) {
-	cmd = new(DeleteRoute)
-	cmd.ui = ui
-	cmd.config = config
-	cmd.routeRepo = routeRepo
-	return
+func init() {
+	command_registry.Register(&DeleteRoute{})
 }
 
-func (cmd *DeleteRoute) Metadata() command_metadata.CommandMetadata {
-	return command_metadata.CommandMetadata{
+func (cmd *DeleteRoute) MetaData() command_registry.CommandMetadata {
+	fs := make(map[string]flags.FlagSet)
+	fs["f"] = &cliFlags.BoolFlag{Name: "f", Usage: T("Force deletion without confirmation")}
+	fs["n"] = &cliFlags.StringFlag{Name: "n", Usage: T("Hostname")}
+
+	return command_registry.CommandMetadata{
 		Name:        "delete-route",
 		Description: T("Delete a route"),
 		Usage:       T("CF_NAME delete-route DOMAIN [-n HOSTNAME] [-f]"),
-		Flags: []cli.Flag{
-			cli.BoolFlag{Name: "f", Usage: T("Force deletion without confirmation")},
-			flag_helpers.NewStringFlag("n", T("Hostname")),
-		},
+		Flags:       fs,
 	}
 }
 
-func (cmd *DeleteRoute) GetRequirements(requirementsFactory requirements.Factory, c *cli.Context) (reqs []requirements.Requirement, err error) {
-	if len(c.Args()) != 1 {
-		cmd.ui.FailWithUsage(c)
+func (cmd *DeleteRoute) Requirements(requirementsFactory requirements.Factory, fc flags.FlagContext) (reqs []requirements.Requirement, err error) {
+	if len(fc.Args()) != 1 {
+		cmd.ui.Failed(T("Incorrect Usage. Requires an argument\n\n") + command_registry.Commands.CommandUsage("delete-route"))
 	}
 
-	cmd.domainReq = requirementsFactory.NewDomainRequirement(c.Args()[0])
+	cmd.domainReq = requirementsFactory.NewDomainRequirement(fc.Args()[0])
 
 	reqs = []requirements.Requirement{
 		requirementsFactory.NewLoginRequirement(),
@@ -53,7 +50,14 @@ func (cmd *DeleteRoute) GetRequirements(requirementsFactory requirements.Factory
 	return
 }
 
-func (cmd *DeleteRoute) Run(c *cli.Context) {
+func (cmd *DeleteRoute) SetDependency(deps command_registry.Dependency, pluginCall bool) command_registry.Command {
+	cmd.ui = deps.Ui
+	cmd.config = deps.Config
+	cmd.routeRepo = deps.RepoLocator.GetRouteRepository()
+	return cmd
+}
+
+func (cmd *DeleteRoute) Execute(c flags.FlagContext) {
 	host := c.String("n")
 	domainName := c.Args()[0]
 
