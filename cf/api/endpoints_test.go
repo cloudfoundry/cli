197927af8a71f4fea645ddacc13c195b5967dd89
Atul Kshirsagar <atul.kshirsagar@gmail.com>
2015-09-30 17:53:33 -0700

    Add cf router-groups command
    [#100975070]
    
    Signed-off-by: Fermin Ordaz <fermin.ordaz@gmail.com>

--- cf/api/endpoints_test.go
+++ cf/api/endpoints_test.go
@@ -34,6 +34,7 @@ func validApiInfoEndpoint(w http.ResponseWriter, r *http.Request) {
   "description": "Cloud Foundry sponsored by Pivotal",
   "authorization_endpoint": "https://login.example.com",
   "logging_endpoint": "wss://loggregator.foo.example.org:4443",
+  "routing_endpoint": "http://api.example.com/routing",
   "api_version": "42.0.0",
 	"min_cli_version": "6.5.0",
 	"min_recommended_cli_version": "6.7.0"
@@ -51,6 +52,7 @@ func apiInfoEndpointWithoutLogURL(w http.ResponseWriter, r *http.Request) {
   "name": "vcap",
   "build": "2222",
   "support": "http://support.cloudfoundry.com",
+  "routing_endpoint": "http://api.example.com/routing",
   "version": 2,
   "description": "Cloud Foundry sponsored by Pivotal",
   "authorization_endpoint": "https://login.example.com",
@@ -112,6 +114,7 @@ var _ = Describe("Endpoints Repository", func() {
 				Expect(config.HasSpace()).To(BeFalse())
 				Expect(config.MinCliVersion()).To(Equal("6.5.0"))
 				Expect(config.MinRecommendedCliVersion()).To(Equal("6.7.0"))
+				Expect(config.RoutingApiEndpoint()).To(Equal("http://api.example.com/routing"))
 			})
 
 			Context("when the api endpoint does not change", func() {
---
+++ cf/api/routing_api_test.go
@@ -0,0 +1,118 @@
+package api_test
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/cloudfoundry/cli/cf/api"
+	"github.com/cloudfoundry/cli/cf/configuration/core_config"
+	"github.com/cloudfoundry/cli/cf/models"
+	"github.com/cloudfoundry/cli/cf/net"
+	testconfig "github.com/cloudfoundry/cli/testhelpers/configuration"
+	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/ghttp"
+)
+
+var _ = Describe("RoutingApi", func() {
+
+	var (
+		repo             api.RoutingApiRepository
+		configRepo       core_config.Repository
+		routingApiServer *ghttp.Server
+	)
+
+	BeforeEach(func() {
+		configRepo = testconfig.NewRepositoryWithDefaults()
+		gateway := net.NewCloudControllerGateway(configRepo, time.Now, &testterm.FakeUI{})
+
+		repo = api.NewRoutingApiRepository(configRepo, gateway)
+	})
+
+	AfterEach(func() {
+		routingApiServer.Close()
+	})
+
+	Describe("ListRouterGroups", func() {
+
+		Context("when routing api return router groups", func() {
+			BeforeEach(func() {
+				routingApiServer = ghttp.NewServer()
+				routingApiServer.RouteToHandler("GET", "/v1/router_groups",
+					func(w http.ResponseWriter, req *http.Request) {
+						responseBody := []byte(`[
+			{
+				  "guid": "bad25cff-9332-48a6-8603-b619858e7992",
+					"name": "default-tcp",
+					"type": "tcp"
+			}]`)
+						w.Header().Set("Content-Length", strconv.Itoa(len(responseBody)))
+						w.Header().Set("Content-Type", "application/json")
+						w.WriteHeader(http.StatusOK)
+						w.Write(responseBody)
+					})
+				configRepo.SetRoutingApiEndpoint(routingApiServer.URL())
+			})
+
+			It("lists routing groups", func() {
+				cb := func(grp models.RouterGroup) bool {
+					Expect(grp).To(Equal(models.RouterGroup{
+						Guid: "bad25cff-9332-48a6-8603-b619858e7992",
+						Name: "default-tcp",
+						Type: "tcp",
+					}))
+					return true
+				}
+				err := repo.ListRouterGroups(cb)
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("when routing api returns an empty response ", func() {
+			BeforeEach(func() {
+				routingApiServer = ghttp.NewServer()
+				routingApiServer.RouteToHandler("GET", "/v1/router_groups",
+					func(w http.ResponseWriter, req *http.Request) {
+						responseBody := []byte("[]")
+						w.Header().Set("Content-Length", strconv.Itoa(len(responseBody)))
+						w.Header().Set("Content-Type", "application/json")
+						w.WriteHeader(http.StatusOK)
+						w.Write(responseBody)
+					})
+				configRepo.SetRoutingApiEndpoint(routingApiServer.URL())
+			})
+
+			It("doesn't list any router groups", func() {
+				cb := func(grp models.RouterGroup) bool {
+					Fail(fmt.Sprintf("Not expected to receive callback for router group:%#v", grp))
+					return false
+				}
+				err := repo.ListRouterGroups(cb)
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("when routing api returns an error ", func() {
+			BeforeEach(func() {
+				routingApiServer = ghttp.NewServer()
+				routingApiServer.RouteToHandler("GET", "/v1/router_groups",
+					func(w http.ResponseWriter, req *http.Request) {
+						w.WriteHeader(http.StatusInternalServerError)
+					})
+				configRepo.SetRoutingApiEndpoint(routingApiServer.URL())
+			})
+
+			It("doesn't list any router groups", func() {
+				cb := func(grp models.RouterGroup) bool {
+					Fail(fmt.Sprintf("Not expected to receive callback for router group:%#v", grp))
+					return false
+				}
+				err := repo.ListRouterGroups(cb)
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+})
