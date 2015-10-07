16a79fd5dcbe121dd6282e477867d2e9927090f8
Andrew Bruce <me@andrewbruce.net>
2015-10-07 15:10:06 +0100

    Split org-users into separate printers
    [#63224944]

--- cf/actors/user_printer/user_printer.go
+++ cf/actors/user_printer/user_printer.go
@@ -29,6 +29,74 @@ type SpaceUsersUiPrinter struct {
 	RoleDisplayNames map[string]string
 }
 
+type OrgUsersPluginPrinter struct {
+	UserPrinter
+	UsersMap    map[string]plugin_models.GetOrgUsers_Model
+	Roles       []string
+	UserLister  func(orgGuid string, role string) ([]models.UserFields, error)
+	PluginModel *[]plugin_models.GetOrgUsers_Model
+}
+
+type OrgUsersUiPrinter struct {
+	UserPrinter
+	Roles            []string
+	RoleDisplayNames map[string]string
+	UserLister       func(orgGuid string, role string) ([]models.UserFields, error)
+	Ui               terminal.UI
+}
+
+func (p *OrgUsersPluginPrinter) PrintUsers(org models.Organization, _ models.Space, username string) {
+	for _, role := range p.Roles {
+		users, _ := p.UserLister(org.Guid, role)
+		for _, user := range users {
+			u, found := p.UsersMap[user.Username]
+			if found {
+				u.Roles = append(u.Roles, role)
+				p.UsersMap[user.Username] = u
+			} else {
+				u = plugin_models.GetOrgUsers_Model{}
+				u.Username = user.Username
+				u.Guid = user.Guid
+				u.IsAdmin = user.IsAdmin
+				u.Roles = make([]string, 1)
+				u.Roles[0] = role
+				p.UsersMap[user.Username] = u
+			}
+		}
+	}
+	for _, v := range p.UsersMap {
+		*(p.PluginModel) = append(*(p.PluginModel), v)
+	}
+}
+
+func (p *OrgUsersUiPrinter) PrintUsers(org models.Organization, _ models.Space, username string) {
+	for _, role := range p.Roles {
+		displayName := p.RoleDisplayNames[role]
+		users, apiErr := p.UserLister(org.Guid, role)
+
+		p.Ui.Say("")
+		p.Ui.Say("%s", terminal.HeaderColor(displayName))
+
+		if len(users) == 0 {
+			p.Ui.Say(fmt.Sprintf("  "+T("No %s found"), displayName))
+			continue
+		}
+
+		for _, user := range users {
+			p.Ui.Say("  %s", user.Username)
+		}
+
+		if apiErr != nil {
+			p.Ui.Failed(T("Failed fetching org-users for role {{.OrgRoleToDisplayName}}.\n{{.Error}}",
+				map[string]interface{}{
+					"Error":                apiErr.Error(),
+					"OrgRoleToDisplayName": displayName,
+				}))
+			return
+		}
+	}
+}
+
 func (p *SpaceUsersPluginPrinter) PrintUsers(_ models.Organization, space models.Space, _ string) {
 	for _, role := range p.Roles {
 		users, _ := p.UserLister(space.Guid, role)
