c4d1490278f28aeaebb6f6f5d7ab5d2b33c0c245
Kris Hicks <khicks@pivotal.io>
2015-11-12 09:48:46 -0800

    Update config_data_test
    * Don't dot-import core_config
    * Fix test pollution between marshal/unmarshal tests
    * Add missing tests when unmarshal JSON is not ConfigVersion 3

--- cf/configuration/core_config/config_data_test.go
+++ cf/configuration/core_config/config_data_test.go
@@ -3,121 +3,207 @@ package core_config_test
 import (
 	"regexp"
 
-	. "github.com/cloudfoundry/cli/cf/configuration/core_config"
+	"github.com/cloudfoundry/cli/cf/configuration/core_config"
 	"github.com/cloudfoundry/cli/cf/models"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var exampleJSON = `
-{
-	"ConfigVersion": 3,
-	"Target": "api.example.com",
-	"ApiVersion": "3",
-	"AuthorizationEndpoint": "auth.example.com",
-	"LoggregatorEndPoint": "loggregator.example.com",
-	"DopplerEndPoint": "doppler.example.com",
-	"UaaEndpoint": "uaa.example.com",
-	"RoutingApiEndpoint": "routing-api.example.com",
-	"AccessToken": "the-access-token",
-	"SSHOAuthClient": "ssh-oauth-client-id",
-	"RefreshToken": "the-refresh-token",
-	"OrganizationFields": {
-		"Guid": "the-org-guid",
-		"Name": "the-org",
-		"QuotaDefinition": {
-			"name":"",
-			"memory_limit":0,
-			"instance_memory_limit":0,
-			"total_routes":0,
-			"total_services":0,
-			"non_basic_services_allowed": false
-		}
-	},
-	"SpaceFields": {
-		"Guid": "the-space-guid",
-		"Name": "the-space",
-		"AllowSSH": false
-	},
-	"SSLDisabled": true,
-	"AsyncTimeout": 1000,
-	"Trace": "path/to/some/file",
-	"ColorEnabled": "true",
-	"Locale": "fr_FR",
-	"PluginRepos": [
+var _ = Describe("V3 Config files", func() {
+	var exampleV3JSON = `
+	{
+		"ConfigVersion": 3,
+		"Target": "api.example.com",
+		"ApiVersion": "3",
+		"AuthorizationEndpoint": "auth.example.com",
+		"LoggregatorEndPoint": "loggregator.example.com",
+		"DopplerEndPoint": "doppler.example.com",
+		"UaaEndpoint": "uaa.example.com",
+		"RoutingApiEndpoint": "routing-api.example.com",
+		"AccessToken": "the-access-token",
+		"SSHOAuthClient": "ssh-oauth-client-id",
+		"RefreshToken": "the-refresh-token",
+		"OrganizationFields": {
+			"Guid": "the-org-guid",
+			"Name": "the-org",
+			"QuotaDefinition": {
+				"name":"",
+				"memory_limit":0,
+				"instance_memory_limit":0,
+				"total_routes":0,
+				"total_services":0,
+				"non_basic_services_allowed": false
+			}
+		},
+		"SpaceFields": {
+			"Guid": "the-space-guid",
+			"Name": "the-space",
+			"AllowSSH": false
+		},
+		"SSLDisabled": true,
+		"AsyncTimeout": 1000,
+		"Trace": "path/to/some/file",
+		"ColorEnabled": "true",
+		"Locale": "fr_FR",
+		"PluginRepos": [
 		{
 			"Name": "repo1",
 			"Url": "http://repo.com"
 		}
-	],
-	"MinCliVersion": "6.0.0",
-	"MinRecommendedCliVersion": "6.9.0"
-}`
+		],
+		"MinCliVersion": "6.0.0",
+		"MinRecommendedCliVersion": "6.9.0"
+	}`
 
-var exampleData = &Data{
-	Target:                   "api.example.com",
-	ApiVersion:               "3",
-	AuthorizationEndpoint:    "auth.example.com",
-	LoggregatorEndPoint:      "loggregator.example.com",
-	RoutingApiEndpoint:       "routing-api.example.com",
-	DopplerEndPoint:          "doppler.example.com",
-	UaaEndpoint:              "uaa.example.com",
-	AccessToken:              "the-access-token",
-	RefreshToken:             "the-refresh-token",
-	SSHOAuthClient:           "ssh-oauth-client-id",
-	MinCliVersion:            "6.0.0",
-	MinRecommendedCliVersion: "6.9.0",
-	OrganizationFields: models.OrganizationFields{
-		Guid: "the-org-guid",
-		Name: "the-org",
-	},
-	SpaceFields: models.SpaceFields{
-		Guid: "the-space-guid",
-		Name: "the-space",
-	},
-	SSLDisabled:  true,
-	Trace:        "path/to/some/file",
-	AsyncTimeout: 1000,
-	ColorEnabled: "true",
-	Locale:       "fr_FR",
-	PluginRepos: []models.PluginRepo{
-		models.PluginRepo{
-			Name: "repo1",
-			Url:  "http://repo.com",
+	// V2 by virtue of ConfigVersion only
+	var exampleV2JSON = `
+	{
+		"ConfigVersion": 2,
+		"Target": "api.example.com",
+		"ApiVersion": "3",
+		"AuthorizationEndpoint": "auth.example.com",
+		"LoggregatorEndPoint": "loggregator.example.com",
+		"DopplerEndPoint": "doppler.example.com",
+		"UaaEndpoint": "uaa.example.com",
+		"RoutingApiEndpoint": "routing-api.example.com",
+		"AccessToken": "the-access-token",
+		"SSHOAuthClient": "ssh-oauth-client-id",
+		"RefreshToken": "the-refresh-token",
+		"OrganizationFields": {
+			"Guid": "the-org-guid",
+			"Name": "the-org",
+			"QuotaDefinition": {
+				"name":"",
+				"memory_limit":0,
+				"instance_memory_limit":0,
+				"total_routes":0,
+				"total_services":0,
+				"non_basic_services_allowed": false
+			}
 		},
-	},
-}
+		"SpaceFields": {
+			"Guid": "the-space-guid",
+			"Name": "the-space",
+			"AllowSSH": false
+		},
+		"SSLDisabled": true,
+		"AsyncTimeout": 1000,
+		"Trace": "path/to/some/file",
+		"ColorEnabled": "true",
+		"Locale": "fr_FR",
+		"PluginRepos": [
+		{
+			"Name": "repo1",
+			"Url": "http://repo.com"
+		}
+		],
+		"MinCliVersion": "6.0.0",
+		"MinRecommendedCliVersion": "6.9.0"
+	}`
 
-var _ = Describe("V3 Config files", func() {
-	Describe("serialization", func() {
+	Describe("JsonMarshalV3", func() {
 		It("creates a JSON string from the config object", func() {
-			jsonData, err := exampleData.JsonMarshalV3()
+			data := &core_config.Data{
+				Target:                   "api.example.com",
+				ApiVersion:               "3",
+				AuthorizationEndpoint:    "auth.example.com",
+				LoggregatorEndPoint:      "loggregator.example.com",
+				RoutingApiEndpoint:       "routing-api.example.com",
+				DopplerEndPoint:          "doppler.example.com",
+				UaaEndpoint:              "uaa.example.com",
+				AccessToken:              "the-access-token",
+				RefreshToken:             "the-refresh-token",
+				SSHOAuthClient:           "ssh-oauth-client-id",
+				MinCliVersion:            "6.0.0",
+				MinRecommendedCliVersion: "6.9.0",
+				OrganizationFields: models.OrganizationFields{
+					Guid: "the-org-guid",
+					Name: "the-org",
+				},
+				SpaceFields: models.SpaceFields{
+					Guid: "the-space-guid",
+					Name: "the-space",
+				},
+				SSLDisabled:  true,
+				Trace:        "path/to/some/file",
+				AsyncTimeout: 1000,
+				ColorEnabled: "true",
+				Locale:       "fr_FR",
+				PluginRepos: []models.PluginRepo{
+					models.PluginRepo{
+						Name: "repo1",
+						Url:  "http://repo.com",
+					},
+				},
+			}
 
+			jsonData, err := data.JsonMarshalV3()
 			Expect(err).NotTo(HaveOccurred())
-			Expect(stripWhitespace(string(jsonData))).To(ContainSubstring(stripWhitespace(exampleJSON)))
+
+			re := regexp.MustCompile(`\s+`)
+			actual := re.ReplaceAll(jsonData, []byte{})
+			expected := re.ReplaceAll([]byte(exampleV3JSON), []byte{})
+			Expect(actual).To(Equal(expected))
 		})
 	})
 
-	Describe("parsing", func() {
+	Describe("JsonUnmarshalV3", func() {
 		It("returns an error when the JSON is invalid", func() {
-			configData := NewData()
+			configData := core_config.NewData()
 			err := configData.JsonUnmarshalV3([]byte(`{ "not_valid": ### }`))
-
 			Expect(err).To(HaveOccurred())
 		})
 
-		It("creates a config object from valid JSON", func() {
-			configData := NewData()
-			err := configData.JsonUnmarshalV3([]byte(exampleJSON))
+		It("creates a config object from valid V3 JSON", func() {
+			expectedData := &core_config.Data{
+				ConfigVersion:            3,
+				Target:                   "api.example.com",
+				ApiVersion:               "3",
+				AuthorizationEndpoint:    "auth.example.com",
+				LoggregatorEndPoint:      "loggregator.example.com",
+				RoutingApiEndpoint:       "routing-api.example.com",
+				DopplerEndPoint:          "doppler.example.com",
+				UaaEndpoint:              "uaa.example.com",
+				AccessToken:              "the-access-token",
+				RefreshToken:             "the-refresh-token",
+				SSHOAuthClient:           "ssh-oauth-client-id",
+				MinCliVersion:            "6.0.0",
+				MinRecommendedCliVersion: "6.9.0",
+				OrganizationFields: models.OrganizationFields{
+					Guid: "the-org-guid",
+					Name: "the-org",
+				},
+				SpaceFields: models.SpaceFields{
+					Guid: "the-space-guid",
+					Name: "the-space",
+				},
+				SSLDisabled:  true,
+				Trace:        "path/to/some/file",
+				AsyncTimeout: 1000,
+				ColorEnabled: "true",
+				Locale:       "fr_FR",
+				PluginRepos: []models.PluginRepo{
+					models.PluginRepo{
+						Name: "repo1",
+						Url:  "http://repo.com",
+					},
+				},
+			}
 
+			actualData := core_config.NewData()
+			err := actualData.JsonUnmarshalV3([]byte(exampleV3JSON))
 			Expect(err).NotTo(HaveOccurred())
-			Expect(configData).To(Equal(exampleData))
+
+			Expect(actualData).To(Equal(expectedData))
+		})
+
+		It("returns an empty Data object for non-V3 JSON", func() {
+			actualData := core_config.NewData()
+			err := actualData.JsonUnmarshalV3([]byte(exampleV2JSON))
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(*actualData).To(Equal(core_config.Data{}))
 		})
 	})
 })
-
-var whiteSpaceRegex = regexp.MustCompile(`\s+`)
-
-func stripWhitespace(input string) string {
-	return whiteSpaceRegex.ReplaceAllString(input, "")
-}
