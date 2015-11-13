7566b087c5a26873f0056ad8512637f14f56fa24
Kris Hicks <khicks@pivotal.io>
2015-11-13 08:48:36 -0800

    Revert "Update date format for windows build"
    This reverts commit 663ef3bb1c3db7f18e8eb4ca471045328291d54e.

--- bin/generate-version-and-built-at.ps1
+++ bin/generate-version-and-built-at.ps1
@@ -4,7 +4,7 @@ $BUILT_AT_FILE = $(split-path $MyInvocation.MyCommand.Definition) + "\..\BUILT_A
 $CURRENT_SHA = $(git rev-parse --short HEAD)
 $CURRENT_VERSION = get-content VERSION
 $VERSION_STRING = $CURRENT_VERSION + "+" + $CURRENT_SHA
-$DATE = Get-Date -uformat "%Y%m%dT%H%M"
+$DATE = Get-Date -uformat "%Y-%m-%dT%H:%M:%S+00:00"
 
 $VERSION_STRING | Out-File -Encoding "UTF8" $VERSION_FILE
 $DATE | Out-File -Encoding "UTF8" $BUILT_AT_FILE
