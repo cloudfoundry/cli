40e11d060be6b1d71d51e63464eabc2be890be32
Simon Leung <leungs@us.ibm.com>
2015-11-20 11:08:18 -0800

    Fix reliance on test pollution in registry_test
    [#108632962]
    
    Signed-off-by: Kris Hicks <khicks@pivotal.io>

--- cf/command_registry/registry_test.go
+++ cf/command_registry/registry_test.go
@@ -3,9 +3,10 @@ package command_registry_test
 import (
 	"strings"
 
+	"github.com/cloudfoundry/cli/cf/command_registry"
+
 	. "github.com/cloudfoundry/cli/cf/command_registry/fake_command"
 
-	. "github.com/cloudfoundry/cli/cf/command_registry"
 	. "github.com/cloudfoundry/cli/testhelpers/matchers"
 
 	. "github.com/cloudfoundry/cli/cf/i18n"
@@ -14,148 +15,212 @@ import (
 )
 
 var _ = Describe("CommandRegistry", func() {
-
 	BeforeEach(func() {
-		Register(FakeCommand1{})
+		command_registry.Commands = command_registry.NewRegistry() // because other tests load all the commands into the registry
 	})
 
 	Context("i18n", func() {
 		It("initialize i18n T() func", func() {
-			Ω(T).ToNot(BeNil())
+			Expect(T).ToNot(BeNil())
 		})
 	})
 
-	Context("Register()", func() {
+	Describe("Register()", func() {
+		AfterEach(func() {
+			command_registry.Commands.RemoveCommand("fake-command2")
+		})
+
 		It("registers a command and its alias into the Command Registry map", func() {
-			Ω(Commands.CommandExists("fake-command2")).To(BeFalse())
-			Ω(Commands.CommandExists("fc2")).To(BeFalse())
+			Expect(command_registry.Commands.CommandExists("fake-command2")).To(BeFalse())
+			Expect(command_registry.Commands.CommandExists("fc2")).To(BeFalse())
 
-			Register(FakeCommand2{})
+			command_registry.Register(FakeCommand2{})
 
-			Ω(Commands.CommandExists("fake-command2")).To(BeTrue())
-			Ω(Commands.CommandExists("fc2")).To(BeTrue())
+			Expect(command_registry.Commands.CommandExists("fake-command2")).To(BeTrue())
+			Expect(command_registry.Commands.CommandExists("fc2")).To(BeTrue())
 		})
 	})
 
-	Describe("Commands", func() {
-		Context("CommandExists()", func() {
-			It("returns true the command exists in the list", func() {
-				Ω(Commands.CommandExists("fake-command")).To(BeTrue())
+	Describe("CommandExists()", func() {
+		Context("when the command has been registered", func() {
+			BeforeEach(func() {
+				command_registry.Register(FakeCommand1{})
 			})
 
-			It("returns false if the command doesn't exists in the list", func() {
-				Ω(Commands.CommandExists("non-exist-cmd")).To(BeFalse())
+			AfterEach(func() {
+				command_registry.Commands.RemoveCommand("fake-command")
+			})
+
+			It("returns true the command exists in the list", func() {
+				Expect(command_registry.Commands.CommandExists("fake-command")).To(BeTrue())
 			})
 
 			It("returns true if the alias exists", func() {
-				Ω(Commands.CommandExists("fc1")).To(BeTrue())
-			})
-
-			It("returns false if the command name is an empty string", func() {
-				Ω(Commands.CommandExists("")).To(BeFalse())
+				Expect(command_registry.Commands.CommandExists("fc1")).To(BeTrue())
 			})
 		})
 
-		Context("FindCommand()", func() {
-			It("returns the command interface when found", func() {
-				cmd := Commands.FindCommand("fake-command")
-				Ω(cmd.MetaData().Usage).To(ContainSubstring("Usage of fake-command"))
-				Ω(cmd.MetaData().Description).To(Equal("Description for fake-command"))
-			})
-
-			It("returns the command interface if an command alias is provided", func() {
-				cmd := Commands.FindCommand("fc1")
-				Ω(cmd.MetaData().Usage).To(ContainSubstring("Usage of fake-command"))
-				Ω(cmd.MetaData().Description).To(Equal("Description for fake-command"))
-			})
+		It("returns false when the command has not been registered", func() {
+			Expect(command_registry.Commands.CommandExists("non-exist-cmd")).To(BeFalse())
 		})
 
-		Context("SetCommand()", func() {
-			It("replaces the command in registry with command provided", func() {
-				updatedCmd := FakeCommand1{Data: "This is new data"}
-				oldCmd := Commands.FindCommand("fake-command")
-				Ω(oldCmd).ToNot(Equal(updatedCmd))
-
-				Commands.SetCommand(updatedCmd)
-				oldCmd = Commands.FindCommand("fake-command")
-				Ω(oldCmd).To(Equal(updatedCmd))
-			})
-		})
-
-		Context("RemoveCommand()", func() {
-			It("removes the command in registry with command name provided", func() {
-				Commands = NewRegistry()
-
-				Register(FakeCommand1{})
-				Register(FakeCommand2{})
-				Register(FakeCommand3{})
-
-				Ω(Commands.TotalCommands()).To(Equal(3))
-			})
-		})
-
-		Context("Metadatas()", func() {
-			It("returns all the command's metadata", func() {
-				Commands = NewRegistry()
-
-				Register(FakeCommand1{})
-				Register(FakeCommand2{})
-				Register(FakeCommand3{})
-
-				Ω(len(Commands.Metadatas())).To(Equal(3))
-			})
-		})
-
-		Context("TotalCommands()", func() {
-			It("returns the total number of commands in the registry", func() {
-				Ω(Commands.CommandExists("fake-command")).To(BeTrue())
-
-				Commands.RemoveCommand("fake-command")
-				Ω(Commands.CommandExists("fake-command")).To(BeFalse())
-			})
-		})
-
-		Context("MaxCommandNameLength()", func() {
-			It("returns the length of the longest command name", func() {
-				maxLen := Commands.MaxCommandNameLength()
-				Ω(maxLen).To(Equal(len("this-is-a-really-long-command-name-123123123123123123123")))
-			})
-		})
-
-		Context("CommandUsage()", func() {
-			It("prints the name, description and usage of a command", func() {
-				o := Commands.CommandUsage("fake-command")
-				outputs := strings.Split(o, "\n")
-				Ω(outputs).To(BeInDisplayOrder(
-					[]string{"NAME:"},
-					[]string{"   fake-command", "Description"},
-					[]string{"USAGE:"},
-				))
-			})
-
-			It("prints the flag options", func() {
-				o := Commands.CommandUsage("fake-command")
-				outputs := strings.Split(o, "\n")
-				Ω(outputs).To(BeInDisplayOrder(
-					[]string{"NAME:"},
-					[]string{"USAGE:"},
-					[]string{"OPTIONS:"},
-					[]string{"intFlag", "Usage for"},
-					[]string{"boolFlag", "Usage for"},
-				))
-			})
-
-			It("replaces 'CF_NAME' with executable name from os.Arg[0]", func() {
-				o := Commands.CommandUsage("fake-command")
-				outputs := strings.Split(o, "\n")
-				Ω(outputs).To(BeInDisplayOrder(
-					[]string{"USAGE:"},
-					[]string{"command_registry.test", "Usage of"},
-				))
-				Consistently(outputs).ShouldNot(ContainSubstrings([]string{"CF_NAME"}))
-			})
-
+		It("returns false if the command name is an empty string", func() {
+			Expect(command_registry.Commands.CommandExists("")).To(BeFalse())
 		})
 	})
 
+	Describe("FindCommand()", func() {
+		Context("when the command has been registered", func() {
+			BeforeEach(func() {
+				command_registry.Register(FakeCommand1{})
+			})
+
+			AfterEach(func() {
+				command_registry.Commands.RemoveCommand("fake-command")
+			})
+
+			It("returns the command when the command's name is given", func() {
+				cmd := command_registry.Commands.FindCommand("fake-command")
+				Expect(cmd.MetaData().Usage).To(ContainSubstring("Usage of fake-command"))
+				Expect(cmd.MetaData().Description).To(Equal("Description for fake-command"))
+			})
+
+			It("returns the command when the command's alias is given", func() {
+				cmd := command_registry.Commands.FindCommand("fc1")
+				Expect(cmd.MetaData().Usage).To(ContainSubstring("Usage of fake-command"))
+				Expect(cmd.MetaData().Description).To(Equal("Description for fake-command"))
+			})
+		})
+
+		It("returns nil when the command has not been registered", func() {
+			cmd := command_registry.Commands.FindCommand("fake-command")
+			Expect(cmd).To(BeNil())
+		})
+	})
+
+	Describe("SetCommand()", func() {
+		It("replaces the command in registry with command provided", func() {
+			updatedCmd := FakeCommand1{Data: "This is new data"}
+			oldCmd := command_registry.Commands.FindCommand("fake-command")
+			Expect(oldCmd).ToNot(Equal(updatedCmd))
+
+			command_registry.Commands.SetCommand(updatedCmd)
+			oldCmd = command_registry.Commands.FindCommand("fake-command")
+			Expect(oldCmd).To(Equal(updatedCmd))
+		})
+	})
+
+	Describe("Commands()", func() {
+		Context("when there are commands registered", func() {
+			BeforeEach(func() {
+				command_registry.Register(FakeCommand1{})
+				command_registry.Register(FakeCommand2{})
+				command_registry.Register(FakeCommand3{})
+			})
+
+			AfterEach(func() {
+				command_registry.Commands.RemoveCommand("fake-command1")
+				command_registry.Commands.RemoveCommand("fake-command2")
+				command_registry.Commands.RemoveCommand("this-is-a-really-long-command-name-123123123123123123123") // fake-command3
+			})
+
+			It("returns the total number of registered commands", func() {
+				Expect(command_registry.Commands.TotalCommands()).To(Equal(3))
+			})
+		})
+
+		It("returns 0 when there are no commands registered", func() {
+			Expect(command_registry.Commands.TotalCommands()).To(Equal(0))
+		})
+	})
+
+	Describe("Metadatas()", func() {
+		BeforeEach(func() {
+			command_registry.Register(FakeCommand1{})
+			command_registry.Register(FakeCommand2{})
+			command_registry.Register(FakeCommand3{})
+		})
+
+		AfterEach(func() {
+			command_registry.Commands.RemoveCommand("fake-command")
+			command_registry.Commands.RemoveCommand("fake-command2")
+			command_registry.Commands.RemoveCommand("this-is-a-really-long-command-name-123123123123123123123") // fake-command3
+		})
+
+		It("returns all the metadata for all registered commands", func() {
+			Expect(len(command_registry.Commands.Metadatas())).To(Equal(3))
+		})
+	})
+
+	Describe("RemoveCommand()", func() {
+		BeforeEach(func() {
+			command_registry.Register(FakeCommand1{})
+		})
+
+		It("removes the command in registry with command name provided", func() {
+			command_registry.Commands.RemoveCommand("fake-command")
+			Expect(command_registry.Commands.CommandExists("fake-command")).To(BeFalse())
+		})
+	})
+
+	Describe("MaxCommandNameLength()", func() {
+		BeforeEach(func() {
+			command_registry.Register(FakeCommand1{})
+			command_registry.Register(FakeCommand2{})
+			command_registry.Register(FakeCommand3{})
+		})
+
+		AfterEach(func() {
+			command_registry.Commands.RemoveCommand("fake-command")
+			command_registry.Commands.RemoveCommand("fake-command2")
+			command_registry.Commands.RemoveCommand("this-is-a-really-long-command-name-123123123123123123123") // fake-command3
+		})
+
+		It("returns the length of the longest command name", func() {
+			maxLen := command_registry.Commands.MaxCommandNameLength()
+			Expect(maxLen).To(Equal(len("this-is-a-really-long-command-name-123123123123123123123")))
+		})
+	})
+
+	Describe("CommandUsage()", func() {
+		BeforeEach(func() {
+			command_registry.Register(FakeCommand1{})
+		})
+
+		AfterEach(func() {
+			command_registry.Commands.RemoveCommand("fake-command")
+		})
+
+		It("prints the name, description and usage of a command", func() {
+			o := command_registry.Commands.CommandUsage("fake-command")
+			outputs := strings.Split(o, "\n")
+			Expect(outputs).To(BeInDisplayOrder(
+				[]string{"NAME:"},
+				[]string{"   fake-command", "Description"},
+				[]string{"USAGE:"},
+			))
+		})
+
+		It("prints the flag options", func() {
+			o := command_registry.Commands.CommandUsage("fake-command")
+			outputs := strings.Split(o, "\n")
+			Expect(outputs).To(BeInDisplayOrder(
+				[]string{"NAME:"},
+				[]string{"USAGE:"},
+				[]string{"OPTIONS:"},
+				[]string{"intFlag", "Usage for"},
+				[]string{"boolFlag", "Usage for"},
+			))
+		})
+
+		It("replaces 'CF_NAME' with executable name from os.Arg[0]", func() {
+			o := command_registry.Commands.CommandUsage("fake-command")
+			outputs := strings.Split(o, "\n")
+			Expect(outputs).To(BeInDisplayOrder(
+				[]string{"USAGE:"},
+				[]string{"command_registry.test", "Usage of"},
+			))
+			Consistently(outputs).ShouldNot(ContainSubstrings([]string{"CF_NAME"}))
+		})
+	})
 })
