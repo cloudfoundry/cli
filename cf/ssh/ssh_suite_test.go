b12e5edfe113215cb2ed4e26a96ea0154f5c2508
Kris Hicks <khicks@pivotal.io>
2015-12-02 09:57:05 -0800

    Remove SynchronizedBeforeSuite from ssh suite
    Instead of generating a 1024-bit key per test run, use a fixture.

--- cf/ssh/ssh_suite_test.go
+++ cf/ssh/ssh_suite_test.go
@@ -1,10 +1,10 @@
 package sshCmd_test
 
 import (
-	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
 
 	"github.com/cloudfoundry-incubator/diego-ssh/helpers"
-	"github.com/cloudfoundry-incubator/diego-ssh/keys"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"golang.org/x/crypto/ssh"
@@ -23,37 +23,18 @@ func TestCmd(t *testing.T) {
 	RunSpecs(t, "Cmd Suite")
 }
 
-var _ = SynchronizedBeforeSuite(func() []byte {
-	hostKey, err := keys.RSAKeyPairFactory.NewKeyPair(1024)
+var _ = BeforeSuite(func() {
+	hostKeyBytes, err := ioutil.ReadFile(filepath.Join("..", "..", "fixtures", "host-key"))
+	Expect(err).NotTo(HaveOccurred())
+	hostKey, err := ssh.ParsePrivateKey(hostKeyBytes)
 	Expect(err).NotTo(HaveOccurred())
 
-	privateKey, err := keys.RSAKeyPairFactory.NewKeyPair(1024)
+	privateKeyBytes, err := ioutil.ReadFile(filepath.Join("..", "..", "fixtures", "private-key"))
+	Expect(err).NotTo(HaveOccurred())
+	privateKey, err := ssh.ParsePrivateKey(privateKeyBytes)
 	Expect(err).NotTo(HaveOccurred())
 
-	payload, err := json.Marshal(map[string]string{
-		"host-key":    hostKey.PEMEncodedPrivateKey(),
-		"private-key": privateKey.PEMEncodedPrivateKey(),
-	})
-
-	Expect(err).NotTo(HaveOccurred())
-
-	return payload
-
-}, func(payload []byte) {
-	context := map[string]string{}
-
-	err := json.Unmarshal(payload, &context)
-	Expect(err).NotTo(HaveOccurred())
-
-	hostKeyPem := context["host-key"]
-	hostKey, err := ssh.ParsePrivateKey([]byte(hostKeyPem))
-	Expect(err).NotTo(HaveOccurred())
 	TestHostKey = hostKey
-
-	privateKeyPem := context["private-key"]
-	privateKey, err := ssh.ParsePrivateKey([]byte(privateKeyPem))
-	Expect(err).NotTo(HaveOccurred())
 	TestPrivateKey = privateKey
-
 	TestHostKeyFingerprint = helpers.SHA1Fingerprint(TestHostKey.PublicKey())
 })
