1c404cae48f924a542213414bc999b4b6d209f94
Kris Hicks <khicks@pivotal.io>
2016-02-19 15:59:01 -0800

    Remove named return args from create_app_manifest

--- cf/commands/create_app_manifest.go
+++ cf/commands/create_app_manifest.go
@@ -43,19 +43,18 @@ func (cmd *CreateAppManifest) MetaData() command_registry.CommandMetadata {
 	}
 }
 
-func (cmd *CreateAppManifest) Requirements(requirementsFactory requirements.Factory, fc flags.FlagContext) (reqs []requirements.Requirement, err error) {
+func (cmd *CreateAppManifest) Requirements(requirementsFactory requirements.Factory, fc flags.FlagContext) ([]requirements.Requirement, error) {
 	if len(fc.Args()) != 1 {
 		cmd.ui.Failed(T("Incorrect Usage. Requires APP_NAME as argument\n\n") + command_registry.Commands.CommandUsage("create-app-manifest"))
 	}
 
 	cmd.appReq = requirementsFactory.NewApplicationRequirement(fc.Args()[0])
 
-	reqs = []requirements.Requirement{
+	return []requirements.Requirement{
 		requirementsFactory.NewLoginRequirement(),
 		requirementsFactory.NewTargetedSpaceRequirement(),
 		cmd.appReq,
-	}
-	return
+	}, nil
 }
 
 func (cmd *CreateAppManifest) SetDependency(deps command_registry.Dependency, pluginCall bool) command_registry.Command {
