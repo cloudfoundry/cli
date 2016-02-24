da05ac874873901a1106adbd49f6b4d705ec634c
Kris Hicks <khicks@pivotal.io>
2016-02-24 15:09:37 -0800

    Remove old CI files; replace with new ones
    [#106323354]
    
    Signed-off-by: Chavdar Chernashki <cchernashki@pivotal.io>

--- ci/scripts/windows32-unit.bat
+++
@@ -1,9 +0,0 @@
-set root=%CD%
-
-set /p VERSION=<version\number
-
-call gopath\src\github.com\cloudfoundry\cli\bin\win32_test.bat || exit /b 1
-
-cd %root%
-
-move /y gopath\src\github.com\cloudfoundry\cli\%CF_EXE_NAME% cf-windows%BITS%-%VERSION%.exe
