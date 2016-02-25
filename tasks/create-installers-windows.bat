44a90b63cd52e0469a609562b87116c770ff8dae
Kris Hicks <khicks@pivotal.io>
2016-02-25 09:05:13 -0800

    Revive windows-installer.iss
    [#106323354]

--- tasks/create-installers-windows.bat
+++ tasks/create-installers-windows.bat
@@ -5,8 +5,8 @@ SET /p VERSION=<%ROOT_DIR%\cli\VERSION
 SET PATH=C:\Program Files\GnuWin32\bin;%PATH%
 SET PATH=C:\Program Files (x86)\Inno Setup 5;%PATH%
 
-sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli\ci\scripts\windows-installer.iss
-sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\ci\scripts\windows-installer.iss
+sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli\installers\windows\windows-installer.iss
+sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\installers\windows\windows-installer.iss
 
 pushd %ROOT_DIR%\cf-cli-binaries
 	gzip -d cf-cli-binaries.tgz
@@ -14,9 +14,9 @@ pushd %ROOT_DIR%\cf-cli-binaries
 	MOVE cf-cli_winx64.exe ..\cf.exe
 popd
 
-ISCC %ROOT_DIR%\cli\ci\scripts\windows-installer.iss
+ISCC %ROOT_DIR%\cli\installers\windows\windows-installer.iss
 
-MOVE %ROOT_DIR%\cli\ci\scripts\Output\setup.exe cf_installer.exe
+MOVE %ROOT_DIR%\cli\installers\windows\Output\setup.exe cf_installer.exe
 
 zip %ROOT_DIR%\winstallers\cf-cli-installer_winx64.zip cf_installer.exe
 
@@ -24,8 +24,8 @@ pushd %ROOT_DIR%\cf-cli-binaries
 	MOVE cf-cli_win32.exe ..\cf.exe
 popd
 
-ISCC %ROOT_DIR%\cli\ci\scripts\windows-installer.iss
+ISCC %ROOT_DIR%\cli\installers\windows\windows-installer.iss
 
-MOVE %ROOT_DIR%\cli\ci\scripts\Output\setup.exe cf_installer.exe
+MOVE %ROOT_DIR%\cli\installers\windows\Output\setup.exe cf_installer.exe
 
 zip %ROOT_DIR%\winstallers\cf-cli-installer_win32.zip cf_installer.exe
