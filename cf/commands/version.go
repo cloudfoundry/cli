e96c998807024067124064b6ef5f11fa86ee904f
Can Berk Güder <cbguder@pivotal.io>
2016-02-29 10:51:23 -0800

    Added `version` command
    - Removed -v and --version from global help, but preserved behavior
    
    [Finishes #114264365]
    
    Signed-off-by: Corbin Halliwill <challiwill@pivotal.io>

---
+++ cf/commands/version.go
@@ -0,0 +1,43 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/cloudfoundry/cli/cf"
+	"github.com/cloudfoundry/cli/cf/command_registry"
+	. "github.com/cloudfoundry/cli/cf/i18n"
+	"github.com/cloudfoundry/cli/cf/requirements"
+	"github.com/cloudfoundry/cli/cf/terminal"
+	"github.com/cloudfoundry/cli/flags"
+)
+
+type Version struct {
+	ui terminal.UI
+}
+
+func init() {
+	command_registry.Register(&Version{})
+}
+
+func (cmd *Version) MetaData() command_registry.CommandMetadata {
+	return command_registry.CommandMetadata{
+		Name:        "version",
+		Description: T("Print the version"),
+		Usage: "CF_NAME version" + "\n\n   " + T("'{{.VersionShort}}' and '{{.VersionLong}}' are also accepted.", map[string]string{
+			"VersionShort": "cf -v",
+			"VersionLong":  "cf --version",
+		}),
+	}
+}
+
+func (cmd *Version) SetDependency(deps command_registry.Dependency, pluginCall bool) command_registry.Command {
+	cmd.ui = deps.Ui
+	return cmd
+}
+
+func (cmd *Version) Requirements(requirementsFactory requirements.Factory, context flags.FlagContext) ([]requirements.Requirement, error) {
+	return []requirements.Requirement{}, nil
+}
+
+func (cmd *Version) Execute(context flags.FlagContext) {
+	cmd.ui.Say(fmt.Sprintf("%s version %s-%s", cf.Name, cf.Version, cf.BuiltOnDate))
+}
