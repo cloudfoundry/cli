ce4d57f149b96dcca30f78d333deb94e53a074c4
Corbin Halliwill <challiwill@pivotal.io>
2016-03-11 11:18:55 -0800

    Extract a UsageRequirement
    [#107598946]
    
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

---
+++ cf/requirements/usage_requirement_test.go
@@ -0,0 +1,29 @@
+package requirements_test
+
+import (
+	. "github.com/cloudfoundry/cli/cf/requirements"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("UsageRequirement", func() {
+	It("doesn't return an error when the predicate returns true", func() {
+		err := NewUsageRequirement(nil, "Some error message", func() bool { return true }).Execute()
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("errors when the predicate returns false", func() {
+		usableCmd := usableFunc(func() string { return "Usage text!" })
+
+		err := NewUsageRequirement(usableCmd, "Some error message", func() bool { return false }).Execute()
+
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Some error message"))
+
+		Expect(err.Error()).To(ContainSubstring("Usage text!"))
+	})
+})
+
+type usableFunc func() string
+
+func (u usableFunc) Usage() string { return u() }
