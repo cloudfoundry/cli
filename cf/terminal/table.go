4653f88a24d8167bd595bd2a58041142429faa4e
Andreas Kupries <andreas.kupries@hpe.com>
2016-04-04 11:12:39 -0700

    [HCF-466] Extended cf/terminal/table: Multi-line cells. Suppress header row. Custom per-column transformations. Extended testsuite.

--- cf/terminal/table.go
+++ cf/terminal/table.go
@@ -1,94 +1,334 @@
 package terminal
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"strings"
 )
 
+// Table is the face of tables to the world. It publishes methods to
+// add rows and to print the accumulated table.
 type Table interface {
 	Add(row ...string)
 	Print()
+	NoHeaders()
+	SetTransformer(columnIndex int, tr Transformer)
 }
 
+// PrintableTable is an implementation of the Table interface. It
+// remembers the headers, the added rows, the column widths, and a
+// number of other things.
 type PrintableTable struct {
-	ui              UI
-	headers         []string
-	headerPrinted   bool
-	maxValueLengths []int
-	rows            [][]string
+	ui            UI
+	headers       []string
+	headerPrinted bool
+	columnWidth   []int
+	rowHeight     []int
+	rows          [][]string
+	colSpacing    string
+	transformer   []Transformer
 }
 
-func NewTable(ui UI, headers []string) Table {
-	return &PrintableTable{
-		ui:              ui,
-		headers:         headers,
-		maxValueLengths: make([]int, len(headers)),
+// Transformer is the type of functions used to modify the content of
+// a table cell for actual display. For multi-line content of a cell
+// the transformation is applied to each individual line.
+type Transformer func(s string) string
+
+// NewTable is the constructor function creating a new printable table
+// from a list of headers. The table is also connected to a UI, which
+// is where it will print itself to on demand.
+func NewTable(ui UI, headers []string) *PrintableTable {
+	pt := &PrintableTable{
+		ui:          ui,
+		headers:     headers,
+		columnWidth: make([]int, len(headers)),
+		colSpacing:  "   ",
+		transformer: make([]Transformer, len(headers)),
 	}
+	// Standard colorization, column 0 is auto-highlighted as some
+	// name. Everything else has no transformation (== identity
+	// transform)
+	for i := range pt.transformer {
+		pt.transformer[i] = nop
+	}
+	if 0 < len(headers) {
+		pt.transformer[0] = TableContentHeaderColor
+	}
+	return pt
 }
 
+// NoHeaders disables the printing of the header row for the specified
+// table.
+func (t *PrintableTable) NoHeaders() {
+	// Fake the Print() code into the belief that the headers have
+	// been printed already.
+	t.headerPrinted = true
+}
+
+// SetTransformer specifies a string transformer to apply to the
+// content of the given column in the specified table.
+func (t *PrintableTable) SetTransformer(columnIndex int, tr Transformer) {
+	t.transformer[columnIndex] = tr
+}
+
+// Add extends the table by another row.
 func (t *PrintableTable) Add(row ...string) {
 	t.rows = append(t.rows, row)
 }
 
+// Print formats the table and then prints it to the UI specified at
+// the time of the construction. Afterwards the table is cleared,
+// becoming ready for another round of rows and printing.
 func (t *PrintableTable) Print() {
-	for _, row := range append(t.rows, t.headers) {
-		t.calculateMaxSize(row)
+	result := &bytes.Buffer{}
+
+	t.PrintTo(result)
+
+	// DevNote. With the change to printing into a buffer all
+	// lines now come with a terminating \n. The t.ui.Say() below
+	// will then add another \n to that. To avoid this additional
+	// line we chop off the last \n from the output (if there is
+	// any). Operating on the slice avoids string copying.
+	//
+	// WIBNI if the terminal API had a variant of Say not assuming
+	// that each output is a single line.
+
+	r := result.Bytes()
+	if len(r) > 0 {
+		r = r[0 : len(r)-1]
 	}
 
-	if t.headerPrinted == false {
-		t.printHeader()
+	// Only generate output for a non-empty table.
+	if len(r) > 0 {
+		t.ui.Say("%s", string(r))
+	}
+}
+
+// PrintTo is the core functionality for printing the table, placing
+// the formatted table into the writer given to it as argument. The
+// exported Print() is just a wrapper around this which redirects the
+// result into CF datastructures.
+func (t *PrintableTable) PrintTo(result io.Writer) {
+
+	t.rowHeight = make([]int, len(t.rows)+1)
+
+	rowIndex := 0
+	if !t.headerPrinted {
+		// row transformer header row
+		t.calculateMaxSize(transHeader, rowIndex, t.headers)
+		rowIndex++
+	}
+
+	for _, row := range t.rows {
+		// table is row transformer itself, for content rows
+		t.calculateMaxSize(t, rowIndex, row)
+		rowIndex++
+	}
+
+	rowIndex = 0
+	if !t.headerPrinted {
+		t.printRow(result, transHeader, rowIndex, t.headers)
 		t.headerPrinted = true
+		rowIndex++
 	}
 
-	for _, line := range t.rows {
-		t.printRow(line)
+	for row := range t.rows {
+		t.printRow(result, t, rowIndex, t.rows[row])
+		rowIndex++
 	}
 
+	// Note, printing a table clears it.
 	t.rows = [][]string{}
 }
 
-func (t *PrintableTable) calculateMaxSize(row []string) {
-	for index, value := range row {
-		l := visibleSize(Decolorize(value))
-		if t.maxValueLengths[index] < l {
-			t.maxValueLengths[index] = l
+// calculateMaxSize iterates over the collected rows of the specified
+// table, and their strings, determining the height of each row (in
+// lines), and the width of each column (in characters). The results
+// are stored in the table for use by Print.
+func (t *PrintableTable) calculateMaxSize(transformer rowTransformer, rowIndex int, row []string) {
+
+	// Iterate columns
+	for columnIndex := range row {
+		// Truncate long row, ignore the additional fields.
+		if columnIndex >= len(t.headers) {
+			break
+		}
+
+		// Note that the length of the cell in characters is
+		// __not__ equivalent to its width.  Because it may be
+		// a multi-line value. We have to split the cell into
+		// lines and check the width of each such fragment.
+		// The number of lines founds also goes into the row
+		// height.
+
+		lines := strings.Split(row[columnIndex], "\n")
+		height := len(lines)
+
+		if t.rowHeight[rowIndex] < height {
+			t.rowHeight[rowIndex] = height
+		}
+
+		for i := range lines {
+			value := trim(Decolorize(transformer.Transform(columnIndex, lines[i])))
+			width := visibleSize(value)
+			if t.columnWidth[columnIndex] < width {
+				t.columnWidth[columnIndex] = width
+			}
 		}
 	}
 }
 
-func (t *PrintableTable) printHeader() {
-	output := ""
-	for col, value := range t.headers {
-		output = output + t.cellValue(col, HeaderColor(value))
-	}
-	t.ui.Say(output)
-}
+// printRow is responsible for the layouting, transforming and
+// printing of the string in a single row
+func (t *PrintableTable) printRow(result io.Writer, transformer rowTransformer, rowIndex int, row []string) {
 
-func (t *PrintableTable) printRow(row []string) {
-	output := ""
-	for columnIndex, value := range row {
-		if columnIndex == 0 {
-			value = TableContentHeaderColor(value)
+	height := t.rowHeight[rowIndex]
+
+	// Compute the index of the last column as the min number of
+	// cells in the header and cells in the current row.
+	// Note: math.Min seems to be for float only :(
+	last := len(t.headers) - 1
+	lastr := len(row) - 1
+	if lastr < last {
+		last = lastr
+	}
+
+	// Note how we always print into a line buffer before placing
+	// the assembled line into the result. This allows us to trim
+	// superfluous trailing whitespace from the line before making
+	// it final.
+
+	if height <= 1 {
+		// Easy case, all cells in the row are single-line
+		line := &bytes.Buffer{}
+
+		for columnIndex := range row {
+			// Truncate long row, ignore the additional fields.
+			if columnIndex >= len(t.headers) {
+				break
+			}
+
+			value := trim(transformer.Transform(columnIndex, row[columnIndex]))
+			t.printCellValue(line, columnIndex, last, value)
 		}
 
-		output = output + t.cellValue(columnIndex, value)
-	}
-	t.ui.Say("%s", output)
-}
-
-func (t *PrintableTable) cellValue(col int, value string) string {
-	padding := ""
-	maxVisibleSize := t.maxValueLengths[col]
-
-	if col < len(t.headers)-1 {
-		thisVisibleSize := visibleSize(Decolorize(value))
-		padding = strings.Repeat(` `, maxVisibleSize-thisVisibleSize)
+		fmt.Fprintf(result, "%s\n", trim(string(line.Bytes())))
+		return
 	}
 
-	return fmt.Sprintf("%s%s   ", value, padding)
+	// We have at least one multi-line cell in this row.
+	// Treat it a bit like a mini-table.
+
+	// Step I. Fill the mini-table. Note how it is stored
+	//         column-major, not row-major.
+
+	// [column][row]string
+	sub := make([][]string, len(t.headers))
+	for columnIndex := range row {
+		// Truncate long row, ignore the additional fields.
+		if columnIndex >= len(t.headers) {
+			break
+		}
+		sub[columnIndex] = strings.Split(row[columnIndex], "\n")
+		// (*) Extend the column to the full height.
+		for len(sub[columnIndex]) < height {
+			sub[columnIndex] = append(sub[columnIndex], "")
+		}
+	}
+
+	// Step II. Iterate over the rows, then columns to
+	//          collect the output. This assumes that all
+	//          the rows in sub are the same height. See
+	//          (*) above where that is made true.
+
+	for rowIndex := range sub[0] {
+		line := &bytes.Buffer{}
+
+		for columnIndex := range sub {
+			value := trim(transformer.Transform(columnIndex, sub[columnIndex][rowIndex]))
+			t.printCellValue(line, columnIndex, last, value)
+		}
+
+		fmt.Fprintf(result, "%s\n", trim(string(line.Bytes())))
+	}
 }
 
+// printCellValue pads the specified string to the width of the given
+// column, adds the spacing bewtween columns, and returns the result.
+func (t *PrintableTable) printCellValue(result io.Writer, col, last int, value string) {
+	fmt.Fprint(result, value)
+
+	// Pad all columns, but the last in this row (with the size of
+	// the header row limiting this). This ensures that most of
+	// the irrelevant spacing is not printed. At the moment
+	// irrelevant spacing can only occur when printing a row with
+	// multi-line cells, introducing a physical short line for a
+	// long logical row. Getting rid of that requires fixing in
+	// printRow.
+	//
+	//  Note how the inter-column spacing is also irrelevant for
+	//  that last column.
+
+	if col < last {
+		padlen := t.columnWidth[col] - visibleSize(Decolorize(value))
+		padding := strings.Repeat(" ", padlen)
+		fmt.Fprint(result, padding)
+		fmt.Fprint(result, t.colSpacing)
+	}
+}
+
+// rowTransformer is an interface behind which we can specify how to
+// transform the strings of an entire row on a column-by-column basis.
+type rowTransformer interface {
+	Transform(column int, s string) string
+}
+
+// transformHeader is an implementation of rowTransformer which
+// highlights all columns as a Header.
+type transformHeader struct{}
+
+// transHeader holds a package-global transformHeader to prevent us
+// from continuously allocating a literal of the type whenever we
+// print a header row. Instead all tables use this value.
+var transHeader = &transformHeader{}
+
+// Transform performs the Header highlighting for transformHeader
+func (th *transformHeader) Transform(column int, s string) string {
+	return HeaderColor(s)
+}
+
+// This Transform makes a PrintableTable an implementation of
+// rowTransformer. It performs the per-column transformation for table
+// content, as specified during construction and/or overridden by the
+// user of the table, see SetTransformer.
+func (t *PrintableTable) Transform(column int, s string) string {
+	return t.transformer[column](s)
+}
+
+// nop is the identity transformation which does not transform the
+// string at all.
+func nop(s string) string {
+	return s
+}
+
+// trim is a helper to remove trailing whitespace from a string.
+func trim(s string) string {
+	return strings.TrimRight(s, " \t")
+}
+
+// visibleSize returns the number of columns the string will cover
+// when displayed in the terminal. This is the number of runes,
+// i.e. characters, not the number of bytes it consists of.
 func visibleSize(s string) int {
+	// This code re-implements the basic functionality of
+	// RuneCountInString to account for special cases. Namely
+	// UTF-8 characters taking up 3 bytes (**) appear as double-width.
+	//
+	// (**) I wonder if that is the set of characters outside of
+	// the BMP <=> the set of characters requiring surrogates (2
+	// slots) when encoded in UCS-2.
+
 	r := strings.NewReader(s)
 
 	var size int
