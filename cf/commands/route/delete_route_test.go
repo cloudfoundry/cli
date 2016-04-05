3a7963bf2201555caa5f830cc62b12afe7352339
Sam Pourcyrous <sam.pourcyrous@gmail.com>
2016-04-05 16:20:27 -0700

    Use the passed in port when finding a route to delete
    [#115217191]
    
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

--- cf/commands/route/delete_route_test.go
+++ cf/commands/route/delete_route_test.go
@@ -249,6 +249,21 @@ var _ = Describe("DeleteRoute", func() {
 				})
 			})
 
+			Context("when a port is passed", func() {
+				BeforeEach(func() {
+					err := flagContext.Parse("domain-name", "-f", "--port", "60000")
+					Expect(err).NotTo(HaveOccurred())
+					cmd.Requirements(factory, flagContext)
+				})
+
+				It("tries to find the route with the port", func() {
+					cmd.Execute(flagContext)
+					Expect(routeRepo.FindCallCount()).To(Equal(1))
+					_, _, _, port := routeRepo.FindArgsForCall(0)
+					Expect(port).To(Equal(60000))
+				})
+			})
+
 			Context("when the route can be found", func() {
 				BeforeEach(func() {
 					routeRepo.FindReturns(models.Route{
