25ed44fcb9af91c106153c090629bc3aa189e51d
Chris Hendrix <chendrix@pivotal.io>
2016-04-12 14:50:33 -0700

    github.com/cloudfoundry/cli/testhelpers/plugin_builder

--- plugin_examples/call_cli_cmd/main/main_suite_test.go
+++ plugin_examples/call_cli_cmd/main/main_suite_test.go
@@ -1,7 +1,7 @@
 package main_test
 
 import (
-	"github.com/cloudfoundry/cli/testhelpers/plugin_builder"
+	"github.com/cloudfoundry/cli/testhelpers/pluginbuilder"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -11,7 +11,7 @@ import (
 func TestMain(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	plugin_builder.BuildTestBinary(".", "call_cli_cmd")
+	pluginbuilder.BuildTestBinary(".", "call_cli_cmd")
 
 	RunSpecs(t, "Main Suite")
 }
--- testhelpers/pluginbuilder/plugin_builder.go
+++ testhelpers/pluginbuilder/plugin_builder.go
@@ -1,4 +1,4 @@
-package plugin_builder
+package pluginbuilder
 
 import (
 	"os"
