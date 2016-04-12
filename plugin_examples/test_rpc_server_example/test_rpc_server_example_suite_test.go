b07fe6886fdabe89f2e26bdd27e46ae3f6ec39bc
Chris Hendrix <chendrix@pivotal.io>
2016-04-12 14:50:33 -0700

    github.com/cloudfoundry/cli/testhelpers/plugin_builder

--- plugin_examples/test_rpc_server_example/test_rpc_server_example_suite_test.go
+++ plugin_examples/test_rpc_server_example/test_rpc_server_example_suite_test.go
@@ -1,7 +1,7 @@
 package main_test
 
 import (
-	"github.com/cloudfoundry/cli/testhelpers/plugin_builder"
+	"github.com/cloudfoundry/cli/testhelpers/pluginbuilder"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -11,7 +11,7 @@ import (
 func TestTestRpcServerExample(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	plugin_builder.BuildTestBinary("", "test_rpc_server_example")
+	pluginbuilder.BuildTestBinary("", "test_rpc_server_example")
 
 	RunSpecs(t, "TestRpcServerExample Suite")
 }
