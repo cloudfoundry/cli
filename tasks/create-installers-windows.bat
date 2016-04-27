f8f2fcb537b9f1614fe6385cd71d33aa97f562f6
Jeff Pak <jeffrey.pak@emc.com>
2016-04-27 14:54:42 -0700

    Revert "Windows x64 installer puts things in correct directory"
    This reverts commit 8395f535da662d52c20a20b8d55c402ee9a193fd.
    
    [#66903140]
    
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

--- tasks/create-installers-windows.bat
+++ tasks/create-installers-windows.bat
@@ -5,8 +5,8 @@ SET /p VERSION=<%ROOT_DIR%\cli\VERSION
 SET PATH=C:\Program Files\GnuWin32\bin;%PATH%
 SET PATH=C:\Program Files (x86)\Inno Setup 5;%PATH%
 
-sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
-sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
+sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli\installers\windows\windows-installer.iss
+sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\installers\windows\windows-installer.iss
 
 pushd %ROOT_DIR%\cf-cli-binaries
 	gzip -d cf-cli-binaries.tgz
@@ -14,20 +14,17 @@ pushd %ROOT_DIR%\cf-cli-binaries
 	MOVE cf-cli_winx64.exe ..\cf.exe
 popd
 
-ISCC %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
+ISCC %ROOT_DIR%\cli\installers\windows\windows-installer.iss
 
 MOVE %ROOT_DIR%\cli\installers\windows\Output\setup.exe cf_installer.exe
 
 zip %ROOT_DIR%\winstallers\cf-cli-installer_winx64.zip cf_installer.exe
 
-sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x86.iss
-sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\installers\windows\windows-installer-x86.iss
-
 pushd %ROOT_DIR%\cf-cli-binaries
 	MOVE cf-cli_win32.exe ..\cf.exe
 popd
 
-ISCC %ROOT_DIR%\cli\installers\windows\windows-installer-x86.iss
+ISCC %ROOT_DIR%\cli\installers\windows\windows-installer.iss
 
 MOVE %ROOT_DIR%\cli\installers\windows\Output\setup.exe cf_installer.exe
 
