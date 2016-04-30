05109b7308c40b1770d66c94d79ef07c13537a52
Chris Hendrix <chendrix@pivotal.io>
2016-04-29 17:09:48 -0700

    Use InnoSetup to sign the generated windows exe installer
    [finishes #66476084]
    
    Signed-off-by: Nick Wei <n.wei@pivotal.io>

--- ci/tasks/create-installers-windows.bat
+++ ci/tasks/create-installers-windows.bat
@@ -5,8 +5,15 @@ SET /p VERSION=<%ROOT_DIR%\cli\VERSION
 SET PATH=C:\Program Files\GnuWin32\bin;%PATH%
 SET PATH=C:\Program Files (x86)\Inno Setup 5;%PATH%
 
+REM You must have added to your Inno Setup a Signtool: (http://revolution.screenstepslive.com/s/revolution/m/10695/l/95041-signing-installers-you-create-with-inno-setup)
+REM Name: "signtool", Command: "signtool.exe $p"
+REM This is to add "signtool.exe" to your path, so it does not need to be fully qualified in the configuration above
+SET PATH=C:\Program Files (x86)\Windows Kits\10\bin\x64;%PATH%
+
 sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli\installers\windows\windows-installer.iss
 sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\installers\windows\windows-installer.iss
+sed -i -e "s/SIGNTOOL_CERT_PASSWORD/%SIGNTOOL_CERT_PASSWORD%/" %ROOT_DIR%\cli\installers\windows\windows-installer.iss
+sed -i -e "s/SIGNTOOL_CERT_PATH/%SIGNTOOL_CERT_PATH%/" %ROOT_DIR%\cli\installers\windows\windows-installer.iss
 
 pushd %ROOT_DIR%\cf-cli-binaries
 	gzip -d cf-cli-binaries.tgz
