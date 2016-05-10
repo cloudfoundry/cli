bdfb5451cfd7c306802f34571c87d6e3ba5b54e6
Jeff Pak <jeffrey.pak@emc.com>
2016-05-09 17:28:45 -0700

    Add cats-windows-diego job
    [#114532269]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

---
+++ ci/tasks/cats-windows-diego.bat
@@ -0,0 +1,29 @@
+SET GOPATH=%CD%\cf-release-repo
+SET GATSPATH=%GOPATH%\src\github.com\cloudfoundry\cf-acceptance-tests
+SET GOPATH=%GOPATH%;%GATSPATH%\Godeps\_workspace
+
+SET PATH=C:\Go\bin;%PATH%
+SET PATH=C:\Program Files\Git\cmd\;%PATH%
+SET PATH=%GOPATH%\bin;%PATH
+SET PATH=C:\Program Files\GnuWin32\bin;%PATH%
+SET PATH=C:\Program Files\cURL\bin;%PATH%
+SET PATH=%CD%;%PATH%
+
+SET /p DOMAIN=<%CD%\bosh-lite-lock\name
+call %CD%\cli\ci\tasks\create-cats-config.bat
+SET CONFIG=%CD%\config.json
+
+go get -v github.com/onsi/ginkgo/ginkgo
+
+pushd %CD%\cf-cli-binaries
+	gzip -d cf-cli-binaries.tgz
+	tar -xvf cf-cli-binaries.tar
+	MOVE %CD%\cf-cli_winx64.exe ..\cf.exe
+popd
+
+go get -v github.com/onsi/ginkgo/ginkgo
+
+cd %GATSPATH%
+ginkgo.exe -r -slowSpecThreshold=120 -nodes=2 ^
+  -skip=NO_DIEGO_SUPPORT ^
+  . apps backend_compatibility detect docker internet_dependent security_groups ssh routing
