fccc501a513f902f56f85516c4c37cfedcdfdfac
Corbin Halliwill <challiwill@pivotal.io>
2016-06-03 13:45:24 -0700

    Introduce NumberArguments requirement
    [#115646053]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

---
+++ cf/requirements/number_arguments.go
@@ -0,0 +1,37 @@
+package requirements
+
+import (
+	"strings"
+
+	. "github.com/cloudfoundry/cli/cf/i18n"
+)
+
+type NumberArguments struct {
+	passedArgs   []string
+	expectedArgs []string
+}
+
+func NewNumberArguments(passedArgs []string, expectedArgs ...string) NumberArguments {
+	return NumberArguments{
+		passedArgs:   passedArgs,
+		expectedArgs: expectedArgs,
+	}
+}
+
+func (r NumberArguments) Execute() error {
+	if len(r.passedArgs) != len(r.expectedArgs) {
+		return NumberArgumentsError{ExpectedArgs: r.expectedArgs}
+	}
+
+	return nil
+}
+
+type NumberArgumentsError struct {
+	ExpectedArgs []string
+}
+
+func (e NumberArgumentsError) Error() string {
+	return T("Incorrect Usage. Requires {{.Arguments}}", map[string]string{
+		"Arguments": strings.Join(e.ExpectedArgs, ", "),
+	})
+}
---
+++ cf/requirements/number_arguments_test.go
@@ -0,0 +1,26 @@
+package requirements_test
+
+import (
+	. "github.com/cloudfoundry/cli/cf/requirements"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("NumberArguments", func() {
+	It("returns an error if the number of arguments doesn't match", func() {
+		args := []string{"one", "two"}
+		numberArgumentsRequirement := NewNumberArguments(args, "SPACE")
+
+		err := numberArgumentsRequirement.Execute()
+		Expect(err).To(MatchError(NumberArgumentsError{ExpectedArgs: []string{"SPACE"}}))
+	})
+
+	It("returns nil if the number of arguments matches", func() {
+		args := []string{"one"}
+		numberArgumentsRequirement := NewNumberArguments(args, "SPACE")
+
+		err := numberArgumentsRequirement.Execute()
+		Expect(err).NotTo(HaveOccurred())
+	})
+})
