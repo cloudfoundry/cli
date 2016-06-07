517b9ce86a0cba56b49ee75e4c9ef26231ea841d
Jeff Pak <jeffrey.pak@emc.com>
2016-06-07 11:41:10 -0700

    Remove lintdebt from plugin_examples
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

--- plugin_examples/basic_plugin.go
+++ plugin_examples/basic_plugin.go
@@ -6,27 +6,22 @@ import (
 	"github.com/cloudfoundry/cli/plugin"
 )
 
-/*
-*	This is the struct implementing the interface defined by the core CLI. It can
-*	be found at  "github.com/cloudfoundry/cli/plugin/plugin.go"
-*
- */
+// BasicPlugin is the struct implementing the interface defined by the core CLI. It can
+// be found at  "github.com/cloudfoundry/cli/plugin/plugin.go"
 type BasicPlugin struct{}
 
-/*
-*	This function must be implemented by any plugin because it is part of the
-*	plugin interface defined by the core CLI.
-*
-*	Run(....) is the entry point when the core CLI is invoking a command defined
-*	by a plugin. The first parameter, plugin.CliConnection, is a struct that can
-*	be used to invoke cli commands. The second paramter, args, is a slice of
-*	strings. args[0] will be the name of the command, and will be followed by
-*	any additional arguments a cli user typed in.
-*
-*	Any error handling should be handled with the plugin itself (this means printing
-*	user facing errors). The CLI will exit 0 if the plugin exits 0 and will exit
-*	1 should the plugin exits nonzero.
- */
+// Run must be implemented by any plugin because it is part of the
+// plugin interface defined by the core CLI.
+//
+// Run(....) is the entry point when the core CLI is invoking a command defined
+// by a plugin. The first parameter, plugin.CliConnection, is a struct that can
+// be used to invoke cli commands. The second paramter, args, is a slice of
+// strings. args[0] will be the name of the command, and will be followed by
+// any additional arguments a cli user typed in.
+//
+// Any error handling should be handled with the plugin itself (this means printing
+// user facing errors). The CLI will exit 0 if the plugin exits 0 and will exit
+// 1 should the plugin exits nonzero.
 func (c *BasicPlugin) Run(cliConnection plugin.CliConnection, args []string) {
 	// Ensure that we called the command basic-plugin-command
 	if args[0] == "basic-plugin-command" {
@@ -34,20 +29,18 @@ func (c *BasicPlugin) Run(cliConnection plugin.CliConnection, args []string) {
 	}
 }
 
-/*
-*	This function must be implemented as part of the	plugin interface
-*	defined by the core CLI.
-*
-*	GetMetadata() returns a PluginMetadata struct. The first field, Name,
-*	determines the name of the plugin which should generally be without spaces.
-*	If there are spaces in the name a user will need to properly quote the name
-*	during uninstall otherwise the name will be treated as seperate arguments.
-*	The second value is a slice of Command structs. Our slice only contains one
-*	Command Struct, but could contain any number of them. The first field Name
-*	defines the command `cf basic-plugin-command` once installed into the CLI. The
-*	second field, HelpText, is used by the core CLI to display help information
-*	to the user in the core commands `cf help`, `cf`, or `cf -h`.
- */
+// GetMetadata must be implemented as part of the plugin interface
+// defined by the core CLI.
+//
+// GetMetadata() returns a PluginMetadata struct. The first field, Name,
+// determines the name of the plugin which should generally be without spaces.
+// If there are spaces in the name a user will need to properly quote the name
+// during uninstall otherwise the name will be treated as seperate arguments.
+// The second value is a slice of Command structs. Our slice only contains one
+// Command Struct, but could contain any number of them. The first field Name
+// defines the command `cf basic-plugin-command` once installed into the CLI. The
+// second field, HelpText, is used by the core CLI to display help information
+// to the user in the core commands `cf help`, `cf`, or `cf -h`.
 func (c *BasicPlugin) GetMetadata() plugin.PluginMetadata {
 	return plugin.PluginMetadata{
 		Name: "MyBasicPlugin",
@@ -76,12 +69,10 @@ func (c *BasicPlugin) GetMetadata() plugin.PluginMetadata {
 	}
 }
 
-/*
-* Unlike most Go programs, the `Main()` function will not be used to run all of the
-* commands provided in your plugin. Main will be used to initialize the plugin
-* process, as well as any dependencies you might require for your
-* plugin.
- */
+// Unlike most Go programs, the `Main()` function will not be used to run all of the
+// commands provided in your plugin. Main will be used to initialize the plugin
+// process, as well as any dependencies you might require for your
+// plugin.
 func main() {
 	// Any initialization for your plugin can be handled here
 	//
