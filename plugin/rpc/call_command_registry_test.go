660e791574774d1d9139cc65dd7a3994bfecf387
Nick Wei <nwei@pivotal.io>
2016-06-14 09:01:33 -0700

    Remove testhelpers/terminal from cf/plugin
    [#120872829]

--- plugin/rpc/call_command_registry_test.go
+++ plugin/rpc/call_command_registry_test.go
@@ -1,16 +1,15 @@
 package rpc_test
 
 import (
+	"os"
+
 	"github.com/cloudfoundry/cli/cf/commandregistry"
+	"github.com/cloudfoundry/cli/cf/terminal/terminalfakes"
+	"github.com/cloudfoundry/cli/cf/trace/tracefakes"
 	. "github.com/cloudfoundry/cli/plugin/rpc"
 	. "github.com/cloudfoundry/cli/plugin/rpc/fakecommand"
-
-	"github.com/cloudfoundry/cli/cf/trace/tracefakes"
-	. "github.com/cloudfoundry/cli/testhelpers/matchers"
-	testterm "github.com/cloudfoundry/cli/testhelpers/terminal"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"os"
 )
 
 var _ = Describe("calling commands in commandregistry", func() {
@@ -18,7 +17,7 @@ var _ = Describe("calling commands in commandregistry", func() {
 	_ = FakeCommand1{} //make sure fake_command is imported and self-registered with init()
 
 	var (
-		ui         *testterm.FakeUI
+		ui         *terminalfakes.FakeUI
 		deps       commandregistry.Dependency
 		fakeLogger *tracefakes.FakePrinter
 	)
@@ -26,7 +25,7 @@ var _ = Describe("calling commands in commandregistry", func() {
 	BeforeEach(func() {
 		fakeLogger = new(tracefakes.FakePrinter)
 		deps = commandregistry.NewDependency(os.Stdout, fakeLogger)
-		ui = &testterm.FakeUI{}
+		ui = new(terminalfakes.FakeUI)
 		deps.UI = ui
 
 		cmd := commandregistry.Commands.FindCommand("fake-command")
@@ -36,33 +35,31 @@ var _ = Describe("calling commands in commandregistry", func() {
 		commandregistry.Commands.SetCommand(cmd2.SetDependency(deps, true))
 	})
 
-	It("runs the command requirements", func() {
-		NewCommandRunner().Command([]string{"fake-command"}, deps, false)
-		Expect(ui.Outputs()).To(ContainSubstrings([]string{"Requirement executed"}))
+	Context("when not expecting the command to fail", func() {
+		BeforeEach(func() {
+			err := NewCommandRunner().Command([]string{"fake-command"}, deps, false)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should set dependencies, execute requirements, and execute the command", func() {
+			Expect(ui.SayArgsForCall(0)).To(ContainSubstring("SetDependency() called, pluginCall true"))
+			Expect(ui.SayArgsForCall(1)).To(ContainSubstring("SetDependency() called, pluginCall false"))
+			Expect(ui.SayArgsForCall(2)).To(ContainSubstring("Requirement executed"))
+			Expect(ui.SayArgsForCall(3)).To(ContainSubstring("Command Executed"))
+		})
 	})
 
-	It("calls the command Execute() func", func() {
-		NewCommandRunner().Command([]string{"fake-command"}, deps, false)
-		Expect(ui.Outputs()).To(ContainSubstrings([]string{"Command Executed"}))
+	Context("when expecting the command to fail", func() {
+		It("returns an error if any of the requirements fail", func() {
+			err := NewCommandRunner().Command([]string{"fake-command2"}, deps, false)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Requirement executed and failed"))
+		})
+
+		It("returns an error if invalid flag is provided", func() {
+			err := NewCommandRunner().Command([]string{"fake-command", "-badFlag"}, deps, false)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Invalid flag: -badFlag"))
+		})
 	})
-
-	It("sets the dependency of the command", func() {
-		NewCommandRunner().Command([]string{"fake-command"}, deps, false)
-		Expect(ui.Outputs()).To(ContainSubstrings([]string{"SetDependency() called, pluginCall true"}))
-	})
-
-	It("returns an error if any of the requirements fail", func() {
-		err := NewCommandRunner().Command([]string{"fake-command2"}, deps, false)
-
-		Expect(err).To(HaveOccurred())
-		Expect(err.Error()).To(ContainSubstring("Requirement executed and failed"))
-	})
-
-	It("returns an error if invalid flag is provided", func() {
-		err := NewCommandRunner().Command([]string{"fake-command", "-badFlag"}, deps, false)
-
-		Expect(err).To(HaveOccurred())
-		Expect(err.Error()).To(ContainSubstring("Invalid flag: -badFlag"))
-	})
-
 })
