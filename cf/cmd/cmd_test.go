be7fd34d844f780824b36051c8b9a13f391475ac
Corbin Halliwill <challiwill@pivotal.io>
2016-07-18 17:01:04 -0700

    use 'Consistently()' with 'ShouldNot()'
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- cf/cmd/cmd_test.go
+++ cf/cmd/cmd_test.go
@@ -52,13 +52,13 @@ var _ = Describe("main", func() {
 
 	Describe("Help menu with -h/--help", func() {
 		It("prints the help output with our custom template when run with 'cf -h'", func() {
-			output := Cf("-h").Wait(1 * time.Second)
+			output := Cf("-h")
 			Eventually(output.Out.Contents).Should(ContainSubstring("A command line tool to interact with Cloud Foundry"))
 			Eventually(output.Out.Contents).Should(ContainSubstring("CF_TRACE=true"))
 		})
 
 		It("prints the help output with our custom template when run with 'cf --help'", func() {
-			output := Cf("--help").Wait(1 * time.Second)
+			output := Cf("--help")
 			Eventually(output.Out.Contents).Should(ContainSubstring("A command line tool to interact with Cloud Foundry"))
 			Eventually(output.Out.Contents).Should(ContainSubstring("CF_TRACE=true"))
 		})
@@ -87,13 +87,13 @@ var _ = Describe("main", func() {
 
 	Describe("Shows version with -v or --version", func() {
 		It("prints the cf version if '-v' flag is provided", func() {
-			output := Cf("-v").Wait(1 * time.Second)
+			output := Cf("-v")
 			Eventually(output.Out.Contents).Should(ContainSubstring("cf version"))
 			Eventually(output).Should(Exit(0))
 		})
 
 		It("prints the cf version if '--version' flag is provided", func() {
-			output := Cf("--version").Wait(1 * time.Second)
+			output := Cf("--version")
 			Eventually(output.Out.Contents).Should(ContainSubstring("cf version"))
 			Eventually(output).Should(Exit(0))
 		})
@@ -111,27 +111,27 @@ var _ = Describe("main", func() {
 		// Normally cf curl only shows the output of the response
 		// When using trace, it also shows the request/response information
 		It("enables verbose output when -v is provided before a command", func() {
-			output := Cf("-v", "curl", "/v2/info").Wait(5 * time.Second)
-			Eventually(output.Out.Contents).ShouldNot(ContainSubstring("Invalid flag: -v"))
+			output := Cf("-v", "curl", "/v2/info")
+			Consistently(output.Out.Contents).ShouldNot(ContainSubstring("Invalid flag: -v"))
 			Eventually(output.Out.Contents).Should(ContainSubstring("GET /v2/info HTTP/1.1"))
 		})
 
 		It("enables verbose output when -v is provided after a command", func() {
-			output := Cf("curl", "/v2/info", "-v").Wait(5 * time.Second)
-			Eventually(output.Out.Contents).ShouldNot(ContainSubstring("Invalid flag: -v"))
+			output := Cf("curl", "/v2/info", "-v")
+			Consistently(output.Out.Contents).ShouldNot(ContainSubstring("Invalid flag: -v"))
 			Eventually(output.Out.Contents).Should(ContainSubstring("GET /v2/info HTTP/1.1"))
 		})
 	})
 
 	Describe("Shows debug information with -b or --build", func() {
 		It("prints the golang version if '--build' flag is provided", func() {
-			output := Cf("--build").Wait(1 * time.Second)
+			output := Cf("--build")
 			Eventually(output.Out.Contents).Should(ContainSubstring("was built with Go version:"))
 			Eventually(output).Should(Exit(0))
 		})
 
 		It("prints the golang version if '-b' flag is provided", func() {
-			output := Cf("-b").Wait(1 * time.Second)
+			output := Cf("-b")
 			Eventually(output.Out.Contents).Should(ContainSubstring("was built with Go version:"))
 			Eventually(output).Should(Exit(0))
 		})
@@ -139,7 +139,7 @@ var _ = Describe("main", func() {
 
 	Describe("Commands /w new command structure", func() {
 		It("prints usage help for all commands by providing `help` flag", func() {
-			output := Cf("api", "-h").Wait(1 * time.Second)
+			output := Cf("api", "-h")
 			Eventually(output.Out.Contents).Should(ContainSubstring("USAGE"))
 			Eventually(output.Out.Contents).Should(ContainSubstring("OPTIONS"))
 		})
@@ -179,7 +179,7 @@ var _ = Describe("main", func() {
 	})
 
 	It("can print help menu by executing only the command `cf`", func() {
-		output := Cf().Wait(3 * time.Second)
+		output := Cf()
 		Eventually(output.Out.Contents).Should(ContainSubstring("A command line tool to interact with Cloud Foundry"))
 	})
 
@@ -191,17 +191,17 @@ var _ = Describe("main", func() {
 
 	Describe("Plugins", func() {
 		It("Can call a plugin command from the Plugins configuration if it does not exist as a cf command", func() {
-			output := Cf("test_1_cmd1").Wait(3 * time.Second)
+			output := Cf("test_1_cmd1")
 			Eventually(output.Out).Should(Say("You called cmd1 in test_1"))
 		})
 
 		It("Can call a plugin command via alias if it does not exist as a cf command", func() {
-			output := Cf("test_1_cmd1_alias").Wait(3 * time.Second)
+			output := Cf("test_1_cmd1_alias")
 			Eventually(output.Out).Should(Say("You called cmd1 in test_1"))
 		})
 
 		It("Can call another plugin command when more than one plugin is installed", func() {
-			output := Cf("test_2_cmd1").Wait(3 * time.Second)
+			output := Cf("test_2_cmd1")
 			Eventually(output.Out).Should(Say("You called cmd1 in test_2"))
 		})
 
@@ -222,8 +222,8 @@ var _ = Describe("main", func() {
 		})
 
 		It("shows help with a '-h' or '--help' flag in plugin command", func() {
-			output := Cf("test_1_cmd1", "-h").Wait(3 * time.Second)
-			Eventually(output.Out).ShouldNot(Say("You called cmd1 in test_1"))
+			output := Cf("test_1_cmd1", "-h")
+			Consistently(output.Out).ShouldNot(Say("You called cmd1 in test_1"))
 			Eventually(output.Out.Contents).Should(ContainSubstring("USAGE:"))
 			Eventually(output.Out.Contents).Should(ContainSubstring("OPTIONS:"))
 		})
@@ -239,42 +239,42 @@ var _ = Describe("main", func() {
 		})
 
 		It("Passes all arguments to a plugin", func() {
-			output := Cf("my-say", "foo").Wait(3 * time.Second)
+			output := Cf("my-say", "foo")
 			Eventually(output.Out).Should(Say("foo"))
 		})
 
 		It("Passes all arguments and flags to a plugin", func() {
-			output := Cf("my-say", "foo", "--loud").Wait(3 * time.Second)
+			output := Cf("my-say", "foo", "--loud")
 			Eventually(output.Out).Should(Say("FOO"))
 		})
 
 		It("Calls a plugin that calls core commands", func() {
-			output := Cf("awesomeness").Wait(3 * time.Second)
+			output := Cf("awesomeness")
 			Eventually(output.Out).Should(Say("my-say")) //look for another plugin
 		})
 
 		It("Sends stdoutput to the plugin to echo", func() {
-			output := Cf("core-command", "plugins").Wait(3 * time.Second)
+			output := Cf("core-command", "plugins")
 			Eventually(output.Out.Contents).Should(MatchRegexp("Command output from the plugin(.*\\W)*awesomeness(.*\\W)*FIN"))
 		})
 
 		It("Can call a core commmand from a plugin without terminal output", func() {
-			output := Cf("core-command-quiet", "plugins").Wait(3 * time.Second)
+			output := Cf("core-command-quiet", "plugins")
 			Eventually(output.Out.Contents).Should(MatchRegexp("^\n---------- Command output from the plugin"))
 		})
 
 		It("Can call a plugin that requires stdin (interactive)", func() {
-			session := CfWithIo("input", "silly\n").Wait(5 * time.Second)
+			session := CfWithIo("input", "silly\n")
 			Eventually(session.Out).Should(Say("silly"))
 		})
 
 		It("exits 1 when a plugin panics", func() {
-			session := Cf("panic").Wait(5 * time.Second)
+			session := Cf("panic")
 			Eventually(session).Should(Exit(1))
 		})
 
 		It("exits 1 when a plugin exits 1", func() {
-			session := Cf("exit1").Wait(5 * time.Second)
+			session := Cf("exit1")
 			Eventually(session).Should(Exit(1))
 		})
 
