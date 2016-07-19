37a7c8d53524e36ac1cd293491c125c6c60869b0
Anand Gaitonde <agaitonde@pivotal.io>
2016-07-19 16:08:12 -0700

    introduce go-flags
    DuhDuhDUH
    
    [#126256605]
    
    Signed-off-by: Corbin Halliwill <challiwill@pivotal.io>

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"code.cloudfoundry.org/cli/cf/cmd"
+	"code.cloudfoundry.org/cli/commands/v2"
+	"github.com/jessevdk/go-flags"
 )
 
 func main() {
-	traceEnv := os.Getenv("CF_TRACE")
-	cmd.Main(traceEnv, os.Args)
+	parser := flags.NewParser(&v2.Commands, flags.HelpFlag)
+	_, err := parser.Parse()
+
+	if flagsErr, ok := err.(*flags.Error); ok {
+		if flagsErr.Type == flags.ErrUnknownCommand ||
+			flagsErr.Type == flags.ErrUnknownFlag ||
+			flagsErr.Type == flags.ErrHelp {
+			cmd.Main(os.Getenv("CF_TRACE"), os.Args)
+		}
+		fmt.Println("Missed go-flags err, fix it:", flagsErr.Type, err.Error())
+		os.Exit(1)
+	} else {
+		fmt.Println("THIS IS BAD, FIX IT:", err.Error())
+		os.Exit(1)
+	}
 }
