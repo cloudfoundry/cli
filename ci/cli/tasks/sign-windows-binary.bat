a290841fd4f3d9ea085c4ba4d7a08122c57104a6
Difan Zhao <dzhao@pivotal.io>
2016-08-05 15:45:48 -0700

    move binary to the right output
    [#127770261]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- ci/cli/tasks/sign-windows-binary.bat
+++ ci/cli/tasks/sign-windows-binary.bat
@@ -3,4 +3,4 @@ SET PATH=C:\Program Files (x86)\Windows Kits\10\bin\x64;%PATH%
 signtool sign /f %SIGNTOOL_CERT_PATH% /p %SIGNTOOL_CERT_PASSWORD% /t http://timestamp.comodoca.com/authenticode %CD%\windows-signing\cf-cli_win32.exe
 signtool sign /f %SIGNTOOL_CERT_PATH% /p %SIGNTOOL_CERT_PASSWORD% /t http://timestamp.comodoca.com/authenticode %CD%\windows-signing\cf-cli_winx64.exe
 
-MOVE %CD%\windows-signing  %CD%\signed
+MOVE %CD%\windows-signing\*  %CD%\signed
