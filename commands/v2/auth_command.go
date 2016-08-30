830237c695b9dba942aa1a07b124ea4e606d0de6
Difan Zhao <dzhao@pivotal.io>
2016-08-30 12:23:45 -0700

    fix capitalization of examples on auth help
    [#126256617]
    
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- commands/v2/auth_command.go
+++ commands/v2/auth_command.go
@@ -10,7 +10,7 @@ import (
 
 type AuthCommand struct {
 	RequiredArgs    flags.Authentication `positional-args:"yes"`
-	usage           interface{}          `usage:"CF_NAME auth USERNAME PASSWORD\n\nWARNING:\n   Providing your password as a command line option is highly discouraged\n   Your password may be visible to others and may be recorded in your shell history\n\nExamples:\n   CF_NAME auth name@example.com \"my password\" (use quotes for passwords with a space)\n   CF_NAME auth name@example.com \"\\\"password\\\"\" (escape quotes if used in password)"`
+	usage           interface{}          `usage:"CF_NAME auth USERNAME PASSWORD\n\nWARNING:\n   Providing your password as a command line option is highly discouraged\n   Your password may be visible to others and may be recorded in your shell history\n\nEXAMPLES:\n   CF_NAME auth name@example.com \"my password\" (use quotes for passwords with a space)\n   CF_NAME auth name@example.com \"\\\"password\\\"\" (escape quotes if used in password)"`
 	relatedCommands interface{}          `related_commands:"api, login, target"`
 }
 
