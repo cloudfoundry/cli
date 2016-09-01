e1e47ccd6e1743a2181de11b9bb858f82f17c989
Anand Gaitonde <agaitonde@pivotal.io>
2016-09-01 14:24:26 -0700

    use custom flag for memory and update go-flags
    - go-flags now supports negative values for int values, but not for
    anything else. Need to update go-flags to support '-' values for custom
    flags.
    
    [#127131179]
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

---
+++ commands/flags/memory_with_unlimited.go
@@ -0,0 +1,8 @@
+package flags
+
+type MemoryWithUnlimited int64
+
+//TODO:Code for this flag exists in cf/formatters/bytes.go, move tests from there to here
+func (m *MemoryWithUnlimited) UnmarshalFlag(val string) error {
+	return nil
+}
