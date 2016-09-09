17d81e970e6957b82fc29a064f4cddd1a3eb50f4
Difan Zhao <dzhao@pivotal.io>
2016-09-09 15:52:46 -0700

    add options to cf space usage
    [finish #125115361]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- commands/v2/space_command.go
+++ commands/v2/space_command.go
@@ -12,7 +12,7 @@ type SpaceCommand struct {
 	RequiredArgs       flags.Space `positional-args:"yes"`
 	GUID               bool        `long:"guid" description:"Retrieve and display the given space's guid.  All other output for the space is suppressed."`
 	SecurityGroupRules bool        `long:"security-group-rules" description:"Retrieve the rules for all the security groups associated with the space"`
-	usage              interface{} `usage:"CF_NAME space SPACE"`
+	usage              interface{} `usage:"CF_NAME space SPACE [--guid] [--security-group-rules]"`
 	relatedCommands    interface{} `related_commands:"space-users"`
 }
 
