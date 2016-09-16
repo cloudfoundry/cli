6038d2107010eb998e27d704df6758f90d16495d
Difan Zhao <dzhao@pivotal.io>
2016-09-16 12:12:16 -0700

    remove signing from pipeline
    [#130541949]
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- cli/tasks/create-installers-windows.bat
+++ cli/tasks/create-installers-windows.bat
@@ -5,15 +5,10 @@ SET /p VERSION=<%ROOT_DIR%\cli\VERSION
 SET PATH=C:\Program Files\GnuWin32\bin;%PATH%
 SET PATH=C:\Program Files (x86)\Inno Setup 5;%PATH%
 
-REM You must have added to your Inno Setup a Signtool: (http://revolution.screenstepslive.com/s/revolution/m/10695/l/95041-signing-installers-you-create-with-inno-setup)
-REM Name: "signtool", Command: "signtool.exe $p"
-REM This is to add "signtool.exe" to your path, so it does not need to be fully qualified in the configuration above
 SET PATH=C:\Program Files (x86)\Windows Kits\10\bin\x64;%PATH%
 
 sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
 sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
-sed -i -e "s/SIGNTOOL_CERT_PASSWORD/%SIGNTOOL_CERT_PASSWORD%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
-sed -i -e "s/SIGNTOOL_CERT_PATH/%SIGNTOOL_CERT_PATH%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
 
 pushd %ROOT_DIR%\cf-cli-binaries
 	gzip -d cf-cli-binaries.tgz
@@ -29,8 +24,6 @@ zip %ROOT_DIR%\winstallers\cf-cli-installer_winx64.zip cf_installer.exe
 
 sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x86.iss
 sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\installers\windows\windows-installer-x86.iss
-sed -i -e "s/SIGNTOOL_CERT_PASSWORD/%SIGNTOOL_CERT_PASSWORD%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x86.iss
-sed -i -e "s/SIGNTOOL_CERT_PATH/%SIGNTOOL_CERT_PATH%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x86.iss
 
 pushd %ROOT_DIR%\cf-cli-binaries
 	MOVE cf-cli_win32.exe ..\cf.exe
