3c875e96957c17310301c75b30c04fcc5a8cacea
Anand Gaitonde <agaitonde@pivotal.io>
2016-09-20 16:31:49 -0700

    move installers and VERSION into ci directory
    [#130691251]
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- ci/cli/tasks/create-installers-windows.bat
+++ ci/cli/tasks/create-installers-windows.bat
@@ -1,14 +1,14 @@
 SET ROOT_DIR=%CD%
 SET ESCAPED_ROOT_DIR=%ROOT_DIR:\=\\%
-SET /p VERSION=<%ROOT_DIR%\cli\VERSION
+SET /p VERSION=<%ROOT_DIR%\cli-ci\ci\VERSION
 
 SET PATH=C:\Program Files\GnuWin32\bin;%PATH%
 SET PATH=C:\Program Files (x86)\Inno Setup 5;%PATH%
 
 SET PATH=C:\Program Files (x86)\Windows Kits\10\bin\x64;%PATH%
 
-sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
-sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
+sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli-ci\ci\installers\windows\windows-installer-x64.iss
+sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli-ci\ci\installers\windows\windows-installer-x64.iss
 
 pushd %ROOT_DIR%\cf-cli-binaries
 	gzip -d cf-cli-binaries.tgz
@@ -16,21 +16,21 @@ pushd %ROOT_DIR%\cf-cli-binaries
 	MOVE cf-cli_winx64.exe ..\cf.exe
 popd
 
-ISCC %ROOT_DIR%\cli\installers\windows\windows-installer-x64.iss
+ISCC %ROOT_DIR%\cli-ci\ci\installers\windows\windows-installer-x64.iss
 
-MOVE %ROOT_DIR%\cli\installers\windows\Output\mysetup.exe cf_installer.exe
+MOVE %ROOT_DIR%\cli-ci\ci\installers\windows\Output\mysetup.exe cf_installer.exe
 
 zip %ROOT_DIR%\winstallers\cf-cli-installer_winx64.zip cf_installer.exe
 
-sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli\installers\windows\windows-installer-x86.iss
-sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli\installers\windows\windows-installer-x86.iss
+sed -i -e "s/VERSION/%VERSION%/" %ROOT_DIR%\cli-ci\ci\installers\windows\windows-installer-x86.iss
+sed -i -e "s/CF_SOURCE/%ESCAPED_ROOT_DIR%\\cf.exe/" %ROOT_DIR%\cli-ci\ci\installers\windows\windows-installer-x86.iss
 
 pushd %ROOT_DIR%\cf-cli-binaries
 	MOVE cf-cli_win32.exe ..\cf.exe
 popd
 
-ISCC %ROOT_DIR%\cli\installers\windows\windows-installer-x86.iss
+ISCC %ROOT_DIR%\cli-ci\ci\installers\windows\windows-installer-x86.iss
 
-MOVE %ROOT_DIR%\cli\installers\windows\Output\mysetup.exe cf_installer.exe
+MOVE %ROOT_DIR%\cli-ci\ci\installers\windows\Output\mysetup.exe cf_installer.exe
 
 zip %ROOT_DIR%\winstallers\cf-cli-installer_win32.zip cf_installer.exe
