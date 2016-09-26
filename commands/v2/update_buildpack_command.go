ab42a5342aebf67527e37ba402902828aeec3a83
Anand Gaitonde <agaitonde@pivotal.io>
2016-09-26 13:54:24 -0700

    path should be a string, not an int
    [#131127335]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- commands/v2/update_buildpack_command.go
+++ commands/v2/update_buildpack_command.go
@@ -14,7 +14,7 @@ type UpdateBuildpackCommand struct {
 	Enable          bool            `long:"enable" description:"Enable the buildpack to be used for staging"`
 	Order           int             `short:"i" description:"The order in which the buildpacks are checked during buildpack auto-detection"`
 	Lock            bool            `long:"lock" description:"Lock the buildpack to prevent updates"`
-	Path            int             `short:"p" description:"Path to directory or zip file"`
+	Path            string          `short:"p" description:"Path to directory or zip file"`
 	Unlock          bool            `long:"unlock" description:"Unlock the buildpack to enable updates"`
 	usage           interface{}     `usage:"CF_NAME update-buildpack BUILDPACK [-p PATH] [-i POSITION] [--enable|--disable] [--lock|--unlock]\n\nTIP:\n   Path should be a zip file, a url to a zip file, or a local directory. Position is a positive integer, sets priority, and is sorted from lowest to highest."`
 	relatedCommands interface{}     `related_commands:"buildpacks, rename-buildpack"`
