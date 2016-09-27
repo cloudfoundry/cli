ccaaba4a26da09c329163fdf11e35d6fc0009e38
Matthew Boedicker <mboedicker@pivotal.io>
2016-09-27 12:16:52 -0700

    Replace init() function with more idiomatic var _
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- utils/generic/slice_test.go
+++ utils/generic/slice_test.go
@@ -6,18 +6,16 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func init() {
-	Describe("IsSliceable", func() {
-		It("should return false when the type cannot be sliced", func() {
-			Expect(generic.IsSliceable("bad slicing")).To(BeFalse())
-		})
-
-		It("should return true if the type can be sliced", func() {
-			Expect(generic.IsSliceable([]string{"a string"})).To(BeTrue())
-		})
-
-		It("should return true if the type can be sliced", func() {
-			Expect(generic.IsSliceable([]interface{}{1, 2, 3})).To(BeTrue())
-		})
+var _ = Describe("IsSliceable", func() {
+	It("should return false when the type cannot be sliced", func() {
+		Expect(generic.IsSliceable("bad slicing")).To(BeFalse())
 	})
-}
+
+	It("should return true if the type can be sliced", func() {
+		Expect(generic.IsSliceable([]string{"a string"})).To(BeTrue())
+	})
+
+	It("should return true if the type can be sliced", func() {
+		Expect(generic.IsSliceable([]interface{}{1, 2, 3})).To(BeTrue())
+	})
+})
