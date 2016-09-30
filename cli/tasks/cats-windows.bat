c4da8e5b734d72c47168ccac581d033cd72c3731
Nick Wei <nwei@pivotal.io>
2016-09-30 16:33:36 -0700

    remove skipPackages ginkgo flag from pipeline and tasks
    [#131477843]
    
    Signed-off-by: Matthew Boedicker <mboedicker@pivotal.io>

--- cli/tasks/cats-windows.bat
+++ cli/tasks/cats-windows.bat
@@ -24,4 +24,4 @@ popd
 go get -v github.com/onsi/ginkgo/ginkgo
 
 cd %GATSPATH%
-ginkgo.exe -r -slowSpecThreshold=120 -skipPackage="logging,services,v3,routing_api,routing,backend_compatibility,ssh" -skip="NO_DEA_SUPPORT|go makes the app reachable via its bound route|SSO|takes effect after a restart, not requiring a push|doesn't die when printing 32MB|exercises basic loggregator|firehose data|Downloads the droplet for the app" -nodes=2
+ginkgo.exe -r -slowSpecThreshold=120 -skip="NO_DEA_SUPPORT|go makes the app reachable via its bound route|SSO|takes effect after a restart, not requiring a push|doesn't die when printing 32MB|exercises basic loggregator|firehose data|Downloads the droplet for the app" -nodes=2
