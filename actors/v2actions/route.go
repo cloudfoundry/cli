f2b1932c1639f0f6ce6d3f44a9d006bcc17afec8
Matthew Boedicker <mboedicker@pivotal.io>
2016-10-07 14:26:13 -0700

    Add delete orphaned routes command
    [#126256743, #131127157]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

---
+++ actors/v2actions/route.go
@@ -0,0 +1,40 @@
+package v2actions
+
+import "fmt"
+
+type Route struct {
+	GUID     string
+	Hostname string
+	Domain   string
+	Path     string
+	Port     int
+}
+
+func (actor Actor) GetOrphanedRoutes(spaceGUID string) ([]Route, Warnings, error) {
+	return nil, nil, nil
+}
+
+func (actor Actor) DeleteRoute(routeGUID string) (Warnings, error) {
+	return nil, nil
+}
+
+func (r Route) String() string {
+	var routeString string
+
+	if r.Hostname != "" {
+		routeString = fmt.Sprintf("%s.%s", r.Hostname, r.Domain)
+	} else {
+		routeString = r.Domain
+	}
+
+	if r.Port != 0 {
+		routeString = fmt.Sprintf("%s:%d", routeString, r.Port)
+		return routeString
+	}
+
+	if r.Path != "" {
+		routeString = fmt.Sprintf("%s/%s", routeString, r.Path)
+	}
+
+	return routeString
+}
