e010ca91fcd6b0610f0d3d2cf93a5ebd6341d895
Anand Gaitonde <agaitonde@pivotal.io>
2016-10-12 11:11:50 -0700

    run integration tests from cli repo on Linux and Windows
    [#132181413]
    
    Signed-off-by: Matthew Boedicker <mboedicker@pivotal.io>

---
+++ ci/cli/tasks/integration-windows.bat
@@ -0,0 +1,25 @@
+SET GOPATH=%CD%\go
+SET GATSPATH=%GOPATH%\src\code.cloudfoundry.org\cli
+
+SET PATH=C:\Go\bin;%PATH%
+SET PATH=C:\Program Files\Git\cmd\;%PATH%
+SET PATH=%GOPATH%\bin;%PATH%
+SET PATH=C:\Program Files\GnuWin32\bin;%PATH%
+SET PATH=C:\Program Files\cURL\bin;%PATH%
+SET PATH=%CD%;%PATH%
+
+SET /p DOMAIN=<%CD%\bosh-lite-lock\name
+SET CF_API=api.%DOMAIN%
+call %CD%\cli-ci\ci\cli\tasks\create-cats-config.bat
+SET CONFIG=%CD%\config.json
+
+pushd %CD%\cf-cli-binaries
+	gzip -d cf-cli-binaries.tgz
+	tar -xvf cf-cli-binaries.tar
+	MOVE %CD%\cf-cli_winx64.exe ..\cf.exe
+popd
+
+go get -v github.com/onsi/ginkgo/ginkgo
+
+cd %GATSPATH%
+ginkgo.exe -r -nodes=4 -slowSpecThreshold=30 -randomizeSuites ./integration
