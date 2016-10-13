44f5d1d318ddcba14884effca620586632317db9
Matthew Boedicker <mboedicker@pivotal.io>
2016-10-12 19:06:53 -0700

    add api call to get service instances from space endpoint
    [#129631341, #126256767]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- api/cloudcontroller/ccv2/service_instance.go
+++ api/cloudcontroller/ccv2/service_instance.go
@@ -7,9 +7,13 @@ import (
 	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv2/internal"
 )
 
+const USER_PROVIDED_SERVICE = "user_provided_service_instance"
+const MANAGED_SERVICE = "managed_service_instance"
+
 type ServiceInstance struct {
 	GUID string
 	Name string
+	Type string
 }
 
 func (serviceInstance *ServiceInstance) UnmarshalJSON(data []byte) error {
@@ -17,6 +21,7 @@ func (serviceInstance *ServiceInstance) UnmarshalJSON(data []byte) error {
 		Metadata internal.Metadata
 		Entity   struct {
 			Name string
+			Type string
 		}
 	}
 	err := json.Unmarshal(data, &ccServiceInstance)
@@ -26,13 +31,69 @@ func (serviceInstance *ServiceInstance) UnmarshalJSON(data []byte) error {
 
 	serviceInstance.GUID = ccServiceInstance.Metadata.GUID
 	serviceInstance.Name = ccServiceInstance.Entity.Name
+	serviceInstance.Type = ccServiceInstance.Entity.Type
 	return nil
+}
+
+func (serviceInstance ServiceInstance) UserProvided() bool {
+	return serviceInstance.Type == USER_PROVIDED_SERVICE
+}
+
+func (serviceInstance ServiceInstance) Managed() bool {
+	return serviceInstance.Type == MANAGED_SERVICE
 }
 
 func (client *CloudControllerClient) GetServiceInstances(queries []Query) ([]ServiceInstance, Warnings, error) {
 	request := cloudcontroller.Request{
 		RequestName: internal.ServiceInstancesRequest,
 		Query:       FormatQueryParameters(queries),
+	}
+
+	allServiceInstancesList := []ServiceInstance{}
+	allWarningsList := Warnings{}
+
+	for {
+		var serviceInstances []ServiceInstance
+		wrapper := PaginatedWrapper{
+			Resources: &serviceInstances,
+		}
+		response := cloudcontroller.Response{
+			Result: &wrapper,
+		}
+
+		err := client.connection.Make(request, &response)
+		allWarningsList = append(allWarningsList, response.Warnings...)
+		if err != nil {
+			return nil, allWarningsList, err
+		}
+
+		allServiceInstancesList = append(allServiceInstancesList, serviceInstances...)
+
+		if wrapper.NextURL == "" {
+			break
+		}
+		request = cloudcontroller.Request{
+			URI:    wrapper.NextURL,
+			Method: "GET",
+		}
+	}
+
+	return allServiceInstancesList, allWarningsList, nil
+}
+
+func (client *CloudControllerClient) GetSpaceServiceInstances(spaceGUID string, includeUserProvidedServices bool, queries []Query) ([]ServiceInstance, Warnings, error) {
+	query := FormatQueryParameters(queries)
+
+	if includeUserProvidedServices {
+		query.Add("return_user_provided_service_instances", "true")
+	}
+
+	request := cloudcontroller.Request{
+		RequestName: internal.SpaceServiceInstancesRequest,
+		Params: map[string]string{
+			"guid": spaceGUID,
+		},
+		Query: query,
 	}
 
 	allServiceInstancesList := []ServiceInstance{}
