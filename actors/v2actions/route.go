ea711206605df47affb95c6b4b9f9b4a2463918f
Matthew Boedicker <mboedicker@pivotal.io>
2016-10-14 11:50:52 -0700

    godoc comments for actor methods
    - fixed error typo
    
    [#126256743, #131127157]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- actors/v2actions/route.go
+++ actors/v2actions/route.go
@@ -6,14 +6,20 @@ import (
 	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv2"
 )
 
+// Route represents a CLI Route.
 type Route ccv2.Route
 
+// OrphanedRoutesNotFoundError is an error wrapper that represents the case
+// when no orphaned routes are found.
 type OrphanedRoutesNotFoundError struct{}
 
+// Error method to display the error message.
 func (e OrphanedRoutesNotFoundError) Error() string {
-	return fmt.Sprintf("No orpahend routes were found.")
+	return fmt.Sprintf("No orphaned routes were found.")
 }
 
+// GetOrphanedRoutesBySpace returns a list of orphaned routes associated with
+// the provided Space GUID.
 func (actor Actor) GetOrphanedRoutesBySpace(spaceGUID string) ([]Route, Warnings, error) {
 	var (
 		orphanedRoutes []Route
@@ -57,6 +63,7 @@ func (actor Actor) GetOrphanedRoutesBySpace(spaceGUID string) ([]Route, Warnings
 	return orphanedRoutes, allWarnings, err
 }
 
+// DeleteRouteByGUID deletes the Route associated with the provided Route GUID.
 func (actor Actor) DeleteRouteByGUID(routeGUID string) (Warnings, error) {
 	warnings, err := actor.CloudControllerClient.DeleteRoute(routeGUID)
 	return Warnings(warnings), err
