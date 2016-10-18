fb538043ea9762366dbaba1e8bc5192151c5941d
Matthew Boedicker <mboedicker@pivotal.io>
2016-10-18 14:41:54 -0700

    improve help description for bind-route-service --path option
    [finishes #130413513]

--- commands/v2/bind_route_service_command.go
+++ commands/v2/bind_route_service_command.go
@@ -12,7 +12,7 @@ type BindRouteServiceCommand struct {
 	RequiredArgs           flags.RouteServiceArgs `positional-args:"yes"`
 	ParametersAsJSON       string                 `short:"c" description:"Valid JSON object containing service-specific configuration parameters, provided inline or in a file. For a list of supported configuration parameters, see documentation for the particular service offering."`
 	Hostname               string                 `long:"hostname" short:"n" description:"Hostname used in combination with DOMAIN to specify the route to bind"`
-	Path                   string                 `long:"path" description:"Path for the HTTP route"`
+	Path                   string                 `long:"path" description:"Path used in combination with HOSTNAME and DOMAIN to specify the route to bind"`
 	usage                  interface{}            `usage:"CF_NAME bind-route-service DOMAIN SERVICE_INSTANCE [--hostname HOSTNAME] [--path PATH] [-c PARAMETERS_AS_JSON]\n\nEXAMPLES:\n   CF_NAME bind-route-service example.com myratelimiter --hostname myapp --path foo\n   CF_NAME bind-route-service example.com myratelimiter -c file.json\n   CF_NAME bind-route-service example.com myratelimiter -c '{\"valid\":\"json\"}'\n\n   In Windows PowerShell use double-quoted, escaped JSON: \"{\\\"valid\\\":\\\"json\\\"}\"\n   In Windows Command Line use single-quoted, escaped JSON: '{\\\"valid\\\":\\\"json\\\"}'"`
 	relatedCommands        interface{}            `related_commands:"routes, services"`
 	BackwardsCompatibility bool                   `short:"f" hidden:"true" description:"This is for backwards compatibility"`
