2fe2e246fb9818fd51069f3a6527b84bc5615a7c
Matthew Boedicker <mboedicker@pivotal.io>
2016-10-31 09:05:29 -0700

    add ORG to usage message
    [#130167035]

---
+++ integration/delete_space_command_test.go
@@ -0,0 +1,19 @@
+package integration
+
+import (
+	. "code.cloudfoundry.org/cli/integration/helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("delete-space command", func() {
+	Describe("help", func() {
+		It("shows usage", func() {
+			session := CF("help", "delete-space")
+			Eventually(session).Should(Exit(0))
+			Expect(session).To(Say("delete-space SPACE \\[-o ORG\\] \\[-f\\]"))
+		})
+	})
+})
