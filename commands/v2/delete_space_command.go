81377c2161d76ca310222898d1a918016e8f10ec
Matthew Boedicker <mboedicker@pivotal.io>
2016-10-31 09:05:29 -0700

    add ORG to usage message
    [#130167035]

--- commands/v2/delete_space_command.go
+++ commands/v2/delete_space_command.go
@@ -12,7 +12,7 @@ type DeleteSpaceCommand struct {
 	RequiredArgs flags.Space `positional-args:"yes"`
 	Force        bool        `short:"f" description:"Force deletion without confirmation"`
 	Org          string      `short:"o" description:"Delete space within specified org"`
-	usage        interface{} `usage:"CF_NAME delete-space SPACE [-o] [-f]"`
+	usage        interface{} `usage:"CF_NAME delete-space SPACE [-o ORG] [-f]"`
 }
 
 func (_ DeleteSpaceCommand) Setup(config commands.Config, ui commands.UI) error {
