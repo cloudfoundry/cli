831141cfd8a65fe8a1cc5ef8ac607e9f874b63b3
Matthew Boedicker <mboedicker@pivotal.io>
2016-11-08 15:24:58 -0800

    integration tests for verbose flag and proxy
    [finishes #127286907]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- integration/proxy_connection_test.go
+++ integration/proxy_connection_test.go
@@ -1,11 +1,32 @@
 package integration
 
 import (
+	"code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
 )
 
-var _ = Describe("trace", func() {
-	It("handles a proxy", func() {
-		Skip("Fill in during #127286907")
+var _ = Describe("proxy", func() {
+	var proxyURL string
+	BeforeEach(func() {
+		proxyURL = "127.0.0.1:9999"
+	})
+
+	Context("V2 Legacy", func() {
+		It("handles a proxy", func() {
+			session := helpers.CFWithEnv(map[string]string{"http_proxy": proxyURL}, "api", apiURL)
+			Eventually(session).Should(Say("Error performing request: Get https://api.bosh-lite.com/v2/info: http: error connecting to proxy http://%s: dial tcp %s: getsockopt: connection refused", proxyURL, proxyURL))
+			Eventually(session).Should(Exit(1))
+		})
+	})
+
+	Context("V3", func() {
+		It("handles a proxy", func() {
+			session := helpers.CFWithEnv(map[string]string{"http_proxy": proxyURL}, "run-task", "app", "echo")
+			Eventually(session.Err).Should(Say("Get https://api.bosh-lite.com: http: error connecting to proxy http://%s: dial tcp %s: getsockopt: connection refused", proxyURL, proxyURL))
+			Eventually(session).Should(Exit(1))
+		})
 	})
 })
--- integration/verbose_flag_test.go
+++ integration/verbose_flag_test.go
@@ -12,26 +12,81 @@ import (
 )
 
 var _ = Describe("Verbose", func() {
-	DescribeTable("displays verbose output",
-		func(command func() *Session) {
-			login := exec.Command("cf", "auth", "admin", "admin")
-			loginSession, err := Start(login, GinkgoWriter, GinkgoWriter)
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(loginSession).Should(Exit(0))
+	Context("v2 legacy", func() {
+		DescribeTable("displays verbose output",
+			func(command func() *Session) {
+				login := exec.Command("cf", "auth", "admin", "admin")
+				loginSession, err := Start(login, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(loginSession).Should(Exit(0))
 
-			session := command()
-			Eventually(session).Should(Say("REQUEST:"))
-			Eventually(session).Should(Say("GET /v2/organizations"))
-			Eventually(session).Should(Say("RESPONSE:"))
-			Eventually(session).Should(Exit(0))
-		},
+				session := command()
+				Eventually(session).Should(Say("REQUEST:"))
+				Eventually(session).Should(Say("GET /v2/organizations"))
+				Eventually(session).Should(Say("RESPONSE:"))
+				Eventually(session).Should(Exit(0))
+			},
 
-		Entry("when the -v option is provided with additional command", func() *Session {
-			return helpers.CF("-v", "orgs")
-		}),
+			Entry("when the -v option is provided with additional command", func() *Session {
+				return helpers.CF("-v", "orgs")
+			}),
 
-		Entry("when the CF_TRACE env variable is set", func() *Session {
-			return helpers.CFWithEnv(map[string]string{"CF_TRACE": "true"}, "orgs")
-		}),
-	)
+			Entry("when the CF_TRACE env variable is set", func() *Session {
+				return helpers.CFWithEnv(map[string]string{"CF_TRACE": "true"}, "orgs")
+			}),
+		)
+	})
+
+	Context("v2 refactor", func() {
+		DescribeTable("displays verbose output",
+			func(command func() *Session) {
+				Skip("skip #133310639")
+				orgName := helpers.PrefixedRandomName("testorg")
+				spaceName := helpers.PrefixedRandomName("testspace")
+				setupCF(orgName, spaceName)
+
+				defer helpers.CF("delete-org", "-f", orgName)
+				session := command()
+
+				Eventually(session).Should(Say("REQUEST:"))
+				Eventually(session).Should(Say("GET /v2/apps"))
+				Eventually(session).Should(Say("RESPONSE:"))
+				Eventually(session).Should(Exit(1))
+			},
+
+			Entry("when the -v option is provided with additional command", func() *Session {
+				return helpers.CF("-v", "unbind-service", "app-name", "service-instance")
+			}),
+
+			Entry("when the CF_TRACE env variable is set", func() *Session {
+				return helpers.CFWithEnv(map[string]string{"CF_TRACE": "true"}, "unbind-service", "app-name", "service-instance")
+			}),
+		)
+	})
+
+	Context("v3", func() {
+		DescribeTable("displays verbose output",
+			func(command func() *Session) {
+				orgName := helpers.PrefixedRandomName("testorg")
+				spaceName := helpers.PrefixedRandomName("testspace")
+				setupCF(orgName, spaceName)
+
+				defer helpers.CF("delete-org", "-f", orgName)
+				session := command()
+
+				Eventually(session).Should(Say("REQUEST:"))
+				Eventually(session).Should(Say("GET /v3/apps"))
+				Eventually(session).Should(Say("RESPONSE:"))
+				Eventually(session).Should(Exit(1))
+			},
+
+			Entry("when the -v option is provided with additional command", func() *Session {
+				return helpers.CF("-v", "run-task", "app", "echo")
+			}),
+
+			Entry("when the CF_TRACE env variable is set", func() *Session {
+				return helpers.CFWithEnv(map[string]string{"CF_TRACE": "true"}, "run-task", "app", "echo")
+			}),
+		)
+	})
 })
