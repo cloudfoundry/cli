08ddb119899578d9a1d35999a0c2f46e590b215b
Anand Gaitonde <agaitonde@pivotal.io>
2016-11-08 16:07:35 -0800

    do not hardcode api URL in proxy integration tests
    [#127286907]
    
    Signed-off-by: Matthew Boedicker <mboedicker@pivotal.io>

--- integration/proxy_connection_test.go
+++ integration/proxy_connection_test.go
@@ -17,7 +17,7 @@ var _ = Describe("proxy", func() {
 	Context("V2 Legacy", func() {
 		It("handles a proxy", func() {
 			session := helpers.CFWithEnv(map[string]string{"http_proxy": proxyURL}, "api", apiURL)
-			Eventually(session).Should(Say("Error performing request: Get https://api.bosh-lite.com/v2/info: http: error connecting to proxy http://%s: dial tcp %s: getsockopt: connection refused", proxyURL, proxyURL))
+			Eventually(session).Should(Say("Error performing request: Get https://%s/v2/info: http: error connecting to proxy http://%s: dial tcp %s: getsockopt: connection refused", apiURL, proxyURL, proxyURL))
 			Eventually(session).Should(Exit(1))
 		})
 	})
@@ -25,7 +25,7 @@ var _ = Describe("proxy", func() {
 	Context("V3", func() {
 		It("handles a proxy", func() {
 			session := helpers.CFWithEnv(map[string]string{"http_proxy": proxyURL}, "run-task", "app", "echo")
-			Eventually(session.Err).Should(Say("Get https://api.bosh-lite.com: http: error connecting to proxy http://%s: dial tcp %s: getsockopt: connection refused", proxyURL, proxyURL))
+			Eventually(session.Err).Should(Say("Get https://%s: http: error connecting to proxy http://%s: dial tcp %s: getsockopt: connection refused", apiURL, proxyURL, proxyURL))
 			Eventually(session).Should(Exit(1))
 		})
 	})
