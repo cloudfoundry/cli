4d2fa0d7899730473bbaa3342ff17eff28d5b789
Jeff Pak <jeffrey.pak@emc.com>
2016-11-16 14:46:05 -0800

    handle int parsing if passed in string
    [finishes #134202499]
    
    Signed-off-by: Matthew Boedicker <mboedicker@pivotal.io>

--- integration/update_buildpack_command_test.go
+++ integration/update_buildpack_command_test.go
@@ -4,6 +4,7 @@ import (
 	. "code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
 )
 
@@ -14,6 +15,15 @@ var _ = Describe("unbind-service command", func() {
 			Eventually(session).Should(Exit(1))
 
 			Expect(session.Err.Contents()).To(BeEquivalentTo("Incorrect Usage: the required argument `BUILDPACK` was not provided\n\n"))
+		})
+	})
+
+	Context("when the wrong data type is provided as the position argument", func() {
+		It("outputs an error message to the user, provides help text, and exits 1", func() {
+			session := CF("update-buildpack", "some-buildpack", "-i", "not-an-integer")
+			Eventually(session).Should(Exit(1))
+			Expect(session.Err).To(Say("Incorrect Usage: invalid argument for flag `-i' \\(expected int\\)"))
+			Expect(session.Out).To(Say("cf update-buildpack BUILDPACK")) // help
 		})
 	})
 })
