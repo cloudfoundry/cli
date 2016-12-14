dee476b3da6c93bd08de3a9b8a15629cef5eaee8
Matthew Boedicker <mboedicker@pivotal.io>
2016-12-14 15:54:54 -0800

    do not . import helpers
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- integration/isolated/delete_space_command_test.go
+++ integration/isolated/delete_space_command_test.go
@@ -1,7 +1,7 @@
 package isolated
 
 import (
-	. "code.cloudfoundry.org/cli/integration/helpers"
+	"code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
@@ -11,7 +11,7 @@ import (
 var _ = Describe("delete-space command", func() {
 	Describe("help", func() {
 		It("shows usage", func() {
-			session := CF("help", "delete-space")
+			session := helpers.CF("help", "delete-space")
 			Eventually(session).Should(Exit(0))
 			Expect(session).To(Say("delete-space SPACE \\[-o ORG\\] \\[-f\\]"))
 		})
