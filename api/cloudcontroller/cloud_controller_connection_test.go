a6659d2ca9c658852f555e770f8bdb72d9b3c498
Anand Gaitonde <agaitonde@pivotal.io>
2016-12-20 13:48:48 -0800

    switching to using a custom DNS record instead of xip.io
    this test became super flaky, a custom DNS record was added to our ci
    domain that points to 127.0.0.1.
    
    Signed-off-by: Alex Melnik <aleksandr.s.melnik@hpe.com>

--- api/cloudcontroller/cloud_controller_connection_test.go
+++ api/cloudcontroller/cloud_controller_connection_test.go
@@ -186,15 +186,16 @@ var _ = Describe("Cloud Controller Connection", func() {
 						connection = NewConnection(Config{})
 					})
 
+					// loopback.cli.ci.cf-app.com is a custom DNS record setup to point to 127.0.0.1
 					It("returns a SSLValidationHostnameError", func() {
-						altHostURL := strings.Replace(server.URL(), "127.0.0.1", "127.0.0.1.xip.io", -1)
+						altHostURL := strings.Replace(server.URL(), "127.0.0.1", "loopback.cli.ci.cf-app.com", -1)
 						request, err := http.NewRequest(http.MethodGet, altHostURL, nil)
 						Expect(err).ToNot(HaveOccurred())
 
 						var response Response
 						err = connection.Make(request, &response)
 						Expect(err).To(MatchError(SSLValidationHostnameError{
-							Message: "x509: certificate is valid for example.com, not 127.0.0.1.xip.io",
+							Message: "x509: certificate is valid for example.com, not loopback.cli.ci.cf-app.com",
 						}))
 					})
 				})
