846d28423e354187614a0adb241e607a846bfc34
Nick Wei <nwei@pivotal.io>
2016-12-20 09:10:35 -0800

    integration tests for delete-user cleans up after itself
    [#136152875]

--- integration/isolated/delete_user_command_test.go
+++ integration/isolated/delete_user_command_test.go
@@ -1,6 +1,9 @@
 package isolated
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -10,25 +13,53 @@ import (
 
 var _ = Describe("delete-user command", func() {
 	BeforeEach(func() {
-		helpers.RunIfExperimental("")
+		helpers.RunIfExperimental("skip until #133310639")
 	})
+
 	Context("when the logged in user is authorized to delete-users", func() {
 		BeforeEach(func() {
 			helpers.LoginCF()
 		})
+
 		Context("when deleting a user that exists in multiple origins", func() {
-			var (
-				newUser string
-			)
+			var newUser string
+
 			BeforeEach(func() {
 				newUser = helpers.RandomUsername()
 				session := helpers.CF("create-user", newUser, "--origin", "ldap")
 				Eventually(session).Should(Exit(0))
 				session = helpers.CF("create-user", newUser, helpers.RandomPassword())
 				Eventually(session).Should(Exit(0))
-				session = helpers.CF("create-user", newUser, "--origin", "some-other-origin")
-				Eventually(session).Should(Exit(0))
 			})
+
+			AfterEach(func() {
+				// Doing the cleanup here because it can't easily be done in
+				// bin/cleanup-integration.
+				session := helpers.CF("curl", "/v2/users")
+				Eventually(session).Should(Exit(0))
+
+				var usersResponse struct {
+					Resources []struct {
+						Metadata struct {
+							GUID string `json:"guid"`
+						} `json:"metadata"`
+						Entity struct {
+							UserName string `json:"username"`
+						} `json:"entity"`
+					} `json:"resources"`
+				}
+
+				err := json.Unmarshal(session.Out.Contents(), &usersResponse)
+				Expect(err).NotTo(HaveOccurred())
+
+				for _, user := range usersResponse.Resources {
+					if user.Entity.UserName == newUser {
+						session = helpers.CF("curl", "-X", "DELETE", fmt.Sprintf("/v2/users/%s", user.Metadata.GUID))
+						Eventually(session).Should(Exit(0))
+					}
+				}
+			})
+
 			It("errors with DuplicateUsernameError", func() {
 				session := helpers.CF("delete-user", "-f", newUser)
 				Eventually(session.Out).Should(Say("FAILED"))
