4220368644cca0d487bd4f5dbdec931d24e53252
Nick Wei <nwei@pivotal.io>
2016-12-21 16:42:34 -0800

    the verbose integration test should be testing against a redacted request body
    [#136397599]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- integration/isolated/verbose_flag_test.go
+++ integration/isolated/verbose_flag_test.go
@@ -230,7 +230,7 @@ var _ = Describe("Verbose", func() {
 				Eventually(session).Should(Say("REQUEST:"))
 				Eventually(session).Should(Say("POST /oauth/token"))
 				Eventually(session).Should(Say("User-Agent: cf/[\\w.+]+ \\(%s; %s %s\\)", runtime.Version(), runtime.GOARCH, runtime.GOOS))
-				Eventually(session).Should(Say("grant_type=refresh_token")) //This is required to test the previous line. If it fails, the previous matcher went too far.
+				Eventually(session).Should(Say("\\[PRIVATE DATA HIDDEN\\]")) //This is required to test the previous line. If it fails, the previous matcher went too far.
 				Eventually(session).Should(Say("RESPONSE:"))
 				Eventually(session).Should(Exit(1))
 			},
