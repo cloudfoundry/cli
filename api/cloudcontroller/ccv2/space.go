40d964a4aafbba4860a4fe00be1f51ef1e3dd964
Nick Wei <nwei@pivotal.io>
2017-01-06 09:14:47 -0800

    add the api layer for target command

--- api/cloudcontroller/ccv2/space.go
+++ api/cloudcontroller/ccv2/space.go
@@ -1,5 +1,12 @@
 package ccv2
 
+import (
+	"encoding/json"
+
+	"code.cloudfoundry.org/cli/api/cloudcontroller"
+	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv2/internal"
+)
+
 // Space represents a Cloud Controller Space.
 type Space struct {
 	GUID     string
@@ -7,6 +14,47 @@ type Space struct {
 	AllowSSH bool
 }
 
+// UnmarshalJSON helps unmarshal a Cloud Controller Space response.
+func (space *Space) UnmarshalJSON(data []byte) error {
+	var ccSpace struct {
+		Metadata internal.Metadata `json:"metadata"`
+		Entity   struct {
+			Name     string `json:"name"`
+			AllowSSH bool   `json:"allow_ssh"`
+		} `json:"entity"`
+	}
+	if err := json.Unmarshal(data, &ccSpace); err != nil {
+		return err
+	}
+
+	space.GUID = ccSpace.Metadata.GUID
+	space.Name = ccSpace.Entity.Name
+	space.AllowSSH = ccSpace.Entity.AllowSSH
+	return nil
+}
+
+// GetSpaces returns back a list of Spaces based off of the provided queries.
 func (client *Client) GetSpaces(queries []Query) ([]Space, Warnings, error) {
-	return nil, nil, nil
+	request, err := client.newHTTPRequest(requestOptions{
+		RequestName: internal.SpacesRequest,
+		Query:       FormatQueryParameters(queries),
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var fullSpacesList []Space
+	warnings, err := client.paginate(request, Space{}, func(item interface{}) error {
+		if space, ok := item.(Space); ok {
+			fullSpacesList = append(fullSpacesList, space)
+		} else {
+			return cloudcontroller.UnknownObjectInListError{
+				Expected:   Space{},
+				Unexpected: item,
+			}
+		}
+		return nil
+	})
+
+	return fullSpacesList, warnings, err
 }
