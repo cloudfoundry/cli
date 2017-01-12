0cb3b5475d4a8f093cc6fcef2a5b6d68fe05effc
Anand Gaitonde <agaitonde@pivotal.io>
2017-01-11 19:05:14 -0800

    hidding the 'tt' flag since goflags has an issue supporting it
    [Finishes #137073155]

--- command/v2/ssh_command.go
+++ command/v2/ssh_command.go
@@ -13,7 +13,7 @@ type SSHCommand struct {
 	AppInstanceIndex    int          `long:"app-instance-index" short:"i" description:"Application instance index"`
 	Command             string       `long:"command" short:"c" description:"Command to run. This flag can be defined more than once."`
 	DisablePseudoTTY    bool         `long:"disable-pseudo-tty" short:"T" description:"Disable pseudo-tty allocation"`
-	ForcePseudoTTY      bool         `long:"force-pseudo-tty" short:"F" description:"Force pseudo-tty allocation"`
+	ForcePseudoTTY      bool         `long:"force-pseudo-tty" description:"Force pseudo-tty allocation"`
 	LocalPort           string       `short:"L" description:"Local port forward specification. This flag can be defined more than once."`
 	RemotePseudoTTY     bool         `long:"request-pseudo-tty" short:"t" description:"Request pseudo-tty allocation"`
 	SkipHostValidation  bool         `long:"skip-host-validation" short:"k" description:"Skip host key validation"`
