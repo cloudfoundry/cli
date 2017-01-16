da37801c0936acb61986e563d2dbc0f22ebe4de3
Matthew Boedicker <mboedicker@pivotal.io>
2017-01-16 08:20:30 -0800

    Remove worker IP output

--- ci/cli/tasks/cats-windows.bat
+++ ci/cli/tasks/cats-windows.bat
@@ -9,8 +9,6 @@ SET PATH=C:\Program Files\cURL\bin;%PATH%
 SET PATH=C:\Program Files\CloudFoundry;%PATH%
 SET PATH=%CD%;%PATH%
 
-curl ifconfig.co/json
-
 SET CONFIG=%CD%\cats-config\integration_config.json
 
 pushd %CD%\cf-cli-binaries
