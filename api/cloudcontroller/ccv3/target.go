999882db71ff10023514c1d2c78a38b62f5f0f9c
Anand Gaitonde <agaitonde@pivotal.io>
2017-01-25 18:17:19 -0800

    godoc for error handling, pre-alpha warning, and missing explainations

--- api/cloudcontroller/ccv3/target.go
+++ api/cloudcontroller/ccv3/target.go
@@ -10,20 +10,25 @@ import (
 // TargetSettings represents configuration for establishing a connection to the
 // Cloud Controller server.
 type TargetSettings struct {
-	DialTimeout       time.Duration
+	// DialTimeout is the DNS timeout used to make all requests to the Cloud
+	// Controller.
+	DialTimeout time.Duration
+
+	// SkipSSLValidation controls whether a client verifies the server's
+	// certificate chain and host name. If SkipSSLValidation is true, TLS accepts
+	// any certificate presented by the server and any host name in that
+	// certificate for *all* client requests going forward.
+	//
+	// In this mode, TLS is susceptible to man-in-the-middle attacks. This should
+	// be used only for testing.
 	SkipSSLValidation bool
 
+	// URL is a fully qualified URL to the Cloud Controller API.
 	URL string
 }
 
-// TargetCF sets the client to use the Cloud Controller at the fully qualified
-// API URL. skipSSLValidation controls whether a client verifies the server's
-// certificate chain and host name. If skipSSLValidation is true, TLS accepts
-// any certificate presented by the server and any host name in that
-// certificate for *all* client requests going forward.
-//
-// In this mode, TLS is susceptible to man-in-the-middle attacks. This should
-// be used only for testing.
+// TargetCF sets the client to use the Cloud Controller specified in the
+// configuration. Any other configuration is also applied to the client.
 func (client *Client) TargetCF(settings TargetSettings) (Warnings, error) {
 	client.cloudControllerURL = settings.URL
 
--- api/cloudcontroller/connection.go
+++ api/cloudcontroller/connection.go
@@ -1,3 +1,13 @@
+// Package cloudcontroller contains shared utilies between the V2 and V3
+// clients.
+//
+// These sets of packages are still under development/pre-pre-pre...alpha. Use
+// at your own risk! Functionality and design may change without warning.
+//
+// Where are the clients?
+//
+// These clients live in ccv2 and ccv3 packages. Each of them only works with
+// the V2 and V3 api respectively.
 package cloudcontroller
 
 import "net/http"
