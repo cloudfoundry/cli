8f68ecdbb1d20b7e00e867fabd9d17dd0021aada
Dan Jahner <djahner@pivotal.io>
2017-02-18 10:31:29 -0800

    Add support for SHA256 SSH fingerprints

--- cf/ssh/ssh_test.go
+++ cf/ssh/ssh_test.go
@@ -228,6 +228,18 @@ var _ = Describe("SSH", func() {
 				listener.Close()
 			})
 
+			Context("when the SHA256 fingerprint does not match", func() {
+				BeforeEach(func() {
+					sshEndpointFingerprint = "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00"
+				})
+
+				It("returns an error'", func() {
+					err := callback("", addr, TestHostKey.PublicKey())
+					Expect(err).To(MatchError(MatchRegexp("Host key verification failed\\.")))
+					Expect(err).To(MatchError(MatchRegexp("The fingerprint of the received key was \".*\"")))
+				})
+			})
+
 			Context("when the SHA1 fingerprint does not match", func() {
 				BeforeEach(func() {
 					sshEndpointFingerprint = "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00"
