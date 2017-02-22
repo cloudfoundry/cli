b210fab0509547fcea6e0d17cabe999707225cea
Nick Wei <nwei@pivotal.io>
2017-02-22 13:58:28 -0800

    create-buildpack integration test cleans after itself

--- integration/isolated/create_buildpack_command_test.go
+++ integration/isolated/create_buildpack_command_test.go
@@ -2,6 +2,7 @@ package isolated
 
 import (
 	"io/ioutil"
+	"os"
 
 	. "code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
@@ -10,13 +11,25 @@ import (
 	. "github.com/onsi/gomega/gexec"
 )
 
-var _ = Describe("create-buildpack command", func() {
+var _ = FDescribe("create-buildpack command", func() {
 	Context("when the wrong data type is provided as the position argument", func() {
-		It("outputs an error message to the user, provides help text, and exits 1", func() {
-			// Avoid file does not exist error.
-			err := ioutil.WriteFile("some-file", []byte{}, 0400)
-			Expect(err).NotTo(HaveOccurred())
+		var filename string
 
+		BeforeEach(func() {
+			// The args take a filepath. Creating a real file will avoid the file
+			// does not exist error, and trigger the correct error case we are
+			// testing.
+			filename = "some-file"
+			err := ioutil.WriteFile(filename, []byte{}, 0400)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			err := os.Remove(filename)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("outputs an error message to the user, provides help text, and exits 1", func() {
 			session := CF("create-buildpack", "some-buildpack", "some-file", "not-an-integer")
 			Eventually(session).Should(Exit(1))
 			Expect(session.Err).To(Say("Incorrect usage: Value for POSITION must be integer"))
