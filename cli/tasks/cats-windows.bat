547a060e677efa63b205ea9367e376b975d5ccd2
Jesse Alford <jalford@pivotal.io>
2017-03-29 10:40:12 -0700

    reconfigure the pipeline to consume cf-acceptance-tests directly
    [#142614001]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- cli/tasks/cats-windows.bat
+++ cli/tasks/cats-windows.bat
@@ -1,10 +1,10 @@
-SET GOPATH=%CD%\cf-release-repo
-SET GATSPATH=%GOPATH%\src\github.com\cloudfoundry\cf-acceptance-tests
+SET GOPATH=%CD%\gopath
+SET CATSPATH=%GOPATH%\src\github.com\cloudfoundry\cf-acceptance-tests
 SET CF_DIAL_TIMEOUT=15
 
 SET PATH=C:\Go\bin;%PATH%
 SET PATH=C:\Program Files\Git\cmd\;%PATH%
-SET PATH=%CD%\cf-release-repo\bin;%PATH%
+SET PATH=%GOPATH%\bin;%PATH%
 SET PATH=C:\Program Files\GnuWin32\bin;%PATH%
 SET PATH=C:\Program Files\cURL\bin;%PATH%
 SET PATH=C:\Program Files\CloudFoundry;%PATH%
@@ -16,9 +16,8 @@ pushd %CD%\cf-cli-binaries
 	gzip -d cf-cli-binaries.tgz
 	tar -xvf cf-cli-binaries.tar
 	MOVE %CD%\cf-cli_winx64.exe ..\cf.exe
-	dir ..
 popd
 
-cd %GATSPATH%
+cd %CATSPATH%
 
 ginkgo.exe -flakeAttempts=2 -slowSpecThreshold=120 -skip="NO_DEA_SUPPORT|go makes the app reachable via its bound route|SSO|takes effect after a restart, not requiring a push|doesn't die when printing 32MB|exercises basic loggregator|firehose data|dotnet-core|transparently proxies both reserved" -nodes=%NODES%
