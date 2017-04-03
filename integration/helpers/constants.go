3cb2a530944c64f05736281a48946c5d4f43cc5f
Stu Pollock <spollock@pivotal.io>
2017-04-03 12:27:53 -0700

    make timestamp regex match more real possible times
    [#141914049]
    
    Signed-off-by: Matthew Boedicker <mboedicker@pivotal.io>

--- integration/helpers/constants.go
+++ integration/helpers/constants.go
@@ -3,5 +3,5 @@ package helpers
 const (
 	GUIDRegex = "[\\da-f]{8}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{12}"
 	//IPAddressRegex = "\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}"
-	ISO8601Regex = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{1,3}-\\d{2}00"
+	ISO8601Regex = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{1,3}[+-]\\d{4}"
 )
