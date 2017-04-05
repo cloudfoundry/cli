c2c60e3ae111e64f9ff12b0a478355284940165f
Stu Pollock <spollock@pivotal.io>
2017-04-05 12:31:28 -0700

    prevent `cf space` and `cf org` from throwing
    - a nil pointer dereference error when targeting an old version of CAPI
    
    [#142414393]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- command/v2/space_command.go
+++ command/v2/space_command.go
@@ -58,9 +58,9 @@ func (cmd *SpaceCommand) Setup(config command.Config, ui command.UI) error {
 		if _, ok := err.(sharedV3.V3APIDoesNotExistError); !ok {
 			return err
 		}
+	} else {
+		cmd.ActorV3 = v3action.NewActor(ccClientV3, config)
 	}
-
-	cmd.ActorV3 = v3action.NewActor(ccClientV3, config)
 
 	return nil
 }
