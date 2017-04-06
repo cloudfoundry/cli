0598f510fd6d1ae68d8bfe5953822b2d791824a6
Nick Wei <nwei@pivotal.io>
2017-04-06 15:42:41 -0700

    fix running app count, add red color to app and start commands
    - discovered a bug in the app display where the "running/total" instance
    display was counting non-functional instances as "running".
    - added super custom display for the app and start commands to highlight
    red when the app is not 100% up.
    
    [Fixes #142882319]
    
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

---
+++ util/ui/ui_for_app.go
@@ -0,0 +1,31 @@
+package ui
+
+import (
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+func (ui *UI) DisplayKeyValueTableForApp(table [][]string) {
+	runningInstances := strings.Split(table[2][1], "/")[0]
+	state := table[1][1]
+
+	if runningInstances == "0" && state != ui.TranslateText("stopped") {
+		redColor := color.New(color.FgRed, color.Bold)
+		table[1][1] = ui.modifyColor(table[1][1], redColor)
+		table[2][1] = ui.modifyColor(table[2][1], redColor)
+	}
+	ui.DisplayKeyValueTable("", table, 3)
+}
+
+func (ui *UI) DisplayInstancesTableForApp(table [][]string) {
+	redColor := color.New(color.FgRed, color.Bold)
+	trDown, trCrashed := ui.TranslateText("down"), ui.TranslateText("crashed")
+
+	for i, row := range table {
+		if row[1] == trDown || row[1] == trCrashed {
+			table[i][1] = ui.modifyColor(row[1], redColor)
+		}
+	}
+	ui.DisplayTableWithHeader("", table, 3)
+}
