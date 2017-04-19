fd26377d1f947d714143cfd094bd3095c2891e1f
Nick Wei <nwei@pivotal.io>
2017-04-19 14:43:05 -0700

    parallelized plugins integrations tests
    - removed -failFast from any of our integrated test suites
    
    [finishes #143975669]
    
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

--- cli/tasks/cats-windows.bat
+++ cli/tasks/cats-windows.bat
@@ -20,4 +20,4 @@ popd
 
 cd %CATSPATH%
 
-ginkgo.exe -flakeAttempts=2 -slowSpecThreshold=120 -skip="NO_DEA_SUPPORT|go makes the app reachable via its bound route|SSO|takes effect after a restart, not requiring a push|doesn't die when printing 32MB|exercises basic loggregator|firehose data|dotnet-core|transparently proxies both reserved" -nodes=%NODES% -failFast
+ginkgo.exe -flakeAttempts=2 -slowSpecThreshold=120 -skip="NO_DEA_SUPPORT|go makes the app reachable via its bound route|SSO|takes effect after a restart, not requiring a push|doesn't die when printing 32MB|exercises basic loggregator|firehose data|dotnet-core|transparently proxies both reserved" -nodes=%NODES%
