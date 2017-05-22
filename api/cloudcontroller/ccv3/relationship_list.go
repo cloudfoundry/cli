9f9782956b0116a93e210c2191dabaa9f594cdbd
Anand Gaitonde <agaitonde@pivotal.io>
2017-05-22 11:46:57 -0700

    split up Relationship and RelationshipList into their own files

---
+++ api/cloudcontroller/ccv3/relationship_list.go
@@ -0,0 +1,69 @@
+package ccv3
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"code.cloudfoundry.org/cli/api/cloudcontroller"
+	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv3/internal"
+)
+
+// RelationshipList represents a one to many relationship.
+type RelationshipList struct {
+	GUIDs []string
+}
+
+func (r RelationshipList) MarshalJSON() ([]byte, error) {
+	var ccRelationship struct {
+		Data []map[string]string `json:"data"`
+	}
+
+	for _, guid := range r.GUIDs {
+		ccRelationship.Data = append(
+			ccRelationship.Data,
+			map[string]string{
+				"guid": guid,
+			})
+	}
+
+	return json.Marshal(ccRelationship)
+}
+
+func (r *RelationshipList) UnmarshalJSON(data []byte) error {
+	var ccRelationships struct {
+		Data []map[string]string `json:"data"`
+	}
+
+	err := json.Unmarshal(data, &ccRelationships)
+	if err != nil {
+		return err
+	}
+
+	for _, partner := range ccRelationships.Data {
+		r.GUIDs = append(r.GUIDs, partner["guid"])
+	}
+	return nil
+}
+
+// EntitleIsolationSegmentToOrganizations will create a link between the
+// isolation segment and the list of organizations provided.
+func (client *Client) EntitleIsolationSegmentToOrganizations(isolationSegmentGUID string, organizationGUIDs []string) (RelationshipList, Warnings, error) {
+	body, err := json.Marshal(RelationshipList{GUIDs: organizationGUIDs})
+	if err != nil {
+		return RelationshipList{}, nil, err
+	}
+
+	request, err := client.newHTTPRequest(requestOptions{
+		RequestName: internal.PostIsolationSegmentRelationshipOrganizationsRequest,
+		URIParams:   internal.Params{"guid": isolationSegmentGUID},
+		Body:        bytes.NewReader(body),
+	})
+
+	var relationships RelationshipList
+	response := cloudcontroller.Response{
+		Result: &relationships,
+	}
+
+	err = client.connection.Make(request, &response)
+	return relationships, response.Warnings, err
+}
