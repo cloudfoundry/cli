e4418d8a606cdc2ca86380ed108dba46eca071c9
Stu Pollock <spollock@pivotal.io>
2017-06-05 08:03:57 -0700

    repair some failing config tests
    [#146536865]

--- util/configv3/home_dir.go
+++ util/configv3/home_dir.go
@@ -9,16 +9,14 @@ import (
 
 // ConfigFilePath returns the location of the config file
 func ConfigFilePath() string {
-	return filepath.Join(homeDirectory(true), ".cf", "config.json")
+	return filepath.Join(HomeDirectory(true), ".cf", "config.json")
 }
 
-func homeDirectory(checkCFHome bool) string {
-	var homeDir string
+func HomeDirectory(checkCFHome bool) string {
 	switch {
 	case checkCFHome && os.Getenv("CF_HOME") != "":
-		homeDir = os.Getenv("CF_HOME")
+		return os.Getenv("CF_HOME")
 	default:
-		homeDir = os.Getenv("HOME")
+		return os.Getenv("HOME")
 	}
-	return homeDir
 }
