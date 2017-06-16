a9f81449ab9ca6d57c746c21158921f38e26c5d3
Jen Spinney <jennifer.spinney@suse.com>
2017-06-16 10:56:54 -0700

    added translatable droplet error
    [#145975379]
    
    Signed-off-by: Alex Melnik <aleksandr.melnik@suse.com>

--- actor/v3action/droplet.go
+++ actor/v3action/droplet.go
@@ -1,6 +1,9 @@
 package v3action
 
-import "code.cloudfoundry.org/cli/api/cloudcontroller/ccv3"
+import (
+	"code.cloudfoundry.org/cli/api/cloudcontroller/ccerror"
+	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv3"
+)
 
 // Droplet represents a Cloud Controller droplet.
 type Droplet struct {
@@ -10,6 +13,15 @@ type Droplet struct {
 }
 
 type Buildpack ccv3.Buildpack
+
+// AssignDropletError is returned when assigning the current droplet of an app
+// fails
+type AssignDropletError struct {
+}
+
+func (a AssignDropletError) Error() string {
+	return "Unable to assign current droplet. Ensure the droplet exists and belongs to this app."
+}
 
 // SetApplicationDroplet sets the droplet for an application.
 func (actor Actor) SetApplicationDroplet(appName string, spaceGUID string, dropletGUID string) (Warnings, error) {
@@ -22,6 +34,10 @@ func (actor Actor) SetApplicationDroplet(appName string, spaceGUID string, dropl
 	_, apiWarnings, err := actor.CloudControllerClient.SetApplicationDroplet(application.GUID, dropletGUID)
 	actorWarnings := Warnings(apiWarnings)
 	allWarnings = append(allWarnings, actorWarnings...)
+
+	if _, ok := err.(ccerror.UnprocessableEntityError); ok {
+		return allWarnings, AssignDropletError{}
+	}
 
 	return allWarnings, err
 }
