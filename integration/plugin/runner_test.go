552d0a96373fb81e572f2d2bc446d5e6f9cb8a2b
Stu Pollock <spollock@pivotal.io>
2017-06-23 10:54:00 -0700

    make CF_HOME work with installed plugin commands
    - the old code did not read CF_HOME if CF_PLUGIN_HOME was
    not set, while trying to execute plugin commands
    
    [finish #147709643]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- integration/plugin/runner_test.go
+++ integration/plugin/runner_test.go
@@ -1,27 +1,16 @@
 package plugin
 
 import (
+	"os"
+
 	"code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("running plugins", func() {
-	Describe("plugin command alias", func() {
-		BeforeEach(func() {
-			installTestPlugin()
-		})
-
-		AfterEach(func() {
-			uninstallTestPlugin()
-		})
-
-		It("can call a command by its alias", func() {
-			confirmTestPluginOutput("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", "You called Test Plugin Command With Alias!")
-		})
-	})
-
 	Describe("panic handling", func() {
 		BeforeEach(func() {
 			session := helpers.CF("install-plugin", "-f", panicTestPluginPath)
@@ -31,6 +20,56 @@ var _ = Describe("running plugins", func() {
 		It("will exit 1 if the plugin panics", func() {
 			session := helpers.CF("freak-out")
 			Eventually(session).Should(Exit(1))
+		})
+	})
+
+	Describe("when running plugin commands while CF_HOME is set", func() {
+		Context("when CF_PLUGIN_HOME is unset", func() {
+			BeforeEach(func() {
+				Expect(os.Setenv("CF_PLUGIN_HOME", "")).NotTo(HaveOccurred())
+			})
+
+			Context("when a plugin is installed", func() {
+				BeforeEach(func() {
+					installTestPlugin()
+				})
+
+				AfterEach(func() {
+					uninstallTestPlugin()
+				})
+
+				It("lists the installed plugins", func() {
+					session := helpers.CF("plugins")
+					Eventually(session).Should(Say("Username"))
+					Eventually(session).Should(Exit(0))
+				})
+
+				It("is able to run an installed plugin command", func() {
+					confirmTestPluginOutput("Username", "admin")
+				})
+			})
+		})
+
+		Context("when CF_PLUGIN_HOME is set", func() {
+			Context("when a plugin is installed", func() {
+				BeforeEach(func() {
+					installTestPlugin()
+				})
+
+				AfterEach(func() {
+					uninstallTestPlugin()
+				})
+
+				It("lists the installed plugins", func() {
+					session := helpers.CF("plugins")
+					Eventually(session).Should(Say("TestPluginCommandWithAlias"))
+					Eventually(session).Should(Exit(0))
+				})
+
+				It("can call a command by its alias", func() {
+					confirmTestPluginOutput("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", "You called Test Plugin Command With Alias!")
+				})
+			})
 		})
 	})
 })
