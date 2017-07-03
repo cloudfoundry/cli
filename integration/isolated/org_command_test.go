02bcae3a7d69e43c881fdea9322e17d48eeb2e28
Stu Pollock <spollock@pivotal.io>
2017-07-03 11:54:24 -0700

    augmented integration tests for displaying iso segs
    [#138786401]

--- integration/isolated/org_command_test.go
+++ integration/isolated/org_command_test.go
@@ -1,6 +1,7 @@
 package isolated
 
 import (
+	"fmt"
 	"sort"
 
 	"code.cloudfoundry.org/cli/integration/helpers"
@@ -75,7 +76,7 @@ var _ = Describe("org command", func() {
 			It("displays org not found and exits 1", func() {
 				session := helpers.CF("org", orgName)
 				userName, _ := helpers.GetCredentials()
-				Eventually(session.Out).Should(Say("Getting info for org %s as %s...", orgName, userName))
+				Eventually(session.Out).Should(Say("Getting info for org %s as %s\\.\\.\\.", orgName, userName))
 				Eventually(session.Out).Should(Say("FAILED"))
 				Eventually(session.Err).Should(Say("Organization '%s' not found.", orgName))
 				Eventually(session).Should(Exit(1))
@@ -98,6 +99,7 @@ var _ = Describe("org command", func() {
 			Context("when no flags are used", func() {
 				var (
 					domainName            string
+					orgGUID               string
 					quotaName             string
 					spaceName2            string
 					isolationSegmentName1 string
@@ -105,6 +107,7 @@ var _ = Describe("org command", func() {
 				)
 
 				BeforeEach(func() {
+					orgGUID = helpers.GetOrgGUID(orgName)
 					domainName = helpers.DomainName("")
 					domain := helpers.NewDomain(orgName, domainName)
 					domain.Create()
@@ -125,10 +128,24 @@ var _ = Describe("org command", func() {
 					isolationSegmentName2 = helpers.IsolationSegmentName()
 					Eventually(helpers.CF("create-isolation-segment", isolationSegmentName2)).Should(Exit(0))
 					Eventually(helpers.CF("enable-org-isolation", orgName, isolationSegmentName2)).Should(Exit(0))
+
+					isolationSegmentsSorted := []string{isolationSegmentName1, isolationSegmentName2}
+					sort.Strings(isolationSegmentsSorted)
+
+					// TODO:  Replace the following curl with `cf set-org-default-isolation-segment`
+					defaultIsolationSegmentGUID := helpers.GetIsolationSegmentGUID(isolationSegmentsSorted[0])
+					Eventually(helpers.CF("curl", fmt.Sprintf("/v3/organizations/%s/relationships/default_isolation_segment", orgGUID),
+						"-X", "PATCH", "-d", fmt.Sprintf("'{\"data\":{\"guid\":\"%s\"}}'", defaultIsolationSegmentGUID))).Should(Exit(0))
+					// Eventually(helpers.CF("set-org-default-isolation-segment", orgName, isolationSegmentsSorted[0])).Should(Exit(0))
 				})
 
 				// @TODO remove and add to cleanup script
 				AfterEach(func() {
+					// TODO:  Replace the following curl with `cf set-org-default-isolation-segment`
+					Eventually(helpers.CF("curl", fmt.Sprintf("/v3/organizations/%s/relationships/default_isolation_segment", orgGUID),
+						"-X", "PATCH", "-d", "'{\"data\":{\"guid\":null}}'")).Should(Exit(0))
+					// Eventually(helpers.CF("reset-org-default-isolation-segment", orgName)).Should(Exit(0))
+
 					Eventually(helpers.CF("delete-org", "-f", orgName))
 					Eventually(helpers.CF("delete-isolation-segment", "-f", isolationSegmentName1))
 					Eventually(helpers.CF("delete-isolation-segment", "-f", isolationSegmentName2))
@@ -137,7 +154,7 @@ var _ = Describe("org command", func() {
 				It("displays a table with org domains, quotas, spaces, space quotas and isolation segments, and exits 0", func() {
 					session := helpers.CF("org", orgName)
 					userName, _ := helpers.GetCredentials()
-					Eventually(session.Out).Should(Say("Getting info for org %s as %s...", orgName, userName))
+					Eventually(session.Out).Should(Say("Getting info for org %s as %s\\.\\.\\.", orgName, userName))
 
 					Eventually(session.Out).Should(Say("name:\\s+%s", orgName))
 
@@ -153,7 +170,7 @@ var _ = Describe("org command", func() {
 
 					isolationSegmentsSorted := []string{isolationSegmentName1, isolationSegmentName2}
 					sort.Strings(isolationSegmentsSorted)
-					Eventually(session.Out).Should(Say("isolation segments:\\s+.*%s,.* %s", isolationSegmentsSorted[0], isolationSegmentsSorted[1]))
+					Eventually(session.Out).Should(Say("isolation segments:\\s+.*%s \\(default\\),.* %s", isolationSegmentsSorted[0], isolationSegmentsSorted[1]))
 
 					Eventually(session).Should(Exit(0))
 				})
