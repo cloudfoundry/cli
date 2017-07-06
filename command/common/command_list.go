45b63732cb147debc1a799d0adc8e28e62d06e07
Stu Pollock <spollock@pivotal.io>
2017-07-06 16:01:00 -0700

    implement v3-restart
    [finishes #147787885]
    
    Signed-off-by: Jen Spinney <jennifer.spinney@suse.com>

--- command/common/command_list.go
+++ command/common/command_list.go
@@ -23,6 +23,7 @@ type commandList struct {
 	V3Stop          v3.V3StopCommand          `command:"v3-stop" description:"Stop an app"`
 	V3Push          v3.V3PushCommand          `command:"v3-push" description:"Push a new app or sync changes to an existing app"`
 	V3App           v3.V3AppCommand           `command:"v3-app" description:"Display an app"`
+	V3Restart       v3.V3RestartCommand       `command:"v3-restart" description:"Stop all instances of the app, then start them again. This may cause downtime."`
 
 	AddPluginRepo                      plugin.AddPluginRepoCommand                  `command:"add-plugin-repo" description:"Add a new plugin repository"`
 	AllowSpaceSSH                      v2.AllowSpaceSSHCommand                      `command:"allow-space-ssh" description:"Allow SSH access for the space"`
