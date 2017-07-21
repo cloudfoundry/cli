2b70d77f051c428b765c2f509f22678febb79f85
Alex Melnik <aleksandr.melnik@suse.com>
2017-07-21 11:29:25 -0700

    in integration-windows, we do not use the CATs config
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- ci/cli/tasks/integration-windows.bat
+++ ci/cli/tasks/integration-windows.bat
@@ -1,19 +1,14 @@
 SET GOPATH=%CD%\go
-SET GATSPATH=%GOPATH%\src\code.cloudfoundry.org\cli
 SET CF_DIAL_TIMEOUT=15
 
 SET PATH=C:\Go\bin;%PATH%
-SET PATH=C:\Program Files\Git\cmd\;%PATH%
 SET PATH=%GOPATH%\bin;%PATH%
 SET PATH=C:\Program Files\GnuWin32\bin;%PATH%
-SET PATH=C:\Program Files\cURL\bin;%PATH%
 SET PATH=%CD%;%PATH%
 
 SET /p DOMAIN=<%CD%\bosh-lite-lock\name
 SET /p CF_PASSWORD=<%CD%\cf-credentials\cf-password
 SET CF_API=https://api.%DOMAIN%
-call %CD%\cli-ci\ci\cli\tasks\create-cats-config.bat
-SET CONFIG=%CD%\config.json
 
 pushd %CD%\cf-cli-binaries
 	gzip -d cf-cli-binaries.tgz
@@ -23,6 +18,6 @@ popd
 
 go get -v -u github.com/onsi/ginkgo/ginkgo
 
-cd %GATSPATH%
+cd %GOPATH%\src\code.cloudfoundry.org\cli
 ginkgo.exe -r -nodes=16 -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/isolated ./integration/plugin ./integration/push || exit 1
 ginkgo.exe -r -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/global || exit 1
