020c594dbf872dfff13bfc595c79ee83825014f8
Alex Melnik <aleksandr.melnik@suse.com>
2017-07-21 12:19:52 -0700

    added unset-env to set-env see also
    [finishes #148570729]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

---
+++ integration/global/set_env_command_test.go
@@ -0,0 +1,24 @@
+package global
+
+import (
+	"code.cloudfoundry.org/cli/integration/helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+)
+
+var _ = Describe("set-env command", func() {
+	Context("when the --help flag provided", func() {
+		It("displays the usage text", func() {
+			session := helpers.CF("set-env", "--help")
+			Eventually(session).Should(Say("NAME:"))
+			Eventually(session).Should(Say("set-env - Set an env variable for an app"))
+			Eventually(session).Should(Say("USAGE:"))
+			Eventually(session).Should(Say("cf set-env APP_NAME ENV_VAR_NAME ENV_VAR_VALUE"))
+			Eventually(session).Should(Say("ALIAS:"))
+			Eventually(session).Should(Say("se"))
+			Eventually(session).Should(Say("SEE ALSO:"))
+			Eventually(session).Should(Say("apps, env, restart, set-running-environment-variable-group, set-staging-environment-variable-group, unset-env"))
+		})
+	})
+})
