5448053b310b2351507ad136b046a26dbac1c0c3
Anand Gaitonde <agaitonde@pivotal.io>
2017-07-26 11:25:59 -0700

    add integration test for manifest properties
    [#149195571]

--- integration/push/simple_manifest_only_test.go
+++ integration/push/simple_manifest_only_test.go
@@ -27,10 +27,16 @@ var _ = Describe("push with a simple manifest and no flags", func() {
 				It("uses the manifest for app settings", func() {
 					helpers.WithHelloWorldApp(func(dir string) {
 						helpers.WriteManifest(filepath.Join(dir, "manifest.yml"), map[string]interface{}{
-							"applications": []map[string]string{
+							"applications": []map[string]interface{}{
 								{
-									"name": appName,
-									"path": dir,
+									"name":       appName,
+									"path":       dir,
+									"command":    "echo 'hi' && $HOME/boot.sh",
+									"buildpack":  "staticfile_buildpack",
+									"disk_quota": "300M",
+									"instances":  2,
+									"memory":     "70M",
+									"stack":      "cflinuxfs2",
 								},
 							},
 						})
@@ -40,6 +46,12 @@ var _ = Describe("push with a simple manifest and no flags", func() {
 						Eventually(session).Should(Say("Creating app with these attributes\\.\\.\\."))
 						Eventually(session).Should(Say("\\+\\s+name:\\s+%s", appName))
 						Eventually(session).Should(Say("\\s+path:\\s+%s", regexp.QuoteMeta(dir)))
+						Eventually(session).Should(Say("\\s+buildpack:\\s+staticfile_buildpack"))
+						Eventually(session).Should(Say("\\s+command:\\s+%s", regexp.QuoteMeta("echo 'hi' && $HOME/boot.sh")))
+						Eventually(session).Should(Say("\\s+disk quota:\\s+300M"))
+						Eventually(session).Should(Say("\\s+instances:\\s+2"))
+						Eventually(session).Should(Say("\\s+memory:\\s+70M"))
+						Eventually(session).Should(Say("\\s+stack:\\s+cflinuxfs2"))
 						Eventually(session).Should(Say("\\s+routes:"))
 						Eventually(session).Should(Say("(?i)\\+\\s+%s.%s", appName, defaultSharedDomain()))
 						Eventually(session).Should(Say("Mapping routes\\.\\.\\."))
@@ -50,11 +62,17 @@ var _ = Describe("push with a simple manifest and no flags", func() {
 						Eventually(session).Should(Say("Staging complete"))
 						Eventually(session).Should(Say("Waiting for app to start\\.\\.\\."))
 						Eventually(session).Should(Say("requested state:\\s+started"))
+						Eventually(session).Should(Say("start command:\\s+%s", regexp.QuoteMeta("echo 'hi' && $HOME/boot.sh")))
 						Eventually(session).Should(Exit(0))
 					})
 
 					session := helpers.CF("app", appName)
 					Eventually(session).Should(Say("name:\\s+%s", appName))
+					Eventually(session).Should(Say("instances:\\s+\\d/2"))
+					Eventually(session).Should(Say("usage:\\s+70M x 2"))
+					Eventually(session).Should(Say("stack:\\s+cflinuxfs2"))
+					Eventually(session).Should(Say("buildpack:\\s+staticfile_buildpack"))
+					Eventually(session).Should(Say("#0.* of 70M"))
 					Eventually(session).Should(Exit(0))
 				})
 			})
