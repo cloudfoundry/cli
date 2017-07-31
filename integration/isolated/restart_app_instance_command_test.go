5ee429cb86ea54d322dd1368b698d68b404910da
Maria Shaldibina <mshaldibina@pivotal.io>
2017-07-31 13:58:36 -0700

    add integration tests for restart-app-instance command
    [finishes #149455835]

---
+++ integration/isolated/restart_app_instance_command_test.go
@@ -0,0 +1,128 @@
+package isolated
+
+import (
+	"code.cloudfoundry.org/cli/integration/helpers"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = XDescribe("restart command", func() {
+	Describe("help", func() {
+		Context("when --help flag is set", func() {
+			It("Displays command usage to output", func() {
+				session := helpers.CF("restart-app-instance", "--help")
+
+				Eventually(session).Should(Say("NAME:"))
+				Eventually(session).Should(Say("restart-app-instance - Terminate the running application instance at the given index and instantiate a new instance of the application with the same index"))
+				Eventually(session).Should(Say("USAGE:"))
+				Eventually(session).Should(Say("cf restart-app-instance APP_NAME INDEX"))
+				Eventually(session).Should(Say("SEE ALSO:"))
+				Eventually(session).Should(Say("restart"))
+				Eventually(session).Should(Exit(0))
+			})
+		})
+	})
+
+	Context("when the environment is not setup correctly", func() {
+		Context("when no API endpoint is set", func() {
+			BeforeEach(func() {
+				helpers.UnsetAPI()
+			})
+
+			It("fails with no API endpoint set message", func() {
+				session := helpers.CF("restart-app-instance", "wut", "0")
+				Eventually(session.Out).Should(Say("FAILED"))
+				Eventually(session.Err).Should(Say("No API endpoint set. Use 'cf login' or 'cf api' to target an endpoint."))
+				Eventually(session).Should(Exit(1))
+			})
+		})
+
+		Context("when not logged in", func() {
+			BeforeEach(func() {
+				helpers.LogoutCF()
+			})
+
+			It("fails with not logged in message", func() {
+				session := helpers.CF("restart-app-instance", "wut", "0")
+				Eventually(session.Out).Should(Say("FAILED"))
+				Eventually(session.Err).Should(Say("Not logged in. Use 'cf login' to log in."))
+				Eventually(session).Should(Exit(1))
+			})
+		})
+
+		Context("when there is no org set", func() {
+			BeforeEach(func() {
+				helpers.LogoutCF()
+				helpers.LoginCF()
+			})
+
+			It("fails with no targeted org error message", func() {
+				session := helpers.CF("restart-app-instance", "wut", "0")
+				Eventually(session.Out).Should(Say("FAILED"))
+				Eventually(session.Err).Should(Say("No org targeted, use 'cf target -o ORG' to target an org."))
+				Eventually(session).Should(Exit(1))
+			})
+		})
+
+		Context("when there is no space set", func() {
+			BeforeEach(func() {
+				helpers.LogoutCF()
+				helpers.LoginCF()
+				helpers.TargetOrg(ReadOnlyOrg)
+			})
+
+			It("fails with no targeted space error message", func() {
+				session := helpers.CF("restart-app-instance", "wut", "0")
+				Eventually(session.Out).Should(Say("FAILED"))
+				Eventually(session.Err).Should(Say("No space targeted, use 'cf target -s SPACE' to target a space."))
+				Eventually(session).Should(Exit(1))
+			})
+		})
+	})
+
+	Context("when the environment is set up correctly", func() {
+		var (
+			orgName   string
+			spaceName string
+			userName  string
+			appName   string
+		)
+
+		BeforeEach(func() {
+			orgName = helpers.NewOrgName()
+			spaceName = helpers.NewSpaceName()
+			appName = helpers.NewAppName()
+
+			setupCF(orgName, spaceName)
+			userName, _ = helpers.GetCredentials()
+		})
+
+		Context("when the app does not exist", func() {
+			It("tells the user that the start is not found and exits 1", func() {
+				session := helpers.CF("restart", appName, "0")
+
+				Eventually(session.Out).Should(Say("FAILED"))
+				Eventually(session.Err).Should(Say("App %s not found", appName))
+				Eventually(session).Should(Exit(1))
+			})
+		})
+
+		Context("when the app does exist", func() {
+			BeforeEach(func() {
+				helpers.WithHelloWorldApp(func(appDir string) {
+					Eventually(helpers.CF("push", appName, "-p", appDir, "-b", "staticfile_buildpack")).Should(Exit(0))
+				})
+
+				It("restarts app instance", func() {
+					session := helpers.CF("restart", appName)
+					Eventually(session).Should(Say("Restarting instance %d of the app %s in org %s / space %s as %s\\.\\.\\.", 10, appName, orgName, spaceName, userName))
+					Eventually(session.Out).Should(Say("OK"))
+					Eventually(session).Should(Exit(0))
+				})
+			})
+		})
+	})
+})
