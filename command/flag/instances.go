62877cbf521cfb373331928d84891d02deb1dc0e
Maria Shaldibina <mshaldibina@pivotal.io>
2017-08-02 08:53:28 -0700

    added a custom flag type for the instances flag option in scale
    - added the types package
    - added NullInt type to distinguish between user provided and default
    values
    
    [#148649153]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

---
+++ command/flag/instances.go
@@ -0,0 +1,21 @@
+package flag
+
+import (
+	"code.cloudfoundry.org/cli/types"
+	flags "github.com/jessevdk/go-flags"
+)
+
+type Instances struct {
+	types.NullInt
+}
+
+func (i *Instances) UnmarshalFlag(val string) error {
+	err := i.ParseFlagValue(val)
+	if err != nil || i.Value < 0 {
+		return &flags.Error{
+			Type:    flags.ErrRequired,
+			Message: "invalid argument for flag '-i' (expected int > 0)",
+		}
+	}
+	return nil
+}
---
+++ types/null_int_test.go
@@ -0,0 +1,37 @@
+package types_test
+
+import (
+	. "code.cloudfoundry.org/cli/types"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("NullInt", func() {
+	var nullInt NullInt
+
+	Describe("ParseFlagValue", func() {
+		Context("when the empty string is provided", func() {
+			It("sets IsSet to false", func() {
+				err := nullInt.ParseFlagValue("")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(nullInt).To(Equal(NullInt{Value: 0, IsSet: false}))
+			})
+		})
+
+		Context("when an invalid integer is provided", func() {
+			It("returns an error", func() {
+				err := nullInt.ParseFlagValue("abcdef")
+				Expect(err).To(HaveOccurred())
+				Expect(nullInt).To(Equal(NullInt{Value: 0, IsSet: false}))
+			})
+		})
+
+		Context("when a valid integer is provided", func() {
+			It("stores the integer and sets IsSet to true", func() {
+				err := nullInt.ParseFlagValue("0")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(nullInt).To(Equal(NullInt{Value: 0, IsSet: true}))
+			})
+		})
+	})
+})
