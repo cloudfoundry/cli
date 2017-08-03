e785504380836451af74351abf475b9449534e48
Anand Gaitonde <agaitonde@pivotal.io>
2017-08-03 15:03:32 -0700

    find all service instances for a given app
    [#149933827]

--- command/translatableerror/service_instance_not_found_error.go
+++ command/translatableerror/service_instance_not_found_error.go
@@ -1,15 +1,20 @@
 package translatableerror
 
 type ServiceInstanceNotFoundError struct {
+	GUID string
 	Name string
 }
 
-func (ServiceInstanceNotFoundError) Error() string {
+func (e ServiceInstanceNotFoundError) Error() string {
+	if e.Name == "" {
+		return "Service instance (GUID: {{.GUID}}) not found"
+	}
 	return "Service instance {{.ServiceInstance}} not found"
 }
 
 func (e ServiceInstanceNotFoundError) Translate(translate func(string, ...interface{}) string) string {
 	return translate(e.Error(), map[string]interface{}{
+		"GUID":            e.GUID,
 		"ServiceInstance": e.Name,
 	})
 }
