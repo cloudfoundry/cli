c0f6622d26c1844bff3e1e3243636c70b41f4f58
Maria Shaldibina <mshaldibina@pivotal.io>
2017-08-14 14:07:57 -0700

    document NullInt
    [#148649153]
    
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

--- types/null_int.go
+++ types/null_int.go
@@ -2,11 +2,14 @@ package types
 
 import "strconv"
 
+// NullInt is a wrapper around integer values that can be null or an integer.
+// Use IsSet to check if the value is provided, instead of checking against 0.
 type NullInt struct {
 	Value int
 	IsSet bool
 }
 
+// ParseFlagValue is used to parse a user provided flag argument.
 func (n *NullInt) ParseFlagValue(val string) error {
 	if val == "" {
 		return nil
