8526532c578c5788f969586c243af64fee7de571
Anand Gaitonde <agaitonde@pivotal.io>
2017-09-12 14:43:50 -0700

    replace RouteInDifferentSpaceError's message with old translation
    [#150559374]

--- command/translatableerror/route_in_different_space_error.go
+++ command/translatableerror/route_in_different_space_error.go
@@ -5,11 +5,11 @@ type RouteInDifferentSpaceError struct {
 }
 
 func (e RouteInDifferentSpaceError) Error() string {
-	return "Route {{.Route}} has been registered to another space."
+	return "The route {{.URL}} is already in use.\nTIP: Change the hostname with -n HOSTNAME or use --random-route to generate a new route and then push again."
 }
 
 func (e RouteInDifferentSpaceError) Translate(translate func(string, ...interface{}) string) string {
 	return translate(e.Error(), map[string]interface{}{
-		"Route": e.Route,
+		"URL": e.Route,
 	})
 }
