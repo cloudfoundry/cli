f97495d95bcc69edf5e8886b63a9e8ac90c31b4c
Zac Bergquist <zbergquist99@gmail.com>
2017-09-18 10:53:08 -0600

    Add v3 list spaces call

---
+++ api/cloudcontroller/ccv3/space_test.go
@@ -0,0 +1,130 @@
+package ccv3_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"code.cloudfoundry.org/cli/api/cloudcontroller/ccerror"
+	. "code.cloudfoundry.org/cli/api/cloudcontroller/ccv3"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/ghttp"
+)
+
+var _ = Describe("Spaces", func() {
+	var client *Client
+
+	BeforeEach(func() {
+		client = NewTestClient()
+	})
+
+	Describe("GetSpaces", func() {
+		Context("when spaces exist", func() {
+			BeforeEach(func() {
+				response1 := fmt.Sprintf(`{
+	"pagination": {
+		"next": {
+			"href": "%s/v3/spaces?names=some-space-name&page=2&per_page=2"
+		}
+	},
+  "resources": [
+    {
+      "name": "space-name-1",
+      "guid": "space-guid-1"
+    },
+    {
+      "name": "space-name-2",
+      "guid": "space-guid-2"
+    }
+  ]
+}`, server.URL())
+				response2 := `{
+	"pagination": {
+		"next": null
+	},
+	"resources": [
+	  {
+      "name": "space-name-3",
+		  "guid": "space-guid-3"
+		}
+	]
+}`
+
+				server.AppendHandlers(
+					CombineHandlers(
+						VerifyRequest(http.MethodGet, "/v3/spaces", "names=some-space-name"),
+						RespondWith(http.StatusOK, response1, http.Header{"X-Cf-Warnings": {"this is a warning"}}),
+					),
+				)
+				server.AppendHandlers(
+					CombineHandlers(
+						VerifyRequest(http.MethodGet, "/v3/spaces", "names=some-space-name&page=2&per_page=2"),
+						RespondWith(http.StatusOK, response2, http.Header{"X-Cf-Warnings": {"this is another warning"}}),
+					),
+				)
+			})
+
+			It("returns the queried spaces and all warnings", func() {
+				spaces, warnings, err := client.GetSpaces(url.Values{
+					NameFilter: []string{"some-space-name"},
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(spaces).To(ConsistOf(
+					Space{Name: "space-name-1", GUID: "space-guid-1"},
+					Space{Name: "space-name-2", GUID: "space-guid-2"},
+					Space{Name: "space-name-3", GUID: "space-guid-3"},
+				))
+				Expect(warnings).To(ConsistOf("this is a warning", "this is another warning"))
+			})
+		})
+
+		Context("when the cloud controller returns errors and warnings", func() {
+			BeforeEach(func() {
+				response := `{
+  "errors": [
+    {
+      "code": 10008,
+      "detail": "The request is semantically invalid: command presence",
+      "title": "CF-UnprocessableEntity"
+    },
+    {
+      "code": 10010,
+      "detail": "Space not found",
+      "title": "CF-SpaceNotFound"
+    }
+  ]
+}`
+				server.AppendHandlers(
+					CombineHandlers(
+						VerifyRequest(http.MethodGet, "/v3/spaces"),
+						RespondWith(http.StatusTeapot, response, http.Header{"X-Cf-Warnings": {"this is a warning"}}),
+					),
+				)
+			})
+
+			It("returns the error and all warnings", func() {
+				_, warnings, err := client.GetSpaces(nil)
+				Expect(err).To(MatchError(ccerror.V3UnexpectedResponseError{
+					ResponseCode: http.StatusTeapot,
+					V3ErrorResponse: ccerror.V3ErrorResponse{
+						Errors: []ccerror.V3Error{
+							{
+								Code:   10008,
+								Detail: "The request is semantically invalid: command presence",
+								Title:  "CF-UnprocessableEntity",
+							},
+							{
+								Code:   10010,
+								Detail: "Space not found",
+								Title:  "CF-SpaceNotFound",
+							},
+						},
+					},
+				}))
+				Expect(warnings).To(ConsistOf("this is a warning"))
+			})
+		})
+	})
+})
