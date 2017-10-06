dd26bf741cf32b4539170f7e74af7d633ffc9576
Sebastian Vidrio <svidrio@pivotal.io>
2017-10-06 09:54:30 -0700

    pending i18n integration test waiting on translations
    [#151737497]
    
    Signed-off-by: Michael Maximilien <maxim@us.ibm.com>

--- integration/isolated/table_alignment_test.go
+++ integration/isolated/table_alignment_test.go
@@ -39,7 +39,9 @@ var _ = Describe("table alignment", func() {
 
 		// Developer note: The spacing in this test is significant and explicit. Do
 		// not replace with a regex.
-		It("aligns the table correctly", func() {
+		// TODO: add these strings to the i18n/resources
+		// See: https://www.pivotaltracker.com/story/show/151737497
+		XIt("aligns the table correctly", func() {
 			username, _ := helpers.GetCredentials()
 			session := helpers.CFWithEnv(map[string]string{"LANG": "ja-JP.utf8"}, "target")
 			Eventually(session.Out).Should(Say("API エンドポイント:   %s", apiURL))
