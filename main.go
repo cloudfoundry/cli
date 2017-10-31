673cc72cfc3a484992ad86c350f44c51efae24f8
An Yu <anyu@pivotal.io>
2017-10-31 14:44:04 -0700

    refactored main a bit to run cleanup before os exit on ssh errors
    [finishes #152291574]
    
    Signed-off-by: Alex Melnik <aleksandr.melnik@suse.com>

--- main.go
+++ main.go
@@ -35,98 +35,109 @@ var ParseErr = errors.New("incorrect type for arg")
 
 func main() {
 	defer panichandler.HandlePanic()
-	parse(os.Args[1:])
+	exitStatus := parse(os.Args[1:])
+	if exitStatus != 0 {
+		os.Exit(exitStatus)
+	}
 }
 
-func parse(args []string) {
+func parse(args []string) int {
 	parser := flags.NewParser(&common.Commands, flags.HelpFlag)
 	parser.CommandHandler = executionWrapper
 	extraArgs, err := parser.ParseArgs(args)
 	if err == nil {
-		return
+		return 0
 	}
 
 	if flagErr, ok := err.(*flags.Error); ok {
-		switch flagErr.Type {
-		case flags.ErrHelp, flags.ErrUnknownFlag, flags.ErrExpectedArgument, flags.ErrInvalidChoice:
-			_, found := reflect.TypeOf(common.Commands).FieldByNameFunc(
-				func(fieldName string) bool {
-					field, _ := reflect.TypeOf(common.Commands).FieldByName(fieldName)
-					return parser.Active != nil && parser.Active.Name == field.Tag.Get("command")
-				},
-			)
-
-			if found && flagErr.Type == flags.ErrUnknownFlag && parser.Active.Name == "set-env" {
-				newArgs := []string{}
-				for _, arg := range args {
-					if arg[0] == '-' {
-						newArgs = append(newArgs, fmt.Sprintf("%s%s", v2.WorkAroundPrefix, arg))
-					} else {
-						newArgs = append(newArgs, arg)
-					}
-				}
-				parse(newArgs)
-				return
-			}
-
-			if flagErr.Type == flags.ErrUnknownFlag || flagErr.Type == flags.ErrExpectedArgument || flagErr.Type == flags.ErrInvalidChoice {
-				fmt.Fprintf(os.Stderr, "Incorrect Usage: %s\n\n", flagErr.Error())
-			}
-
-			if found {
-				parse([]string{"help", parser.Active.Name})
-			} else {
-				switch len(extraArgs) {
-				case 0:
-					parse([]string{"help"})
-				case 1:
-					if !isOption(extraArgs[0]) || (len(args) > 1 && extraArgs[0] == "-a") {
-						parse([]string{"help", extraArgs[0]})
-					} else {
-						parse([]string{"help"})
-					}
-				default:
-					if isCommand(extraArgs[0]) {
-						parse([]string{"help", extraArgs[0]})
-					} else {
-						parse(extraArgs[1:])
-					}
-				}
-			}
-
-			if flagErr.Type == flags.ErrUnknownFlag || flagErr.Type == flags.ErrExpectedArgument || flagErr.Type == flags.ErrInvalidChoice {
-				os.Exit(1)
-			}
-		case flags.ErrRequired:
-			fmt.Fprintf(os.Stderr, "Incorrect Usage: %s\n\n", flagErr.Error())
-			parse([]string{"help", args[0]})
-			os.Exit(1)
-		case flags.ErrMarshal:
-			errMessage := strings.Split(flagErr.Message, ":")
-			fmt.Fprintf(os.Stderr, "Incorrect Usage: %s\n\n", errMessage[0])
-			parse([]string{"help", args[0]})
-			os.Exit(1)
-		case flags.ErrUnknownCommand:
-			cmd.Main(os.Getenv("CF_TRACE"), os.Args)
-		case flags.ErrCommandRequired:
-			if common.Commands.VerboseOrVersion {
-				parse([]string{"version"})
-			} else {
-				parse([]string{"help"})
-			}
-		default:
-			fmt.Fprintf(os.Stderr, "Unexpected flag error\ntype: %s\nmessage: %s\n", flagErr.Type, flagErr.Error())
-		}
+		return handleFlagErrorAndCommandHelp(flagErr, parser, extraArgs, args)
 	} else if err == ErrFailed {
-		os.Exit(1)
+		return 1
 	} else if err == ParseErr {
 		fmt.Println()
 		parse([]string{"help", args[0]})
-		os.Exit(1)
-	} else {
-		fmt.Fprintf(os.Stderr, "Unexpected error: %s\n", err.Error())
-		os.Exit(1)
+		return 1
+	} else if exitError, ok := err.(*ssh.ExitError); ok {
+		return exitError.ExitStatus()
 	}
+
+	fmt.Fprintf(os.Stderr, "Unexpected error: %s\n", err.Error())
+	return 1
+}
+
+func handleFlagErrorAndCommandHelp(flagErr *flags.Error, parser *flags.Parser, extraArgs []string, originalArgs []string) int {
+	switch flagErr.Type {
+	case flags.ErrHelp, flags.ErrUnknownFlag, flags.ErrExpectedArgument, flags.ErrInvalidChoice:
+		_, found := reflect.TypeOf(common.Commands).FieldByNameFunc(
+			func(fieldName string) bool {
+				field, _ := reflect.TypeOf(common.Commands).FieldByName(fieldName)
+				return parser.Active != nil && parser.Active.Name == field.Tag.Get("command")
+			},
+		)
+
+		if found && flagErr.Type == flags.ErrUnknownFlag && parser.Active.Name == "set-env" {
+			newArgs := []string{}
+			for _, arg := range originalArgs {
+				if arg[0] == '-' {
+					newArgs = append(newArgs, fmt.Sprintf("%s%s", v2.WorkAroundPrefix, arg))
+				} else {
+					newArgs = append(newArgs, arg)
+				}
+			}
+			parse(newArgs)
+			return 0
+		}
+
+		if flagErr.Type == flags.ErrUnknownFlag || flagErr.Type == flags.ErrExpectedArgument || flagErr.Type == flags.ErrInvalidChoice {
+			fmt.Fprintf(os.Stderr, "Incorrect Usage: %s\n\n", flagErr.Error())
+		}
+
+		var helpErrored int
+		if found {
+			helpErrored = parse([]string{"help", parser.Active.Name})
+		} else {
+			switch len(extraArgs) {
+			case 0:
+				helpErrored = parse([]string{"help"})
+			case 1:
+				if !isOption(extraArgs[0]) || (len(originalArgs) > 1 && extraArgs[0] == "-a") {
+					helpErrored = parse([]string{"help", extraArgs[0]})
+				} else {
+					helpErrored = parse([]string{"help"})
+				}
+			default:
+				if isCommand(extraArgs[0]) {
+					helpErrored = parse([]string{"help", extraArgs[0]})
+				} else {
+					helpErrored = parse(extraArgs[1:])
+				}
+			}
+		}
+
+		if helpErrored > 0 || flagErr.Type == flags.ErrUnknownFlag || flagErr.Type == flags.ErrExpectedArgument || flagErr.Type == flags.ErrInvalidChoice {
+			return 1
+		}
+	case flags.ErrRequired:
+		fmt.Fprintf(os.Stderr, "Incorrect Usage: %s\n\n", flagErr.Error())
+		parse([]string{"help", originalArgs[0]})
+		return 1
+	case flags.ErrMarshal:
+		errMessage := strings.Split(flagErr.Message, ":")
+		fmt.Fprintf(os.Stderr, "Incorrect Usage: %s\n\n", errMessage[0])
+		parse([]string{"help", originalArgs[0]})
+		return 1
+	case flags.ErrUnknownCommand:
+		cmd.Main(os.Getenv("CF_TRACE"), os.Args)
+	case flags.ErrCommandRequired:
+		if common.Commands.VerboseOrVersion {
+			parse([]string{"version"})
+		} else {
+			parse([]string{"help"})
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "Unexpected flag error\ntype: %s\nmessage: %s\n", flagErr.Type, flagErr.Error())
+	}
+	return 0
 }
 
 func isCommand(s string) bool {
@@ -216,7 +227,7 @@ func handleError(err error, commandUI UI) error {
 				"ExitCode": exitStatus,
 			})
 		}
-		os.Exit(exitStatus)
+		return err
 	}
 
 	commandUI.DisplayError(err)
