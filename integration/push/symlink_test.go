5923b92489b9dcb96bbc3c8ebb413fd27a1e18bb
Sebastian Vidrio <svidrio@pivotal.io>
2017-12-06 15:09:17 -0800

    add integration tests for push with symlinks
    [finishes #79898768, #132669013]

--- integration/push/symlink_test.go
+++ integration/push/symlink_test.go
@@ -86,27 +86,169 @@ var _ = Describe("push with symlink path", func() {
 				Eventually(session).Should(Exit(0))
 			})
 		})
+
+		Context("push with a single app manifest", func() {
+			Context("when the path property is a symlinked path", func() {
+				It("should push with the absolute path of the app", func() {
+					helpers.WithHelloWorldApp(func(dir string) {
+						Expect(os.Symlink(dir, symlinkedPath)).ToNot(HaveOccurred())
+
+						helpers.WriteManifest(filepath.Join(runningDir, "manifest.yml"), map[string]interface{}{
+							"applications": []map[string]string{
+								{
+									"name": appName,
+									"path": symlinkedPath,
+								},
+							},
+						})
+
+						session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: runningDir}, PushCommandName)
+						Eventually(session).Should(Say("path:\\s+(\\/private)?%s", regexp.QuoteMeta(dir)))
+						Eventually(session).Should(Exit(0))
+					})
+				})
+			})
+		})
+	})
+})
+
+var _ = Describe("push with symlinked resources", func() {
+	var (
+		appName string
+	)
+
+	BeforeEach(func() {
+		appName = helpers.NewAppName()
 	})
 
-	Context("push with a single app manifest", func() {
-		Context("when the path property is a symlinked path", func() {
-			It("should push with the absolute path of the app", func() {
-				helpers.WithHelloWorldApp(func(dir string) {
-					Expect(os.Symlink(dir, symlinkedPath)).ToNot(HaveOccurred())
+	Context("when pushing a directory", func() {
+		Context("when the directory contains a symlink to a file in the directory", func() {
+			Context("when the file exists", func() {
+				It("should push the symlink", func() {
+					helpers.WithHelloWorldApp(func(dir string) {
+						targetFile := filepath.Join(dir, "targetFile")
+						Expect(ioutil.WriteFile(targetFile, []byte("foo bar baz"), 0777)).ToNot(HaveOccurred())
+						relativePath, err := filepath.Rel(dir, targetFile)
 
-					helpers.WriteManifest(filepath.Join(runningDir, "manifest.yml"), map[string]interface{}{
-						"applications": []map[string]string{
-							{
-								"name": appName,
-								"path": symlinkedPath,
-							},
-						},
+						err = os.Symlink(relativePath, filepath.Join(dir, "symlinkFile"))
+						Expect(err).ToNot(HaveOccurred())
+
+						session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "--no-start")
+
+						Eventually(session).Should(Exit(0))
 					})
 
-					session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: runningDir}, PushCommandName)
-					Eventually(session).Should(Say("path:\\s+(\\/private)?%s", regexp.QuoteMeta(dir)))
+					helpers.VerifyAppPackageContents(appName, "symlinkFile", "targetFile", "Staticfile", "index.html")
+				})
+			})
+
+			Context("when the file doesn't exists", func() {
+				It("should push the symlink", func() {
+					helpers.WithHelloWorldApp(func(dir string) {
+						tempFile, err := ioutil.TempFile(dir, "tempFile")
+						Expect(err).ToNot(HaveOccurred())
+						tempFile.Close()
+						relativePath, err := filepath.Rel(dir, tempFile.Name())
+
+						err = os.Symlink(relativePath, filepath.Join(dir, "symlinkFile"))
+						Expect(err).ToNot(HaveOccurred())
+						Expect(os.Remove(tempFile.Name()))
+
+						session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "--no-start")
+
+						Eventually(session).Should(Exit(0))
+					})
+
+					helpers.VerifyAppPackageContents(appName, "symlinkFile", "Staticfile", "index.html")
+				})
+			})
+		})
+
+		Context("when the directory contains a symlink to subdirectory in the directory", func() {
+			It("should push the symlink", func() {
+				helpers.WithHelloWorldApp(func(dir string) {
+					targetDir, err := ioutil.TempDir(dir, "target-dir")
+					Expect(err).ToNot(HaveOccurred())
+					relativePath, err := filepath.Rel(dir, targetDir)
+
+					err = os.Symlink(relativePath, filepath.Join(dir, "symlinkFile"))
+					Expect(err).ToNot(HaveOccurred())
+					Expect(os.RemoveAll(targetDir)).ToNot(HaveOccurred())
+
+					session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "--no-start")
+
 					Eventually(session).Should(Exit(0))
 				})
+
+				helpers.VerifyAppPackageContents(appName, "symlinkFile", "Staticfile", "index.html")
+			})
+		})
+
+		Context("when the directory contains a symlink to a file outside the directory", func() {
+			var targetPath string
+
+			BeforeEach(func() {
+				tmpFile, err := ioutil.TempFile("", "push-symlink-integration-")
+				Expect(err).ToNot(HaveOccurred())
+				tmpFile.Close()
+
+				targetPath = tmpFile.Name()
+			})
+
+			AfterEach(func() {
+				Expect(os.Remove(targetPath))
+			})
+
+			Context("when pushing a symlinked file", func() {
+				It("should push the symlinked file", func() {
+					helpers.WithHelloWorldApp(func(dir string) {
+						err := os.Symlink(targetPath, filepath.Join(dir, "symlinkFile"))
+						Expect(err).ToNot(HaveOccurred())
+
+						session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "--no-start")
+
+						Eventually(session.Err).Should(Say("The app upload is invalid: Symlink\\(s\\) point outside of root folder"))
+						Eventually(session).Should(Exit(1))
+					})
+				})
+			})
+		})
+	})
+
+	Context("when pushing an archive", func() {
+		var archive string
+
+		AfterEach(func() {
+			Expect(os.RemoveAll(archive)).ToNot(HaveOccurred())
+		})
+
+		Context("when the archive contains a symlink to a file in the directory", func() {
+			BeforeEach(func() {
+				helpers.WithHelloWorldApp(func(appDir string) {
+					helpers.WithHelloWorldApp(func(appDir string) {
+						tmpfile, err := ioutil.TempFile("", "push-archive-integration")
+						Expect(err).ToNot(HaveOccurred())
+						archive = tmpfile.Name()
+						Expect(tmpfile.Close()).ToNot(HaveOccurred())
+
+						targetFile := filepath.Join(appDir, "targetFile")
+						Expect(ioutil.WriteFile(targetFile, []byte("some random data"), 0777)).ToNot(HaveOccurred())
+						relativePath, err := filepath.Rel(appDir, targetFile)
+
+						err = os.Symlink(relativePath, filepath.Join(appDir, "symlinkFile"))
+						Expect(err).ToNot(HaveOccurred())
+
+						err = helpers.Zipit(appDir, archive, "")
+						Expect(err).ToNot(HaveOccurred())
+					})
+				})
+			})
+
+			It("should push the symlink", func() {
+				session := helpers.CF(PushCommandName, appName, "--no-start", "-p", archive)
+
+				Eventually(session).Should(Exit(0))
+				helpers.VerifyAppPackageContents(appName, "symlinkFile", "targetFile", "Staticfile", "index.html")
 			})
 		})
 	})
