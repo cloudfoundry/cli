5c0244592d5aa566fb10ea2644baa82c6729baa8
Anand Gaitonde <agaitonde@pivotal.io>
2018-01-08 09:43:54 -0800

    fix tests
    [#152495056]

--- integration/push/http_random_route_test.go
+++ integration/push/http_random_route_test.go
@@ -26,13 +26,13 @@ var _ = Describe("HTTP random route", func() {
 				helpers.WithHelloWorldApp(func(dir string) {
 					session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "--random-route", "--no-start")
 					Eventually(session).Should(Say("routes:"))
-					Eventually(session).Should(Say("(?i)\\+\\s+%s-[\\w\\d]+-[\\w\\d]+.%s", appName, defaultSharedDomain()))
+					Eventually(session).Should(Say("(?i)\\+\\s+%s-[\\w]+-[\\w]+.%s", appName, defaultSharedDomain()))
 					Eventually(session).Should(Exit(0))
 				})
 
 				appSession := helpers.CF("app", appName)
 				Eventually(appSession).Should(Say("name:\\s+%s", appName))
-				Eventually(appSession).Should(Say("(?i)routes:\\s+%s-[\\w\\d]+-[\\w\\d]+.%s", appName, defaultSharedDomain()))
+				Eventually(appSession).Should(Say("(?i)routes:\\s+%s-[\\w]+-[\\w]+.%s", appName, defaultSharedDomain()))
 				Eventually(appSession).Should(Exit(0))
 			})
 		})
@@ -41,7 +41,7 @@ var _ = Describe("HTTP random route", func() {
 			It("does not generate a random route for the app", func() {
 				helpers.WithHelloWorldApp(func(dir string) {
 					session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "--random-route", "--no-start")
-					Eventually(session).ShouldNot(Say("\\+\\s+%s-adj", appName))
+					Eventually(session).ShouldNot(Say("\\+\\s+%s-[\\w]+", appName))
 					Eventually(session).Should(Exit(0))
 				})
 			})
@@ -61,7 +61,7 @@ var _ = Describe("HTTP random route", func() {
 				It("does not create a new route with the provided domain name", func() {
 					helpers.WithHelloWorldApp(func(dir string) {
 						session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "--random-route", "-d", domain.Name, "--no-start")
-						Eventually(session).ShouldNot(Say("\\+\\s+%s-adj[\\d]+-noun[\\d]+\\.%s", appName, domain.Name))
+						Eventually(session).ShouldNot(Say("(?i)\\+\\s+%s-[\\w]+-[\\w]+.%s", appName, domain.Name))
 						Eventually(session).Should(Exit(0))
 					})
 				})
@@ -83,7 +83,7 @@ var _ = Describe("HTTP random route", func() {
 			It("creates a new route with the provided domain", func() {
 				helpers.WithHelloWorldApp(func(dir string) {
 					session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "--random-route", "-d", domain.Name, "--no-start")
-					Eventually(session).Should(Say("\\+\\s+%s-adj[\\d]+-noun[\\d]+\\.%s", appName, domain.Name))
+					Eventually(session).Should(Say("(?i)\\+\\s+%s-[\\w]+-[\\w]+.%s", appName, domain.Name))
 					Eventually(session).Should(Exit(0))
 				})
 			})
@@ -107,7 +107,7 @@ var _ = Describe("HTTP random route", func() {
 				helpers.WriteManifest(path.Join(dir, "manifest.yml"), manifest)
 				session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "--no-start")
 				Eventually(session).Should(Say("routes:"))
-				Eventually(session).Should(Say("(?i)\\+\\s+%s-[\\w\\d]+-[\\w\\d]+.%s", appName, defaultSharedDomain()))
+				Eventually(session).Should(Say("(?i)\\+\\s+%s-[\\w]+-[\\w]+.%s", appName, defaultSharedDomain()))
 				Eventually(session).Should(Exit(0))
 			})
 		})
@@ -115,7 +115,7 @@ var _ = Describe("HTTP random route", func() {
 		It("generates a random route for the app", func() {
 			session := helpers.CF("app", appName)
 			Eventually(session).Should(Say("name:\\s+%s", appName))
-			Eventually(session).Should(Say("(?i)routes:\\s+%s-[\\w\\d]+-[\\w\\d]+.%s", appName, defaultSharedDomain()))
+			Eventually(session).Should(Say("(?i)routes:\\s+%s-[\\w]+-[\\w]+.%s", appName, defaultSharedDomain()))
 			Eventually(session).Should(Exit(0))
 		})
 
@@ -125,7 +125,7 @@ var _ = Describe("HTTP random route", func() {
 					helpers.WriteManifest(path.Join(dir, "manifest.yml"), manifest)
 
 					session := helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "--no-start")
-					Eventually(session).ShouldNot(Say("\\+\\s+%s-adj", appName))
+					Eventually(session).ShouldNot(Say("\\+\\s+%s-[\\w]+", appName))
 					Eventually(session).Should(Exit(0))
 				})
 			})
