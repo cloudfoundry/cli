88b9c31d1553d5583e0f0d4d6011548cea2c870d
An Yu <anyu@pivotal.io>
2018-01-26 15:09:22 -0800

    remove service_instance_sharing feature flag tests
    There were some issues with service_instance_sharing on the testing
    stacks. Removing until those can be addressed.
    
    - Fixed implementation of feature_flags in test setup
    
    Signed-off-by: Sebastian Vidrio <svidrio@pivotal.io>

--- integration/helpers/feature_flags.go
+++ integration/helpers/feature_flags.go
@@ -6,11 +6,18 @@ import (
 )
 
 func EnableFeatureFlag(flagName string) {
-	session := CF("enable-feature-flag", flagName)
-	Eventually(session).Should(Exit(0))
+	tempHome := SetHomeDir()
+	SetAPI()
+	LoginCF()
+	Eventually(CF("enable-feature-flag", flagName)).Should(Exit(0))
+	DestroyHomeDir(tempHome)
 }
 
 func DisableFeatureFlag(flagName string) {
+	tempHome := SetHomeDir()
+	SetAPI()
+	LoginCF()
 	session := CF("disable-feature-flag", flagName)
 	Eventually(session).Should(Exit(0))
+	DestroyHomeDir(tempHome)
 }
