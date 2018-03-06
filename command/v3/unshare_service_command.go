13774cd6a78babe8fc3ec65e5ea8464d120f6dbe
Sebastian Vidrio <svidrio@pivotal.io>
2018-03-06 11:07:23 -0800

    display experimental warnings on unshare-service command
    [finishes #155713357]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- command/v3/unshare_service_command.go
+++ command/v3/unshare_service_command.go
@@ -67,6 +67,9 @@ func (cmd *UnshareServiceCommand) Setup(config command.Config, ui command.UI) er
 }
 
 func (cmd UnshareServiceCommand) Execute(args []string) error {
+	cmd.UI.DisplayText(command.ExperimentalWarning)
+	cmd.UI.DisplayNewline()
+
 	err := command.MinimumAPIVersionCheck(cmd.Actor.CloudControllerV3APIVersion(), ccversion.MinVersionShareServiceV3)
 	if err != nil {
 		return err
