5ee4ffb87f8c0bfec4e41285fbe9961ec685fa3a
Sebastian Vidrio <svidrio@pivotal.io>
2018-03-06 11:15:54 -0800

    make this less flaky
    [#155644192]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- util/randomword/generator_test.go
+++ util/randomword/generator_test.go
@@ -18,21 +18,33 @@ var _ = Describe("Generator", func() {
 
 	Describe("RandomAdjective", func() {
 		It("generates a random adjective each time it is called", func() {
-			adj := gen.RandomAdjective()
-			// We wait for 3 millisecond because the seed we use to generate the
-			// randomness has a unit of 1 nanosecond plus random test flakiness
-			time.Sleep(3)
-			Expect(adj).ToNot(Equal(gen.RandomAdjective()))
+			setOne := []string{}
+			setTwo := []string{}
+
+			for i := 0; i < 3; i++ {
+				setOne = append(setOne, gen.RandomAdjective())
+				// We wait for 3 millisecond because the seed we use to generate the
+				// randomness has a unit of 1 nanosecond plus random test flakiness
+				time.Sleep(3)
+				setTwo = append(setTwo, gen.RandomAdjective())
+			}
+			Expect(setOne).ToNot(ConsistOf(setTwo))
 		})
 	})
 
 	Describe("RandomNoun", func() {
 		It("generates a random noun each time it is called", func() {
-			noun := gen.RandomNoun()
-			// We wait for 3 millisecond because the seed we use to generate the
-			// randomness has a unit of 1 nanosecond plus random test flakiness
-			time.Sleep(10)
-			Expect(noun).ToNot(Equal(gen.RandomNoun()))
+			setOne := []string{}
+			setTwo := []string{}
+
+			for i := 0; i < 3; i++ {
+				setOne = append(setOne, gen.RandomNoun())
+				// We wait for 3 millisecond because the seed we use to generate the
+				// randomness has a unit of 1 nanosecond plus random test flakiness
+				time.Sleep(3)
+				setTwo = append(setTwo, gen.RandomNoun())
+			}
+			Expect(setOne).ToNot(ConsistOf(setTwo))
 		})
 	})
 
