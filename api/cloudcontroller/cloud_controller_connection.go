f85e2cddbc492aaaa9223c854f34cc9577779428
Anand Gaitonde <agaitonde@pivotal.io>
2018-05-07 14:42:55 -0700

    reorganizing based on style rules
    [#149934052]

--- api/cloudcontroller/cloud_controller_connection.go
+++ api/cloudcontroller/cloud_controller_connection.go
@@ -13,17 +13,17 @@ import (
 	"code.cloudfoundry.org/cli/api/cloudcontroller/ccerror"
 )
 
+// Config is for configuring a CloudControllerConnection.
+type Config struct {
+	DialTimeout       time.Duration
+	SkipSSLValidation bool
+}
+
 // CloudControllerConnection represents a connection to the Cloud Controller
 // server.
 type CloudControllerConnection struct {
 	HTTPClient *http.Client
 	UserAgent  string
-}
-
-// Config is for configuring a CloudControllerConnection.
-type Config struct {
-	DialTimeout       time.Duration
-	SkipSSLValidation bool
 }
 
 // NewConnection returns a new CloudControllerConnection with provided
@@ -60,24 +60,49 @@ func (connection *CloudControllerConnection) Make(request *Request, passedRespon
 	return connection.populateResponse(response, passedResponse)
 }
 
-func (*CloudControllerConnection) processRequestErrors(request *http.Request, err error) error {
-	switch e := err.(type) {
-	case *url.Error:
-		switch urlErr := e.Err.(type) {
-		case x509.UnknownAuthorityError:
-			return ccerror.UnverifiedServerError{
-				URL: request.URL.String(),
-			}
-		case x509.HostnameError:
-			return ccerror.SSLValidationHostnameError{
-				Message: urlErr.Error(),
-			}
-		default:
-			return ccerror.RequestError{Err: e}
+func (*CloudControllerConnection) handleStatusCodes(response *http.Response, passedResponse *Response) error {
+	if response.StatusCode == http.StatusNoContent {
+		passedResponse.RawResponse = []byte("{}")
+	} else {
+		rawBytes, err := ioutil.ReadAll(response.Body)
+		defer response.Body.Close()
+		if err != nil {
+			return err
 		}
-	default:
-		return err
+
+		passedResponse.RawResponse = rawBytes
 	}
+
+	if response.StatusCode >= 400 {
+		return ccerror.RawHTTPStatusError{
+			StatusCode:  response.StatusCode,
+			RawResponse: passedResponse.RawResponse,
+			RequestIDs:  response.Header["X-Vcap-Request-Id"],
+		}
+	}
+
+	return nil
+}
+
+// handleWarnings looks for the "X-Cf-Warnings" header in the cloud controller
+// response and URI decodes them. The value can contain multiple warnings that
+// are comma separated.
+func (*CloudControllerConnection) handleWarnings(response *http.Response) ([]string, error) {
+	rawWarnings := response.Header.Get("X-Cf-Warnings")
+	rawWarnings, err := url.QueryUnescape(rawWarnings)
+	if err != nil {
+		return nil, err
+	}
+
+	var warnings []string
+	if rawWarnings != "" {
+		for _, warning := range strings.Split(rawWarnings, ",") {
+			warningTrimmed := strings.Trim(warning, " ")
+			warnings = append(warnings, warningTrimmed)
+		}
+	}
+
+	return warnings, nil
 }
 
 func (connection *CloudControllerConnection) populateResponse(response *http.Response, passedResponse *Response) error {
@@ -111,47 +136,22 @@ func (connection *CloudControllerConnection) populateResponse(response *http.Res
 	return nil
 }
 
-// handleWarnings looks for the "X-Cf-Warnings" header in the cloud controller
-// response and URI decodes them. The value can contain multiple warnings that
-// are comma separated.
-func (*CloudControllerConnection) handleWarnings(response *http.Response) ([]string, error) {
-	rawWarnings := response.Header.Get("X-Cf-Warnings")
-	rawWarnings, err := url.QueryUnescape(rawWarnings)
-	if err != nil {
-		return nil, err
-	}
-
-	var warnings []string
-	if rawWarnings != "" {
-		for _, warning := range strings.Split(rawWarnings, ",") {
-			warningTrimmed := strings.Trim(warning, " ")
-			warnings = append(warnings, warningTrimmed)
+func (*CloudControllerConnection) processRequestErrors(request *http.Request, err error) error {
+	switch e := err.(type) {
+	case *url.Error:
+		switch urlErr := e.Err.(type) {
+		case x509.UnknownAuthorityError:
+			return ccerror.UnverifiedServerError{
+				URL: request.URL.String(),
+			}
+		case x509.HostnameError:
+			return ccerror.SSLValidationHostnameError{
+				Message: urlErr.Error(),
+			}
+		default:
+			return ccerror.RequestError{Err: e}
 		}
+	default:
+		return err
 	}
-
-	return warnings, nil
-}
-
-func (*CloudControllerConnection) handleStatusCodes(response *http.Response, passedResponse *Response) error {
-	if response.StatusCode == http.StatusNoContent {
-		passedResponse.RawResponse = []byte("{}")
-	} else {
-		rawBytes, err := ioutil.ReadAll(response.Body)
-		defer response.Body.Close()
-		if err != nil {
-			return err
-		}
-
-		passedResponse.RawResponse = rawBytes
-	}
-
-	if response.StatusCode >= 400 {
-		return ccerror.RawHTTPStatusError{
-			StatusCode:  response.StatusCode,
-			RawResponse: passedResponse.RawResponse,
-			RequestIDs:  response.Header["X-Vcap-Request-Id"],
-		}
-	}
-
-	return nil
 }
