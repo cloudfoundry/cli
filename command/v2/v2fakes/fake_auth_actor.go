8ca2d94675e8a1dd0facebbb14c60b6932b6440e
Torsten Luh <torsten.luh@sap.com>
2018-08-10 07:37:36 +0000

    Use generated fakes
    [#159412764] https://www.pivotaltracker.com/story/show/159412764
    
    Signed-off-by: Florian Tack <florian.tack@sap.com>

--- command/v2/v2fakes/fake_auth_actor.go
+++ command/v2/v2fakes/fake_auth_actor.go
@@ -9,11 +9,12 @@ import (
 )
 
 type FakeAuthActor struct {
-	AuthenticateStub        func(ID string, secret string, grantType constant.GrantType) error
+	AuthenticateStub        func(ID string, secret string, origin string, grantType constant.GrantType) error
 	authenticateMutex       sync.RWMutex
 	authenticateArgsForCall []struct {
 		ID        string
 		secret    string
+		origin    string
 		grantType constant.GrantType
 	}
 	authenticateReturns struct {
@@ -32,12 +33,13 @@ func (fake *FakeAuthActor) Authenticate(ID string, secret string, origin string,
 	fake.authenticateArgsForCall = append(fake.authenticateArgsForCall, struct {
 		ID        string
 		secret    string
+		origin    string
 		grantType constant.GrantType
-	}{ID, secret, grantType})
-	fake.recordInvocation("Authenticate", []interface{}{ID, secret, grantType})
+	}{ID, secret, origin, grantType})
+	fake.recordInvocation("Authenticate", []interface{}{ID, secret, origin, grantType})
 	fake.authenticateMutex.Unlock()
 	if fake.AuthenticateStub != nil {
-		return fake.AuthenticateStub(ID, secret, grantType)
+		return fake.AuthenticateStub(ID, secret, origin, grantType)
 	}
 	if specificReturn {
 		return ret.result1
@@ -51,10 +53,10 @@ func (fake *FakeAuthActor) AuthenticateCallCount() int {
 	return len(fake.authenticateArgsForCall)
 }
 
-func (fake *FakeAuthActor) AuthenticateArgsForCall(i int) (string, string, constant.GrantType) {
+func (fake *FakeAuthActor) AuthenticateArgsForCall(i int) (string, string, string, constant.GrantType) {
 	fake.authenticateMutex.RLock()
 	defer fake.authenticateMutex.RUnlock()
-	return fake.authenticateArgsForCall[i].ID, fake.authenticateArgsForCall[i].secret, fake.authenticateArgsForCall[i].grantType
+	return fake.authenticateArgsForCall[i].ID, fake.authenticateArgsForCall[i].secret, fake.authenticateArgsForCall[i].origin, fake.authenticateArgsForCall[i].grantType
 }
 
 func (fake *FakeAuthActor) AuthenticateReturns(result1 error) {
