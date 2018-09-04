42272b7174dbbccf659b8874950850b89c5eda40
Will Murphy <wmurphy@pivotal.io>
2018-09-04 15:21:25 -0700

    Put stacks last in output of "cf buildpacks"
    For backwards compatibility with scripts that may be parsing the output
    of `cf buildpacks`, we moved the new column "stacks" to the rightmost
    position in the output.
    
    Also add integration tests for buildpacks command, and add a helper
    method for checking the unrefactored commands' behavior when the user
    is not logged in or not targeting a CF correctly. (Old commands write
    these errors to STDOUT instead of STDERR.)
    
    [Finishes #160187405]
    
    Co-authored-by: Ryan Tang <rtang@pivotal.io>

---
+++ integration/global/buildpacks_command_test.go
@@ -0,0 +1,69 @@
+package global
+
+import (
+	"fmt"
+
+	"code.cloudfoundry.org/cli/integration/helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("buildpacks command", func() {
+
+	When("--help is passed", func() {
+		It("displays the help message", func() {
+			session := helpers.CF("buildpacks", "--help")
+			Eventually(session).Should(Say("NAME:"))
+			Eventually(session).Should(Say("buildpacks - List all buildpacks"))
+			Eventually(session).Should(Say("USAGE:"))
+			Eventually(session).Should(Say("cf buildpacks"))
+			Eventually(session).Should(Say("SEE ALSO:"))
+			Eventually(session).Should(Say("push"))
+			Eventually(session).Should(Exit(0))
+		})
+	})
+
+	When("environment is not set up", func() {
+		It("displays an error and exits 1", func() {
+			Skip("Unrefactored command is writing login errors to STDOUT; remove skip when refactored")
+			helpers.CheckEnvironmentTargetedCorrectly(false, false, ReadOnlyOrg, "buildpacks")
+		})
+
+		It("displays an error and exits 1", func() {
+			helpers.UnrefactoredCheckEnvironmentTargetedCorrectly(false, false, ReadOnlyOrg, "buildpacks")
+		})
+	})
+
+	When("too many args are passed", func() {
+		It("displays FAILED, then usage, then exits 1", func() {
+			session := helpers.CF("buildpacks", "no-further-args-allowed")
+			Eventually(session).Should(Say("FAILED"))
+			Eventually(session).Should(Say("NAME:"))
+			Eventually(session).Should(Say("buildpacks - List all buildpacks"))
+			Eventually(session).Should(Say("USAGE:"))
+			Eventually(session).Should(Say("cf buildpacks"))
+			Eventually(session).Should(Exit(1))
+		})
+	})
+
+	When("the command is called correctly", func() {
+		It("lists the buildpacks", func() {
+			helpers.LoginCF()
+			session := helpers.CF("buildpacks")
+			Eventually(session).Should(Say("Getting buildpacks..."))
+			Eventually(session).Should(Say("buildpack\\s+position\\s+enabled\\s+locked\\s+filename\\s+stack"))
+
+			buildpackNameRegex := `staticfile_buildpack`
+			positionRegex := `\d+`
+			boolRegex := `(true|false)`
+			buildpackFileRegex := `staticfile-buildpack-\S+`
+			stackRegex := `(cflinuxfs[23]|windows.+)`
+
+			Eventually(session).Should(Say(fmt.Sprintf("%s\\s+%s\\s+%s\\s+%s\\s+%s\\s+%s", buildpackNameRegex,
+				positionRegex, boolRegex, boolRegex, buildpackFileRegex, stackRegex)))
+			Eventually(session).Should(Exit(0))
+		})
+	})
+})
--- integration/helpers/environment.go
+++ integration/helpers/environment.go
@@ -63,3 +63,38 @@ func CheckEnvironmentTargetedCorrectly(targetedOrganizationRequired bool, target
 	Eventually(session.Err).Should(Say("No API endpoint set\\. Use 'cf login' or 'cf api' to target an endpoint\\."))
 	Eventually(session).Should(Exit(1))
 }
+
+func UnrefactoredCheckEnvironmentTargetedCorrectly(targetedOrganizationRequired bool, targetedSpaceRequired bool, testOrg string, command ...string) {
+	LoginCF()
+
+	if targetedOrganizationRequired {
+		By("errors if org is not targeted")
+		session := CF(command...)
+		Eventually(session).Should(Say("FAILED"))
+		Eventually(session).Should(Say("No org targeted, use 'cf target -o ORG' to target an org\\."))
+		Eventually(session).Should(Exit(1))
+
+		if targetedSpaceRequired {
+			By("errors if space is not targeted")
+			TargetOrg(testOrg)
+			session := CF(command...)
+			Eventually(session).Should(Say("FAILED"))
+			Eventually(session).Should(Say("No space targeted, use 'cf target -s SPACE' to target a space\\."))
+			Eventually(session).Should(Exit(1))
+		}
+	}
+
+	By("errors if user not logged in")
+	LogoutCF()
+	session := CF(command...)
+	Eventually(session).Should(Say("FAILED"))
+	Eventually(session).Should(Say("Not logged in\\. Use 'cf login' to log in\\."))
+	Eventually(session).Should(Exit(1))
+
+	By("errors if cli not targeted")
+	UnsetAPI()
+	session = CF(command...)
+	Eventually(session).Should(Say("FAILED"))
+	Eventually(session).Should(Say("No API endpoint set\\. Use 'cf login' or 'cf api' to target an endpoint\\."))
+	Eventually(session).Should(Exit(1))
+}
