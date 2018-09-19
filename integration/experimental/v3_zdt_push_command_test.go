b4d0da956632e8f54f1be51ced1f9c3994754ed4
Tom Viehman <tviehman@pivotal.io>
2018-09-19 15:02:33 -0700

    fix version assertion
    [#160008817]

--- integration/experimental/v3_zdt_push_command_test.go
+++ integration/experimental/v3_zdt_push_command_test.go
@@ -135,7 +135,7 @@ var _ = Describe("v3-zdt-push command", func() {
 			It("fails with error message that the minimum version is not met", func() {
 				session := helpers.CF("v3-zdt-push", appName)
 				Eventually(session).Should(Say("FAILED"))
-				Eventually(session.Err).Should(Say("This command requires CF API version 3\\.27\\.0 or higher\\."))
+				Eventually(session.Err).Should(Say("This command requires CF API version 3\\.57\\.0 or higher\\."))
 				Eventually(session).Should(Exit(1))
 			})
 		})
@@ -154,7 +154,7 @@ var _ = Describe("v3-zdt-push command", func() {
 			It("fails with error message that the minimum version is not met", func() {
 				session := helpers.CF("v3-zdt-push", appName)
 				Eventually(session).Should(Say("FAILED"))
-				Eventually(session.Err).Should(Say("This command requires CF API version 3\\.27\\.0 or higher\\."))
+				Eventually(session.Err).Should(Say("This command requires CF API version 3\\.57\\.0 or higher\\."))
 				Eventually(session).Should(Exit(1))
 			})
 		})
