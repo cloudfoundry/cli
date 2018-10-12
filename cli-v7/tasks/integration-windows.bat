0bfa1fbb06d8e323d5e75cb0c135aca182f1ba40
Anand Gaitonde <agaitonde@pivotal.io>
2018-10-12 14:38:50 -0700

    Add windows integration tests to v7 pipeline
    [#160853831]
    
    Signed-off-by: Eli Wrenn <ewrenn@pivotal.io>

--- cli-v7/tasks/integration-windows.bat
+++ cli-v7/tasks/integration-windows.bat
@@ -15,11 +15,11 @@ SET CF_INT_API=https://api.%DOMAIN%
 pushd %CD%\cf-cli-binaries
 	7z e cf-cli-binaries.tgz
 	7z x cf-cli-binaries.tar
-	MOVE %CD%\cf-cli_winx64.exe ..\cf.exe
+	MOVE %CD%\cf7-cli_winx64.exe ..\cf.exe
 popd
 
 go get -v -u github.com/onsi/ginkgo/ginkgo
 
 cd %GOPATH%\src\code.cloudfoundry.org\cli
-ginkgo.exe -r -nodes=16 -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/isolated ./integration/v6/isolated ./integration/shared/plugin ./integration/v6/push || exit 1
-ginkgo.exe -r -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/global ./integration/v6/global || exit 1
+ginkgo.exe -r -nodes=16 -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/isolated ./integration/v7/isolated ./integration/shared/plugin || exit 1
+ginkgo.exe -r -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/global ./integration/v7/global || exit 1
