ee58ecd3ef12830f582506a8c939f88fc34f823f
Eli Wrenn <ewrenn@pivotal.io>
2018-10-12 12:14:48 -0700

    duplicate v6 pipeline into cli-v7
    [#160853831]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

---
+++ ci/cli-v7/tasks/integration-windows.bat
@@ -0,0 +1,25 @@
+SET GOPATH=%CD%\go
+SET CF_DIAL_TIMEOUT=15
+
+SET PATH=C:\Go\bin;%PATH%
+SET PATH=%GOPATH%\bin;%PATH%
+SET PATH=C:\Program Files\GnuWin32\bin;%PATH%
+SET PATH=%CD%;%PATH%
+
+SET /p DOMAIN=<%CD%\bosh-lock\name
+SET /p CF_INT_PASSWORD=<%CD%\cf-credentials\cf-password
+SET /p CF_INT_OIDC_PASSWORD=<%CD%\cf-credentials\uaa-oidc-password
+SET CF_INT_OIDC_USERNAME=admin-oidc
+SET CF_INT_API=https://api.%DOMAIN%
+
+pushd %CD%\cf-cli-binaries
+	7z e cf-cli-binaries.tgz
+	7z x cf-cli-binaries.tar
+	MOVE %CD%\cf-cli_winx64.exe ..\cf.exe
+popd
+
+go get -v -u github.com/onsi/ginkgo/ginkgo
+
+cd %GOPATH%\src\code.cloudfoundry.org\cli
+ginkgo.exe -r -nodes=16 -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/isolated ./integration/v6/isolated ./integration/shared/plugin ./integration/v6/push || exit 1
+ginkgo.exe -r -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/global ./integration/v6/global || exit 1
---
+++ ci/cli-v7/tasks/units-windows.bat
@@ -0,0 +1,15 @@
+SET TARGET_V7=true
+SET GOPATH=%CD%\gopath
+SET PATH=C:\Go\bin;C:\Program Files\Git\cmd\;%GOPATH%\bin;%PATH%
+
+cd %GOPATH%\src\code.cloudfoundry.org\cli
+
+powershell -command set-executionpolicy remotesigned
+
+go version
+
+go get -u github.com/onsi/ginkgo/ginkgo
+
+ginkgo version
+
+ginkgo -r -race -randomizeAllSpecs -randomizeSuites -skipPackage integration -flakeAttempts=2 .
