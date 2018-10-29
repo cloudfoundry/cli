71788ab9e88bae45f770610315cf181f84b7264d
Anand Gaitonde <agaitonde@pivotal.io>
2018-10-29 14:03:03 -0700

    use helper.CheckEnvironmentTargetedCorrectly to clean up duplicate tests
    [Finishes #161515732]
    
    Signed-off-by: Mike Kenyon <mkenyon@pivotal.io>

--- integration/v7/isolated/get_health_check_command_test.go
+++ integration/v7/isolated/get_health_check_command_test.go
@@ -47,59 +47,8 @@ var _ = Describe("get-health-check command", func() {
 	})
 
 	When("the environment is not setup correctly", func() {
-		When("no API endpoint is set", func() {
-			BeforeEach(func() {
-				helpers.UnsetAPI()
-			})
-
-			It("fails with no API endpoint set message", func() {
-				session := helpers.CF("get-health-check", appName)
-				Eventually(session).Should(Say("FAILED"))
-				Eventually(session.Err).Should(Say(`No API endpoint set\. Use 'cf login' or 'cf api' to target an endpoint\.`))
-				Eventually(session).Should(Exit(1))
-			})
-		})
-
-		When("not logged in", func() {
-			BeforeEach(func() {
-				helpers.LogoutCF()
-			})
-
-			It("fails with not logged in message", func() {
-				session := helpers.CF("get-health-check", appName)
-				Eventually(session).Should(Say("FAILED"))
-				Eventually(session.Err).Should(Say(`Not logged in\. Use 'cf login' to log in\.`))
-				Eventually(session).Should(Exit(1))
-			})
-		})
-
-		When("there is no org set", func() {
-			BeforeEach(func() {
-				helpers.LogoutCF()
-				helpers.LoginCF()
-			})
-
-			It("fails with no org targeted error message", func() {
-				session := helpers.CF("get-health-check", appName)
-				Eventually(session).Should(Say("FAILED"))
-				Eventually(session.Err).Should(Say(`No org targeted, use 'cf target -o ORG' to target an org\.`))
-				Eventually(session).Should(Exit(1))
-			})
-		})
-
-		When("there is no space set", func() {
-			BeforeEach(func() {
-				helpers.LogoutCF()
-				helpers.LoginCF()
-				helpers.TargetOrg(ReadOnlyOrg)
-			})
-
-			It("fails with no space targeted error message", func() {
-				session := helpers.CF("get-health-check", appName)
-				Eventually(session).Should(Say("FAILED"))
-				Eventually(session.Err).Should(Say(`No space targeted, use 'cf target -s SPACE' to target a space\.`))
-				Eventually(session).Should(Exit(1))
-			})
+		It("fails with the appropriate errors", func() {
+			helpers.CheckEnvironmentTargetedCorrectly(true, true, ReadOnlyOrg, "get-health-check", appName)
 		})
 	})
 
