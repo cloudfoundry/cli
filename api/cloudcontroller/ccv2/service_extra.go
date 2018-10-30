4ba4a741acfc97cf4826fcf655bf81bac9cc4e0c
Jen Spinney <jspinney@pivotal.io>
2018-10-30 16:31:55 -0700

    Alphabetize and add GoDoc to ServiceExtra struct.
    [finishes #161251875]

--- api/cloudcontroller/ccv2/service_extra.go
+++ api/cloudcontroller/ccv2/service_extra.go
@@ -2,9 +2,10 @@ package ccv2
 
 // ServiceExtra contains extra service related properties.
 type ServiceExtra struct {
+	// DocumentationURL is the URL of the documentation page for the service.
+	DocumentationURL string `json:"documentationUrl"`
+
 	// Shareable is true if the service is shareable across organizations and
 	// spaces.
 	Shareable bool
-
-	DocumentationURL string `json:"documentationUrl"`
 }
