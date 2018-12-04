20a8ecd27544d703d0209c23d2956629613f940a
Mike Kenyon <mkenyon@pivotal.io>
2018-12-04 12:09:31 -0800

    Fix comments in ccv3/stack.go
    Looks like it was copy-pastaed from space.go

--- api/cloudcontroller/ccv3/stack.go
+++ api/cloudcontroller/ccv3/stack.go
@@ -6,15 +6,15 @@ import (
 )
 
 type Stack struct {
-	// GUID is a unique space identifier.
+	// GUID is a unique stack identifier.
 	GUID string `json:"guid"`
-	// Name is the name of the space.
+	// Name is the name of the stack.
 	Name string `json:"name"`
 	// Description is the description for the stack
 	Description string `json:"description"`
 }
 
-// GetSpaces lists spaces with optional filters.
+// GetStacks lists stacks with optional filters.
 func (client *Client) GetStacks(query ...Query) ([]Stack, Warnings, error) {
 	request, err := client.newHTTPRequest(requestOptions{
 		RequestName: internal.GetStacksRequest,
