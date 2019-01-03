4f6e4ead487326e7891461be1e3d1504911abac6
Spencer Hawley <shawley@pivotal.io>
2019-01-02 17:09:20 -0800

    Update v7 push fake
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- command/v7/v7fakes/fake_v7actor_for_push.go
+++ command/v7/v7fakes/fake_v7actor_for_push.go
@@ -72,19 +72,6 @@ type FakeV7ActorForPush struct {
 		result3 v7action.Warnings
 		result4 error
 	}
-	PollStartStub        func(string) (v7action.Warnings, error)
-	pollStartMutex       sync.RWMutex
-	pollStartArgsForCall []struct {
-		arg1 string
-	}
-	pollStartReturns struct {
-		result1 v7action.Warnings
-		result2 error
-	}
-	pollStartReturnsOnCall map[int]struct {
-		result1 v7action.Warnings
-		result2 error
-	}
 	RestartApplicationStub        func(string) (v7action.Warnings, error)
 	restartApplicationMutex       sync.RWMutex
 	restartApplicationArgsForCall []struct {
@@ -361,69 +348,6 @@ func (fake *FakeV7ActorForPush) GetStreamingLogsForApplicationByNameAndSpaceRetu
 	}{result1, result2, result3, result4}
 }
 
-func (fake *FakeV7ActorForPush) PollStart(arg1 string) (v7action.Warnings, error) {
-	fake.pollStartMutex.Lock()
-	ret, specificReturn := fake.pollStartReturnsOnCall[len(fake.pollStartArgsForCall)]
-	fake.pollStartArgsForCall = append(fake.pollStartArgsForCall, struct {
-		arg1 string
-	}{arg1})
-	fake.recordInvocation("PollStart", []interface{}{arg1})
-	fake.pollStartMutex.Unlock()
-	if fake.PollStartStub != nil {
-		return fake.PollStartStub(arg1)
-	}
-	if specificReturn {
-		return ret.result1, ret.result2
-	}
-	fakeReturns := fake.pollStartReturns
-	return fakeReturns.result1, fakeReturns.result2
-}
-
-func (fake *FakeV7ActorForPush) PollStartCallCount() int {
-	fake.pollStartMutex.RLock()
-	defer fake.pollStartMutex.RUnlock()
-	return len(fake.pollStartArgsForCall)
-}
-
-func (fake *FakeV7ActorForPush) PollStartCalls(stub func(string) (v7action.Warnings, error)) {
-	fake.pollStartMutex.Lock()
-	defer fake.pollStartMutex.Unlock()
-	fake.PollStartStub = stub
-}
-
-func (fake *FakeV7ActorForPush) PollStartArgsForCall(i int) string {
-	fake.pollStartMutex.RLock()
-	defer fake.pollStartMutex.RUnlock()
-	argsForCall := fake.pollStartArgsForCall[i]
-	return argsForCall.arg1
-}
-
-func (fake *FakeV7ActorForPush) PollStartReturns(result1 v7action.Warnings, result2 error) {
-	fake.pollStartMutex.Lock()
-	defer fake.pollStartMutex.Unlock()
-	fake.PollStartStub = nil
-	fake.pollStartReturns = struct {
-		result1 v7action.Warnings
-		result2 error
-	}{result1, result2}
-}
-
-func (fake *FakeV7ActorForPush) PollStartReturnsOnCall(i int, result1 v7action.Warnings, result2 error) {
-	fake.pollStartMutex.Lock()
-	defer fake.pollStartMutex.Unlock()
-	fake.PollStartStub = nil
-	if fake.pollStartReturnsOnCall == nil {
-		fake.pollStartReturnsOnCall = make(map[int]struct {
-			result1 v7action.Warnings
-			result2 error
-		})
-	}
-	fake.pollStartReturnsOnCall[i] = struct {
-		result1 v7action.Warnings
-		result2 error
-	}{result1, result2}
-}
-
 func (fake *FakeV7ActorForPush) RestartApplication(arg1 string) (v7action.Warnings, error) {
 	fake.restartApplicationMutex.Lock()
 	ret, specificReturn := fake.restartApplicationReturnsOnCall[len(fake.restartApplicationArgsForCall)]
@@ -498,8 +422,6 @@ func (fake *FakeV7ActorForPush) Invocations() map[string][][]interface{} {
 	defer fake.getApplicationSummaryByNameAndSpaceMutex.RUnlock()
 	fake.getStreamingLogsForApplicationByNameAndSpaceMutex.RLock()
 	defer fake.getStreamingLogsForApplicationByNameAndSpaceMutex.RUnlock()
-	fake.pollStartMutex.RLock()
-	defer fake.pollStartMutex.RUnlock()
 	fake.restartApplicationMutex.RLock()
 	defer fake.restartApplicationMutex.RUnlock()
 	copiedInvocations := map[string][][]interface{}{}
