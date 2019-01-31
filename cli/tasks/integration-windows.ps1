4ec7ceb7a0acff5978eb0b067589d5d1553602ba
Magesh Kumar Murali <mmurali@pivotal.io>
2019-01-31 15:33:38 -0800

    Import Certificate for CF environments
    This commit changes the integration-windows task to import the
    certificates for the CF environments before running the integration
    tests. These certificates are required to run the SSL related tests
    in the CI environments.
    
    [#163233217]
    
    Signed-off-by: Alex Shan <ashan@pivotal.io>

---
+++ cli/tasks/integration-windows.ps1
@@ -0,0 +1,36 @@
+$ErrorActionPreference = "Stop"
+$GOPATH="$pwd\go"
+$CF_DIAL_TIMEOUT=15
+
+
+$PATH="C:\Go\bin;$PATH"
+$PATH="$Env:GOPATH\bin;$PATH"
+$PATH="C:\Program Files\GnuWin32\bin;$PATH"
+$PATH="$pwd;$PATH"
+
+$DOMAIN = Get-Content $pwd\bosh-lock\name -Raw
+$Env:CF_INT_API = "https://api.$DOMAIN"
+$Env:CF_INT_PASSWORD = Get-Content $pwd\cf-credentials\cf-password -Raw
+$Env:CF_INT_OIDC_PASSWORD = Get-Content $pwd\cf-credentials\uaa-oidc-password -Raw
+$Env:CF_INT_OIDC_USERNAME = "admin-oidc"
+$CF_INT_NAME = $DOMAIN.split(".")[0]
+$Env:SKIP_SSL_VALIDATION ="false"
+
+Import-Certificate -Filepath "$pwd\cf-credentials\cert_dir\$CF_INT_NAME.lb.cert" -CertStoreLocation "cert:\LocalMachine\root"
+
+Import-Certificate -Filepath "$pwd\cf-credentials\cert_dir\$CF_INT_NAME.router.ca" -CertStoreLocation "cert:\LocalMachine\root"
+
+pushd $pwd\cf-cli-binaries
+	7z e cf-cli-binaries.tgz -y
+	7z x cf-cli-binaries.tar -y
+	Move-Item -Path $pwd\cf-cli_winx64.exe  -Destination ..\cf.exe -Force
+popd
+
+go get -v -u github.com/onsi/ginkgo/ginkgo
+
+$RUN_ID=(openssl rand -hex 16)
+
+cd $GOPATH\src\code.cloudfoundry.org\cli
+ginkgo.exe -r -nodes=16 -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/isolated ./integration/v6/isolated ./integration/shared/plugin ./integration/v6/push
+ginkgo.exe -r -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/global ./integration/v6/global
+
