0c1fa09d953c40af6779803683864280c879c24e
Magesh Kumar Murali <mmurali@pivotal.io>
2019-02-07 15:20:50 -0800

    Revert "Added integration test for targetting org in login command"
    This reverts commit 8a5b6179536b1ecc2cefe8646fb45579e4f3ac72.
    
    Signed-off-by: Alex Shan <ashan@pivotal.io>

--- integration/helpers/org_and_space.go
+++ integration/helpers/org_and_space.go
@@ -57,26 +57,6 @@ func GetSpaceGUID(spaceName string) string {
 	return strings.TrimSpace(string(session.Out.Contents()))
 }
 
-func GetAllOrgs() []string {
-	session := CF("orgs")
-	Eventually(session).Should(Exit(0))
-	trimmedContents := strings.TrimSpace(string(session.Out.Contents()))
-	if strings.Contains(trimmedContents, "No orgs found.") {
-		return []string{}
-	}
-
-	orgs := strings.SplitN(trimmedContents, "name\n", 2)[1]
-	return strings.Split(orgs, "\n")
-}
-
-func DeleteAllOrgs() {
-	orgs := GetAllOrgs()
-	for _, org := range orgs {
-		session := CF("delete-org", org, "-f")
-		Eventually(session).Should(Exit(0))
-	}
-}
-
 func QuickDeleteOrg(orgName string) {
 	// If orgName is empty, the BeforeSuite has failed and attempting to delete
 	// will produce a meaningless error.
