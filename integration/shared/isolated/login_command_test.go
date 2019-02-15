c3ecd9418bf45b53bf40b64bc390758331f4af84
Will Murphy <wmurphy@pivotal.io>
2019-02-15 14:19:16 -0800

    Add integration test for single space login
    When a user runs "cf login" and has access to only one org with only one
    space in it, they should automatically target that org and space.
    
    [Finishes #163539589]

--- integration/shared/isolated/login_command_test.go
+++ integration/shared/isolated/login_command_test.go
@@ -405,6 +405,29 @@ var _ = Describe("login command", func() {
 				Eventually(targetSession).Should(Exit(0))
 				Eventually(targetSession).Should(Say(`org:\s+%s`, orgName))
 			})
+
+			When("the only org available to the user contains only one space", func() {
+				var spaceName string
+
+				BeforeEach(func() {
+					spaceName = helpers.NewSpaceName()
+					session := helpers.CF("create-space", "-o", orgName, spaceName)
+					Eventually(session).Should(Exit(0))
+					roleSession := helpers.CF("set-space-role", username, orgName, spaceName, "SpaceManager")
+					Eventually(roleSession).Should(Exit(0))
+				})
+
+				It("logs the user in and targets the org and the space", func() {
+					session := helpers.CF("login", "-u", username, "-p", password, "-a", apiURL, "--skip-ssl-validation")
+					Eventually(session).Should(Exit(0))
+
+					targetSession := helpers.CF("target")
+					Eventually(targetSession).Should(Exit(0))
+					Eventually(targetSession).Should(Say(`org:\s+%s`, orgName))
+					Eventually(targetSession).Should(Say(`space:\s+%s`, spaceName))
+
+				})
+			})
 		})
 
 		When("the -o flag is not passed", func() {
