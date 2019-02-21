2e2b018cb6df0a63629af14ea2623e6de4282b2a
Eli Wrenn <ewrenn@pivotal.io>
2019-02-21 15:56:24 -0800

    CLI user can list feature flags
    [finishes #163035367]
    
    Signed-off-by: Supraja Narasimhan <snarasimhan@pivotal.io>

---
+++ command/v7/feature_flags_command.go
@@ -0,0 +1,83 @@
+package v7
+
+import (
+	"code.cloudfoundry.org/cli/actor/sharedaction"
+	"code.cloudfoundry.org/cli/actor/v7action"
+	"code.cloudfoundry.org/cli/command"
+	"code.cloudfoundry.org/cli/command/v7/shared"
+	"code.cloudfoundry.org/cli/util/ui"
+)
+
+//go:generate counterfeiter . FeatureFlagsActor
+
+type FeatureFlagsActor interface {
+	GetFeatureFlags() ([]v7action.FeatureFlag, v7action.Warnings, error)
+}
+
+type FeatureFlagsCommand struct {
+	usage           interface{} `usage:"CF_NAME feature-flags"`
+	relatedCommands interface{} `related_commands:"disable-feature-flag, enable-feature-flag"`
+
+	UI          command.UI
+	Config      command.Config
+	SharedActor command.SharedActor
+	Actor       FeatureFlagsActor
+}
+
+func (cmd *FeatureFlagsCommand) Setup(config command.Config, ui command.UI) error {
+	cmd.UI = ui
+	cmd.Config = config
+	cmd.SharedActor = sharedaction.NewActor(config)
+
+	ccClient, _, err := shared.NewClients(config, ui, true, "")
+	if err != nil {
+		return err
+	}
+	cmd.Actor = v7action.NewActor(ccClient, config, nil, nil)
+
+	return nil
+}
+
+func (cmd FeatureFlagsCommand) Execute(args []string) error {
+	err := cmd.SharedActor.CheckTarget(false, false)
+	if err != nil {
+		return err
+	}
+
+	user, err := cmd.Config.CurrentUser()
+	if err != nil {
+		return err
+	}
+
+	cmd.UI.DisplayTextWithFlavor("Getting feature flags as {{.Username}}...", map[string]interface{}{
+		"Username": user.Name,
+	})
+	cmd.UI.DisplayNewline()
+
+	flags, warnings, err := cmd.Actor.GetFeatureFlags()
+	cmd.UI.DisplayWarnings(warnings)
+	if err != nil {
+		return err
+	}
+
+	cmd.displayTable(flags)
+
+	return nil
+}
+
+func (cmd FeatureFlagsCommand) displayTable(featureFlags []v7action.FeatureFlag) {
+	if len(featureFlags) > 0 {
+		var keyValueTable = [][]string{
+			{"name", "state"},
+		}
+		for _, flag := range featureFlags {
+			state := "disabled"
+			if flag.Enabled {
+				state = "enabled"
+			}
+			keyValueTable = append(keyValueTable, []string{flag.Name, state})
+		}
+
+		cmd.UI.DisplayTableWithHeader("", keyValueTable, ui.DefaultTableSpacePadding)
+	}
+}
---
+++ command/v7/feature_flags_command_test.go
@@ -0,0 +1,122 @@
+package v7_test
+
+import (
+	"errors"
+
+	"code.cloudfoundry.org/cli/actor/actionerror"
+	"code.cloudfoundry.org/cli/actor/v7action"
+	"code.cloudfoundry.org/cli/command/commandfakes"
+	. "code.cloudfoundry.org/cli/command/v7"
+	"code.cloudfoundry.org/cli/command/v7/v7fakes"
+	"code.cloudfoundry.org/cli/util/configv3"
+	"code.cloudfoundry.org/cli/util/ui"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+)
+
+var _ = Describe("Feature Flags Command", func() {
+	var (
+		cmd             FeatureFlagsCommand
+		testUI          *ui.UI
+		fakeConfig      *commandfakes.FakeConfig
+		fakeSharedActor *commandfakes.FakeSharedActor
+		fakeActor       *v7fakes.FakeFeatureFlagsActor
+		executeErr      error
+		binaryName      string
+	)
+
+	const tableHeaders = `name\s+state`
+
+	JustBeforeEach(func() {
+		executeErr = cmd.Execute(nil)
+	})
+
+	BeforeEach(func() {
+		testUI = ui.NewTestUI(nil, NewBuffer(), NewBuffer())
+		fakeConfig = new(commandfakes.FakeConfig)
+		fakeSharedActor = new(commandfakes.FakeSharedActor)
+		fakeActor = new(v7fakes.FakeFeatureFlagsActor)
+
+		cmd = FeatureFlagsCommand{
+			UI:          testUI,
+			Config:      fakeConfig,
+			SharedActor: fakeSharedActor,
+			Actor:       fakeActor,
+		}
+
+		binaryName = "faceman"
+		fakeConfig.BinaryNameReturns(binaryName)
+	})
+
+	Context("When the environment is not setup correctly", func() {
+		When("checking target fails", func() {
+			BeforeEach(func() {
+				fakeSharedActor.CheckTargetReturns(actionerror.NotLoggedInError{BinaryName: binaryName})
+			})
+
+			It("returns an error", func() {
+				Expect(executeErr).To(MatchError(actionerror.NotLoggedInError{BinaryName: binaryName}))
+
+				Expect(fakeSharedActor.CheckTargetCallCount()).To(Equal(1))
+				checkTargetedOrg, checkTargetedSpace := fakeSharedActor.CheckTargetArgsForCall(0)
+				Expect(checkTargetedOrg).To(BeFalse())
+				Expect(checkTargetedSpace).To(BeFalse())
+			})
+		})
+	})
+
+	Context("When the environment is setup correctly", func() {
+		BeforeEach(func() {
+			fakeConfig.CurrentUserReturns(configv3.User{Name: "banana"}, nil)
+		})
+
+		When("FeatureFlagsActor returns an error", func() {
+			var expectedErr error
+
+			BeforeEach(func() {
+				warnings := v7action.Warnings{"warning-1", "warning-2"}
+				expectedErr = errors.New("some-error")
+				fakeActor.GetFeatureFlagsReturns(nil, warnings, expectedErr)
+			})
+
+			It("prints that error with warnings", func() {
+				Expect(executeErr).To(Equal(expectedErr))
+
+				Expect(testUI.Err).To(Say("warning-1"))
+				Expect(testUI.Err).To(Say("warning-2"))
+				Expect(testUI.Out).ToNot(Say(tableHeaders))
+			})
+		})
+
+		When("everything is perfect", func() {
+			BeforeEach(func() {
+				flags := []v7action.FeatureFlag{
+					{Name: "flag2", Enabled: true},
+					{Name: "flag1", Enabled: false},
+				}
+				fakeActor.GetFeatureFlagsReturns(flags, v7action.Warnings{"warning-1", "warning-2"}, nil)
+			})
+
+			It("asks the FeatureFlagsActor for a list of feature flags", func() {
+				Expect(fakeActor.GetFeatureFlagsCallCount()).To(Equal(1))
+			})
+
+			It("prints warnings", func() {
+				Expect(testUI.Err).To(Say(`warning-1`))
+				Expect(testUI.Err).To(Say(`warning-2`))
+			})
+
+			It("prints the list of feature flags in alphabetical order", func() {
+				Expect(testUI.Out).To(Say(tableHeaders))
+				Expect(testUI.Out).To(Say(`flag2\s+enabled`))
+				Expect(testUI.Out).To(Say(`flag1\s+disabled`))
+			})
+
+			It("prints the flavor text", func() {
+				Expect(testUI.Out).To(Say("Getting feature flags as banana\\.\\.\\."))
+			})
+		})
+	})
+})
