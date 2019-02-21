d60824eb93412654f453f52582561536fff8409f
Alex Shan <ashan@pivotal.io>
2019-02-21 12:17:57 -0800

    Add deprecation warning for delete_service_auth_token_command
    [#164098144]
    
    Signed-off-by: Brendan Smith <brsmith@pivotal.io>

--- command/v6/delete_service_auth_token_command.go
+++ command/v6/delete_service_auth_token_command.go
@@ -10,12 +10,15 @@ type DeleteServiceAuthTokenCommand struct {
 	RequiredArgs flag.DeleteServiceAuthTokenArgs `positional-args:"yes"`
 	Force        bool                            `short:"f" description:"Force deletion without confirmation"`
 	usage        interface{}                     `usage:"CF_NAME delete-service-auth-token LABEL PROVIDER [-f]"`
+	UI           command.UI
 }
 
-func (DeleteServiceAuthTokenCommand) Setup(config command.Config, ui command.UI) error {
+func (cmd *DeleteServiceAuthTokenCommand) Setup(config command.Config, ui command.UI) error {
+	cmd.UI = ui
 	return nil
 }
 
-func (DeleteServiceAuthTokenCommand) Execute(args []string) error {
+func (cmd *DeleteServiceAuthTokenCommand) Execute(args []string) error {
+	cmd.UI.DisplayDeprecationWarning()
 	return translatableerror.UnrefactoredCommandError{}
 }
