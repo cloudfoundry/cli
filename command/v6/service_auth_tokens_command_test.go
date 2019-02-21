ecd096c2b5ab1c631edb029783edbe549939f574
Alex Shan <ashan@pivotal.io>
2019-02-21 11:40:27 -0800

    Add v6 unit test for service_auth_tokens_command
    [#164098152]
    
    Signed-off-by: Brendan Smith <brsmith@pivotal.io>

---
+++ command/v6/service_auth_tokens_command_test.go
@@ -0,0 +1,37 @@
+package v6_test
+
+import (
+	"code.cloudfoundry.org/cli/command/translatableerror"
+	. "code.cloudfoundry.org/cli/command/v6"
+	"code.cloudfoundry.org/cli/util/ui"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+)
+
+var _ = Describe("Service Auth Tokens Command", func() {
+	var (
+		cmd    ServiceAuthTokensCommand
+		testUI *ui.UI
+	)
+
+	BeforeEach(func() {
+		testUI = ui.NewTestUI(nil, NewBuffer(), NewBuffer())
+
+		cmd = ServiceAuthTokensCommand{
+			UI: testUI,
+		}
+	})
+
+	When("command is called in v6", func() {
+		It("returns an unrefactored command error", func() {
+			err := cmd.Execute(nil)
+			Expect(err).To(MatchError(translatableerror.UnrefactoredCommandError{}))
+		})
+
+		It("displays a deprecation warning", func() {
+			_ = cmd.Execute(nil)
+			Expect(testUI.Err).To(Say("Deprecation warning: This command has been deprecated. This feature will be removed in the future."))
+		})
+	})
+})
