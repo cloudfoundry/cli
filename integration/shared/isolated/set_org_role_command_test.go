e84c0ed74bbe85d210b5b551fbad0e95bb67bec2
Will Murphy <wmurphy@pivotal.io>
2019-02-21 14:16:35 -0800

    Add integration test for set-org-role happy path
    We feel that units should provide sufficient coverage for the individual
    roles, especially since roles are just strings to the CLI.
    
    [Finishes #164089696]
    
    Signed-off-by: Tom Viehman <tviehman@pivotal.io>

---
+++ integration/shared/isolated/set_org_role_command_test.go
@@ -0,0 +1,33 @@
+package isolated
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+
+	"code.cloudfoundry.org/cli/integration/helpers"
+)
+
+var _ = Describe("set-org-role command", func() {
+	When("the org and user both exist", func() {
+		var (
+			username string
+			orgName  string
+		)
+
+		BeforeEach(func() {
+			helpers.LoginCF()
+			orgName = helpers.NewOrgName()
+			helpers.CreateOrg(orgName)
+			username, _ = helpers.CreateUser()
+		})
+
+		It("sets the org role for the user", func() {
+			session := helpers.CF("set-org-role", username, orgName, "OrgAuditor")
+			Eventually(session).Should(Say("Assigning role OrgAuditor to user %s in org %s as admin...", username, orgName))
+			Eventually(session).Should(Say("OK"))
+			Eventually(session).Should(Exit(0))
+		})
+	})
+})
