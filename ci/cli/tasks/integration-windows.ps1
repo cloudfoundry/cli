42543d96d5250ad7b8f8f9df07cf570938e7b8f6
Anand Gaitonde <agaitonde@pivotal.io>
2019-02-22 12:22:18 -0800

    exit 1 when ginkgo fails
    - Powershell scripts don't do this by default and even if you tell them
    to, it doesn't always work with exe(s). So checking the exit code
    manually is the best practice.
    
    Signed-off-by: Eli Wrenn <ewrenn@pivotal.io>

--- ci/cli/tasks/integration-windows.ps1
+++ ci/cli/tasks/integration-windows.ps1
@@ -31,4 +31,12 @@ $Env:RUN_ID=(openssl rand -hex 16)
 
 cd $Env:GOPATH\src\code.cloudfoundry.org\cli
 ginkgo.exe -r -nodes=16 -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/isolated ./integration/v6/isolated ./integration/shared/plugin ./integration/v6/push
+if ($LASTEXITCODE -gt 0)
+{
+	exit 1
+}
 ginkgo.exe -r -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/global ./integration/v6/global
+if ($LASTEXITCODE -gt 0)
+{
+	exit 1
+}
