589127f8c27920267d20571c741c63e4e6b20272
Anand Gaitonde <agaitonde@pivotal.io>
2019-03-13 14:04:05 -0700

    fix gometalinter warnings

--- util/manifestparser/application.go
+++ util/manifestparser/application.go
@@ -10,8 +10,9 @@ type Application struct {
 	FullUnmarshalledApplication map[string]interface{}
 }
 
-// These can be accessed through the top level Application struct
-// To add a field for the CLI to extract from the manifest, just add it to this struct
+// ApplicationModel can be accessed through the top level Application struct To
+// add a field for the CLI to extract from the manifest, just add it to this
+// struct.
 type ApplicationModel struct {
 	Name   string  `yaml:"name"`
 	Docker *Docker `yaml:"docker"`
@@ -22,10 +23,10 @@ func (application Application) MarshalYAML() (interface{}, error) {
 	return application.FullUnmarshalledApplication, nil
 }
 
-func (app *Application) UnmarshalYAML(unmarshal func(v interface{}) error) error {
-	err := unmarshal(&app.FullUnmarshalledApplication)
+func (application *Application) UnmarshalYAML(unmarshal func(v interface{}) error) error {
+	err := unmarshal(&application.FullUnmarshalledApplication)
 	if err != nil {
 		return err
 	}
-	return unmarshal(&app.ApplicationModel)
+	return unmarshal(&application.ApplicationModel)
 }
