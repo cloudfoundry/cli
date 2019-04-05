e48684fa7e5190af9e259c67c66e3ab6ed867a19
Andrew Crump <acrump@pivotal.io>
2019-04-04 17:01:00 -0700

    App dev is able to view the labels set on their app
    [#164747436]

--- actor/v7action/label_test.go
+++ actor/v7action/label_test.go
@@ -11,7 +11,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("UpdateApplicationLabelsByApplicationName", func() {
+var _ = Describe("Label actions", func() {
 	var (
 		actor                     *Actor
 		fakeCloudControllerClient *v7actionfakes.FakeCloudControllerClient
@@ -31,33 +31,52 @@ var _ = Describe("UpdateApplicationLabelsByApplicationName", func() {
 		actor = NewActor(fakeCloudControllerClient, fakeConfig, fakeSharedActor, nil)
 	})
 
-	JustBeforeEach(func() {
-		warnings, executeErr = actor.UpdateApplicationLabelsByApplicationName(appName, spaceGUID, labels)
-	})
-
-	When("there are no client errors", func() {
+	Describe("GetApplicationLabels", func() {
 		BeforeEach(func() {
+
 			fakeCloudControllerClient.GetApplicationsReturns(
-				[]ccv3.Application{ccv3.Application{GUID: "some-guid"}},
-				ccv3.Warnings([]string{"warning-1", "warning-2"}),
+				[]ccv3.Application{
+					ccv3.Application{
+						GUID: "some-other-guid",
+						Metadata: struct {
+							Labels map[string]types.NullString `json:"labels,omitempty"`
+						}{
+							Labels: map[string]types.NullString{
+								"some-key": types.NewNullString("some-value"),
+							},
+						},
+					},
+				},
 				nil,
-			)
-			fakeCloudControllerClient.UpdateApplicationReturns(
-				ccv3.Application{},
-				ccv3.Warnings{"set-app-labels-warnings"},
 				nil,
 			)
 		})
 
-		It("sets the app labels", func() {
-			Expect(fakeCloudControllerClient.UpdateApplicationCallCount()).To(Equal(1))
-			sentApp := fakeCloudControllerClient.UpdateApplicationArgsForCall(0)
+		JustBeforeEach(func() {
+			labels, warnings, executeErr = actor.GetApplicationLabels(appName, spaceGUID)
+		})
+
+		It("gets the app labels", func() {
+			Expect(fakeCloudControllerClient.GetApplicationsCallCount()).To(Equal(1))
+			Expect(labels).To(Equal(map[string]types.NullString{"some-key": types.NewNullString("some-value")}))
+			Expect(warnings).To(BeEmpty())
 			Expect(executeErr).ToNot(HaveOccurred())
-			Expect(sentApp.Metadata.Labels).To(BeEquivalentTo(labels))
 		})
 
-		It("aggregates warnings", func() {
-			Expect(warnings).To(ConsistOf("warning-1", "warning-2", "set-app-labels-warnings"))
+		When("warnings are returned by the API", func() {
+
+			BeforeEach(func() {
+				fakeCloudControllerClient.GetApplicationsReturns(
+					[]ccv3.Application{ccv3.Application{GUID: "some-guid"}},
+					ccv3.Warnings([]string{"warning-1", "warning-2"}),
+					nil,
+				)
+
+			})
+
+			It("returns the warnings", func() {
+				Expect(warnings).To(ConsistOf("warning-1", "warning-2"))
+			})
 		})
 
 		When("there are client errors", func() {
@@ -76,22 +95,73 @@ var _ = Describe("UpdateApplicationLabelsByApplicationName", func() {
 					Expect(executeErr).To(MatchError("get-apps-error"))
 				})
 			})
+		})
 
-			When("UpdateApplication fails", func() {
-				BeforeEach(func() {
-					fakeCloudControllerClient.UpdateApplicationReturns(
-						ccv3.Application{},
-						ccv3.Warnings{"set-app-labels-warnings"},
-						errors.New("update-application-error"),
-					)
-				})
-				It("returns the error and all warnings", func() {
-					Expect(executeErr).To(HaveOccurred())
-					Expect(warnings).To(ConsistOf("warning-1", "warning-2", "set-app-labels-warnings"))
-					Expect(executeErr).To(MatchError("update-application-error"))
-				})
+	})
+
+	Describe("UpdateApplicationLabelsByApplicationName", func() {
+
+		JustBeforeEach(func() {
+			warnings, executeErr = actor.UpdateApplicationLabelsByApplicationName(appName, spaceGUID, labels)
+		})
+
+		When("there are no client errors", func() {
+			BeforeEach(func() {
+				fakeCloudControllerClient.GetApplicationsReturns(
+					[]ccv3.Application{ccv3.Application{GUID: "some-guid"}},
+					ccv3.Warnings([]string{"warning-1", "warning-2"}),
+					nil,
+				)
+				fakeCloudControllerClient.UpdateApplicationReturns(
+					ccv3.Application{},
+					ccv3.Warnings{"set-app-labels-warnings"},
+					nil,
+				)
 			})
 
+			It("sets the app labels", func() {
+				Expect(fakeCloudControllerClient.UpdateApplicationCallCount()).To(Equal(1))
+				sentApp := fakeCloudControllerClient.UpdateApplicationArgsForCall(0)
+				Expect(executeErr).ToNot(HaveOccurred())
+				Expect(sentApp.Metadata.Labels).To(BeEquivalentTo(labels))
+			})
+
+			It("aggregates warnings", func() {
+				Expect(warnings).To(ConsistOf("warning-1", "warning-2", "set-app-labels-warnings"))
+			})
+
+			When("there are client errors", func() {
+				When("GetApplications fails", func() {
+					BeforeEach(func() {
+						fakeCloudControllerClient.GetApplicationsReturns(
+							[]ccv3.Application{ccv3.Application{GUID: "some-guid"}},
+							ccv3.Warnings([]string{"warning-failure-1", "warning-failure-2"}),
+							errors.New("get-apps-error"),
+						)
+					})
+
+					It("returns the error and all warnings", func() {
+						Expect(executeErr).To(HaveOccurred())
+						Expect(warnings).To(ConsistOf("warning-failure-1", "warning-failure-2"))
+						Expect(executeErr).To(MatchError("get-apps-error"))
+					})
+				})
+
+				When("UpdateApplication fails", func() {
+					BeforeEach(func() {
+						fakeCloudControllerClient.UpdateApplicationReturns(
+							ccv3.Application{},
+							ccv3.Warnings{"set-app-labels-warnings"},
+							errors.New("update-application-error"),
+						)
+					})
+					It("returns the error and all warnings", func() {
+						Expect(executeErr).To(HaveOccurred())
+						Expect(warnings).To(ConsistOf("warning-1", "warning-2", "set-app-labels-warnings"))
+						Expect(executeErr).To(MatchError("update-application-error"))
+					})
+				})
+			})
 		})
 	})
 })
