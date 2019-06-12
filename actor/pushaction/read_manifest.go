29a5c1cbab42f4ae38ee057a4933057052f04d07
Andrew Crump <acrump@pivotal.io>
2019-06-12 15:54:52 -0700

    Update buildpack deprecation warning url
    [#166249597](https://www.pivotaltracker.com/story/show/166249597)
    
    Co-authored-by: Alexander Berezovsky <aberezovsky@pivotal.io>

--- actor/pushaction/read_manifest.go
+++ actor/pushaction/read_manifest.go
@@ -15,7 +15,7 @@ func (actor *Actor) ReadManifest(pathToManifest string, pathsToVarsFiles []strin
 func (*Actor) checkForBuildpack(manifestApp []manifest.Application) Warnings {
 	for _, app := range manifestApp {
 		if app.Buildpack.IsSet {
-			return Warnings{"Deprecation warning: Use of 'buildpack' attribute in manifest is deprecated in favor of 'buildpacks'. Please see http://docs.cloudfoundry.org/devguide/deploy-apps/manifest.html#deprecated for alternatives and other app manifest deprecations. This feature will be removed in the future."}
+			return Warnings{"Deprecation warning: Use of 'buildpack' attribute in manifest is deprecated in favor of 'buildpacks'. Please see https://docs.cloudfoundry.org/devguide/deploy-apps/manifest-attributes.html#deprecated for alternatives and other app manifest deprecations. This feature will be removed in the future."}
 		}
 	}
 
