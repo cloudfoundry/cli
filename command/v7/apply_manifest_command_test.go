e3b96abd04587d8bc377425ac0b6096fcbade3d8
Eli Wrenn <ewrenn@pivotal.io>
2019-06-18 17:03:03 -0700

    Remove test assertion that apply manifest command outputs experimental warning
    Co-authored-by: Eli Wrenn <ewrenn@pivotal.io>
    Co-authored-by: Brian Cunnie <bcunnie@pivotal.io>

--- command/v7/apply_manifest_command_test.go
+++ command/v7/apply_manifest_command_test.go
@@ -52,10 +52,6 @@ var _ = Describe("apply-manifest Command", func() {
 		executeErr = cmd.Execute(nil)
 	})
 
-	It("displays the experimental warning", func() {
-		Expect(testUI.Err).To(Say("This command is in EXPERIMENTAL stage and may change without notice"))
-	})
-
 	When("checking target fails", func() {
 		BeforeEach(func() {
 			fakeSharedActor.CheckTargetReturns(actionerror.NoOrganizationTargetedError{BinaryName: binaryName})
