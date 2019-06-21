29a803a1ddac5614bf9a77b6ce22b51132650fde
Niki Maslarski <nikolay.maslarski@sap.com>
2019-06-21 16:41:00 +0300

    Add support for longer time zones in last uploaded assertions.
    [#166819395]
    
    Signed-off-by: Oleksii Fedorov <ofedorov@pivotal.io>

--- integration/v7/push/disk_flag_test.go
+++ integration/v7/push/disk_flag_test.go
@@ -32,7 +32,7 @@ var _ = Describe("push with disk flag", func() {
 			time.Sleep(5 * time.Second)
 			session := helpers.CF("app", appName)
 			Eventually(session).Should(Say(`name:\s+%s`, appName))
-			Eventually(session).Should(Say(`last uploaded:\s+\w{3} \d{1,2} \w{3} \d{2}:\d{2}:\d{2} \w{3} \d{4}`))
+			Eventually(session).Should(Say(`last uploaded:\s+\w{3} \d{1,2} \w{3} \d{2}:\d{2}:\d{2} \w{3,5} \d{4}`))
 			Eventually(session).Should(Say(`\s+state\s+since\s+cpu\s+memory\s+disk`))
 			Eventually(session).Should(Say(`#0\s+running\s+\d{4}-[01]\d-[0-3]\dT[0-2][0-9]:[0-5]\d:[0-5]\dZ.+of.+of 70M`))
 			Eventually(session).Should(Exit(0))
