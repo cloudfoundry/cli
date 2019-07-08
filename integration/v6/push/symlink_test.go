741f6e07e910c13ecc790ce8d941242110d85c41
Luan Santos <lsantos@pivotal.io>
2019-07-08 11:00:26 -0700

    Remove unused import

--- integration/v6/push/symlink_test.go
+++ integration/v6/push/symlink_test.go
@@ -9,7 +9,6 @@ import (
 	"code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
 )
 
