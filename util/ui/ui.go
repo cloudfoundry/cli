26653dd8af7d5f3464162e464b6e3f4a95e6ed70
Harsha Nandiwada <hnandiwada@pivotal.io>
2019-08-13 11:41:59 -0700

    v7: Alphabetize display methods to appease linter
    [Finishes #160467192]
    
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>

--- util/ui/ui.go
+++ util/ui/ui.go
@@ -254,13 +254,6 @@ func (ui *UI) DisplayWarning(template string, templateValues ...map[string]inter
 	fmt.Fprintf(ui.Err, "%s\n\n", ui.TranslateText(template, templateValues...))
 }
 
-// DisplayWarningV7 translates the warning, substitutes in templateValues, and
-// outputs to ui.Err. Only the first map in templateValues is used.
-// This command has one fewer newline than DisplayWarning. Use it before an OK message in V7.
-func (ui *UI) DisplayWarningV7(template string, templateValues ...map[string]interface{}) {
-	fmt.Fprintf(ui.Err, "%s\n", ui.TranslateText(template, templateValues...))
-}
-
 // DisplayWarnings translates the warnings and outputs to ui.Err.
 func (ui *UI) DisplayWarnings(warnings []string) {
 	for _, warning := range warnings {
@@ -269,6 +262,13 @@ func (ui *UI) DisplayWarnings(warnings []string) {
 	if len(warnings) > 0 {
 		fmt.Fprintln(ui.Err)
 	}
+}
+
+// DisplayWarningV7 translates the warning, substitutes in templateValues, and
+// outputs to ui.Err. Only the first map in templateValues is used.
+// This command has one fewer newline than DisplayWarning. Use it before an OK message in V7.
+func (ui *UI) DisplayWarningV7(template string, templateValues ...map[string]interface{}) {
+	fmt.Fprintf(ui.Err, "%s\n", ui.TranslateText(template, templateValues...))
 }
 
 // FlushDeferred diplays text previously deferred (using DeferText) to the UI's
