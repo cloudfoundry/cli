4934877ec33a400786749df4da6872b53d951cac
Andrew Crump <acrump@pivotal.io>
2019-08-23 10:15:10 -0700

    Revert "cf login: complain when we encounter extraneous arguments"
    This reverts commit 3b524318957eef93387a154c5f185ded2042a04d.
    
    [#167948703](https://www.pivotaltracker.com/story/show/167948703)
    
    Co-authored-by: Brendan Smith <brsmith@pivotal.io>

--- prevent_extra_args_v6.go
+++ prevent_extra_args_v6.go
@@ -2,16 +2,6 @@
 
 package main
 
-import (
-	"code.cloudfoundry.org/cli/command/translatableerror"
-	"strings"
-)
-
 func preventExtraArgs(args []string) error {
-	if len(args) > 0 {
-		return translatableerror.TooManyArgumentsError{
-			ExtraArgument: strings.Join(args, " "),
-		}
-	}
 	return nil
 }
