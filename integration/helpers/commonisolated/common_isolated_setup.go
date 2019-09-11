876caf16522d5cd38723a4ca3af38365821b42e6
Oleksii Fedorov <ofedorov@pivotal.io>
2019-09-11 16:07:11 +0200

    Refactor: extract common ginkgo suite setup for isolated integration tests.
    [#168383869]
    
    Co-authored-by: Winna Bridgewater <wbridgewater@pivotal.io>

---
+++ integration/helpers/commonisolated/common_isolated_setup.go
@@ -0,0 +1,107 @@
+package commonisolated
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"code.cloudfoundry.org/cli/api/cloudcontroller/ccversion"
+	"code.cloudfoundry.org/cli/integration/helpers"
+	"code.cloudfoundry.org/cli/integration/helpers/fakeservicebroker"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	CFEventuallyTimeout   = 300 * time.Second
+	CFConsistentlyTimeout = 500 * time.Millisecond
+	RealIsolationSegment  = "persistent_isolation_segment"
+	DockerImage           = "cloudfoundry/diego-docker-app-custom"
+)
+
+func CommonTestIsolated(t *testing.T) {
+	RegisterFailHandler(Fail)
+	var reporters []Reporter
+
+	prBuilderReporter := helpers.GetPRBuilderReporter()
+	if prBuilderReporter != nil {
+		reporters = append(reporters, prBuilderReporter)
+	}
+
+	RunSpecsWithDefaultAndCustomReporters(t, "Isolated Integration Suite", reporters)
+}
+
+func CommonGinkgoSetup(
+	// Per suite Level
+	failureSummaryFilename string,
+	apiURL *string,
+	skipSSLValidation *bool,
+	readOnlyOrg *string,
+	readOnlySpace *string,
+
+	// Per test level
+	homeDir *string,
+) interface{} {
+	var _ = SynchronizedBeforeSuite(func() []byte {
+		_, _ = GinkgoWriter.Write([]byte("==============================Global FIRST Node Synchronized Before Each=============================="))
+		SetDefaultEventuallyTimeout(CFEventuallyTimeout)
+		SetDefaultConsistentlyDuration(CFConsistentlyTimeout)
+
+		helpers.SetupSynchronizedSuite(func() {
+			helpers.EnableFeatureFlag("diego_docker")
+
+			if helpers.IsVersionMet(ccversion.MinVersionShareServiceV3) {
+				helpers.EnableFeatureFlag("service_instance_sharing")
+			}
+		})
+
+		fakeservicebroker.Setup()
+
+		_, _ = GinkgoWriter.Write([]byte("==============================End of Global FIRST Node Synchronized Before Each=============================="))
+
+		return nil
+	}, func(_ []byte) {
+		_, _ = GinkgoWriter.Write([]byte(fmt.Sprintf("==============================Global Node %d Synchronized Before Each==============================", GinkgoParallelNode())))
+		// Ginkgo Globals
+		SetDefaultEventuallyTimeout(CFEventuallyTimeout)
+		SetDefaultConsistentlyDuration(CFConsistentlyTimeout)
+
+		// Setup common environment variables
+		helpers.TurnOffColors()
+
+		*readOnlyOrg, *readOnlySpace = helpers.SetupReadOnlyOrgAndSpace()
+		_, _ = GinkgoWriter.Write([]byte(fmt.Sprintf("==============================End of Global Node %d Synchronized Before Each==============================", GinkgoParallelNode())))
+	})
+
+	var _ = SynchronizedAfterSuite(func() {
+		_, _ = GinkgoWriter.Write([]byte(fmt.Sprintf("==============================Global Node %d Synchronized After Each==============================", GinkgoParallelNode())))
+		*homeDir = helpers.SetHomeDir()
+		helpers.SetAPI()
+		helpers.LoginCF()
+		fakeservicebroker.Cleanup()
+		helpers.QuickDeleteOrg(*readOnlyOrg)
+		helpers.DestroyHomeDir(*homeDir)
+		_, _ = GinkgoWriter.Write([]byte(fmt.Sprintf("==============================End of Global Node %d Synchronized After Each==============================", GinkgoParallelNode())))
+	}, func() {
+		outputRoot := os.Getenv(helpers.PRBuilderOutputEnvVar)
+		if outputRoot != "" {
+			helpers.WriteFailureSummary(outputRoot, failureSummaryFilename)
+		}
+	})
+
+	var _ = BeforeEach(func() {
+		_, _ = GinkgoWriter.Write([]byte("==============================Global Before Each=============================="))
+		*homeDir = helpers.SetHomeDir()
+		*apiURL, *skipSSLValidation = helpers.SetAPI()
+		_, _ = GinkgoWriter.Write([]byte("==============================End of Global Before Each=============================="))
+	})
+
+	var _ = AfterEach(func() {
+		_, _ = GinkgoWriter.Write([]byte("==============================Global After Each==============================\n"))
+		helpers.DestroyHomeDir(*homeDir)
+		_, _ = GinkgoWriter.Write([]byte("==============================End of Global After Each=============================="))
+	})
+
+	return nil
+}
--- integration/v7/isolated/isolated_suite_test.go
+++ integration/v7/isolated/isolated_suite_test.go
@@ -1,22 +1,13 @@
 package isolated
 
 import (
-	"fmt"
-	"os"
+	"code.cloudfoundry.org/cli/integration/helpers/commonisolated"
 	"testing"
-	"time"
-
-	"code.cloudfoundry.org/cli/api/cloudcontroller/ccversion"
-	"code.cloudfoundry.org/cli/integration/helpers"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 )
 
 const (
-	CFEventuallyTimeout   = 300 * time.Second
-	CFConsistentlyTimeout = 500 * time.Millisecond
-	RealIsolationSegment  = "persistent_isolation_segment"
-	DockerImage           = "cloudfoundry/diego-docker-app-custom"
+	RealIsolationSegment = commonisolated.RealIsolationSegment
+	DockerImage          = commonisolated.DockerImage
 )
 
 var (
@@ -26,74 +17,19 @@ var (
 	ReadOnlyOrg       string
 	ReadOnlySpace     string
 
-	// Per Test Level
+	// Per test
 	homeDir string
 )
 
 func TestIsolated(t *testing.T) {
-	RegisterFailHandler(Fail)
-	reporters := []Reporter{}
-
-	prBuilderReporter := helpers.GetPRBuilderReporter()
-	if prBuilderReporter != nil {
-		reporters = append(reporters, prBuilderReporter)
-	}
-
-	RunSpecsWithDefaultAndCustomReporters(t, "Isolated Integration Suite", reporters)
+	commonisolated.CommonTestIsolated(t)
 }
 
-var _ = SynchronizedBeforeSuite(func() []byte {
-	GinkgoWriter.Write([]byte("==============================Global FIRST Node Synchronized Before Each=============================="))
-	SetDefaultEventuallyTimeout(CFEventuallyTimeout)
-	SetDefaultConsistentlyDuration(CFConsistentlyTimeout)
-
-	helpers.SetupSynchronizedSuite(func() {
-		helpers.EnableFeatureFlag("diego_docker")
-
-		if helpers.IsVersionMet(ccversion.MinVersionShareServiceV3) {
-			helpers.EnableFeatureFlag("service_instance_sharing")
-		}
-	})
-	GinkgoWriter.Write([]byte("==============================End of Global FIRST Node Synchronized Before Each=============================="))
-
-	return nil
-}, func(_ []byte) {
-	GinkgoWriter.Write([]byte(fmt.Sprintf("==============================Global Node %d Synchronized Before Each==============================", GinkgoParallelNode())))
-	// Ginkgo Globals
-	SetDefaultEventuallyTimeout(CFEventuallyTimeout)
-	SetDefaultConsistentlyDuration(CFConsistentlyTimeout)
-
-	// Setup common environment variables
-	helpers.TurnOffColors()
-
-	ReadOnlyOrg, ReadOnlySpace = helpers.SetupReadOnlyOrgAndSpace()
-	GinkgoWriter.Write([]byte(fmt.Sprintf("==============================End of Global Node %d Synchronized Before Each==============================", GinkgoParallelNode())))
-})
-
-var _ = SynchronizedAfterSuite(func() {
-	GinkgoWriter.Write([]byte(fmt.Sprintf("==============================Global Node %d Synchronized After Each==============================", GinkgoParallelNode())))
-	homeDir = helpers.SetHomeDir()
-	helpers.SetAPI()
-	helpers.LoginCF()
-	helpers.QuickDeleteOrg(ReadOnlyOrg)
-	helpers.DestroyHomeDir(homeDir)
-	GinkgoWriter.Write([]byte(fmt.Sprintf("==============================End of Global Node %d Synchronized After Each==============================", GinkgoParallelNode())))
-}, func() {
-	outputRoot := os.Getenv(helpers.PRBuilderOutputEnvVar)
-	if outputRoot != "" {
-		helpers.WriteFailureSummary(outputRoot, "summary_isi.txt")
-	}
-})
-
-var _ = BeforeEach(func() {
-	GinkgoWriter.Write([]byte("==============================Global Before Each=============================="))
-	homeDir = helpers.SetHomeDir()
-	apiURL, skipSSLValidation = helpers.SetAPI()
-	GinkgoWriter.Write([]byte("==============================End of Global Before Each=============================="))
-})
-
-var _ = AfterEach(func() {
-	GinkgoWriter.Write([]byte("==============================Global After Each==============================\n"))
-	helpers.DestroyHomeDir(homeDir)
-	GinkgoWriter.Write([]byte("==============================End of Global After Each=============================="))
-})
+var _ = commonisolated.CommonGinkgoSetup(
+	"summary_isi.txt",
+	&apiURL,
+	&skipSSLValidation,
+	&ReadOnlyOrg,
+	&ReadOnlySpace,
+	&homeDir,
+)
