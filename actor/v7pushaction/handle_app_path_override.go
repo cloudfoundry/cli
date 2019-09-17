596b642a702ec82eef91aea90e6acf5f5928d5bb
Brian Cunnie <bcunnie@pivotal.io>
2019-09-17 12:31:29 -0700

    push with `-p` and `-f` with relative path fails
    - TODO: strip out `session.*.Contents()` in the integration tests
    
    [#168545851]
    
    Co-authored-by: Supraja Narasimhan <snarasimhan@pivotal.io>

--- actor/v7pushaction/handle_app_path_override.go
+++ actor/v7pushaction/handle_app_path_override.go
@@ -33,23 +33,23 @@ func HandleAppPathOverride(manifest pushmanifestparser.Manifest, overrides FlagO
 		}
 
 		var finalPath = manifest.Applications[i].Path
-		if !filepath.IsAbs(finalPath) {
+		if !filepath.IsAbs(finalPath) && overrides.ProvidedAppPath == "" {
 			finalPath = filepath.Join(filepath.Dir(manifest.PathToManifest), finalPath)
 		}
 
-		finalPath, err := filepath.EvalSymlinks(finalPath)
+		finalPathNoLinks, err := filepath.EvalSymlinks(finalPath)
 
 		if err != nil {
 			if os.IsNotExist(err) {
 				return manifest, pushmanifestparser.InvalidManifestApplicationPathError{
-					Path: manifest.Applications[i].Path,
+					Path: finalPath,
 				}
 			}
 
 			return manifest, err
 		}
 
-		manifest.Applications[i].Path = finalPath
+		manifest.Applications[i].Path = finalPathNoLinks
 	}
 
 	return manifest, nil
