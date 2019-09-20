abcd2929c550ed42bc500e52374ce061a4c688ae
Supraja Narasimhan <snarasimhan@pivotal.io>
2019-09-19 17:21:40 -0700

    v7: WIP support for --endpoint flag
    - First-pass integration tests for A/Cs written
    - A/C 2 is the failing integration test to be used for driving
    implementation when this story is picked up
    
    [#168549300]
    
    Authored-by: Supraja Narasimhan <snarasimhan@pivotal.io>

--- integration/v7/push/health_check_test.go
+++ integration/v7/push/health_check_test.go
@@ -9,6 +9,7 @@ import (
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
+	"path/filepath"
 )
 
 var _ = Describe("push with health check type", func() {
@@ -63,42 +64,136 @@ var _ = Describe("push with health check type", func() {
 				Eventually(session).Should(Exit(0))
 			})
 		})
-	})
 
-	Context("creating the application", func() {
-		When("setting a http health check type", func() {
-			It("should set the health check type to http and use the default health check endpoint", func() {
-				helpers.WithMultiEndpointApp(func(dir string) {
-					Eventually(helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "-u", "http", "--endpoint", "/other_endpoint.html")).Should(Exit(0))
+		FWhen("setting the app's health check http endpoint", func() {
+			When("manifest has non-http (e.g. port) health check type", func() {
+
+				// A/C # 1
+				When("manifest has http health check type", func() {
+					When("only the endpoint flag override is specified", func() {
+						It("should update the endpoint", func() {
+							helpers.WithMultiEndpointApp(func(dir string) {
+								helpers.WriteManifest(filepath.Join(dir, "manifest.yml"), map[string]interface{}{
+									"applications": []map[string]string{
+										{
+											"name":                  "some-app",
+											"health-check-type":     "http",
+											"health-check-endpoint": "/",
+										},
+									},
+								})
+
+								session := helpers.CustomCF(
+									helpers.CFEnv{WorkingDirectory: dir},
+									PushCommandName, appName,
+									"--endpoint", "/third_endpoint.html")
+								Eventually(session).Should(Exit(0))
+							})
+
+							session := helpers.CF("get-health-check", appName)
+							Eventually(session).Should(Say("web\\s+http\\s+/third_endpoint.html"))
+							Eventually(session).Should(Exit(0)) //})
+						})
+					})
+
+					When("both health check type and endpoint flag overrides are specified", func() {
+						// A/C # 2
+						When("only the endpoint flag override is specified", func() {
+							It("should fail with an error indicating that endpoint cannot be set for health check type port", func() {
+								helpers.WithMultiEndpointApp(func(dir string) {
+									helpers.WriteManifest(filepath.Join(dir, "manifest.yml"), map[string]interface{}{
+										"applications": []map[string]string{
+											{
+												"name":                  "some-app",
+												"health-check-type":     "http",
+												"health-check-endpoint": "/",
+											},
+										},
+									})
+
+									session := helpers.CustomCF(
+										helpers.CFEnv{WorkingDirectory: dir},
+										PushCommandName, appName,
+										"--endpoint", "/third_endpoint.html")
+									// Because we no longer require that override --endpoint must be sent with override
+									// --health-check-type=http
+									// If manifest health-check-type is non-http
+									// We will transform the manifest to have this combo:
+									//   health-check-type: port # i.e. non-http
+									//   health-check-endpoint: /health # i.e. any real endpoint on the app
+									// This will then fail with a CAPI error when CAPI apply manifest endpoint complains
+									//
+									//Eventually(session).Should(Say("[error message about a conflict about how endpoints can't be set for port type health checks]"))
+									Eventually(session).Should(Exit(1))
+								})
+							})
+						})
+
+						// A/C # 3
+						It("should update the health check type and endpoint", func() {
+							helpers.WithMultiEndpointApp(func(dir string) {
+								helpers.WriteManifest(filepath.Join(dir, "manifest.yml"), map[string]interface{}{
+									"applications": []map[string]string{
+										{
+											"name":              "some-app",
+											"health-check-type": "port",
+										},
+									},
+								})
+
+								session := helpers.CustomCF(
+									helpers.CFEnv{WorkingDirectory: dir},
+									PushCommandName, appName,
+									"--health-check-type", "http",
+									"--endpoint", "/third_endpoint.html")
+								Eventually(session).Should(Exit(0))
+							})
+
+							session := helpers.CF("get-health-check", appName)
+							Eventually(session).Should(Say("web\\s+http\\s+/third_endpoint.html"))
+							Eventually(session).Should(Exit(0))
+						})
+					})
+
 				})
-
-				session := helpers.CF("get-health-check", appName)
-				Eventually(session).Should(Say("web\\s+http\\s+/other_endpoint.html"))
-				Eventually(session).Should(Exit(0))
 			})
 		})
 
-		When("setting a port health check type", func() {
-			It("it should set the health check type to port", func() {
-				helpers.WithMultiEndpointApp(func(dir string) {
-					Eventually(helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "-u", "port")).Should(Exit(0))
-				})
+		Context("creating the application", func() {
+			When("setting a http health check type", func() {
+				It("should set the health check type to http and use the default health check endpoint", func() {
+					helpers.WithMultiEndpointApp(func(dir string) {
+						Eventually(helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "-u", "http", "--endpoint", "/other_endpoint.html")).Should(Exit(0))
+					})
 
-				session := helpers.CF("get-health-check", appName)
-				Eventually(session).Should(Say("web\\s+port"))
-				Eventually(session).Should(Exit(0))
+					session := helpers.CF("get-health-check", appName)
+					Eventually(session).Should(Say("web\\s+http\\s+/other_endpoint.html"))
+					Eventually(session).Should(Exit(0))
+				})
 			})
-		})
 
-		When("setting a process health check type", func() {
-			It("it should set the health check type to process", func() {
-				helpers.WithMultiEndpointApp(func(dir string) {
-					Eventually(helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "-u", "process")).Should(Exit(0))
+			When("setting a port health check type", func() {
+				It("it should set the health check type to port", func() {
+					helpers.WithMultiEndpointApp(func(dir string) {
+						Eventually(helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "-u", "port")).Should(Exit(0))
+					})
+
+					session := helpers.CF("get-health-check", appName)
+					Eventually(session).Should(Say("web\\s+port"))
+					Eventually(session).Should(Exit(0))
 				})
+			})
 
-				session := helpers.CF("get-health-check", appName)
-				Eventually(session).Should(Say("web\\s+process"))
-				Eventually(session).Should(Exit(0))
+			When("setting a process health check type", func() {
+				It("it should set the health check type to process", func() {
+					helpers.WithMultiEndpointApp(func(dir string) {
+						Eventually(helpers.CustomCF(helpers.CFEnv{WorkingDirectory: dir}, PushCommandName, appName, "-u", "process")).Should(Exit(0))
+					})
+
+					session := helpers.CF("get-health-check", appName)
+					Eventually(session).Should(Say("web\\s+process"))
+					Eventually(session).Should(Exit(0))
+				})
 			})
 		})
 	})
