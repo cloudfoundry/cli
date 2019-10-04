9de2b9c5d946b7367bcfdf34aef85be950328699
Merric de Launey <mdelauney@pivotal.io>
2019-10-04 10:02:52 -0700

    Less flaky login command integration test.
    The integration tests would periodically fail. The problem was that
    there was a global list of routes added to the ginkgo HTTP test servers,
    which would have worked fine if there were but one test server; however,
    we have many test servers, and if one of the earlier ones beat us to the
    punch of registering the route, our test would fail through no fault of
    its own.
    
    fixes:
    ```
    /Users/pivotal/go/src/code.cloudfoundry.org/cli/integration/shared/isolated/login_command_test.go:949
    
    Received Unhandled Request
    Expected
        <string>: GET /v3/organizations?order_by=name HTTP/1.1
        Host: 127.0.0.1:63364
        Accept: application/json
        Accept-Encoding: gzip
        Authorization: bearer eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJleHAiOjE1NzAxNDQyMzgsInVzZXJfbmFtZSI6InNvbWUtdXNlciJ9.
        User-Agent: cf/7.0.0-beta.26+7447f00cf.2019-10-03 (go1.13.1; amd64 darwin)
    
    to be nil
    ```
    
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>
    Co-authored-by: Brian Cunnie <bcunnie@pivotal.io>

--- integration/helpers/ginkgo_routing_extension.go
+++ integration/helpers/ginkgo_routing_extension.go
@@ -23,7 +23,7 @@ var seenRoutes map[string]bool
 // endpoint specified by "pathAndQuery", returning a response with status code "status" and
 // response body "body".
 func AddHandler(ser *ghttp.Server, method string, pathAndQuery string, status int, body []byte) {
-	u, err := url.Parse(pathAndQuery)
+	p, err := url.Parse(pathAndQuery)
 	if err != nil {
 		panic(err)
 	}
@@ -32,22 +32,22 @@ func AddHandler(ser *ghttp.Server, method string, pathAndQuery string, status in
 		seenRoutes = make(map[string]bool)
 	}
 
-	responses[key(method, u)] = resp{status, body}
+	responses[key(ser.URL(), method, p)] = resp{status, body}
 
-	if !seenRoutes[key(method, u)] {
-		ser.RouteToHandler(method, u.Path, func(w http.ResponseWriter, r *http.Request) {
-			res, ok := responses[key(r.Method, r.URL)]
+	if !seenRoutes[key(ser.URL(), method, p)] {
+		ser.RouteToHandler(method, p.Path, func(w http.ResponseWriter, r *http.Request) {
+			res, ok := responses[key(ser.URL(), r.Method, r.URL)]
 			if !ok {
-				Expect(errors.New("Unexpected request: " + key(r.Method, r.URL))).ToNot(HaveOccurred())
+				Expect(errors.New("Unexpected request: " + key(ser.URL(), r.Method, r.URL))).ToNot(HaveOccurred())
 			}
 			w.WriteHeader(res.status)
 			_, err := w.Write(res.body)
 			Expect(err).ToNot(HaveOccurred())
 		})
-		seenRoutes[key(method, u)] = true
+		seenRoutes[key(ser.URL(), method, p)] = true
 	}
 }
 
-func key(method string, url fmt.Stringer) string {
-	return strings.ToLower(method + url.String())
+func key(url string, method string, pathAndQuery fmt.Stringer) string {
+	return strings.ToLower(url + method + pathAndQuery.String())
 }
