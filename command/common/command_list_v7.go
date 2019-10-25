8e5b709524fbc4c2c98b266ceab7bb916fcdcdc8
Oleksii Fedorov <ofedorov@pivotal.io>
2019-10-25 11:43:06 +0100

    "As a v7 CLI user, I can run `cf rename-service-broker -h` to get help" [#169123696]
    Co-authored-by: Felisia Martini <fmartini@pivotal.io>

--- command/common/command_list_v7.go
+++ command/common/command_list_v7.go
@@ -123,7 +123,7 @@ type commandList struct {
 	RenameBuildpack                    v6.RenameBuildpackCommand                    `command:"rename-buildpack" description:"Rename a buildpack"`
 	RenameOrg                          v7.RenameOrgCommand                          `command:"rename-org" description:"Rename an org"`
 	RenameService                      v6.RenameServiceCommand                      `command:"rename-service" description:"Rename a service instance"`
-	RenameServiceBroker                v6.RenameServiceBrokerCommand                `command:"rename-service-broker" description:"Rename a service broker"`
+	RenameServiceBroker                v7.RenameServiceBrokerCommand                `command:"rename-service-broker" description:"Rename a service broker"`
 	RenameSpace                        v7.RenameSpaceCommand                        `command:"rename-space" description:"Rename a space"`
 	RepoPlugins                        plugin.RepoPluginsCommand                    `command:"repo-plugins" description:"List all available plugins in specified repository or in all added repositories"`
 	ResetOrgDefaultIsolationSegment    v6.ResetOrgDefaultIsolationSegmentCommand    `command:"reset-org-default-isolation-segment" description:"Reset the default isolation segment used for apps in spaces of an org"`
