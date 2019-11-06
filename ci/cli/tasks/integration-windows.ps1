37807945e34f493f3bfebb19e0153cb6bd09c8cd
Eric Promislow <eric.promislow@suse.com>
2019-11-06 15:15:35 -0800

    Update concourse tasks with correct plugin integration paths
    Co-authored-by: Joseph Palermo <jpalermo@pivotal.io>

--- ci/cli/tasks/integration-windows.ps1
+++ ci/cli/tasks/integration-windows.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = "Stop"
 echo "CF_INT_CLIENT_CREDENTIALS_TEST_MODE: $Env:CF_INT_CLIENT_CREDENTIALS_TEST_MODE"
 
 cd $Env:GOPATH\src\code.cloudfoundry.org\cli
-ginkgo.exe -r -nodes=16 -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/isolated ./integration/v6/isolated ./integration/shared/plugin ./integration/v6/push
+ginkgo.exe -r -nodes=16 -flakeAttempts=2 -slowSpecThreshold=60 -randomizeAllSpecs ./integration/shared/isolated ./integration/v6/isolated ./integration/v6/plugin ./integration/v6/push
 if ($LASTEXITCODE -gt 0)
 {
 	exit 1
