ec221d323b7e4d27582f3347ad1747c3f5a618b9
Steve Taylor <staylor@pivotal.io>
2019-11-20 16:36:24 -0800

    Display potential error from GetCurrentSpace
    [#169612720]

--- integration/assets/test_plugin_v7/test_plugin_v7.go
+++ integration/assets/test_plugin_v7/test_plugin_v7.go
@@ -20,8 +20,12 @@ func (c *Test1) Run(cliConnection plugin.CliConnection, args []string) {
 		result, _ := cliConnection.GetApp(args[1])
 		fmt.Println("Done GetApp:", result)
 	case "GetCurrentSpace":
-		result, _ := cliConnection.GetCurrentSpace()
-		fmt.Printf("Done GetCurrentSpace:, result:%v, name: %s, guid: %s\n", result, result.Name, result.GUID)
+		result, err := cliConnection.GetCurrentSpace()
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+		} else {
+			fmt.Printf("Done GetCurrentSpace:, result:%v, name: %s, guid: %s\n", result, result.Name, result.GUID)
+		}
 	case "TestPluginCommandWithAliasV7", "Cool-V7":
 		fmt.Println("You called Test Plugin Command V7 With Alias!")
 	case "AccessToken":
