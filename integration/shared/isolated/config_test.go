66cf1e3fb3139cb7d3bd7799baae6cb043c36c6c
Alexander Berezovsky <aberezovsky@pivotal.io>
2019-12-09 14:17:19 -0800

    Delete vacuous test.
    - Upgrading to golang 1.13, we noticed that this test was attempting to
    create a timeout by communicating with an unreachable ip address.  We
    noticed this because now the `net` package responds with a more helpful
    "network unreachable" error, causing this test to fail.
    - We refactored the code to actually time out by pointing at
    www.pivotal.io:81.
    - We thought more about it, and removed setting CF_DIAL_TIMEOUT, and
    guess what?  That passes!  Because it times out.
    - We decided that testing that we see a network timeout occur when
    there's a network timeout is unhelpful: that test belongs in golang's
    network stack.
    
    [#168846870](https://www.pivotaltracker.com/story/show/168846870)

--- integration/shared/isolated/config_test.go
+++ integration/shared/isolated/config_test.go
@@ -5,7 +5,6 @@ import (
 	"path/filepath"
 
 	helpers "code.cloudfoundry.org/cli/integration/helpers"
-	"code.cloudfoundry.org/cli/util/configv3"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
@@ -95,26 +94,6 @@ var _ = Describe("Config", func() {
 				session := helpers.CFWithEnv(map[string]string{"CF_COLOR": "false"}, "help")
 				Consistently(session).ShouldNot(Say("\x1b\\[1m"))
 				Eventually(session).Should(Exit(0))
-			})
-		})
-	})
-
-	Describe("Dial Timeout", func() {
-		When("the dial timeout is set", func() {
-			BeforeEach(func() {
-				config, err := configv3.LoadConfig()
-				Expect(err).ToNot(HaveOccurred())
-
-				config.ConfigFile.Target = "http://1.2.3.4"
-
-				err = config.WriteConfig()
-				Expect(err).ToNot(HaveOccurred())
-			})
-
-			It("times out connection attempts after the dial timeout has passed", func() {
-				session := helpers.CFWithEnv(map[string]string{"CF_DIAL_TIMEOUT": "1"}, "unbind-service", "banana", "pants")
-				Eventually(session.Err).Should(Say("dial tcp 1.2.3.4:80: i/o timeout"))
-				Eventually(session).Should(Exit())
 			})
 		})
 	})
