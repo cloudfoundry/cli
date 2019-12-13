94056014b7fef2b700237b3a299627ca12348fb2
Supraja Narasimhan <snarasimhan@pivotal.io>
2019-12-13 13:56:48 -0800

    Sort methods in ui_v7 alphabetically
    To appease the linter
    
    [#169106926]
    
    Co-authored-by: Supraja Narasimhan <snarasimhan@pivotal.io>
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>

--- util/ui/ui_v7.go
+++ util/ui/ui_v7.go
@@ -6,17 +6,17 @@ import (
 	"fmt"
 )
 
+// DisplayWarning translates the warning, substitutes in templateValues, and
+// outputs to ui.Err. Only the first map in templateValues is used.
+// This command has one fewer newline than DisplayWarning. Use it before an OK message in V7.
+func (ui *UI) DisplayWarning(template string, templateValues ...map[string]interface{}) {
+	fmt.Fprintf(ui.Err, "%s\n", ui.TranslateText(template, templateValues...))
+}
+
 // Translates warnings and outputs them to ui.Err.
 // Prints each warning with a trailing newline.
 func (ui *UI) DisplayWarnings(warnings []string) {
 	for _, warning := range warnings {
 		fmt.Fprintf(ui.Err, "%s\n", ui.TranslateText(warning))
 	}
-}
-
-// DisplayWarning translates the warning, substitutes in templateValues, and
-// outputs to ui.Err. Only the first map in templateValues is used.
-// This command has one fewer newline than DisplayWarning. Use it before an OK message in V7.
-func (ui *UI) DisplayWarning(template string, templateValues ...map[string]interface{}) {
-	fmt.Fprintf(ui.Err, "%s\n", ui.TranslateText(template, templateValues...))
 }
