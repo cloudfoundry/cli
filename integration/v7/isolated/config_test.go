84a6d3b10b8b6877b42cc393e12f55fe16578c2d
Eric Promislow <eric.promislow@suse.com>
2019-12-19 17:27:25 -0800

    The ConfigVersion change saga continues
    Possible that we need to do the restoring in the `It` test and
    not an `AfterEach`
    
    Also try restoring the skip-ssl-validation setting, although this
    should only be required on dev workstations.
    
    [#1702476450](https://www.pivotaltracker.com/story/show/170247645)
    
    Authored-by: Eric Promislow <eric.promislow@suse.com>

--- integration/v7/isolated/config_test.go
+++ integration/v7/isolated/config_test.go
@@ -1,7 +1,7 @@
 package isolated
 
 import (
-	helpers "code.cloudfoundry.org/cli/integration/helpers"
+	"code.cloudfoundry.org/cli/integration/helpers"
 	"code.cloudfoundry.org/cli/util/configv3"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -11,17 +11,14 @@ var _ = Describe("Config", func() {
 	Describe("Version Management", func() {
 		var oldTarget string
 		var oldVersion int
+		var oldSkipSSLValidation bool
 
 		BeforeEach(func() {
 			config := helpers.GetConfig()
 			oldTarget = config.Target()
 			oldVersion = config.ConfigFile.ConfigVersion
-		})
-		AfterEach(func() {
-			helpers.SetConfig(func(config *configv3.Config) {
-				config.ConfigFile.ConfigVersion = oldVersion
-				config.ConfigFile.Target = oldTarget
-			})
+			oldSkipSSLValidation = config.ConfigFile.SkipSSLValidation
+
 		})
 
 		It("reset config to default if version mismatch", func() {
@@ -33,6 +30,11 @@ var _ = Describe("Config", func() {
 			config := helpers.GetConfig()
 			Expect(config.ConfigFile.ConfigVersion).To(Equal(configv3.CurrentConfigVersion))
 			Expect(config.ConfigFile.Target).To(Equal(""))
+			helpers.SetConfig(func(config *configv3.Config) {
+				config.ConfigFile.ConfigVersion = oldVersion
+				config.ConfigFile.Target = oldTarget
+				config.ConfigFile.SkipSSLValidation = oldSkipSSLValidation
+			})
 		})
 	})
 })
