149906099aa52b0363ab0d78ed6ff31b66a5f46f
Andrew Crump <acrump@pivotal.io>
2019-12-20 10:02:30 -0800

    Fix config test for client credentials
    - Use `cf plugins` to demonstrate that config is cleared. The LoginCF
      helper previously used would error if the API endpoint was not set
    - Remove restoration of old config values - each tests runs in its own
      home directory and so has a separate copy of the config
    
    [#170247645](https://www.pivotaltracker.com/story/show/170247645)

--- integration/v7/isolated/config_test.go
+++ integration/v7/isolated/config_test.go
@@ -9,32 +9,15 @@ import (
 
 var _ = Describe("Config", func() {
 	Describe("Version Management", func() {
-		var oldTarget string
-		var oldVersion int
-		var oldSkipSSLValidation bool
-
-		BeforeEach(func() {
-			config := helpers.GetConfig()
-			oldTarget = config.Target()
-			oldVersion = config.ConfigFile.ConfigVersion
-			oldSkipSSLValidation = config.ConfigFile.SkipSSLValidation
-
-		})
-
 		It("reset config to default if version mismatch", func() {
 			helpers.SetConfig(func(config *configv3.Config) {
 				config.ConfigFile.ConfigVersion = configv3.CurrentConfigVersion - 1
 				config.ConfigFile.Target = "api.my-target"
 			})
-			helpers.LoginCF()
+			helpers.CF("plugins")
 			config := helpers.GetConfig()
 			Expect(config.ConfigFile.ConfigVersion).To(Equal(configv3.CurrentConfigVersion))
 			Expect(config.ConfigFile.Target).To(Equal(""))
-			helpers.SetConfig(func(config *configv3.Config) {
-				config.ConfigFile.ConfigVersion = oldVersion
-				config.ConfigFile.Target = oldTarget
-				config.ConfigFile.SkipSSLValidation = oldSkipSSLValidation
-			})
 		})
 	})
 })
