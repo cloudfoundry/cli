e4b0ffa1141fc6a6c6d60de2623f4f9ff1a7e7c9
Reid Mitchell <rmitchell@pivotal.io>
2020-01-10 10:43:08 -0800

    Backfill integration test for v6 quotas command
    [#165132044]
    
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

---
+++ integration/v6/global/quotas_command_test.go
@@ -0,0 +1,43 @@
+package global
+
+import (
+	"code.cloudfoundry.org/cli/integration/helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("quotas command", func() {
+	var (
+		quotaName string
+
+		totalMemory        string
+		instanceMemory     string
+		routes             string
+		serviceInstances   string
+		appInstances       string
+		reservedRoutePorts string
+	)
+
+	BeforeEach(func() {
+		helpers.LoginCF()
+		helpers.TargetOrgAndSpace(ReadOnlyOrg, ReadOnlySpace)
+		quotaName = helpers.QuotaName()
+		totalMemory = "24M"
+		instanceMemory = "6M"
+		routes = "8"
+		serviceInstances = "2"
+		appInstances = "3"
+		reservedRoutePorts = "1"
+		session := helpers.CF("create-quota", quotaName, "-m", totalMemory, "-i", instanceMemory, "-r", routes, "-s", serviceInstances, "-a", appInstances, "--allow-paid-service-plans", "--reserved-route-ports", reservedRoutePorts)
+		Eventually(session).Should(Exit(0))
+	})
+
+	It("lists the org quotas", func() {
+		session := helpers.CF("quotas")
+		Eventually(session).Should(Say(`name\s+total memory\s+instance memory\s+routes\s+service instances\s+paid plans\s+app instances\s+route ports`))
+		Eventually(session).Should(Say(`%s\s+%s\s+%s\s+%s\s+%s\s+%s\s+%s\s+%s`, quotaName, totalMemory, instanceMemory, routes, serviceInstances, "allowed", appInstances, reservedRoutePorts))
+		Eventually(session).Should(Exit(0))
+	})
+})
