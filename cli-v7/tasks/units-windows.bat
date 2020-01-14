3d94a1967e53b819324121907be8328e4888693d
Reid Mitchell <rmitchell@pivotal.io>
2020-01-14 10:55:41 -0800

    CI: Simplify V7 pipeline's resources
    Three resources point to the same git repo:
    - cli
    - cli-ci
    - cli-integration
    
    This is unnecessary; they can all use the same resource (they all point
    to the master branch, and there are no special triggers).
    
    We have merged them into one resource, `cli`.
    
    [finishes #167390121]
    
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>
    Co-authored-by: Brian Cunnie <bcunnie@pivotal.io>

--- cli-v7/tasks/units-windows.bat
+++ cli-v7/tasks/units-windows.bat
@@ -2,6 +2,9 @@ SET TARGET_V7=true
 SET GOPATH=%CD%\gopath
 SET PATH=C:\Go\bin;C:\Program Files\Git\cmd\;%GOPATH%\bin;%PATH%
 
+md %GOPATH%\src\code.cloudfoundry.org\
+mklink /D %CD%\cli %GOPATH%\src\code.cloudfoundry.org\
+
 cd %GOPATH%\src\code.cloudfoundry.org\cli
 
 powershell -command set-executionpolicy remotesigned
