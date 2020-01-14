6c7bfa4790da76bd4dc16c785a9074e054b84d81
Reid Mitchell <rmitchell@pivotal.io>
2020-01-14 12:02:26 -0800

    🐞 CI: V7 pipeline
    Symlinks are unnecessary & troublesome; judicious use of `path` does
    what we need.
    
    fixes
    <https://ci.cli.fun/teams/main/pipelines/V7/jobs/units/builds/1395>:
    ```
    md C:\...\gopath\src\code.cloudfoundry.org\
    Cannot create a file when that file already exists.
    ```
    
    [finishes #167390121]
    
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>
    Co-authored-by: Brian Cunnie <bcunnie@pivotal.io>

--- ci/cli-v7/tasks/units-windows.bat
+++ ci/cli-v7/tasks/units-windows.bat
@@ -2,9 +2,6 @@ SET TARGET_V7=true
 SET GOPATH=%CD%\gopath
 SET PATH=C:\Go\bin;C:\Program Files\Git\cmd\;%GOPATH%\bin;%PATH%
 
-md %GOPATH%\src\code.cloudfoundry.org\
-mklink /D %CD%\cli %GOPATH%\src\code.cloudfoundry.org\cli
-
 cd %GOPATH%\src\code.cloudfoundry.org\cli
 
 powershell -command set-executionpolicy remotesigned
