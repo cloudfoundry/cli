9e5c1798fb08d2a9d0bf5e3cbac5b9756e3abef6
Supraja Narasimhan <snarasimhan@pivotal.io>
2020-01-16 17:01:20 -0800

    Refactor v7action.OrganizationQuota
    Previously it made use of ccv3 structs which broke abstraction barriers
    as the command level tried to access said structs.
    
    Flattened the hierarchy so everything was top level.
    
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>

--- actor/v7action/organization_quota.go
+++ actor/v7action/organization_quota.go
@@ -3,9 +3,28 @@ package v7action
 import (
 	"code.cloudfoundry.org/cli/actor/actionerror"
 	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv3"
+	"code.cloudfoundry.org/cli/types"
 )
 
-type OrganizationQuota ccv3.OrgQuota
+type OrganizationQuota struct {
+	// GUID is the unique ID of the organization quota.
+	GUID string
+	// Name is the name of the organization quota
+	Name string
+
+	//the various limits that are associated with applications
+	TotalMemory       types.NullInt
+	InstanceMemory    types.NullInt
+	TotalAppInstances types.NullInt
+
+	//the various limits that are associated with services
+	TotalServiceInstances types.NullInt
+	PaidServicePlans      bool
+
+	//the various limits that are associated with routes
+	TotalRoutes     types.NullInt
+	TotalRoutePorts types.NullInt
+}
 
 func (actor Actor) GetOrganizationQuotas() ([]OrganizationQuota, Warnings, error) {
 	ccv3OrgQuotas, warnings, err := actor.CloudControllerClient.GetOrganizationQuotas()
@@ -15,7 +34,7 @@ func (actor Actor) GetOrganizationQuotas() ([]OrganizationQuota, Warnings, error
 
 	var orgQuotas []OrganizationQuota
 	for _, quota := range ccv3OrgQuotas {
-		orgQuotas = append(orgQuotas, OrganizationQuota(quota))
+		orgQuotas = append(orgQuotas, convertToOrganizationQuota(quota))
 	}
 
 	return orgQuotas, Warnings(warnings), nil
@@ -36,6 +55,22 @@ func (actor Actor) GetOrganizationQuotaByName(orgQuotaName string) (Organization
 	if len(ccv3OrgQuotas) == 0 {
 		return OrganizationQuota{}, Warnings(warnings), actionerror.OrganizationQuotaNotFoundForNameError{Name: orgQuotaName}
 	}
+	orgQuota := convertToOrganizationQuota(ccv3OrgQuotas[0])
 
-	return OrganizationQuota(ccv3OrgQuotas[0]), Warnings(warnings), nil
+	return orgQuota, Warnings(warnings), nil
+}
+
+func convertToOrganizationQuota(ccv3OrgQuota ccv3.OrgQuota) OrganizationQuota {
+	orgQuota := OrganizationQuota{
+		GUID:                  ccv3OrgQuota.GUID,
+		Name:                  ccv3OrgQuota.Name,
+		TotalMemory:           ccv3OrgQuota.Apps.TotalMemory,
+		InstanceMemory:        ccv3OrgQuota.Apps.InstanceMemory,
+		TotalAppInstances:     ccv3OrgQuota.Apps.TotalAppInstances,
+		TotalServiceInstances: ccv3OrgQuota.Services.TotalServiceInstances,
+		PaidServicePlans:      ccv3OrgQuota.Services.PaidServicePlans,
+		TotalRoutes:           ccv3OrgQuota.Routes.TotalRoutes,
+		TotalRoutePorts:       ccv3OrgQuota.Routes.TotalRoutePorts,
+	}
+	return orgQuota
 }
