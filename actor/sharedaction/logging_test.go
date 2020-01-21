1f91a2dceb539304ca5e9842f4812a281d6623bd
Andrew Crump <acrump@pivotal.io>
2020-01-21 10:56:02 -0800

    Add unit test for coverage for sharedaction logging
    - Remove duplicate log_cache_client_test units in loggingaction
    
    [#170138644](https://www.pivotaltracker.com/story/show/170138644)
    
    Co-authored-by: Eric Promislow <eric.promislow@suse.com>

--- actor/sharedaction/logging_test.go
+++ actor/sharedaction/logging_test.go
@@ -1,14 +1,13 @@
-package loggingaction_test
+package sharedaction_test
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"time"
 
-	"code.cloudfoundry.org/cli/actor/loggingaction"
+	"code.cloudfoundry.org/cli/actor/sharedaction"
 
-	"code.cloudfoundry.org/cli/actor/loggingaction/loggingactionfakes"
+	"code.cloudfoundry.org/cli/actor/sharedaction/sharedactionfakes"
 	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
 	logcache "code.cloudfoundry.org/log-cache/pkg/client"
 	. "github.com/onsi/ginkgo"
@@ -17,25 +16,38 @@ import (
 
 var _ = Describe("Logging Actions", func() {
 	var (
-		fakeLogCacheClient *loggingactionfakes.FakeLogCacheClient
+		fakeLogCacheClient *sharedactionfakes.FakeLogCacheClient
 	)
 
 	BeforeEach(func() {
-		fakeLogCacheClient = new(loggingactionfakes.FakeLogCacheClient)
+		fakeLogCacheClient = new(sharedactionfakes.FakeLogCacheClient)
 	})
 
 	Describe("LogMessage", func() {
 		Describe("Staging", func() {
 			When("the log is a staging log", func() {
 				It("returns true", func() {
-					message := loggingaction.LogMessage{SourceType: "STG"}
+					message := *sharedaction.NewLogMessage(
+						"some-message",
+						"OUT",
+						time.Unix(0, 0),
+						"STG",
+						"some-source-instance",
+					)
+
 					Expect(message.Staging()).To(BeTrue())
 				})
 			})
 
 			When("the log is any other kind of log", func() {
 				It("returns false", func() {
-					message := loggingaction.LogMessage{SourceType: "APP"}
+					message := *sharedaction.NewLogMessage(
+						"some-message",
+						"OUT",
+						time.Unix(0, 0),
+						"APP",
+						"some-source-instance",
+					)
 					Expect(message.Staging()).To(BeFalse())
 				})
 			})
@@ -46,7 +58,7 @@ var _ = Describe("Logging Actions", func() {
 		var (
 			expectedAppGUID string
 
-			messages      <-chan loggingaction.LogMessage
+			messages      <-chan sharedaction.LogMessage
 			errs          <-chan error
 			stopStreaming context.CancelFunc
 		)
@@ -61,7 +73,7 @@ var _ = Describe("Logging Actions", func() {
 		})
 
 		JustBeforeEach(func() {
-			messages, errs, stopStreaming = loggingaction.GetStreamingLogs(expectedAppGUID, fakeLogCacheClient)
+			messages, errs, stopStreaming = sharedaction.GetStreamingLogs(expectedAppGUID, fakeLogCacheClient)
 		})
 
 		When("receiving logs", func() {
@@ -108,13 +120,13 @@ var _ = Describe("Logging Actions", func() {
 				}
 			})
 
-			It("converts them to log messages, sorts them, and passes them through the messages channel", func() {
+			It("converts them to log messages and passes them through the messages channel", func() {
 				Eventually(messages).Should(HaveLen(4))
-				var message loggingaction.LogMessage
+				var message sharedaction.LogMessage
 				Expect(messages).To(Receive(&message))
-				Expect(message.Message).To(Equal("message-1"))
+				Expect(message.Message()).To(Equal("message-1"))
 				Expect(messages).To(Receive(&message))
-				Expect(message.Message).To(Equal("message-2"))
+				Expect(message.Message()).To(Equal("message-2"))
 
 				Expect(errs).ToNot(Receive())
 			})
@@ -138,7 +150,7 @@ var _ = Describe("Logging Actions", func() {
 					readStart <- start
 
 					return []*loggregator_v2.Envelope{{
-						// 2 seconds in the past to get past Walk delay
+						// 6 seconds in the past to get past Walk delay
 						Timestamp:  time.Now().Add(-6 * time.Second).UnixNano(),
 						SourceId:   "some-app-guid",
 						InstanceId: "some-source-instance",
@@ -212,102 +224,6 @@ var _ = Describe("Logging Actions", func() {
 				Eventually(errs).Should(HaveLen(2))
 				Eventually(errs).Should(Receive(MatchError("error number 1")))
 				Eventually(errs).Should(Receive(MatchError("error number 2")))
-			})
-		})
-	})
-
-	Describe("GetRecentLogs", func() {
-		When("the application can be found", func() {
-			When("Log Cache returns logs", func() {
-				BeforeEach(func() {
-					messages := []*loggregator_v2.Envelope{
-						{
-							Timestamp:  int64(20),
-							SourceId:   "some-app-guid",
-							InstanceId: "some-source-instance",
-							Message: &loggregator_v2.Envelope_Log{
-								Log: &loggregator_v2.Log{
-									Payload: []byte("message-2"),
-									Type:    loggregator_v2.Log_OUT,
-								},
-							},
-							Tags: map[string]string{
-								"source_type": "some-source-type",
-							},
-						},
-						{
-							Timestamp:  int64(10),
-							SourceId:   "some-app-guid",
-							InstanceId: "some-source-instance",
-							Message: &loggregator_v2.Envelope_Log{
-								Log: &loggregator_v2.Log{
-									Payload: []byte("message-1"),
-									Type:    loggregator_v2.Log_OUT,
-								},
-							},
-							Tags: map[string]string{
-								"source_type": "some-source-type",
-							},
-						},
-					}
-
-					fakeLogCacheClient.ReadReturns(messages, nil)
-				})
-
-				It("returns all the recent logs and warnings", func() {
-					messages, err := loggingaction.GetRecentLogs("some-app-guid", fakeLogCacheClient)
-					Expect(err).ToNot(HaveOccurred())
-
-					Expect(messages[0].Message).To(Equal("message-1"))
-					Expect(messages[0].MessageType).To(Equal("OUT"))
-					Expect(messages[0].Timestamp).To(Equal(time.Unix(0, 10)))
-					Expect(messages[0].SourceType).To(Equal("some-source-type"))
-					Expect(messages[0].SourceInstance).To(Equal("some-source-instance"))
-
-					Expect(messages[1].Message).To(Equal("message-2"))
-					Expect(messages[1].MessageType).To(Equal("OUT"))
-					Expect(messages[1].Timestamp).To(Equal(time.Unix(0, 20)))
-					Expect(messages[1].SourceType).To(Equal("some-source-type"))
-					Expect(messages[1].SourceInstance).To(Equal("some-source-instance"))
-				})
-			})
-
-			When("Log Cache returns non-log envelopes", func() {
-				BeforeEach(func() {
-					messages := []*loggregator_v2.Envelope{
-						{
-							Timestamp:  int64(10),
-							SourceId:   "some-app-guid",
-							InstanceId: "some-source-instance",
-							Message:    &loggregator_v2.Envelope_Counter{},
-							Tags: map[string]string{
-								"source_type": "some-source-type",
-							},
-						},
-					}
-
-					fakeLogCacheClient.ReadReturns(messages, nil)
-				})
-
-				It("ignores them", func() {
-					messages, err := loggingaction.GetRecentLogs("some-app-guid", fakeLogCacheClient)
-					Expect(err).ToNot(HaveOccurred())
-					Expect(messages).To(BeEmpty())
-				})
-			})
-
-			When("Log Cache errors", func() {
-				var expectedErr error
-
-				BeforeEach(func() {
-					expectedErr = errors.New("ZOMG")
-					fakeLogCacheClient.ReadReturns(nil, expectedErr)
-				})
-
-				It("returns error and warnings", func() {
-					_, err := loggingaction.GetRecentLogs("some-app-guid", fakeLogCacheClient)
-					Expect(err).To(MatchError(expectedErr))
-				})
 			})
 		})
 	})
