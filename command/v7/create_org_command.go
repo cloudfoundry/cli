42420788c929a17c3cf63a7136e0ff80472c1ecd
Mona Mohebbi <mmohebbi@pivotal.io>
2020-02-04 10:48:08 -0800

    v7: CLI User can apply a quota while creating an org
    Using `--quota` or `-q` flag, a CLI user can now apply a quota to an org
    in the create-org command.
    
    [#167326215]
    
    Co-authored-by: Mona Mohebbi <mmohebbi@pivotal.io>
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- command/v7/create_org_command.go
+++ command/v7/create_org_command.go
@@ -16,12 +16,14 @@ import (
 type CreateOrgActor interface {
 	CreateOrganization(orgName string) (v7action.Organization, v7action.Warnings, error)
 	CreateOrgRole(roleType constant.RoleType, orgGUID string, userNameOrGUID string, userOrigin string, isClient bool) (v7action.Warnings, error)
+	ApplyOrganizationQuotaByName(quotaName string, orgGUID string) (v7action.Warnings, error)
 }
 
 type CreateOrgCommand struct {
 	RequiredArgs    flag.Organization `positional-args:"yes"`
+	Quota           string            `short:"q" long:"quota" description:"Quota to assign to the newly created org"`
 	usage           interface{}       `usage:"CF_NAME create-org ORG"`
-	relatedCommands interface{}       `related_commands:"create-space, orgs, quotas, set-org-role"`
+	relatedCommands interface{}       `related_commands:"create-space, orgs, org-quotas, set-org-role"`
 
 	UI          command.UI
 	Config      command.Config
@@ -74,6 +76,21 @@ func (cmd CreateOrgCommand) Execute(args []string) error {
 		return err
 	}
 	cmd.UI.DisplayOK()
+
+	if cmd.Quota != "" {
+		cmd.UI.DisplayTextWithFlavor("Setting org quota {{.Quota}} to org {{.Organization}} as {{.User}}...",
+			map[string]interface{}{
+				"Quota":        cmd.Quota,
+				"Organization": orgName,
+				"User":         user.Name,
+			})
+		warnings, err = cmd.Actor.ApplyOrganizationQuotaByName(cmd.Quota, org.GUID)
+		cmd.UI.DisplayWarnings(warnings)
+		if err != nil {
+			return err
+		}
+		cmd.UI.DisplayOK()
+	}
 
 	cmd.UI.DisplayTextWithFlavor("Assigning role OrgManager to user {{.User}} in org {{.Organization}} as {{.User}}...",
 		map[string]interface{}{
--- command/v7/create_org_command_test.go
+++ command/v7/create_org_command_test.go
@@ -2,14 +2,15 @@ package v7_test
 
 import (
 	"errors"
+	"fmt"
 
-	"code.cloudfoundry.org/cli/actor/actionerror"
-	"code.cloudfoundry.org/cli/actor/v7action"
 	"code.cloudfoundry.org/cli/api/cloudcontroller/ccerror"
 	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv3/constant"
 	"code.cloudfoundry.org/cli/command/flag"
 	"code.cloudfoundry.org/cli/util/configv3"
 
+	"code.cloudfoundry.org/cli/actor/actionerror"
+	"code.cloudfoundry.org/cli/actor/v7action"
 	"code.cloudfoundry.org/cli/command/commandfakes"
 	v7 "code.cloudfoundry.org/cli/command/v7"
 	"code.cloudfoundry.org/cli/command/v7/v7fakes"
@@ -29,7 +30,9 @@ var _ = Describe("create-org Command", func() {
 		binaryName      string
 		executeErr      error
 
-		orgName string
+		orgName         string
+		quotaName       string
+		currentUsername string
 	)
 
 	BeforeEach(func() {
@@ -41,17 +44,21 @@ var _ = Describe("create-org Command", func() {
 		binaryName = "faceman"
 		fakeConfig.BinaryNameReturns(binaryName)
 		orgName = "some-org"
-	})
+		currentUsername = "bob"
+		fakeConfig.CurrentUserReturns(configv3.User{Name: currentUsername}, nil)
+		quotaName = "quota-name"
 
-	JustBeforeEach(func() {
 		cmd = v7.CreateOrgCommand{
 			UI:           testUI,
 			Config:       fakeConfig,
 			SharedActor:  fakeSharedActor,
 			Actor:        fakeActor,
 			RequiredArgs: flag.Organization{Organization: orgName},
+			Quota:        quotaName,
 		}
+	})
 
+	JustBeforeEach(func() {
 		executeErr = cmd.Execute(nil)
 	})
 
@@ -70,96 +77,139 @@ var _ = Describe("create-org Command", func() {
 		})
 	})
 
-	When("the environment is setup correctly", func() {
-		var currentUsername string
+	It("prints text indicating it is creating a org", func() {
+		Expect(executeErr).NotTo(HaveOccurred())
+		Expect(testUI.Out).To(Say(`Creating org %s as %s\.\.\.`, orgName, currentUsername))
+	})
 
+	When("creating the org errors", func() {
 		BeforeEach(func() {
-			currentUsername = "bob"
-			fakeConfig.CurrentUserReturns(configv3.User{Name: currentUsername}, nil)
+			fakeActor.CreateOrganizationReturns(v7action.Organization{}, v7action.Warnings{"warnings-1", "warnings-2"}, errors.New("err-create-org"))
 		})
 
-		It("prints text indicating it is creating a org", func() {
-			Expect(executeErr).NotTo(HaveOccurred())
-			Expect(testUI.Out).To(Say(`Creating org %s as %s\.\.\.`, orgName, currentUsername))
+		It("returns an error and displays warnings", func() {
+			Expect(executeErr).To(MatchError("err-create-org"))
+			Expect(testUI.Err).To(Say("warnings-1"))
+			Expect(testUI.Err).To(Say("warnings-2"))
 		})
+	})
 
-		When("creating the org errors", func() {
-			BeforeEach(func() {
-				fakeActor.CreateOrganizationReturns(v7action.Organization{}, v7action.Warnings{"warnings-1", "warnings-2"}, errors.New("err-create-org"))
-			})
-
-			It("returns an error and displays warnings", func() {
-				Expect(executeErr).To(MatchError("err-create-org"))
-				Expect(testUI.Err).To(Say("warnings-1"))
-				Expect(testUI.Err).To(Say("warnings-2"))
-			})
-		})
-
-		When("creating the org is successful", func() {
-			BeforeEach(func() {
-				fakeActor.CreateOrganizationReturns(v7action.Organization{Name: orgName, GUID: "some-org-guid"}, v7action.Warnings{"warnings-1", "warnings-2"}, nil)
-			})
-
-			It("creates the org", func() {
-				Expect(fakeActor.CreateOrganizationCallCount()).To(Equal(1))
-				expectedOrgName := fakeActor.CreateOrganizationArgsForCall(0)
-				Expect(expectedOrgName).To(Equal(orgName))
-			})
-
-			It("assigns org manager to the admin", func() {
-				Expect(testUI.Out).To(Say(`Assigning role OrgManager to user %s in org %s as %s\.\.\.`, currentUsername, orgName, currentUsername))
-				Expect(fakeActor.CreateOrgRoleCallCount()).To(Equal(1))
-				givenRoleType, givenOrgGuid, givenUserName, givenOrigin, givenIsClient := fakeActor.CreateOrgRoleArgsForCall(0)
-				Expect(givenRoleType).To(Equal(constant.OrgManagerRole))
-				Expect(givenOrgGuid).To(Equal("some-org-guid"))
-				Expect(givenUserName).To(Equal(currentUsername))
-				Expect(givenOrigin).To(Equal(""))
-				Expect(givenIsClient).To(BeFalse())
-			})
-
-			It("prints all warnings, text indicating creation completion, ok and then a tip", func() {
-				Expect(executeErr).ToNot(HaveOccurred())
-				Expect(testUI.Err).To(Say("warnings-1"))
-				Expect(testUI.Err).To(Say("warnings-2"))
-				Expect(testUI.Out).To(Say("OK"))
-				Expect(testUI.Out).To(Say(`TIP: Use 'cf target -o "%s"' to target new org`, orgName))
-			})
-
-			When("creating the org manager role errors", func() {
-				BeforeEach(func() {
-					fakeActor.CreateOrgRoleReturns(
-						v7action.Warnings{"role-create-warning-1"},
-						errors.New("err-create-role"))
-				})
-
-				It("returns an error and displays warnings", func() {
-					Expect(executeErr).To(MatchError("err-create-role"))
-					Expect(testUI.Err).To(Say("role-create-warning-1"))
-				})
-			})
-		})
-
-		When("the org already exists", func() {
-			BeforeEach(func() {
-				fakeActor.CreateOrganizationReturns(
-					v7action.Organization{},
-					v7action.Warnings{"some-warning"},
-					ccerror.OrganizationNameTakenError{
-						UnprocessableEntityError: ccerror.UnprocessableEntityError{
-							Message: "Organization 'some-org' already exists.",
-						},
+	When("the org already exists", func() {
+		BeforeEach(func() {
+			fakeActor.CreateOrganizationReturns(
+				v7action.Organization{},
+				v7action.Warnings{"some-warning"},
+				ccerror.OrganizationNameTakenError{
+					UnprocessableEntityError: ccerror.UnprocessableEntityError{
+						Message: "Organization 'some-org' already exists.",
 					},
-				)
-			})
+				},
+			)
+		})
 
-			It("displays all warnings, that the org already exists, and does not error", func() {
-				Expect(executeErr).ToNot(HaveOccurred())
+		It("displays all warnings, that the org already exists, and does not error", func() {
+			Expect(executeErr).ToNot(HaveOccurred())
 
-				Expect(testUI.Err).To(Say("some-warning"))
-				Expect(testUI.Out).To(Say(`Creating org %s as %s\.\.\.`, orgName, currentUsername))
-				Expect(testUI.Out).To(Say(`Organization '%s' already exists\.`, orgName))
-				Expect(testUI.Out).To(Say("OK"))
-			})
+			Expect(testUI.Err).To(Say("some-warning"))
+			Expect(testUI.Out).To(Say(`Creating org %s as %s\.\.\.`, orgName, currentUsername))
+			Expect(testUI.Out).To(Say(`Organization '%s' already exists\.`, orgName))
+			Expect(testUI.Out).To(Say("OK"))
+		})
+	})
+
+	When("applying the quota errors", func() {
+		BeforeEach(func() {
+			cmd.Quota = "quota-name"
+			fakeActor.CreateOrganizationReturns(v7action.Organization{Name: orgName, GUID: "some-org-guid"}, v7action.Warnings{}, nil)
+			fakeActor.ApplyOrganizationQuotaByNameReturns(v7action.Warnings{"quota-warnings-1", "quota-warnings-2"}, errors.New("quota-error"))
+
+		})
+
+		It("returns an error and displays warnings", func() {
+			Expect(executeErr).To(MatchError("quota-error"))
+			Expect(testUI.Err).To(Say("quota-warnings-1"))
+			Expect(testUI.Err).To(Say("quota-warnings-2"))
+		})
+	})
+
+	When("the quota does not exist", func() {
+		BeforeEach(func() {
+			fakeActor.ApplyOrganizationQuotaByNameReturns(v7action.Warnings{"quota-warnings-1", "quota-warnings-2"}, actionerror.OrganizationQuotaNotFoundForNameError{Name: quotaName})
+		})
+
+		It("returns an error and displays warnings", func() {
+			Expect(testUI.Err).To(Say("quota-warnings-1"))
+			Expect(testUI.Err).To(Say("quota-warnings-2"))
+			Expect(executeErr).To(MatchError(fmt.Sprintf("Organization quota with name '%s' not found.", quotaName)))
+		})
+	})
+
+	When("assigning the org manager role errors", func() {
+		BeforeEach(func() {
+			fakeActor.CreateOrganizationReturns(v7action.Organization{Name: orgName, GUID: "some-org-guid"}, v7action.Warnings{"warnings-1", "warnings-2"}, nil)
+			fakeActor.CreateOrgRoleReturns(
+				v7action.Warnings{"role-create-warning-1"},
+				errors.New("err-create-role"))
+		})
+
+		It("returns an error and displays warnings", func() {
+			Expect(executeErr).To(MatchError("err-create-role"))
+			Expect(testUI.Err).To(Say("role-create-warning-1"))
+		})
+	})
+
+	When("creating the org is successful", func() {
+		var orgGUID = "some-org-guid"
+		BeforeEach(func() {
+			fakeActor.CreateOrganizationReturns(v7action.Organization{Name: orgName, GUID: orgGUID}, v7action.Warnings{"warnings-1", "warnings-2"}, nil)
+			fakeActor.ApplyOrganizationQuotaByNameReturns(v7action.Warnings{"quota-warnings-1", "quota-warnings-2"}, nil)
+			fakeActor.CreateOrgRoleReturns(
+				v7action.Warnings{"role-create-warning-1"},
+				nil)
+		})
+
+		It("prints all warnings, text indicating creation completion, ok and then a tip", func() {
+			Expect(executeErr).ToNot(HaveOccurred())
+
+			Expect(testUI.Out).To(Say(`Creating org %s as %s\.\.\.`, orgName, currentUsername))
+			Expect(testUI.Err).To(Say("warnings-1"))
+			Expect(testUI.Err).To(Say("warnings-2"))
+			Expect(testUI.Out).To(Say("OK"))
+
+			Expect(testUI.Out).To(Say(`Setting org quota %s to org %s as %s\.\.\.`, quotaName, orgName, currentUsername))
+			Expect(testUI.Err).To(Say("quota-warnings-1"))
+			Expect(testUI.Err).To(Say("quota-warnings-2"))
+			Expect(testUI.Out).To(Say("OK"))
+
+			Expect(testUI.Out).To(Say(`Assigning role OrgManager to user %s in org %s as %s\.\.\.`, currentUsername, orgName, currentUsername))
+			Expect(testUI.Err).To(Say("role-create-warning-1"))
+			Expect(testUI.Out).To(Say("OK"))
+
+			Expect(testUI.Out).To(Say(`TIP: Use 'cf target -o "%s"' to target new org`, orgName))
+		})
+
+		It("creates the org", func() {
+			Expect(fakeActor.CreateOrganizationCallCount()).To(Equal(1))
+			expectedOrgName := fakeActor.CreateOrganizationArgsForCall(0)
+			Expect(expectedOrgName).To(Equal(orgName))
+		})
+
+		It("applies he quota to the org", func() {
+			Expect(fakeActor.CreateOrgRoleCallCount()).To(Equal(1))
+			passedQuotaName, passedGuid := fakeActor.ApplyOrganizationQuotaByNameArgsForCall(0)
+			Expect(passedQuotaName).To(Equal(quotaName))
+			Expect(passedGuid).To(Equal(orgGUID))
+		})
+
+		It("assigns org manager to the admin", func() {
+			Expect(testUI.Out).To(Say(`Assigning role OrgManager to user %s in org %s as %s\.\.\.`, currentUsername, orgName, currentUsername))
+			Expect(fakeActor.CreateOrgRoleCallCount()).To(Equal(1))
+			givenRoleType, givenOrgGuid, givenUserName, givenOrigin, givenIsClient := fakeActor.CreateOrgRoleArgsForCall(0)
+			Expect(givenRoleType).To(Equal(constant.OrgManagerRole))
+			Expect(givenOrgGuid).To(Equal("some-org-guid"))
+			Expect(givenUserName).To(Equal(currentUsername))
+			Expect(givenOrigin).To(Equal(""))
+			Expect(givenIsClient).To(BeFalse())
 		})
 	})
 })
