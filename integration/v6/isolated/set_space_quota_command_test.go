c1717c833e8368c5e88ba9942c49874c6c2597a8
Brian Cunnie <bcunnie@pivotal.io>
2020-02-07 14:23:31 -0800

    v6: `set-space-quota` has richer isolation tests
    - help text
    - if org not targeted
    
    [#165135182]
    
    Authored-by: Brian Cunnie <bcunnie@pivotal.io>

--- integration/v6/isolated/set_space_quota_command_test.go
+++ integration/v6/isolated/set_space_quota_command_test.go
@@ -1,6 +1,7 @@
 package isolated
 
 import (
+	. "code.cloudfoundry.org/cli/cf/util/testhelpers/matchers"
 	"code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -26,6 +27,38 @@ var _ = Describe("set-space-quota command", func() {
 
 	AfterEach(func() {
 		helpers.QuickDeleteOrg(orgName)
+	})
+
+	Describe("help", func() {
+		When("--help flag is set", func() {
+			It("appears in cf help -a", func() {
+				session := helpers.CF("help", "-a")
+				Eventually(session).Should(Exit(0))
+				Expect(session).To(HaveCommandInCategoryWithDescription("set-space-quota", "SPACE ADMIN", "Assign a space quota definition to a space"))
+			})
+
+			It("Displays command usage to output", func() {
+				session := helpers.CF("set-space-quota", "--help")
+				Eventually(session).Should(Say("NAME:"))
+				Eventually(session).Should(Say("set-space-quota - Assign a space quota definition to a space"))
+				Eventually(session).Should(Say("USAGE:"))
+				Eventually(session).Should(Say(`cf set-space-quota SPACE_NAME SPACE_QUOTA_NAME`))
+				Eventually(session).Should(Say("SEE ALSO:"))
+				Eventually(session).Should(Say("space, space-quotas, spaces"))
+				Eventually(session).Should(Exit(0))
+			})
+		})
+	})
+
+	When("the environment is not setup correctly", func() {
+		It("fails with the appropriate errors", func() {
+			helpers.LoginCF()
+			session := helpers.CF("set-space-quota", spaceName, quotaName)
+			Eventually(session).Should(Say("FAILED"))
+			Eventually(session.Out).Should(Say("No org targeted, use 'cf target -o ORG' to target an org\\."))
+			Eventually(session).Should(Exit(1))
+			//helpers.CheckEnvironmentTargetedCorrectly(true, false, orgName, "set-space-quota", spaceName, quotaName)
+		})
 	})
 
 	It("sets the space quota on a space", func() {
