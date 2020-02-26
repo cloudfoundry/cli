4653ac30f4687fb0ec6abb425c64dde519046717
Derik Evangelista <devangelista@pivotal.io>
2020-02-26 11:30:09 +0000

    Fix golangci-lint error
    Co-authored-by: George Blue <gblue@pivotal.io>

--- actor/actionerror/service_not_found_error.go
+++ actor/actionerror/service_not_found_error.go
@@ -9,10 +9,15 @@ type ServiceNotFoundError struct {
 func (e ServiceNotFoundError) Error() string {
 	if e.Name != "" && e.Broker != "" {
 		return fmt.Sprintf("Service offering '%s' for service broker '%s' not found.", e.Name, e.Broker)
-	} else if e.Name != "" {
+	}
+
+	if e.Name != "" {
 		return fmt.Sprintf("Service offering '%s' not found.", e.Name)
-	} else if e.Broker != "" {
+	}
+
+	if e.Broker != "" {
 		return fmt.Sprintf("No service offerings found for service broker '%s'.", e.Broker)
 	}
+
 	return "No service offerings found."
 }
