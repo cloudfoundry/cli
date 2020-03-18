f3eb1d0d62e5d1be88bfc087fd5e474ea2282ca2
Alexander Berezovsky <aberezovsky@pivotal.io>
2020-03-18 12:03:52 -0700

    WIP on log cache message unit tests
    - Interrupting to confirm what a real log cache message should look like
    (specifically source instance)
    
    Co-authored-by: Nick Webb <nwebb@pivotal.io>

--- cf/api/logs/log_cache_message.go
+++ cf/api/logs/log_cache_message.go
@@ -1,6 +1,7 @@
 package logs
 
 import (
+	"fmt"
 	"time"
 
 	"code.cloudfoundry.org/cli/actor/sharedaction"
@@ -21,9 +22,58 @@ func (m *logCacheMessage) ToSimpleLog() string {
 }
 
 func (m *logCacheMessage) GetSourceName() string {
-	return ""
+	return m.msg.SourceType()
 }
 
 func (m *logCacheMessage) ToLog(loc *time.Location) string {
-	return ""
+
+	logMsg := m.msg
+
+	sourceName := logMsg.SourceType()
+	sourceID := logMsg.SourceInstance()
+	t := logMsg.Timestamp()
+	timeFormat := "2006-01-02T15:04:05.00-0700"
+	timeString := t.In(loc).Format(timeFormat)
+
+	var logHeader string
+
+	if sourceID == "" {
+		logHeader = fmt.Sprintf("%s [%s]", timeString, sourceName)
+	} else {
+		logHeader = fmt.Sprintf("%s [%s/%s]", timeString, sourceName, sourceID)
+	}
+
+	// coloredLogHeader := terminal.LogSysHeaderColor(logHeader)
+
+	// // Calculate padding
+	// longestHeader := fmt.Sprintf("%s  [HEALTH/10] ", timeFormat)
+	// expectedHeaderLength := utf8.RuneCountInString(longestHeader)
+	// headerPadding := strings.Repeat(" ", max(0, expectedHeaderLength-utf8.RuneCountInString(logHeader)))
+
+	// logHeader += headerPadding
+	// coloredLogHeader += headerPadding
+
+	// msgText := logMsg.Message()
+	// msgText = strings.TrimRight(msgText, "\r\n")
+
+	// msgLines := strings.Split(msgText, "\n")
+	// contentPadding := strings.Repeat(" ", utf8.RuneCountInString(logHeader))
+	// coloringFunc := terminal.LogStdoutColor
+
+	// logType := logMsg.Type()
+	// if logType == "ERR" {
+	//     coloringFunc = terminal.LogStderrColor
+	// } else {
+	//     logType = "OUT"
+	// }
+
+	// logContent := fmt.Sprintf("%s %s", logType, msgLines[0])
+	// for _, msgLine := range msgLines[1:] {
+	//     logContent = fmt.Sprintf("%s\n%s%s", logContent, contentPadding, msgLine)
+	// }
+
+	// logContent = coloringFunc(logContent)
+
+	// return fmt.Sprintf("%s%s", coloredLogHeader, logContent)
+	return fmt.Sprintf("%s", logHeader)
 }
--- cf/api/logs/log_cache_message_test.go
+++ cf/api/logs/log_cache_message_test.go
@@ -1,16 +1,48 @@
 package logs_test
 
 import (
+	"time"
+
+	"code.cloudfoundry.org/cli/actor/sharedaction"
+	"code.cloudfoundry.org/cli/cf/api/logs"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("log messages", func() {
+	message := *sharedaction.NewLogMessage(
+		"some-message",
+		"OUT",
+		time.Unix(0, 0),
+		"STG",
+		"some-source-instance",
+	)
+	logCacheMessage := logs.NewLogCacheMessage(message)
 
 	Describe("ToSimpleLog", func() {
 		It("returns the message", func() {
-			Expect(true).To(Equal(false))
+			Expect(logCacheMessage.ToSimpleLog()).To(Equal("some-message"))
 		})
 	})
 
+	Describe("GetSourceName", func() {
+		It("returns the message", func() {
+			// TODO: reconsider whether this should return SourceType or SourceInstance
+			Expect(logCacheMessage.GetSourceName()).To(Equal("STG"))
+		})
+	})
+
+	Describe("ToLog", func() {
+		// timezone handling
+		// sourceID sets the logHeader format
+		// coloring of log header on logType value
+		// trimming of message text
+		// padding calculation
+		// multi-line log messages
+		// handles both STDERR and STDOUT differently
+
+		It("correctly formats the log", func() {
+			Expect(logCacheMessage.ToLog(time.UTC)).To(Equal("1970-01-01T00:00:00.00+0000 [STG/0] OUT some-message"))
+		})
+	})
 })
