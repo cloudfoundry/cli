07138534626073510e32fc63ceaefc82fb8bdc03
George Blue <gblue@pivotal.io>
2020-04-09 23:46:23 +0100

    Test: specify buildpack on test app for legacy envs
    Some of the legacy envs have a Go buildpack that's so old it will not
    run the test broker, so we should specify that it should use the
    latest buildpack.

--- integration/helpers/servicebrokerstub/app_deploy.go
+++ integration/helpers/servicebrokerstub/app_deploy.go
@@ -48,6 +48,7 @@ func ensureAppIsPushed() {
 			"push", appName(),
 			"-p", pathToApp,
 			"-m", defaultMemoryLimit,
+			"-b ", "https://github.com/cloudfoundry/go-buildpack.git", // Some legacy envs have buildpack that's too old
 		)
 		session.Wait()
 		return session.ExitCode() == 0
