caa1177fb308dd673038444137d7ffa4fb3f7d9e
Reid Mitchell <rmitchell@pivotal.io>
2020-04-14 15:06:52 -0700

    v7: Remove unnecessary unit test for packages command
    This was asserting on the absence of a warning that was removed a long
    time ago. The test wasn't really giving us anything (and was
    mis-titled).
    
    Authored-by: Reid Mitchell <rmitchell@pivotal.io>

--- command/v7/packages_command_test.go
+++ command/v7/packages_command_test.go
@@ -65,10 +65,6 @@ var _ = Describe("packages Command", func() {
 		executeErr = cmd.Execute(nil)
 	})
 
-	It("displays the experimental warning", func() {
-		Expect(testUI.Err).NotTo(Say("This command is in EXPERIMENTAL stage and may change without notice"))
-	})
-
 	When("checking target fails", func() {
 		BeforeEach(func() {
 			fakeSharedActor.CheckTargetReturns(actionerror.NoOrganizationTargetedError{BinaryName: binaryName})
