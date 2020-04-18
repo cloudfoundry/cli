865594dc6ef0a073492f0966fec149896851a36e
Reid Mitchell <rmitchell@pivotal.io>
2020-04-17 19:09:58 -0700

    v7: Minor style tweaks to `logout`
    - Display username with flavor (color) before running
    - Remove Setup method, since it can use the method inherited from
    BaseCommand
    
    Authored-by: Reid Mitchell <rmitchell@pivotal.io>

--- command/v7/logout_command.go
+++ command/v7/logout_command.go
@@ -1,21 +1,9 @@
 package v7
 
-import (
-	"code.cloudfoundry.org/cli/command"
-)
-
 type LogoutCommand struct {
 	BaseCommand
 
 	usage interface{} `usage:"CF_NAME logout"`
-}
-
-func (cmd *LogoutCommand) Setup(config command.Config, ui command.UI) error {
-	err := cmd.BaseCommand.Setup(config, ui)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func (cmd LogoutCommand) Execute(args []string) error {
@@ -24,7 +12,7 @@ func (cmd LogoutCommand) Execute(args []string) error {
 		return err
 	}
 
-	cmd.UI.DisplayText("Logging out {{.Username}}...",
+	cmd.UI.DisplayTextWithFlavor("Logging out {{.Username}}...",
 		map[string]interface{}{
 			"Username": user.Name,
 		})
