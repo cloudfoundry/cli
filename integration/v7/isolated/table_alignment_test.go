d81be05d6ab17493ccbb5f85a07c354af4674227
Reid Mitchell <rmitchell@pivotal.io>
2020-04-20 15:17:22 -0700

    v7: Temporarily fix table_alignment_test by using untranslated string
    See TODO comment in this commit.
    
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>
    Co-authored-by: Brian Cunnie <bcunnie@pivotal.io>

--- integration/v7/isolated/table_alignment_test.go
+++ integration/v7/isolated/table_alignment_test.go
@@ -45,7 +45,9 @@ var _ = Describe("table alignment", func() {
 			username, _ := helpers.GetCredentials()
 			session := helpers.CFWithEnv(map[string]string{"LANG": "ja-JP.utf8"}, "target")
 			Eventually(session).Should(Say("API エンドポイント:   %s", apiURL))
-			Eventually(session).Should(Say(`API バージョン:      [\d.]+`))
+			// TODO: "version" here should be translated for all languages. We have translation resources for "api version"
+			// (lowercase), which is what this said in V6, but we don't yet have them for "API version" (uppercase).
+			Eventually(session).Should(Say(`API version:      [\d.]+`))
 			Eventually(session).Should(Say("ユーザー:            %s", username))
 			Eventually(session).Should(Say("組織:                %s", ReadOnlyOrg))
 			Eventually(session).Should(Say("スペース:            %s", ReadOnlySpace))
