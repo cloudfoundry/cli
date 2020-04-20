f3427d8c8ccd2c5f5f82d378d2fa665d7448f7c9
Reid Mitchell <rmitchell@pivotal.io>
2020-04-20 10:46:02 -0700

    🐞: V7: `API` is properly capitalized for multibyte test
    fixes <https://ci.cli.fun/teams/main/pipelines/V7/jobs/integration-linux/builds/1410>:
    ```
      No future change is possible.  Bailing out early after 0.420s.
      Got stuck at:
          API エンドポイント:   https://api.nopa.cli.fun
          API version:          3.83.0
          ユーザー:             admin
          組織:                 INTEGRATION-ORG-30fc628b-e803-4470-65b7-911ac3e33337
          スペース:             INTEGRATION-SPACE-6ff2af20-830b-4765-5f01-95634778a871
    
      Waiting for:
          API エンドポイント   https://api.nopa.cli.fun
    ```
    
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>
    Co-authored-by: Brian Cunnie <bcunnie@pivotal.io>

---
+++ integration/v7/isolated/table_alignment_test.go
@@ -0,0 +1,55 @@
+package isolated
+
+import (
+	"code.cloudfoundry.org/cli/integration/helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("table alignment", func() {
+	BeforeEach(func() {
+		helpers.LoginCF()
+	})
+
+	When("output is in English", func() {
+		BeforeEach(func() {
+			helpers.LoginCF()
+			helpers.TargetOrgAndSpace(ReadOnlyOrg, ReadOnlySpace)
+		})
+
+		// Developer note: The spacing in this test is significant and explicit. Do
+		// not replace with a regex.
+		It("aligns the table correctly", func() {
+			username, _ := helpers.GetCredentials()
+			session := helpers.CF("target")
+			Eventually(session).Should(Say("API endpoint:   %s", apiURL))
+			Eventually(session).Should(Say(`API version:    [\d.]+`))
+			Eventually(session).Should(Say("user:           %s", username))
+			Eventually(session).Should(Say("org:            %s", ReadOnlyOrg))
+			Eventually(session).Should(Say("space:          %s", ReadOnlySpace))
+			Eventually(session).Should(Exit(0))
+		})
+	})
+
+	When("output is in language with multibyte characters", func() {
+		BeforeEach(func() {
+			helpers.LoginCF()
+			helpers.TargetOrgAndSpace(ReadOnlyOrg, ReadOnlySpace)
+		})
+
+		// Developer note: The spacing in this test is significant and explicit. Do
+		// not replace with a regex.
+		It("aligns the table correctly", func() {
+			username, _ := helpers.GetCredentials()
+			session := helpers.CFWithEnv(map[string]string{"LANG": "ja-JP.utf8"}, "target")
+			Eventually(session).Should(Say("API エンドポイント:   %s", apiURL))
+			Eventually(session).Should(Say(`API バージョン:      [\d.]+`))
+			Eventually(session).Should(Say("ユーザー:            %s", username))
+			Eventually(session).Should(Say("組織:                %s", ReadOnlyOrg))
+			Eventually(session).Should(Say("スペース:            %s", ReadOnlySpace))
+			Eventually(session).Should(Exit(0))
+		})
+	})
+})
