e462162c675c6e5daf74ab697a711aade6f99002
Reid Mitchell <rmitchell@pivotal.io>
2020-04-20 16:18:50 -0700

    🐞: Table alignment test has correct alignment
    Ten spaces not six.
    
    fixes <https://ci.cli.fun/teams/main/pipelines/V7/jobs/integration-linux/builds/1414>:
    ```
      Got stuck at:
    
          API version:          3.83.0
          ユーザー:             admin
          組織:                 INTEGRATION-ORG-48e3ff4f-385d-4922-587d-c4a9bad4af2a
          スペース:             INTEGRATION-SPACE-aa991904-ec1d-4513-49e9-5159d366d612
    
      Waiting for:
          API version:      [\d.]+
    
      /tmp/build/06eff510/go/src/code.cloudfoundry.org/cli/integration/v7/isolated/table_alignment_test.go:50
    ```
    
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>
    Co-authored-by: Brian Cunnie <bcunnie@pivotal.io>

--- integration/v7/isolated/table_alignment_test.go
+++ integration/v7/isolated/table_alignment_test.go
@@ -47,7 +47,7 @@ var _ = Describe("table alignment", func() {
 			Eventually(session).Should(Say("API エンドポイント:   %s", apiURL))
 			// TODO: "version" here should be translated for all languages. We have translation resources for "api version"
 			// (lowercase), which is what this said in V6, but we don't yet have them for "API version" (uppercase).
-			Eventually(session).Should(Say(`API version:      [\d.]+`))
+			Eventually(session).Should(Say(`API version:          [\d.]+`))
 			Eventually(session).Should(Say("ユーザー:            %s", username))
 			Eventually(session).Should(Say("組織:                %s", ReadOnlyOrg))
 			Eventually(session).Should(Say("スペース:            %s", ReadOnlySpace))
