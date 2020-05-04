eb56e334cde84d5d4aca5acb25fc986a6b291cb8
Sarah Weinstein <sweinstein@pivotal.io>
2020-05-04 16:44:24 -0700

    Remove useless unit test
    Co-authored-by: Sarah Weinstein <sweinstein@pivotal.io>
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- command/v7/start_command_test.go
+++ command/v7/start_command_test.go
@@ -75,10 +75,6 @@ var _ = Describe("start Command", func() {
 		executeErr = cmd.Execute(nil)
 	})
 
-	It("displays the experimental warning", func() {
-		Expect(testUI.Err).NotTo(Say("This command is in EXPERIMENTAL stage and may change without notice"))
-	})
-
 	When("checking target fails", func() {
 		BeforeEach(func() {
 			fakeSharedActor.CheckTargetReturns(actionerror.NoOrganizationTargetedError{BinaryName: binaryName})
