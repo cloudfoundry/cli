9b7d89fb8d441de71ec23639ef25c1d46f49547b
Jenna Goldstrich <jgoldstrich@pivotal.io>
2020-05-12 00:02:17 +0000

    Add work-around for legacy environments log cache url for v6 login command actor
    [#171221394](https://www.pivotaltracker.com/story/show/171221394)

--- actor/v3action/target.go
+++ actor/v3action/target.go
@@ -1,6 +1,8 @@
 package v3action
 
 import (
+	"strings"
+
 	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv3"
 	"code.cloudfoundry.org/cli/util/configv3"
 )
@@ -18,13 +20,20 @@ func (actor Actor) SetTarget(settings TargetSettings) (Warnings, error) {
 	if err != nil {
 		return Warnings(warnings), err
 	}
+	//TODO Remove this condition when earliest supportest CAPI is 1.87.0
+	//We have to do this because the current legacy supported CAPI version as of 2020 does not display the log cache url, this will break if a foundation on legacy CAPI have non-standard logcache urls
+
+	logCacheUrl := info.LogCache()
+	if logCacheUrl == "" {
+		logCacheUrl = strings.Replace(settings.URL, "api", "log-cache", 1)
+	}
 	actor.Config.SetTargetInformation(configv3.TargetInformationArgs{
 		Api:               settings.URL,
 		ApiVersion:        info.CloudControllerAPIVersion(),
 		Auth:              info.UAA(),
 		MinCLIVersion:     "", // Oldest supported V3 version should be OK
 		Doppler:           info.Logging(),
-		LogCache:          info.LogCache(),
+		LogCache:          logCacheUrl,
 		Routing:           info.Routing(),
 		SkipSSLValidation: settings.SkipSSLValidation,
 	})
