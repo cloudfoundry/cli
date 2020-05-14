f86a4a4f17297bdbc54aefb9076ec39bd595fe13
Nick Webb <nwebb@pivotal.io>
2020-05-14 20:53:19 +0000

    Introduce a WIP windows unit pipeline
    - This pipeline is pulling from a WIP branch of the CLI and will allow
    us to quickly iterate on fixes for the windows flake without affecting
    master
    - cli resource is set to trigger: false to avoid running these units at
    the same time as v6 units (not sure if our windows worker is capable of
    this)
    
    [#172687176](https://www.pivotaltracker.com/story/show/172687176)

---
+++ ci/cli-windows-wip/tasks/units-windows.bat
@@ -0,0 +1,14 @@
+SET GOPATH=%CD%\gopath
+SET PATH=C:\Go\bin;C:\Program Files\Git\cmd\;%GOPATH%\bin;%PATH%
+
+cd %GOPATH%\src\code.cloudfoundry.org\cli
+
+powershell -command set-executionpolicy remotesigned
+
+go version
+
+go get -u github.com/onsi/ginkgo/ginkgo
+
+ginkgo version
+
+ginkgo -r -randomizeAllSpecs -randomizeSuites -skipPackage integration -flakeAttempts=2 .
