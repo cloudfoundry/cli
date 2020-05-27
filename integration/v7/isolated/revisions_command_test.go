7f1246c4ebc1028967ce2e7e8bbc4c51e46ef8b5
James Palmer <jpalmer@pivotal.io>
2020-05-27 22:34:12 +0000

    Add integration test for revisions command
    [#171722596](https://www.pivotaltracker.com/story/show/171722596)
    
    Signed-off-by: Jenna Goldstrich <jgoldstrich@pivotal.io>

---
+++ integration/v7/isolated/revisions_command_test.go
@@ -0,0 +1,97 @@
+package isolated
+
+import (
+	"regexp"
+
+	. "code.cloudfoundry.org/cli/cf/util/testhelpers/matchers"
+	"code.cloudfoundry.org/cli/integration/helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("revisions command", func() {
+	var (
+		orgName   string
+		spaceName string
+		appName   string
+		username  string
+	)
+
+	BeforeEach(func() {
+		username, _ = helpers.GetCredentials()
+		orgName = helpers.NewOrgName()
+		spaceName = helpers.NewSpaceName()
+		appName = helpers.PrefixedRandomName("app")
+	})
+
+	Describe("help", func() {
+		When("--help flag is set", func() {
+			It("appears in cf help -a", func() {
+				session := helpers.CF("help", "-a")
+				Eventually(session).Should(Exit(0))
+				Expect(session).To(HaveCommandInCategoryWithDescription("revisions", "APPS", "Lists revisions for an app"))
+			})
+
+			It("Displays command usage to output", func() {
+				session := helpers.CF("revisions", "--help")
+				Eventually(session).Should(Say("NAME:"))
+				Eventually(session).Should(Say("revisions - Lists revisions for an app"))
+				Eventually(session).Should(Say("USAGE:"))
+				Eventually(session).Should(Say("cf revisions APP_NAME"))
+				Eventually(session).Should(Exit(0))
+			})
+		})
+	})
+
+	When("targetting and org and space", func() {
+		BeforeEach(func() {
+			helpers.SetupCF(orgName, spaceName)
+		})
+
+		AfterEach(func() {
+			helpers.QuickDeleteOrg(orgName)
+		})
+
+		When("An app name is not provided", func() {
+			It("Returns the incorrect usage text and help information", func() {
+				session := helpers.CF("revisions")
+				Eventually(session).Should(Exit(1))
+				Expect(session.Err.Contents()).Should(ContainSubstring("Incorrect Usage: the required argument `APP_NAME` was not provided"))
+				Expect(session).Should(Say("revisions - Lists revisions for an app"))
+			})
+		})
+
+		When("the provided app does not exist", func() {
+			It("properly displays app not found error", func() {
+				fakeAppName := helpers.PrefixedRandomName("test-fake-app")
+				session := helpers.CF("revisions", fakeAppName)
+				Eventually(session).Should(Exit(1))
+				Expect(session).Should(Say(regexp.QuoteMeta(`Getting revisions for app %s in org %s / space %s as %s...`), fakeAppName, orgName, spaceName, username))
+				Expect(session.Err).Should(Say(regexp.QuoteMeta(`App '%s' not found`), fakeAppName))
+				Expect(session).To(Say("FAILED"))
+			})
+		})
+
+		When("An app has been pushed several times", func() {
+			BeforeEach(func() {
+				helpers.WithHelloWorldApp(func(appDir string) {
+					Eventually(helpers.CF("push", appName, "-p", appDir)).Should(Exit(0))
+					Eventually(helpers.CF("push", appName, "-p", appDir)).Should(Exit(0))
+				})
+			})
+			It("Retrieves the revisions", func() {
+				session := helpers.CF("revisions", appName)
+				Eventually(session).Should(Exit(0))
+				Expect(session).Should(Say(regexp.QuoteMeta(`Getting revisions for app %s in org %s / space %s as %s...`), appName, orgName, spaceName, username))
+				Expect(session).Should(Say("OK"))
+
+				Expect(session.Out.Contents()).Should(ContainSubstring("Initial revision"))
+				Expect(session.Out.Contents()).Should(ContainSubstring("New droplet deployed"))
+
+			})
+		})
+
+	})
+})
