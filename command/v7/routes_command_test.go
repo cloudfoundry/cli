d392e7b843dc8d640f59ac9ef5048ae02d99033e
Sarah Weinstein <sweinstein@pivotal.io>
2020-06-03 13:24:44 -0700

    Fix ordering of table headers for routes command unit test expectation
    [finishes #172510832]
    
    Authored-by: Sarah Weinstein <sweinstein@pivotal.io>

--- command/v7/routes_command_test.go
+++ command/v7/routes_command_test.go
@@ -29,7 +29,7 @@ var _ = Describe("routes Command", func() {
 		binaryName      string
 	)
 
-	const tableHeaders = `space\s+host\s+domain\s+path\s+port\s+protocol\s+apps`
+	const tableHeaders = `space\s+host\s+domain\s+port\s+path\s+protocol\s+apps`
 
 	BeforeEach(func() {
 		testUI = ui.NewTestUI(nil, NewBuffer(), NewBuffer())
