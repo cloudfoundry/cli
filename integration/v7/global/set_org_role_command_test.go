a92aee814ef42e5c0c5b80fccbc76800521d4ee4
Reid Mitchell <rmitchell@pivotal.io>
2020-06-08 09:22:54 -0700

    Update expected error message for set-org/space-role
    Some integration tests were failing due to a recent change where we were
    not defaulting the origin to `uaa` in these commands.
    
    Authored-by: Reid Mitchell <rmitchell@pivotal.io>

--- integration/v7/global/set_org_role_command_test.go
+++ integration/v7/global/set_org_role_command_test.go
@@ -29,7 +29,7 @@ var _ = Describe("set-org-role command", func() {
 			It("prints the error from UAA and exits 1", func() {
 				session := helpers.CF("set-org-role", "not-exists", orgName, "OrgAuditor")
 				Eventually(session).Should(Say("FAILED"))
-				Eventually(session.Err).Should(Say("No user exists with the username 'not-exists' and origin 'uaa'."))
+				Eventually(session.Err).Should(Say("No user exists with the username 'not-exists'."))
 				Eventually(session).Should(Exit(1))
 			})
 		})
