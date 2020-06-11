92773fa22b498e766548d0dbbefe549df8ec37b3
George Blue <blgm@users.noreply.github.com>
2020-06-11 11:47:00 +0100

    refactor: use resources pattern for service instances (#1951)
    * refactor: use resources pattern for service instances
    
    [#172178942]
    
    * refactor: act on comments from code review
    
    [#172178942]

--- actor/v7action/service_instance.go
+++ actor/v7action/service_instance.go
@@ -3,26 +3,25 @@ package v7action
 import (
 	"code.cloudfoundry.org/cli/actor/actionerror"
 	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv3"
+	"code.cloudfoundry.org/cli/resources"
 )
 
-type ServiceInstance ccv3.ServiceInstance
-
-func (actor Actor) GetServiceInstanceByNameAndSpace(serviceInstanceName string, spaceGUID string) (ServiceInstance, Warnings, error) {
+func (actor Actor) GetServiceInstanceByNameAndSpace(serviceInstanceName string, spaceGUID string) (resources.ServiceInstance, Warnings, error) {
 	serviceInstances, warnings, err := actor.CloudControllerClient.GetServiceInstances(
 		ccv3.Query{Key: ccv3.NameFilter, Values: []string{serviceInstanceName}},
 		ccv3.Query{Key: ccv3.SpaceGUIDFilter, Values: []string{spaceGUID}},
 	)
 
 	if err != nil {
-		return ServiceInstance{}, Warnings(warnings), err
+		return resources.ServiceInstance{}, Warnings(warnings), err
 	}
 
 	if len(serviceInstances) == 0 {
-		return ServiceInstance{}, Warnings(warnings), actionerror.ServiceInstanceNotFoundError{Name: serviceInstanceName}
+		return resources.ServiceInstance{}, Warnings(warnings), actionerror.ServiceInstanceNotFoundError{Name: serviceInstanceName}
 	}
 
 	//Handle multiple serviceInstances being returned as GetServiceInstances arnt filtered by space
-	return ServiceInstance(serviceInstances[0]), Warnings(warnings), nil
+	return serviceInstances[0], Warnings(warnings), nil
 }
 
 func (actor Actor) UnshareServiceInstanceByServiceInstanceAndSpace(serviceInstanceGUID string, sharedToSpaceGUID string) (Warnings, error) {
