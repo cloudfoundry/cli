9a6c1e0032f1a421ced863e8fa0ad5672e616cea
George Blue <gblue@pivotal.io>
2020-06-24 17:19:25 +0100

    v8(service): service command show user-provided info
    [#171842806](https://www.pivotaltracker.com/story/show/171842806)

--- command/v7/service_command.go
+++ command/v7/service_command.go
@@ -1,6 +1,10 @@
 package v7
 
-import "code.cloudfoundry.org/cli/command/flag"
+import (
+	"code.cloudfoundry.org/cli/actor/v7action"
+	"code.cloudfoundry.org/cli/command/flag"
+	"code.cloudfoundry.org/cli/resources"
+)
 
 type ServiceCommand struct {
 	BaseCommand
@@ -9,6 +13,8 @@ type ServiceCommand struct {
 	ShowGUID        bool                 `long:"guid" description:"Retrieve and display the given service's guid. All other output for the service is suppressed."`
 	usage           interface{}          `usage:"CF_NAME service SERVICE_INSTANCE"`
 	relatedCommands interface{}          `related_commands:"bind-service, rename-service, update-service"`
+
+	serviceInstance resources.ServiceInstance
 }
 
 func (cmd ServiceCommand) Execute(args []string) error {
@@ -16,12 +22,65 @@ func (cmd ServiceCommand) Execute(args []string) error {
 		return err
 	}
 
-	serviceInstance, warnings, err := cmd.Actor.GetServiceInstanceByNameAndSpace(cmd.RequiredArgs.ServiceInstance, cmd.Config.TargetedSpace().GUID)
+	var (
+		warnings v7action.Warnings
+		err      error
+	)
+
+	cmd.serviceInstance, warnings, err = cmd.Actor.GetServiceInstanceByNameAndSpace(cmd.RequiredArgs.ServiceInstance, cmd.Config.TargetedSpace().GUID)
 	cmd.UI.DisplayWarnings(warnings)
 	if err != nil {
 		return err
 	}
 
-	cmd.UI.DisplayText(serviceInstance.GUID)
+	switch {
+	case cmd.ShowGUID:
+		return cmd.displayGUID()
+	case cmd.serviceInstance.Type == resources.UserProvidedServiceInstance:
+		return cmd.displayUPSI()
+	default:
+		return nil
+	}
+}
+
+func (cmd ServiceCommand) displayGUID() error {
+	cmd.UI.DisplayText(cmd.serviceInstance.GUID)
+	return nil
+}
+
+func (cmd ServiceCommand) displayUPSI() error {
+	if err := cmd.displayInto(); err != nil {
+		return err
+	}
+
+	table := [][]string{
+		{cmd.UI.TranslateText("name:"), cmd.serviceInstance.Name},
+		{cmd.UI.TranslateText("guid:"), cmd.serviceInstance.GUID},
+		{cmd.UI.TranslateText("type:"), string(cmd.serviceInstance.Type)},
+		{cmd.UI.TranslateText("tags:"), cmd.serviceInstance.Tags.String()},
+		{cmd.UI.TranslateText("route service url:"), cmd.serviceInstance.RouteServiceURL.String()},
+		{cmd.UI.TranslateText("syslog drain url:"), cmd.serviceInstance.SyslogDrainURL.String()},
+	}
+
+	cmd.UI.DisplayKeyValueTable("", table, 3)
+	return nil
+}
+
+func (cmd ServiceCommand) displayInto() error {
+	user, err := cmd.Config.CurrentUser()
+	if err != nil {
+		return err
+	}
+
+	cmd.UI.DisplayTextWithFlavor(
+		"Showing info of service {{.ServiceInstanceName}} in org {{.OrgName}} / space {{.SpaceName}} as {{.Username}}...",
+		map[string]interface{}{
+			"ServiceInstanceName": cmd.serviceInstance.Name,
+			"OrgName":             cmd.Config.TargetedOrganization().Name,
+			"SpaceName":           cmd.Config.TargetedSpace().Name,
+			"Username":            user.Name,
+		},
+	)
+	cmd.UI.DisplayNewline()
 	return nil
 }
