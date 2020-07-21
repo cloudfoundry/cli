fd0841e002d872eaf017913da48c1a22860ad472
George Blue <blgm@users.noreply.github.com>
2020-07-21 10:33:57 +0100

    v8(services): add update-service command help (#1986)
    [#173416530](https://www.pivotaltracker.com/story/show/173416530)

--- integration/v7/isolated/update_service_command_test.go
+++ integration/v7/isolated/update_service_command_test.go
@@ -6,12 +6,13 @@ import (
 	"code.cloudfoundry.org/cli/integration/helpers"
 	"code.cloudfoundry.org/cli/integration/helpers/servicebrokerstub"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
 )
 
-var _ = Describe("update-service command", func() {
+var _ = Describe("update-service command (original)", func() {
 	Describe("help", func() {
 		When("--help flag is set", func() {
 			It("displays command usage to output", func() {
@@ -276,6 +277,111 @@ var _ = Describe("update-service command", func() {
 					})
 				})
 			})
+		})
+	})
+})
+
+var _ = Describe("update-service command", func() {
+	const command = "v3-update-service"
+
+	Describe("help", func() {
+		matchHelpMessage := SatisfyAll(
+			Say(`NAME:\n`),
+			Say(`\s+%s - Update a service instance\n`, command),
+			Say(`\n`),
+			Say(`USAGE:\n`),
+			Say(`\s+cf update-service SERVICE_INSTANCE \[-p NEW_PLAN\] \[-c PARAMETERS_AS_JSON\] \[-t TAGS\] \[--upgrade\]\n`),
+			Say(`\n`),
+			Say(`\s+Optionally provide service-specific configuration parameters in a valid JSON object in-line:\n`),
+			Say(`\s+cf update-service SERVICE_INSTANCE -c '{\"name\":\"value\",\"name\":\"value\"}'\n`),
+			Say(`\s+Optionally provide a file containing service-specific configuration parameters in a valid JSON object\.\n`),
+			Say(`\s+The path to the parameters file can be an absolute or relative path to a file:\n`),
+			Say(`\s+cf update-service SERVICE_INSTANCE -c PATH_TO_FILE\n`),
+			Say(`\s+Example of valid JSON object:\n`),
+			Say(`\s+{\n`),
+			Say(`\s+\"cluster_nodes\": {\n`),
+			Say(`\s+\"count\": 5,\n`),
+			Say(`\s+\"memory_mb\": 1024\n`),
+			Say(`\s+}\n`),
+			Say(`\s+}\n`),
+			Say(`\s+ Optionally provide a list of comma-delimited tags that will be written to the VCAP_SERVICES environment variable for any bound applications.\n`),
+			Say(`EXAMPLES:\n`),
+			Say(`\s+cf update-service mydb -p gold\n`),
+			Say(`\s+cf update-service mydb -c '{\"ram_gb\":4}'\n`),
+			Say(`\s+cf update-service mydb -c ~/workspace/tmp/instance_config.json\n`),
+			Say(`\s+cf update-service mydb -t "list, of, tags"\n`),
+			Say(`\s+cf update-service mydb --upgrade\n`),
+			Say(`\s+cf update-service mydb --upgrade --force\n`),
+			Say(`OPTIONS:\n`),
+			Say(`\s+-c\s+Valid JSON object containing service-specific configuration parameters, provided either in-line or in a file\. For a list of supported configuration parameters, see documentation for the particular service offering\.\n`),
+			Say(`\s+-p\s+Change service plan for a service instance\n`),
+			Say(`\s+-t\s+User provided tags\n`),
+			Say(`\s+--upgrade, -u\s+Upgrade the service instance to the latest version of the service plan available. It cannot be combined with flags: -c, -p, -t.\n`),
+			Say(`\s+--force, -f\s+Force the upgrade to the latest available version of the service plan. It can only be used with: -u, --upgrade.\n`),
+			Say(`SEE ALSO:\n`),
+			Say(`\s+rename-service, services, update-user-provided-service\n`),
+		)
+
+		When("the --help flag is specified", func() {
+			It("exits successfully and prints the help message", func() {
+				session := helpers.CF(command, "--help")
+
+				Eventually(session).Should(Exit(0))
+				Expect(session.Out).To(matchHelpMessage)
+				Expect(session.Err.Contents()).To(BeEmpty())
+			})
+		})
+
+		When("the service instance name is omitted", func() {
+			It("fails and prints the help message", func() {
+				session := helpers.CF(command)
+
+				Eventually(session).Should(Exit(1))
+				Expect(session.Out).To(matchHelpMessage)
+				Expect(session.Err).To(Say("Incorrect Usage: the required argument `SERVICE_INSTANCE` was not provided\n"))
+			})
+		})
+
+		When("an extra parameter is provided", func() {
+			It("fails and prints the help message", func() {
+				session := helpers.CF(command, "service-instance-name", "invalid-extra-parameter")
+
+				Eventually(session).Should(Exit(1))
+				Expect(session.Out).To(matchHelpMessage)
+				Expect(session.Err).To(Say(`Incorrect Usage: unexpected argument "invalid-extra-parameter"`))
+			})
+		})
+
+		When("an extra flag is provided", func() {
+			It("fails and prints the help message", func() {
+				session := helpers.CF(command, "service-instance-name", "--invalid")
+
+				Eventually(session).Should(Exit(1))
+				Expect(session.Out).To(matchHelpMessage)
+				Expect(session.Err).To(Say("Incorrect Usage: unknown flag `invalid'"))
+			})
+		})
+
+		When("a flag is provided without a value", func() {
+			DescribeTable(
+				"it fails and prints a help message",
+				func(flag string) {
+					session := helpers.CF(command, "service-instance-name", flag)
+
+					Eventually(session).Should(Exit(1))
+					Expect(session.Out).To(matchHelpMessage)
+					Expect(session.Err).To(Say("Incorrect Usage: expected argument for flag `%s'", flag))
+				},
+				Entry("configuration", "-c"),
+				Entry("plan", "-p"),
+				Entry("tags", "-t"),
+			)
+		})
+	})
+
+	When("the environment is not setup correctly", func() {
+		It("fails with the appropriate errors", func() {
+			helpers.CheckEnvironmentTargetedCorrectly(true, true, ReadOnlyOrg, command, "service-instance-name")
 		})
 	})
 })
