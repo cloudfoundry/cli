449edb65e6023a77ddfcd022d0f0b0736967f106
Felisia Martini <fmartini@pivotal.io>
2020-08-26 15:50:07 +0100

    v8(services): share-service can successfullt share an SI
    [#173658277](https://www.pivotaltracker.com/story/show/173658277)

--- integration/v7/isolated/share_service_command_test.go
+++ integration/v7/isolated/share_service_command_test.go
@@ -85,7 +85,7 @@ var _ = Describe("share-service command", func() {
 		})
 	})
 
-	Describe("command parameters are invalid", func() {
+	Context("share-service command is valid", func() {
 		var (
 			orgName  string
 			username string
@@ -103,19 +103,88 @@ var _ = Describe("share-service command", func() {
 			helpers.QuickDeleteOrg(orgName)
 		})
 
-		Context("service instance cannot be retrieved", func() {
-			It("fails with an error", func() {
-				session := helpers.CF(shareServiceCommand, serviceInstanceName, shareToSpaceName)
-				Eventually(session).Should(Exit(1))
-				Expect(session.Out).To(SatisfyAll(
-					Say("Sharing service instance %s to org %s / space %s as %s...", serviceInstanceName, orgName, shareToSpaceName, username),
-					Say("FAILED"),
-				))
-				Expect(session.Err).To(Say("Service instance %s not found", serviceInstanceName))
+		Describe("command parameters are invalid", func() {
+
+			Context("service instance cannot be retrieved", func() {
+				It("fails with an error", func() {
+					session := helpers.CF(shareServiceCommand, serviceInstanceName, shareToSpaceName)
+					Eventually(session).Should(Exit(1))
+					Expect(session.Out).To(SatisfyAll(
+						Say("Sharing service instance %s to org %s / space %s as %s...", serviceInstanceName, orgName, shareToSpaceName, username),
+						Say("FAILED"),
+					))
+					Expect(session.Err).To(Say("Service instance %s not found", serviceInstanceName))
+				})
+			})
+
+			Context("service instance exists", func() {
+				var broker *servicebrokerstub.ServiceBrokerStub
+
+				BeforeEach(func() {
+					broker = servicebrokerstub.New().Create().EnableServiceAccess()
+
+					serviceInstanceName = helpers.NewServiceInstanceName()
+					helpers.CreateManagedServiceInstance(
+						broker.FirstServiceOfferingName(),
+						broker.FirstServicePlanName(),
+						serviceInstanceName,
+					)
+
+					shareToSpaceName = helpers.NewSpaceName()
+					shareToOrgName = helpers.NewOrgName()
+				})
+
+				AfterEach(func() {
+					broker.Forget()
+				})
+
+				Context("space cannot be retrieved in targeted org", func() {
+					It("fails with an error", func() {
+						session := helpers.CF(shareServiceCommand, serviceInstanceName, shareToSpaceName)
+						Eventually(session).Should(Exit(1))
+						Expect(session.Out).To(SatisfyAll(
+							Say("Sharing service instance %s to org %s / space %s as %s...", serviceInstanceName, orgName, shareToSpaceName, username),
+							Say("FAILED"),
+						))
+						Eventually(session.Err).Should(Say("Space '%s' not found.", shareToSpaceName))
+					})
+				})
+
+				Context("space cannot be retrieved in specified org", func() {
+					BeforeEach(func() {
+						helpers.CreateOrg(shareToOrgName)
+					})
+
+					AfterEach(func() {
+						helpers.QuickDeleteOrg(shareToOrgName)
+					})
+
+					It("fails with an error", func() {
+						session := helpers.CF(shareServiceCommand, serviceInstanceName, shareToSpaceName, "-o", shareToOrgName)
+						Eventually(session).Should(Exit(1))
+						Expect(session.Out).To(SatisfyAll(
+							Say("Sharing service instance %s to org %s / space %s as %s...", serviceInstanceName, shareToOrgName, shareToSpaceName, username),
+							Say("FAILED"),
+						))
+						Eventually(session.Err).Should(Say("Space '%s' not found.", shareToSpaceName))
+					})
+				})
+
+				Context("specified organization cannot be retrieved", func() {
+					It("fails with an error", func() {
+						session := helpers.CF(shareServiceCommand, serviceInstanceName, shareToSpaceName, "-o", shareToOrgName)
+						Eventually(session).Should(Exit(1))
+						Expect(session.Out).To(SatisfyAll(
+							Say("Sharing service instance %s to org %s / space %s as %s...", serviceInstanceName, shareToOrgName, shareToSpaceName, username),
+							Say("FAILED"),
+						))
+						Eventually(session.Err).Should(Say("Organization '%s' not found.", shareToOrgName))
+					})
+				})
 			})
 		})
 
-		Context("service instance exists", func() {
+		Describe("when sharing the service instance succeeds", func() {
 			var broker *servicebrokerstub.ServiceBrokerStub
 
 			BeforeEach(func() {
@@ -127,56 +196,74 @@ var _ = Describe("share-service command", func() {
 					broker.FirstServicePlanName(),
 					serviceInstanceName,
 				)
-
-				shareToSpaceName = helpers.NewSpaceName()
-				shareToOrgName = helpers.NewOrgName()
 			})
 
 			AfterEach(func() {
 				broker.Forget()
 			})
 
-			Context("space cannot be retrieved in targeted org", func() {
-				It("fails with an error", func() {
+			When("space is in targeted org", func() {
+				BeforeEach(func() {
+					shareToSpaceName = helpers.NewSpaceName()
+					helpers.CreateSpace(shareToSpaceName)
+				})
+
+				AfterEach(func() {
+					helpers.QuickDeleteSpace(shareToSpaceName)
+				})
+
+				It("shares the service to space in targeted org", func() {
 					session := helpers.CF(shareServiceCommand, serviceInstanceName, shareToSpaceName)
-					Eventually(session).Should(Exit(1))
+
+					Eventually(session).Should(Exit(0))
 					Expect(session.Out).To(SatisfyAll(
 						Say("Sharing service instance %s to org %s / space %s as %s...", serviceInstanceName, orgName, shareToSpaceName, username),
-						Say("FAILED"),
+						Say("OK"),
 					))
-					Eventually(session.Err).Should(Say("Space '%s' not found.", shareToSpaceName))
+
+					By("validating the service is shared", func() {
+						session = helpers.CF("service", serviceInstanceName)
+						Eventually(session).Should(Exit(0))
+						Expect(session.Out).To(SatisfyAll(
+							Say(`shared with spaces`),
+							Say(`org\s+space\s+bindings`),
+							Say(`%s\s+%s\s+0`, orgName, shareToSpaceName),
+						))
+					})
 				})
 			})
 
-			Context("space cannot be retrieved in specified org", func() {
+			When("the space to share is in specified org", func() {
 				BeforeEach(func() {
+					shareToOrgName = helpers.NewOrgName()
 					helpers.CreateOrg(shareToOrgName)
+
+					shareToSpaceName = helpers.NewSpaceName()
+					helpers.CreateSpaceInOrg(shareToSpaceName, shareToOrgName)
 				})
 
 				AfterEach(func() {
 					helpers.QuickDeleteOrg(shareToOrgName)
 				})
 
-				It("fails with an error", func() {
+				It("shares the service to space in specified org", func() {
 					session := helpers.CF(shareServiceCommand, serviceInstanceName, shareToSpaceName, "-o", shareToOrgName)
-					Eventually(session).Should(Exit(1))
-					Expect(session.Out).To(SatisfyAll(
-						Say("Sharing service instance %s to org %s / space %s as %s...", serviceInstanceName, shareToOrgName, shareToSpaceName, username),
-						Say("FAILED"),
-					))
-					Eventually(session.Err).Should(Say("Space '%s' not found.", shareToSpaceName))
-				})
-			})
 
-			Context("specified organization cannot be retrieved", func() {
-				It("fails with an error", func() {
-					session := helpers.CF(shareServiceCommand, serviceInstanceName, shareToSpaceName, "-o", shareToOrgName)
-					Eventually(session).Should(Exit(1))
+					Eventually(session).Should(Exit(0))
 					Expect(session.Out).To(SatisfyAll(
 						Say("Sharing service instance %s to org %s / space %s as %s...", serviceInstanceName, shareToOrgName, shareToSpaceName, username),
-						Say("FAILED"),
+						Say("OK"),
 					))
-					Eventually(session.Err).Should(Say("Organization '%s' not found.", shareToOrgName))
+
+					By("validating the service is shared", func() {
+						session = helpers.CF("service", serviceInstanceName)
+						Eventually(session).Should(Exit(0))
+						Expect(session.Out).To(SatisfyAll(
+							Say(`shared with spaces`),
+							Say(`org\s+space\s+bindings`),
+							Say(`%s\s+%s\s+0`, shareToOrgName, shareToSpaceName),
+						))
+					})
 				})
 			})
 		})
