ffa5cb4be8e147c4e6c8911b782d90f9820586c2
Felisia Martini <fmartini@pivotal.io>
2020-09-07 12:36:24 +0100

    Remove v6 update-service command

--- actor/v2action/composite/update_service_instance.go
+++ actor/v2action/composite/update_service_instance.go
@@ -1,7 +1,6 @@
 package composite
 
 import (
-	"code.cloudfoundry.org/cli/actor/actionerror"
 	"code.cloudfoundry.org/cli/actor/v2action"
 )
 
@@ -27,44 +26,4 @@ type UpdateServiceInstanceMaintenanceInfoActor interface {
 
 type GetAPIVersionActor interface {
 	CloudControllerAPIVersion() string
-}
-
-type UpdateServiceInstanceCompositeActor struct {
-	GetServiceInstanceActor                   GetServiceInstanceActor
-	GetServicePlanActor                       GetServicePlanActor
-	UpdateServiceInstanceMaintenanceInfoActor UpdateServiceInstanceMaintenanceInfoActor
-	GetAPIVersionActor                        GetAPIVersionActor
-}
-
-// UpgradeManagedServiceInstance requests update on the service instance with the `maintenance_info` available on the plan
-func (c UpdateServiceInstanceCompositeActor) UpgradeServiceInstance(serviceInstance v2action.ServiceInstance) (v2action.Warnings, error) {
-	servicePlan, warnings, err := c.GetServicePlanActor.GetServicePlan(serviceInstance.ServicePlanGUID)
-	if err != nil {
-		return warnings, err
-	}
-
-	if !upgradeIsAvailable(serviceInstance, servicePlan) {
-		return warnings, actionerror.ServiceInstanceUpgradeNotAvailableError{}
-	}
-
-	updateWarnings, err := c.UpdateServiceInstanceMaintenanceInfoActor.UpdateServiceInstanceMaintenanceInfo(
-		serviceInstance.GUID,
-		v2action.MaintenanceInfo(servicePlan.MaintenanceInfo),
-	)
-
-	return append(warnings, updateWarnings...), err
-}
-
-// GetServiceInstanceByNameAndSpace gets the service instance by name and space guid provided
-func (c UpdateServiceInstanceCompositeActor) GetServiceInstanceByNameAndSpace(name string, spaceGUID string) (v2action.ServiceInstance, v2action.Warnings, error) {
-	return c.GetServiceInstanceActor.GetServiceInstanceByNameAndSpace(name, spaceGUID)
-}
-
-// CloudControllerAPIVersion returns the CloudController API version
-func (c UpdateServiceInstanceCompositeActor) CloudControllerAPIVersion() string {
-	return c.GetAPIVersionActor.CloudControllerAPIVersion()
-}
-
-func upgradeIsAvailable(serviceInstance v2action.ServiceInstance, servicePlan v2action.ServicePlan) bool {
-	return serviceInstance.MaintenanceInfo.Version != servicePlan.MaintenanceInfo.Version
 }
