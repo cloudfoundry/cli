6f9914b01b38677b95d48cd2b38041ebe090a86c
Lisa Burns <lisaburns@vmware.com>
2020-09-23 23:48:57 +0000

    Derive deployable column from droplet state on older CAPI
    - Version 3.85.0 of CAPI is within the supported window of the v7 cf CLI
    but does not return `deployable` from /v3/revisions. On this version we
    will fetch droplets and derive the state ourselves
    
    [#174611561](https://www.pivotaltracker.com/story/show/174611561)

--- actor/v7action/revisions.go
+++ actor/v7action/revisions.go
@@ -4,25 +4,41 @@ import (
 	"strconv"
 
 	"code.cloudfoundry.org/cli/actor/actionerror"
+	"code.cloudfoundry.org/cli/actor/versioncheck"
 	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv3"
+	"code.cloudfoundry.org/cli/api/cloudcontroller/ccv3/constant"
 	"code.cloudfoundry.org/cli/resources"
 )
 
+const MinimumCCAPIVersionForDeployable = "3.86.0"
+
 // GetRevisionsByApplicationNameAndSpace returns revisions for application.
 func (actor *Actor) GetRevisionsByApplicationNameAndSpace(appName string, spaceGUID string) ([]resources.Revision, Warnings, error) {
-	app, warnings, appErr := actor.GetApplicationByNameAndSpace(appName, spaceGUID)
+	var warnings Warnings
+	app, appWarnings, appErr := actor.GetApplicationByNameAndSpace(appName, spaceGUID)
+	warnings = append(warnings, appWarnings...)
 	if appErr != nil {
 		return []resources.Revision{}, warnings, appErr
 	}
-	revisions, v3Warnings, apiErr := actor.CloudControllerClient.GetApplicationRevisions(
+	revisions, apiWarnings, apiErr := actor.CloudControllerClient.GetApplicationRevisions(
 		app.GUID,
 		ccv3.Query{Key: ccv3.OrderBy, Values: []string{"-created_at"}},
 	)
-	warnings = append(warnings, v3Warnings...)
+	warnings = append(warnings, apiWarnings...)
 	if apiErr != nil {
 		return []resources.Revision{}, warnings, apiErr
 	}
-
+	versionRequirementMet, versionErr := versioncheck.IsMinimumAPIVersionMet(actor.Config.APIVersion(), MinimumCCAPIVersionForDeployable)
+	if versionErr != nil {
+		return []resources.Revision{}, warnings, versionErr
+	}
+	if versionRequirementMet == false {
+		_, deployableWarnings, deployableErr := actor.setRevisionsDeployableByDropletStateForApp(app.GUID, revisions)
+		warnings = append(warnings, deployableWarnings...)
+		if deployableErr != nil {
+			return []resources.Revision{}, warnings, deployableErr
+		}
+	}
 	return revisions, warnings, nil
 }
 
@@ -44,5 +60,34 @@ func (actor Actor) GetRevisionByApplicationAndVersion(appGUID string, revisionVe
 		return resources.Revision{}, Warnings(warnings), actionerror.RevisionNotFoundError{Version: revisionVersion}
 	}
 
+	versionRequirementMet, versionErr := versioncheck.IsMinimumAPIVersionMet(actor.Config.APIVersion(), MinimumCCAPIVersionForDeployable)
+	if versionErr != nil {
+		return resources.Revision{}, Warnings(warnings), versionErr
+	}
+	if versionRequirementMet == false {
+		_, deployableWarnings, deployableErr := actor.setRevisionsDeployableByDropletStateForApp(appGUID, revisions)
+		warnings = append(warnings, deployableWarnings...)
+		if deployableErr != nil {
+			return resources.Revision{}, Warnings(warnings), deployableErr
+		}
+	}
+
 	return revisions[0], Warnings(warnings), nil
+}
+
+func (actor Actor) setRevisionsDeployableByDropletStateForApp(appGUID string, revisions []resources.Revision) ([]resources.Revision, Warnings, error) {
+	droplets, warnings, err := actor.CloudControllerClient.GetDroplets(
+		ccv3.Query{Key: ccv3.AppGUIDFilter, Values: []string{appGUID}},
+	)
+	if err != nil {
+		return []resources.Revision{}, Warnings(warnings), err
+	}
+	for i := range revisions {
+		for _, droplet := range droplets {
+			if revisions[i].Droplet.GUID == droplet.GUID {
+				revisions[i].Deployable = (droplet.State == constant.DropletStaged)
+			}
+		}
+	}
+	return revisions, Warnings(warnings), nil
 }
--- resources/revision_resource.go
+++ resources/revision_resource.go
@@ -1,10 +1,11 @@
 package resources
 
 type Revision struct {
-	GUID        string `json:"guid"`
-	Version     int    `json:"version"`
-	Deployable  bool   `json:"deployable"`
-	Description string `json:"description"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
+	GUID        string  `json:"guid"`
+	Version     int     `json:"version"`
+	Deployable  bool    `json:"deployable"`
+	Description string  `json:"description"`
+	Droplet     Droplet `json:"droplet"`
+	CreatedAt   string  `json:"created_at"`
+	UpdatedAt   string  `json:"updated_at"`
 }
