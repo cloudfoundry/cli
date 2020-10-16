e8e8a3874c7864d8b7e1a48c96b458e8ea7c6a4e
George Blue <gblue@pivotal.io>
2020-10-16 12:34:22 +0100

    v8(services): replace bind-route-service with V3 version
    [#175142930](https://www.pivotaltracker.com/story/show/175142930)

--- command/common/command_list_v7.go
+++ command/common/command_list_v7.go
@@ -24,8 +24,7 @@ type commandList struct {
 	ApplyManifest                      v7.ApplyManifestCommand                      `command:"apply-manifest" description:"Apply manifest properties to a space"`
 	Apps                               v7.AppsCommand                               `command:"apps" alias:"a" description:"List all apps in the target space"`
 	Auth                               v7.AuthCommand                               `command:"auth" description:"Authenticate non-interactively"`
-	BindRouteService                   v6.BindRouteServiceCommand                   `command:"bind-route-service" alias:"brs" description:"Bind a service instance to an HTTP route"`
-	BindRouteServiceV3                 v7.BindRouteServiceCommand                   `command:"v3-bind-route-service" alias:"brs" description:"Bind a service instance to an HTTP route"`
+	BindRouteService                   v7.BindRouteServiceCommand                   `command:"bind-route-service" alias:"brs" description:"Bind a service instance to an HTTP route"`
 	BindRunningSecurityGroup           v7.BindRunningSecurityGroupCommand           `command:"bind-running-security-group" description:"Bind a security group to the list of security groups to be used for running applications"`
 	BindSecurityGroup                  v7.BindSecurityGroupCommand                  `command:"bind-security-group" description:"Bind a security group to a particular space, or all existing spaces of an org"`
 	BindService                        v6.BindServiceCommand                        `command:"bind-service" alias:"bs" description:"Bind a service instance to an app"`
