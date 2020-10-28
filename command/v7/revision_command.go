5c90f74ccd6d3b898dd5ddf90790e504111d19f7
Nick Webb <nwebb@pivotal.io>
2020-10-28 23:50:04 +0000

    WIP - revision command
    - it works
    - unit tests passing
    - need to update integration tests
    
    [#174502203](https://www.pivotaltracker.com/story/show/174502203)
    
    Co-authored-by: Lisa Burns <lisaburns@vmware.com>

--- command/v7/revision_command.go
+++ command/v7/revision_command.go
@@ -1,8 +1,12 @@
 package v7
 
 import (
+	"fmt"
+	"strconv"
+
 	"code.cloudfoundry.org/cli/command"
 	"code.cloudfoundry.org/cli/command/flag"
+	"code.cloudfoundry.org/cli/resources"
 )
 
 type RevisionCommand struct {
@@ -16,5 +20,54 @@ type RevisionCommand struct {
 func (cmd RevisionCommand) Execute(_ []string) error {
 	cmd.UI.DisplayWarning(command.ExperimentalWarning)
 	cmd.UI.DisplayNewline()
+	err := cmd.SharedActor.CheckTarget(true, true)
+	if err != nil {
+		return err
+	}
+
+	user, err := cmd.Config.CurrentUser()
+	if err != nil {
+		return err
+	}
+
+	appName := cmd.RequiredArgs.AppName
+	cmd.UI.DisplayTextWithFlavor("Showing revision {{.Version}} for app {{.AppName}} in org {{.OrgName}} / space {{.SpaceName}} as {{.Username}}...", map[string]interface{}{
+		"AppName":   appName,
+		"OrgName":   cmd.Config.TargetedOrganization().Name,
+		"SpaceName": cmd.Config.TargetedSpace().Name,
+		"Username":  user.Name,
+		"Version":   cmd.Version.Value,
+	})
+
+	app, _, _ := cmd.Actor.GetApplicationByNameAndSpace(appName, cmd.Config.TargetedSpace().GUID)
+	deployedRevisions, _, _ := cmd.Actor.GetApplicationRevisionsDeployed(app.GUID)
+	revisions, _, _ := cmd.Actor.GetRevisionsByApplicationNameAndSpace(
+		appName,
+		cmd.Config.TargetedSpace().GUID,
+	)
+	for _, revision := range revisions {
+		if revision.Version == cmd.Version.Value {
+			deployed := revisionDeployed(revision, deployedRevisions)
+			keyValueTable := [][]string{
+				{"revision:", fmt.Sprintf("%d", cmd.Version.Value)},
+				{"deployed:", strconv.FormatBool(deployed)},
+				{"description:", revision.Description},
+				{"deployable:", strconv.FormatBool(revision.Deployable)},
+				{"revision GUID:", revision.GUID},
+				{"droplet GUID:", revision.Droplet.GUID},
+				{"created on:", revision.CreatedAt},
+			}
+			cmd.UI.DisplayKeyValueTable("", keyValueTable, 3)
+		}
+	}
 	return nil
+}
+
+func revisionDeployed(revision resources.Revision, deployedRevisions []resources.Revision) bool {
+	for _, deployedRevision := range deployedRevisions {
+		if revision.GUID == deployedRevision.GUID {
+			return true
+		}
+	}
+	return false
 }
--- command/v7/revision_command_test.go
+++ command/v7/revision_command_test.go
@@ -1,9 +1,16 @@
 package v7_test
 
 import (
+	"errors"
+
+	"code.cloudfoundry.org/cli/actor/actionerror"
 	"code.cloudfoundry.org/cli/command/commandfakes"
+	"code.cloudfoundry.org/cli/command/flag"
 	v7 "code.cloudfoundry.org/cli/command/v7"
 	"code.cloudfoundry.org/cli/command/v7/v7fakes"
+	"code.cloudfoundry.org/cli/resources"
+	"code.cloudfoundry.org/cli/types"
+	"code.cloudfoundry.org/cli/util/configv3"
 	"code.cloudfoundry.org/cli/util/ui"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -18,6 +25,7 @@ var _ = Describe("revision Command", func() {
 		fakeSharedActor *commandfakes.FakeSharedActor
 		fakeActor       *v7fakes.FakeActor
 		binaryName      string
+		executeErr      error
 		appName         string
 
 		out *Buffer
@@ -51,5 +59,136 @@ var _ = Describe("revision Command", func() {
 
 	It("displays the experimental warning", func() {
 		Expect(testUI.Err).To(Say("This command is in EXPERIMENTAL stage and may change without notice"))
+	})
+
+	When("checking target fails", func() {
+		BeforeEach(func() {
+			fakeSharedActor.CheckTargetReturns(actionerror.NotLoggedInError{BinaryName: binaryName})
+		})
+
+		It("returns an error", func() {
+			Expect(executeErr).To(MatchError(actionerror.NotLoggedInError{BinaryName: binaryName}))
+
+			Expect(fakeSharedActor.CheckTargetCallCount()).To(Equal(1))
+			checkTargetedOrg, checkTargetedSpace := fakeSharedActor.CheckTargetArgsForCall(0)
+			Expect(checkTargetedOrg).To(BeTrue())
+			Expect(checkTargetedSpace).To(BeTrue())
+		})
+	})
+
+	When("the user is logged in, an org is targeted and a space is targeted", func() {
+		BeforeEach(func() {
+			fakeConfig.TargetedSpaceReturns(configv3.Space{Name: "some-space", GUID: "some-space-guid"})
+			fakeConfig.TargetedOrganizationReturns(configv3.Organization{Name: "some-org"})
+		})
+
+		When("getting the current user returns an error", func() {
+			BeforeEach(func() {
+				fakeConfig.CurrentUserReturns(configv3.User{}, errors.New("some-error"))
+			})
+
+			It("returns the error", func() {
+				Expect(executeErr).To(MatchError("some-error"))
+			})
+		})
+
+		When("getting the current user succeeds", func() {
+			BeforeEach(func() {
+				fakeConfig.CurrentUserReturns(configv3.User{Name: "banana"}, nil)
+			})
+
+			When("when the requested app and revision exist", func() {
+				var revisions []resources.Revision
+				BeforeEach(func() {
+					fakeApp := resources.Application{
+						GUID: "fake-guid",
+						Name: "some-app",
+					}
+					fakeActor.GetApplicationByNameAndSpaceReturns(fakeApp, nil, nil)
+
+					revisions = []resources.Revision{
+						{
+							Version:     3,
+							GUID:        "A68F13F7-7E5E-4411-88E8-1FAC54F73F50",
+							Description: "On a different note",
+							CreatedAt:   "2020-03-10T17:11:58Z",
+							Deployable:  true,
+							Droplet: resources.Droplet{
+								GUID: "droplet-guid",
+							},
+						},
+						{
+							Version:     2,
+							GUID:        "A89F8259-D32B-491A-ABD6-F100AC42D74C",
+							Description: "Something else",
+							CreatedAt:   "2020-03-08T12:43:30Z",
+							Deployable:  true,
+						},
+						{
+							Version:     1,
+							GUID:        "17E0E587-0E53-4A6E-B6AE-82073159F910",
+							Description: "Something",
+							CreatedAt:   "2020-03-04T13:23:32Z",
+							Deployable:  false,
+						},
+					}
+					fakeActor.GetRevisionsByApplicationNameAndSpaceReturns(revisions, nil, nil)
+					fakeActor.GetApplicationByNameAndSpaceReturns(resources.Application{GUID: "app-guid"}, nil, nil)
+					fakeActor.GetApplicationRevisionsDeployedReturns(revisions[0:1], nil, nil)
+
+					cmd.Version = flag.Revision{NullInt: types.NullInt{Value: 3, IsSet: true}}
+				})
+
+				It("gets the app guid", func() {
+					Expect(fakeActor.GetApplicationByNameAndSpaceCallCount()).To(Equal(1))
+					appName, spaceGUID := fakeActor.GetApplicationByNameAndSpaceArgsForCall(0)
+					Expect(appName).To(Equal("some-app"))
+					Expect(spaceGUID).To(Equal("some-space-guid"))
+				})
+
+				It("retrieves the deployed revisions", func() {
+					Expect(fakeActor.GetApplicationRevisionsDeployedCallCount()).To(Equal(1))
+					Expect(fakeActor.GetApplicationRevisionsDeployedArgsForCall(0)).To(Equal("app-guid"))
+				})
+
+				It("displays the revision", func() {
+					Expect(executeErr).ToNot(HaveOccurred())
+
+					Expect(testUI.Out).To(Say(`Showing revision 3 for app some-app in org some-org / space some-space as banana...`))
+					Expect(testUI.Out).To(Say(`revision:        3`))
+					Expect(testUI.Out).To(Say(`deployed:        true`))
+					Expect(testUI.Out).To(Say(`description:     On a different note`))
+					Expect(testUI.Out).To(Say(`deployable:      true`))
+					Expect(testUI.Out).To(Say(`revision GUID:   A68F13F7-7E5E-4411-88E8-1FAC54F73F50`))
+					Expect(testUI.Out).To(Say(`droplet GUID:    droplet-guid`))
+					Expect(testUI.Out).To(Say(`created on:      2020-03-10T17:11:58Z`))
+
+					Expect(fakeActor.GetRevisionsByApplicationNameAndSpaceCallCount()).To(Equal(1))
+					appName, spaceGUID := fakeActor.GetRevisionsByApplicationNameAndSpaceArgsForCall(0)
+					Expect(appName).To(Equal("some-app"))
+					Expect(spaceGUID).To(Equal("some-space-guid"))
+				})
+
+				When("revision is not deployed", func() {
+					BeforeEach(func() {
+						fakeActor.GetApplicationRevisionsDeployedReturns(revisions[1:2], nil, nil)
+					})
+
+					It("displays deployed field correctly", func() {
+						Expect(testUI.Out).To(Say(`deployed:        false`))
+					})
+				})
+
+				When("no revisions were deployed", func() {
+					BeforeEach(func() {
+						fakeActor.GetApplicationRevisionsDeployedReturns([]resources.Revision{}, nil, nil)
+					})
+
+					It("displays deployed field correctly", func() {
+						Expect(testUI.Out).To(Say(`deployed:        false`))
+					})
+				})
+			})
+		})
 	})
 })
