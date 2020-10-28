52acfbc38ea58f0424805a040bfff6b8a3a7b61f
Nick Webb <nwebb@pivotal.io>
2020-10-28 22:08:34 +0000

    Add revision command to CLI
    - No functionality implemented, just help text and wiring through the
    command list
    - Arguments are set up correctly but not used
    
    [#174503944](https://www.pivotaltracker.com/story/show/174503944)
    
    Co-authored-by: Lisa Burns <lisaburns@vmware.com>

---
+++ command/v7/revision_command.go
@@ -0,0 +1,17 @@
+package v7
+
+import (
+	"code.cloudfoundry.org/cli/command/flag"
+)
+
+type RevisionCommand struct {
+	BaseCommand
+	usage           interface{}   `usage:"CF_NAME revision APP_NAME [--version VERSION]"`
+	RequiredArgs    flag.AppName  `positional-args:"yes"`
+	Version         flag.Revision `long:"version" required:"true" description:"The integer representing the specific revision to show"`
+	relatedCommands interface{}   `related_commands:"revisions, rollback"`
+}
+
+func (cmd RevisionCommand) Execute(_ []string) error {
+	return nil
+}
