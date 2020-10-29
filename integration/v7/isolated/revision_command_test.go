b6785c9f946df48aa516ff17919d58a9f60a853d
Alexander Berezovsky <aberezovsky@vmware.com>
2020-10-29 21:52:37 +0000

    Add basic integration test for cf revision
    [#174502203](https://www.pivotaltracker.com/story/show/174502203)
    
    Co-authored-by: Nick Webb <nwebb@pivotal.io>

--- integration/v7/isolated/revision_command_test.go
+++ integration/v7/isolated/revision_command_test.go
@@ -1,6 +1,8 @@
 package isolated
 
 import (
+	"fmt"
+
 	. "code.cloudfoundry.org/cli/cf/util/testhelpers/matchers"
 	"code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
@@ -10,6 +12,20 @@ import (
 )
 
 var _ = Describe("revision command", func() {
+	var (
+		orgName   string
+		spaceName string
+		appName   string
+		username  string
+	)
+
+	BeforeEach(func() {
+		username, _ = helpers.GetCredentials()
+		orgName = helpers.NewOrgName()
+		spaceName = helpers.NewSpaceName()
+		appName = helpers.PrefixedRandomName("app")
+	})
+
 	Describe("help", func() {
 		When("--help flag is set", func() {
 			It("appears in cf help -a", func() {
@@ -31,6 +47,55 @@ var _ = Describe("revision command", func() {
 				Expect(session).To(Say("--version      The integer representing the specific revision to show"))
 				Expect(session).To(Say("SEE ALSO:"))
 				Expect(session).To(Say("revisions, rollback"))
+			})
+		})
+	})
+
+	When("targetting and org and space", func() {
+		BeforeEach(func() {
+			helpers.SetupCF(orgName, spaceName)
+		})
+
+		AfterEach(func() {
+			helpers.QuickDeleteOrg(orgName)
+		})
+
+		// TODO: #173456870 when app not provided, app does not exist, revision doesn't exist cases
+
+		When("the requested app and revision both exist", func() {
+			BeforeEach(func() {
+				helpers.WithHelloWorldApp(func(appDir string) {
+					Eventually(helpers.CF("push", appName, "-p", appDir)).Should(Exit(0))
+					Eventually(helpers.CF("push", appName, "-p", appDir)).Should(Exit(0))
+				})
+			})
+
+			FIt("shows details about the revision", func() {
+				session := helpers.CF("revision", appName, "--version", "1")
+				Eventually(session).Should(Exit(0))
+				Expect(session).Should(Say(
+					fmt.Sprintf("Showing revision 1 for app %s in org %s / space %s as %s...", appName, orgName, spaceName, username),
+				))
+				Expect(session).Should(Say(`revision:        1`))
+				Expect(session).Should(Say(`deployed:        false`))
+				Expect(session).Should(Say(`description:     Initial revision`))
+				Expect(session).Should(Say(`deployable:      true`))
+				// Expect(session).Should(MatchRegexp(`revision GUID:   \w+\n`))
+				// Expect(session).Should(MatchRegexp(`droplet GUID:    \w+\n`))
+				// Expect(session).Should(MatchRegexp(`created on:      \w+\n`))
+
+				session = helpers.CF("revision", appName, "--version", "2")
+				Eventually(session).Should(Exit(0))
+				Expect(session).Should(Say(
+					fmt.Sprintf("Showing revision 2 for app %s in org %s / space %s as %s...", appName, orgName, spaceName, username),
+				))
+				Expect(session).Should(Say(`revision:        2`))
+				Expect(session).Should(Say(`deployed:        true`))
+				Expect(session).Should(Say(`description:     New droplet deployed`))
+				Expect(session).Should(Say(`deployable:      true`))
+				// Expect(session).Should(MatchRegexp(`revision GUID:   \w+\n`))
+				// Expect(session).Should(MatchRegexp(`droplet GUID:    \w+\n`))
+				// Expect(session).Should(MatchRegexp(`created on:      \w+\n`))
 			})
 		})
 	})
