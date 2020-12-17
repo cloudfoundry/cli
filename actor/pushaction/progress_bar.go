bb123dfa910895d9fa469af128bab282ae747a6c
Reid Mitchell <rmitchell@pivotal.io>
2020-12-17 20:00:32 +0000

    First pass
    * deleted everything in v2v3action, v3action, and pushaction, as well as
    anything we could find that pointed to these places. Build and units are
    successful.
    
    [#176044756]

--- actor/pushaction/progress_bar.go
+++
@@ -1,9 +0,0 @@
-package pushaction
-
-import "io"
-
-//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 . ProgressBar
-
-type ProgressBar interface {
-	NewProgressBarWrapper(reader io.Reader, sizeOfFile int64) io.Reader
-}
