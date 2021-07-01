4b1af59d5cd4a222e0933897f3ae4ab52925ddd7
Juan Diego Gonzalez <gojuan@vmware.com>
2021-07-01 15:58:56 +0000

    Fixed push command test for -k flag
    Added WaitForAppDiskToTakeEffect in order to wait until the disk change
    to take effect.
    
    Also, refactored test so that we have Eventually.session should exist,
    and then only having expects afterwards.
    
    Co-authored-by: Juan Diego Gonzalez <gojuan@vmware.com>
    Co-authored-by: Dominic Roberts <dominicr@vmware.com>

--- integration/v7/push/disk_flag_test.go
+++ integration/v7/push/disk_flag_test.go
@@ -1,8 +1,6 @@
 package push
 
 import (
-	"time"
-
 	"code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -29,13 +27,14 @@ var _ = Describe("push with disk flag", func() {
 				Eventually(session).Should(Exit(0))
 			})
 
-			time.Sleep(5 * time.Second)
+			helpers.WaitForAppDiskToTakeEffect(appName, 0, 0, false, "70M")
+
 			session := helpers.CF("app", appName)
-			Eventually(session).Should(Say(`name:\s+%s`, appName))
-			Eventually(session).Should(Say(`last uploaded:\s+%s`, helpers.ReadableDateTimeRegex))
-			Eventually(session).Should(Say(`\s+state\s+since\s+cpu\s+memory\s+disk`))
-			Eventually(session).Should(Say(`#0\s+running\s+\d{4}-[01]\d-[0-3]\dT[0-2][0-9]:[0-5]\d:[0-5]\dZ.+of.+of 70M`))
 			Eventually(session).Should(Exit(0))
+			Expect(session).To(Say(`name:\s+%s`, appName))
+			Expect(session).To(Say(`last uploaded:\s+%s`, helpers.ReadableDateTimeRegex))
+			Expect(session).To(Say(`\s+state\s+since\s+cpu\s+memory\s+disk`))
+			Expect(session).To(Say(`#0\s+running\s+\d{4}-[01]\d-[0-3]\dT[0-2][0-9]:[0-5]\d:[0-5]\dZ.+of.+of 70M`))
 		})
 	})
 })
