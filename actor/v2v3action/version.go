4f40dc326bfabc7cc2e47464bbc1ba478e481588
Jenna Goldstrich <jgoldstrich@vmware.com>
2021-07-16 18:16:48 +0000

    Delete a bunch of old v6 code

--- actor/v2v3action/version.go
+++
@@ -1,6 +0,0 @@
-package v2v3action
-
-// CloudControllerV3APIVersion returns back the V3 Cloud Controller API version.
-func (actor Actor) CloudControllerV3APIVersion() string {
-	return actor.V3Actor.CloudControllerAPIVersion()
-}
