3ec8045fe4b1fb6343ba2f33e384a7b8238cf9b3
Jenna Goldstrich <jgoldstrich@vmware.com>
2021-07-16 18:16:48 +0000

    Delete a bunch of old v6 code

--- actor/v3action/ssh_actor.go
+++
@@ -1,9 +0,0 @@
-package v3action
-
-import "code.cloudfoundry.org/cli/actor/sharedaction"
-
-//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 . SSHActor
-
-type SSHActor interface {
-	ExecuteSecureShell(sshOptions sharedaction.SSHOptions) error
-}
--- actor/v3action/version.go
+++
@@ -1,6 +0,0 @@
-package v3action
-
-// CloudControllerAPIVersion returns back the Cloud Controller API version.
-func (actor Actor) CloudControllerAPIVersion() string {
-	return actor.CloudControllerClient.CloudControllerAPIVersion()
-}
