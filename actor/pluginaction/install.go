e383307431b72f94c5c64d7899c5eec9f30517d2
Jenna Goldstrich <jgoldstrich@vmware.com>
2021-07-28 20:34:47 +0000

    Make v8 use existing plugin api
    Co-authored-by: Dominic Roberts <dominicr@vmware.com>

--- actor/pluginaction/install.go
+++ actor/pluginaction/install.go
@@ -102,12 +102,8 @@ func (actor Actor) GetAndValidatePlugin(pluginMetadata PluginMetadata, commandLi
 	}
 
 	switch cliVersion.Major {
-	case 6, 7:
+	case 6, 7, 8:
 		if pluginLibraryMajorVersion > 1 {
-			return configv3.Plugin{}, actionerror.PluginInvalidLibraryVersionError{}
-		}
-	case 8:
-		if pluginLibraryMajorVersion != 2 {
 			return configv3.Plugin{}, actionerror.PluginInvalidLibraryVersionError{}
 		}
 	default:
