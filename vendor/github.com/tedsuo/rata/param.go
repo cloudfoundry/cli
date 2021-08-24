5ffa262bb2e51ba770d456fd169da89ca0b9801f
Juan Diego Gonzalez <gojuan@vmware.com>
2021-08-24 21:50:59 +0000

    Deleting vendor folder
    Since Golang 1.16+ fully supports go modules, moving forward we want to
    deprecate vendoring.

--- vendor/github.com/tedsuo/rata/param.go
+++
@@ -1,8 +0,0 @@
-package rata
-
-import "net/http"
-
-//  Param returns the parameter with the given name from the given request.
-func Param(req *http.Request, paramName string) string {
-	return req.URL.Query().Get(":" + paramName)
-}
