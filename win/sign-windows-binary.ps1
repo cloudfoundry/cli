1a523089a3c8477062c7a8d55556baa03f0dee69
George Gelashvili <ggelashvili@vmware.com>
2022-02-08 23:01:56 -0800

    make sign-windows-binary a wrapper for signtool
    the .ps1 script now takes a param for binary-to-sign, making it reusable in the workflow

--- win/sign-windows-binary.ps1
+++ win/sign-windows-binary.ps1
@@ -2,11 +2,11 @@
 # SIGNING_KEY_WINDOWS_PFX
 # SIGNING_KEY_WINDOWS_PASSPHRASE
 
+param ($BinaryFilePath)
+
 # add PATH to signtool.exe
 $env:PATH="$env:PATH;C:\Program Files (x86)\Windows Kits\10\bin\x64"
+
 [Convert]::FromBase64String($env:SIGNING_KEY_WINDOWS_PFX) | Set-Content -Path $env:RUNNER_TEMP\cert.pfx -AsByteStream
-pwd
-Get-ChildItem .\out
-Get-Command signtool
-# signtool sign /v /fd SHA256 /f $env:RUNNER_TEMP.pfx /p "$SIGNING_KEY_WINDOWS_PASSPHRASE" .\dist\hello-windows_windows_amd64\hello.exe
-signtool sign /v /p "$env:SIGNING_KEY_WINDOWS_PASSPHRASE" /fd SHA256 /f $env:RUNNER_TEMP\cert.pfx .\out\hello.exe
+
+signtool sign /v /p "$env:SIGNING_KEY_WINDOWS_PASSPHRASE" /fd SHA256 /f "$env:RUNNER_TEMP\cert.pfx" "$BinaryFilePath"
