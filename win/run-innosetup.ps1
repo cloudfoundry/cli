5d0fd4119666b88e7f6b1700ed4498a223998f6a
George Gelashvili <ggelashvili@vmware.com>
2022-02-28 14:01:43 -0800

    build, sign, and upload 32-bit windows installer

--- win/run-innosetup.ps1
+++ win/run-innosetup.ps1
@@ -1,17 +1,17 @@
-param ($InstallerOutputFilePath)
+param ($InnoSetupConfig, $CfBinary, $InstallerOutput)
 
 $ErrorActionPreference = "Stop"
 # in the future, this variable should cause PS to exit on non-zero exit codes from commands/exes (as opposed to PS cmdlets)
 $PSNativeCommandUseErrorActionPreference = $true
 # see https://github.com/PowerShell/PowerShell/issues/3415 and https://github.com/PowerShell/PowerShell-RFC/pull/277
 
-Move-Item out\cf-cli_winx64.exe win\cf8.exe
+Move-Item -Force "$CfBinary" win\cf8.exe
 
 # convert line-endings
 Get-Content win\LICENSE-WITH-3RD-PARTY-LICENSES | Set-Content win\LICENSE
 Get-Content win\CF_NOTICE | Set-Content win\NOTICE
 
-iscc win\windows-installer-v8-x64.iss
-Move-Item win\Output\mysetup.exe "$InstallerOutputFilePath"
+iscc "$InnoSetupConfig"
+Move-Item win\Output\mysetup.exe "$InstallerOutput"
 
 Get-ChildItem win\Output
