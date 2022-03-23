8c514b8be73844de35fc50433ae61c86ca10c872
Juan Diego Gonzalez <gojuan@vmware.com>
2022-03-23 21:27:46 +0000

    Removes/Cleans comments
    Also, small change to sign-windows-binary.ps1 as
    that step was already being done in the sign job
    
    Signed-off-by: George Gelashvili <ggelashvili@vmware.com>

--- .github/win/sign-windows-binary.ps1
+++ .github/win/sign-windows-binary.ps1
@@ -1,12 +1,9 @@
 # expected environment variables
-# SIGNING_KEY_WINDOWS_PFX
 # SIGNING_KEY_WINDOWS_PASSPHRASE
 
 param ($BinaryFilePath)
 
 # add PATH to signtool.exe
 $env:PATH="$env:PATH;C:\Program Files (x86)\Windows Kits\10\bin\x64"
-
-[Convert]::FromBase64String($env:SIGNING_KEY_WINDOWS_PFX) | Set-Content -Path $env:RUNNER_TEMP\cert.pfx -AsByteStream
 
 signtool sign /v /p "$env:SIGNING_KEY_WINDOWS_PASSPHRASE" /fd SHA256 /f "$env:RUNNER_TEMP\cert.pfx" "$BinaryFilePath"
