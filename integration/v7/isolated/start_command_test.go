b3c230599d51ca5fbc46fbd33c6a59f119363c7a
M. Oleske <moleske@pivotal.io>
2022-10-13 15:36:46 -0700

    Use default of 1GB memory instead of 32M
    - a regular deploy of cf-deployment has a default memory limit of 1G
    - if we prefer to use 32M, we should change the tests to push an app specifying that as the limit instead
    
    Co-authored-by: Juan Diego Gonzalez <gojuan@vmware.com>

--- integration/v7/isolated/start_command_test.go
+++ integration/v7/isolated/start_command_test.go
@@ -129,7 +129,7 @@ var _ = Describe("start command", func() {
 					Eventually(session).Should(Say(`routes:\s+%s.%s`, appName, helpers.DefaultSharedDomain()))
 					Eventually(session).Should(Say(`type:\s+web`))
 					Eventually(session).Should(Say(`instances:\s+1/1`))
-					Eventually(session).Should(Say(`memory usage:\s+32M`))
+					Eventually(session).Should(Say(`memory usage:\s+1024M`))
 					Eventually(session).Should(Say(`\s+state\s+since\s+cpu\s+memory\s+disk\s+logging\s+details`))
 					Eventually(session).Should(Say(`#0\s+(starting|running)\s+\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z`))
 
