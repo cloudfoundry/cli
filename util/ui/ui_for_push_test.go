f2b79503181b6eaf3ca7450f9e9493f584e084a0
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-23 05:05:45 -0400

    spelling: aligns
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- util/ui/ui_for_push_test.go
+++ util/ui/ui_for_push_test.go
@@ -35,7 +35,7 @@ var _ = Describe("UI", func() {
 	})
 
 	Describe("DisplayChangesForPush", func() {
-		It("alings all the string types", func() {
+		It("aligns all the string types", func() {
 			changeSet := []Change{
 				{
 					Header:       "h1",
