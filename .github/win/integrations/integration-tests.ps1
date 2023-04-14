7c6c07e7c1eec2440184bc3c9c984550a1f3a7e0
Shwetha Gururaj <gururajsh@vmware.com>
2023-04-14 17:32:40 -0400

    GHA Integration tests Review Changes (#2403)
    Fix GitHub Action tests and upgrade golang
    
    - Upgrade go from 1.18 to 1.19
    
    Co-authored-by: Al Berez <aberezovsky@vmware.com>

--- .github/win/integrations/integration-tests.ps1
+++ .github/win/integrations/integration-tests.ps1
@@ -28,7 +28,7 @@ $Env:PATH="$Env:HOME\go\bin;" + "$Env:PATH"
 $Env:PATH="$Env:GOPATH\bin;" + "$Env:PATH"
 $Env:PATH="$Env:GOROOT\bin;" + "$Env:PATH"
 $Env:PATH="$pwd;" + "$Env:PATH"
-$Env:PATH="$pwd\cli\out;" + "$Env:PATH"
+$Env:PATH="$pwd\out;" + "$Env:PATH"
 
 # This is for DEBUG
 # function Get-Env-Info {
@@ -63,15 +63,23 @@ $Env:CF_DIAL_TIMEOUT=15
 
 Import-Certificate -Filepath "$pwd\$CF_INT_NAME.router.ca" -CertStoreLocation "cert:\LocalMachine\root"
 
+pushd $pwd\cf-cli-binaries
+	7z e cf-cli-windows-binaries.tgz -y
+	7z x cf-cli-windows-binaries.tar -y
+	Move-Item -Path $pwd\cf-cli_winx64.exe  -Destination ..\cf.exe -Force
+popd
+
 New-Item "go/src/code.cloudfoundry.org" -Type Directory
-New-Item -ItemType SymbolicLink -Path "$pwd/go/src/code.cloudfoundry.org/cli" -Target "$pwd/cli"
+New-Item -ItemType SymbolicLink -Path "$pwd/go/src/code.cloudfoundry.org/cli" -Target "$pwd"
 
 cd go/src/code.cloudfoundry.org/cli
-
 go install github.com/akavel/rsrc@v0.10.2
 
+Get-Command make
+Get-Item Makefile
+
 make out/cf-cli_winx64.exe
-Move-Item -Path ./out/cf-cli_winx64.exe  -Destination ./out/cf.exe -Force
+Move-Item -Path $pwd\out\cf-cli_winx64.exe  -Destination $pwd\cf.exe -Force
 
 cf.exe api $Env:CF_INT_API --skip-ssl-validation
 cf.exe auth admin $Env:CF_INT_PASSWORD
