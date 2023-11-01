09fc7424a6e6ba5084997b45d928680bfae6a7ca
M. Oleske <moleske@pivotal.io>
2023-11-01 10:29:38 -0700

    Bump go-log-cache and go-loggregator to latest
    - this keeps us up to date since they maintain those versions

--- actor/sharedaction/logging.go
+++ actor/sharedaction/logging.go
@@ -8,9 +8,9 @@ import (
 	"strings"
 	"time"
 
-	logcache "code.cloudfoundry.org/go-log-cache"
-	"code.cloudfoundry.org/go-log-cache/rpc/logcache_v1"
-	"code.cloudfoundry.org/go-loggregator/v8/rpc/loggregator_v2"
+	logcache "code.cloudfoundry.org/go-log-cache/v2"
+	"code.cloudfoundry.org/go-log-cache/v2/rpc/logcache_v1"
+	"code.cloudfoundry.org/go-loggregator/v9/rpc/loggregator_v2"
 	"github.com/sirupsen/logrus"
 )
 
