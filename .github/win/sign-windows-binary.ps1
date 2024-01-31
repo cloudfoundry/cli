3d6c78befcd33c698625290a9899b341d935212b
George Gelashvili <george.gelashvili@broadcom.com>
2024-01-31 15:07:16 -0800

    Change windows release build and sign to use Digicert Keylocker signing mechanism
    Co-Authored-By: Michael Oleske <michael.oleske@broadcom.com>
    Co-Authored-By: Al Berez <al.berez@broadcom.com>

--- .github/win/sign-windows-binary.ps1
+++
@@ -1,9 +0,0 @@
-# expected environment variables
-# SIGNING_KEY_WINDOWS_PASSPHRASE
-
-param ($BinaryFilePath)
-
-# add PATH to signtool.exe
-$env:PATH="$env:PATH;C:\Program Files (x86)\Windows Kits\10\bin\x64"
-
-signtool sign /v /p "$env:SIGNING_KEY_WINDOWS_PASSPHRASE" /fd SHA256 /f "$env:RUNNER_TEMP\cert.pfx" "$BinaryFilePath"
