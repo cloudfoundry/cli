fb2c85d6dd721cc8b37703711fded53734ff77d4
Shwetha Gururaj <shwetha.gururaj@broadcom.com>
2024-07-14 16:29:24 -0400

    Use latest CAPI

--- integration/shared/isolated/error_handling_test.go
+++ integration/shared/isolated/error_handling_test.go
@@ -19,7 +19,7 @@ var _ = Describe("curl command", func() {
 		It("returns the expected request with verbose output", func() {
 			session := helpers.CF("curl", "-v", "/v2/banana")
 			Eventually(session).Should(Say("GET /v2/banana HTTP/1.1"))
-			Eventually(session).Should(Say(`"error_code": "CF-NotFound"`))
+			Eventually(session).Should(Say(`"error_code":"CF-NotFound"`))
 			Eventually(session).Should(Exit(0))
 		})
 	})
