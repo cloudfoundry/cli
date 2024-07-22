6d9dc1f844e75e33ba35186cc395593764b505d3
Shwetha Gururaj <shwetha.gururaj@broadcom.com>
2024-07-22 13:28:40 -0400

    Update comment

--- api/cloudcontroller/ccv3/constant/deployment.go
+++ api/cloudcontroller/ccv3/constant/deployment.go
@@ -14,7 +14,7 @@ const (
 	// DeploymentDeployed means the deployment is in state 'DEPLOYED'
 	DeploymentDeployed DeploymentState = "DEPLOYED"
 
-	// DeploymentCanceled means the deployment is in state 'CANCELING'
+	// DeploymentCanceling means the deployment is in state 'CANCELING'
 	DeploymentCanceling DeploymentState = "CANCELING"
 
 	// DeploymentFailing means the deployment is in state 'FAILING'
