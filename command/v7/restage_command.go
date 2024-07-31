1bf12acb8fdd06c4471ab14cf3c1477d1d951096
Shwetha Gururaj <shwetha.gururaj@broadcom.com>
2024-07-31 13:46:47 -0400

    Update command/v7/restage_command.go
    Co-authored-by: Sam Gunaratne <385176+Samze@users.noreply.github.com>

--- command/v7/restage_command.go
+++ command/v7/restage_command.go
@@ -15,7 +15,7 @@ type RestageCommand struct {
 	BaseCommand
 
 	RequiredArgs        flag.AppName            `positional-args:"yes"`
-	Strategy            flag.DeploymentStrategy `long:"strategy" description:"Deployment strategy can be canaray, rolling or null."`
+	Strategy            flag.DeploymentStrategy `long:"strategy" description:"Deployment strategy can be canary, rolling or null."`
 	NoWait              bool                    `long:"no-wait" description:"Exit when the first instance of the web process is healthy"`
 	usage               interface{}             `usage:"CF_NAME restage APP_NAME\n\n   This command will cause downtime unless you use '--strategy' flag.\n\nEXAMPLES:\n   CF_NAME restage APP_NAME\n   CF_NAME restage APP_NAME --strategy rolling\n   CF_NAME restage APP_NAME --strategy canray --no-wait"`
 	relatedCommands     interface{}             `related_commands:"restart"`
