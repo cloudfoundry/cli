5d436166f565b3209fcd778213921696a380b98a
Shwetha Gururaj <shwetha.gururaj@broadcom.com>
2024-08-27 16:24:24 -0400

    Replace cflinuxfs3 references with cflinuxfs4

--- command/v7/unset_label_command.go
+++ command/v7/unset_label_command.go
@@ -16,7 +16,7 @@ type UnsetLabelCommand struct {
 	BaseCommand
 
 	RequiredArgs    flag.UnsetLabelArgs `positional-args:"yes"`
-	usage           interface{}         `usage:"CF_NAME unset-label RESOURCE RESOURCE_NAME KEY...\n\nEXAMPLES:\n   cf unset-label app dora ci_signature_sha2\n   cf unset-label org business pci public-facing\n   cf unset-label buildpack go_buildpack go -s cflinuxfs3\n\nRESOURCES:\n   app\n   buildpack\n   domain\n   org\n   route\n   service-broker\n   service-offering\n   service-plan\n   space\n   stack"`
+	usage           interface{}         `usage:"CF_NAME unset-label RESOURCE RESOURCE_NAME KEY...\n\nEXAMPLES:\n   cf unset-label app dora ci_signature_sha2\n   cf unset-label org business pci public-facing\n   cf unset-label buildpack go_buildpack go -s cflinuxfs4\n\nRESOURCES:\n   app\n   buildpack\n   domain\n   org\n   route\n   service-broker\n   service-offering\n   service-plan\n   space\n   stack"`
 	relatedCommands interface{}         `related_commands:"labels, set-label"`
 	BuildpackStack  string              `long:"stack" short:"s" description:"Specify stack to disambiguate buildpacks with the same name"`
 	ServiceBroker   string              `long:"broker" short:"b" description:"Specify a service broker to disambiguate service offerings or service plans with the same name."`
