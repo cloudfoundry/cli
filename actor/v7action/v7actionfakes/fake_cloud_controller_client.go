9bc60906ff0a67f9ae40305ee915738a885cbea6
Shwetha Gururaj <shwetha.gururaj@broadcom.com>
2024-12-19 13:14:06 -0500

    rebase

--- actor/v7action/v7actionfakes/fake_cloud_controller_client.go
+++ actor/v7action/v7actionfakes/fake_cloud_controller_client.go
@@ -14,26 +14,6 @@ import (
 )
 
 type FakeCloudControllerClient struct {
-	AppSSHEndpointStub        func() string
-	appSSHEndpointMutex       sync.RWMutex
-	appSSHEndpointArgsForCall []struct {
-	}
-	appSSHEndpointReturns struct {
-		result1 string
-	}
-	appSSHEndpointReturnsOnCall map[int]struct {
-		result1 string
-	}
-	AppSSHHostKeyFingerprintStub        func() string
-	appSSHHostKeyFingerprintMutex       sync.RWMutex
-	appSSHHostKeyFingerprintArgsForCall []struct {
-	}
-	appSSHHostKeyFingerprintReturns struct {
-		result1 string
-	}
-	appSSHHostKeyFingerprintReturnsOnCall map[int]struct {
-		result1 string
-	}
 	ApplyOrganizationQuotaStub        func(string, string) (resources.RelationshipList, ccv3.Warnings, error)
 	applyOrganizationQuotaMutex       sync.RWMutex
 	applyOrganizationQuotaArgsForCall []struct {
@@ -1452,21 +1432,6 @@ type FakeCloudControllerClient struct {
 		result2 ccv3.Warnings
 		result3 error
 	}
-	GetRevisionStub        func(string) (resources.Revision, ccv3.Warnings, error)
-	getRevisionMutex       sync.RWMutex
-	getRevisionArgsForCall []struct {
-		arg1 string
-	}
-	getRevisionReturns struct {
-		result1 resources.Revision
-		result2 ccv3.Warnings
-		result3 error
-	}
-	getRevisionReturnsOnCall map[int]struct {
-		result1 resources.Revision
-		result2 ccv3.Warnings
-		result3 error
-	}
 	GetRolesStub        func(...ccv3.Query) ([]resources.Role, ccv3.IncludedResources, ccv3.Warnings, error)
 	getRolesMutex       sync.RWMutex
 	getRolesArgsForCall []struct {
@@ -2815,112 +2780,6 @@ type FakeCloudControllerClient struct {
 	}
 	invocations      map[string][][]interface{}
 	invocationsMutex sync.RWMutex
-}
-
-func (fake *FakeCloudControllerClient) AppSSHEndpoint() string {
-	fake.appSSHEndpointMutex.Lock()
-	ret, specificReturn := fake.appSSHEndpointReturnsOnCall[len(fake.appSSHEndpointArgsForCall)]
-	fake.appSSHEndpointArgsForCall = append(fake.appSSHEndpointArgsForCall, struct {
-	}{})
-	stub := fake.AppSSHEndpointStub
-	fakeReturns := fake.appSSHEndpointReturns
-	fake.recordInvocation("AppSSHEndpoint", []interface{}{})
-	fake.appSSHEndpointMutex.Unlock()
-	if stub != nil {
-		return stub()
-	}
-	if specificReturn {
-		return ret.result1
-	}
-	return fakeReturns.result1
-}
-
-func (fake *FakeCloudControllerClient) AppSSHEndpointCallCount() int {
-	fake.appSSHEndpointMutex.RLock()
-	defer fake.appSSHEndpointMutex.RUnlock()
-	return len(fake.appSSHEndpointArgsForCall)
-}
-
-func (fake *FakeCloudControllerClient) AppSSHEndpointCalls(stub func() string) {
-	fake.appSSHEndpointMutex.Lock()
-	defer fake.appSSHEndpointMutex.Unlock()
-	fake.AppSSHEndpointStub = stub
-}
-
-func (fake *FakeCloudControllerClient) AppSSHEndpointReturns(result1 string) {
-	fake.appSSHEndpointMutex.Lock()
-	defer fake.appSSHEndpointMutex.Unlock()
-	fake.AppSSHEndpointStub = nil
-	fake.appSSHEndpointReturns = struct {
-		result1 string
-	}{result1}
-}
-
-func (fake *FakeCloudControllerClient) AppSSHEndpointReturnsOnCall(i int, result1 string) {
-	fake.appSSHEndpointMutex.Lock()
-	defer fake.appSSHEndpointMutex.Unlock()
-	fake.AppSSHEndpointStub = nil
-	if fake.appSSHEndpointReturnsOnCall == nil {
-		fake.appSSHEndpointReturnsOnCall = make(map[int]struct {
-			result1 string
-		})
-	}
-	fake.appSSHEndpointReturnsOnCall[i] = struct {
-		result1 string
-	}{result1}
-}
-
-func (fake *FakeCloudControllerClient) AppSSHHostKeyFingerprint() string {
-	fake.appSSHHostKeyFingerprintMutex.Lock()
-	ret, specificReturn := fake.appSSHHostKeyFingerprintReturnsOnCall[len(fake.appSSHHostKeyFingerprintArgsForCall)]
-	fake.appSSHHostKeyFingerprintArgsForCall = append(fake.appSSHHostKeyFingerprintArgsForCall, struct {
-	}{})
-	stub := fake.AppSSHHostKeyFingerprintStub
-	fakeReturns := fake.appSSHHostKeyFingerprintReturns
-	fake.recordInvocation("AppSSHHostKeyFingerprint", []interface{}{})
-	fake.appSSHHostKeyFingerprintMutex.Unlock()
-	if stub != nil {
-		return stub()
-	}
-	if specificReturn {
-		return ret.result1
-	}
-	return fakeReturns.result1
-}
-
-func (fake *FakeCloudControllerClient) AppSSHHostKeyFingerprintCallCount() int {
-	fake.appSSHHostKeyFingerprintMutex.RLock()
-	defer fake.appSSHHostKeyFingerprintMutex.RUnlock()
-	return len(fake.appSSHHostKeyFingerprintArgsForCall)
-}
-
-func (fake *FakeCloudControllerClient) AppSSHHostKeyFingerprintCalls(stub func() string) {
-	fake.appSSHHostKeyFingerprintMutex.Lock()
-	defer fake.appSSHHostKeyFingerprintMutex.Unlock()
-	fake.AppSSHHostKeyFingerprintStub = stub
-}
-
-func (fake *FakeCloudControllerClient) AppSSHHostKeyFingerprintReturns(result1 string) {
-	fake.appSSHHostKeyFingerprintMutex.Lock()
-	defer fake.appSSHHostKeyFingerprintMutex.Unlock()
-	fake.AppSSHHostKeyFingerprintStub = nil
-	fake.appSSHHostKeyFingerprintReturns = struct {
-		result1 string
-	}{result1}
-}
-
-func (fake *FakeCloudControllerClient) AppSSHHostKeyFingerprintReturnsOnCall(i int, result1 string) {
-	fake.appSSHHostKeyFingerprintMutex.Lock()
-	defer fake.appSSHHostKeyFingerprintMutex.Unlock()
-	fake.AppSSHHostKeyFingerprintStub = nil
-	if fake.appSSHHostKeyFingerprintReturnsOnCall == nil {
-		fake.appSSHHostKeyFingerprintReturnsOnCall = make(map[int]struct {
-			result1 string
-		})
-	}
-	fake.appSSHHostKeyFingerprintReturnsOnCall[i] = struct {
-		result1 string
-	}{result1}
 }
 
 func (fake *FakeCloudControllerClient) ApplyOrganizationQuota(arg1 string, arg2 string) (resources.RelationshipList, ccv3.Warnings, error) {
@@ -9214,73 +9073,6 @@ func (fake *FakeCloudControllerClient) GetProcessesReturnsOnCall(i int, result1 
 	}{result1, result2, result3}
 }
 
-func (fake *FakeCloudControllerClient) GetRevision(arg1 string) (resources.Revision, ccv3.Warnings, error) {
-	fake.getRevisionMutex.Lock()
-	ret, specificReturn := fake.getRevisionReturnsOnCall[len(fake.getRevisionArgsForCall)]
-	fake.getRevisionArgsForCall = append(fake.getRevisionArgsForCall, struct {
-		arg1 string
-	}{arg1})
-	stub := fake.GetRevisionStub
-	fakeReturns := fake.getRevisionReturns
-	fake.recordInvocation("GetRevision", []interface{}{arg1})
-	fake.getRevisionMutex.Unlock()
-	if stub != nil {
-		return stub(arg1)
-	}
-	if specificReturn {
-		return ret.result1, ret.result2, ret.result3
-	}
-	return fakeReturns.result1, fakeReturns.result2, fakeReturns.result3
-}
-
-func (fake *FakeCloudControllerClient) GetRevisionCallCount() int {
-	fake.getRevisionMutex.RLock()
-	defer fake.getRevisionMutex.RUnlock()
-	return len(fake.getRevisionArgsForCall)
-}
-
-func (fake *FakeCloudControllerClient) GetRevisionCalls(stub func(string) (resources.Revision, ccv3.Warnings, error)) {
-	fake.getRevisionMutex.Lock()
-	defer fake.getRevisionMutex.Unlock()
-	fake.GetRevisionStub = stub
-}
-
-func (fake *FakeCloudControllerClient) GetRevisionArgsForCall(i int) string {
-	fake.getRevisionMutex.RLock()
-	defer fake.getRevisionMutex.RUnlock()
-	argsForCall := fake.getRevisionArgsForCall[i]
-	return argsForCall.arg1
-}
-
-func (fake *FakeCloudControllerClient) GetRevisionReturns(result1 resources.Revision, result2 ccv3.Warnings, result3 error) {
-	fake.getRevisionMutex.Lock()
-	defer fake.getRevisionMutex.Unlock()
-	fake.GetRevisionStub = nil
-	fake.getRevisionReturns = struct {
-		result1 resources.Revision
-		result2 ccv3.Warnings
-		result3 error
-	}{result1, result2, result3}
-}
-
-func (fake *FakeCloudControllerClient) GetRevisionReturnsOnCall(i int, result1 resources.Revision, result2 ccv3.Warnings, result3 error) {
-	fake.getRevisionMutex.Lock()
-	defer fake.getRevisionMutex.Unlock()
-	fake.GetRevisionStub = nil
-	if fake.getRevisionReturnsOnCall == nil {
-		fake.getRevisionReturnsOnCall = make(map[int]struct {
-			result1 resources.Revision
-			result2 ccv3.Warnings
-			result3 error
-		})
-	}
-	fake.getRevisionReturnsOnCall[i] = struct {
-		result1 resources.Revision
-		result2 ccv3.Warnings
-		result3 error
-	}{result1, result2, result3}
-}
-
 func (fake *FakeCloudControllerClient) GetRoles(arg1 ...ccv3.Query) ([]resources.Role, ccv3.IncludedResources, ccv3.Warnings, error) {
 	fake.getRolesMutex.Lock()
 	ret, specificReturn := fake.getRolesReturnsOnCall[len(fake.getRolesArgsForCall)]
@@ -15248,10 +15040,6 @@ func (fake *FakeCloudControllerClient) WhoAmIReturnsOnCall(i int, result1 resour
 func (fake *FakeCloudControllerClient) Invocations() map[string][][]interface{} {
 	fake.invocationsMutex.RLock()
 	defer fake.invocationsMutex.RUnlock()
-	fake.appSSHEndpointMutex.RLock()
-	defer fake.appSSHEndpointMutex.RUnlock()
-	fake.appSSHHostKeyFingerprintMutex.RLock()
-	defer fake.appSSHHostKeyFingerprintMutex.RUnlock()
 	fake.applyOrganizationQuotaMutex.RLock()
 	defer fake.applyOrganizationQuotaMutex.RUnlock()
 	fake.applySpaceQuotaMutex.RLock()
@@ -15440,8 +15228,6 @@ func (fake *FakeCloudControllerClient) Invocations() map[string][][]interface{} 
 	defer fake.getProcessSidecarsMutex.RUnlock()
 	fake.getProcessesMutex.RLock()
 	defer fake.getProcessesMutex.RUnlock()
-	fake.getRevisionMutex.RLock()
-	defer fake.getRevisionMutex.RUnlock()
 	fake.getRolesMutex.RLock()
 	defer fake.getRolesMutex.RUnlock()
 	fake.getRouteBindingsMutex.RLock()
