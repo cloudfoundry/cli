56a9737b6ca3509a3aa9133af12136431b4a2e19
jcvrabo <122891959+jcvrabo@users.noreply.github.com>
2025-01-03 17:52:21 +0100

    fix max-inflight restage command test

--- integration/v7/isolated/restage_command_test.go
+++ integration/v7/isolated/restage_command_test.go
@@ -146,13 +146,29 @@ var _ = Describe("restage command", func() {
 						userName, _ := helpers.GetCredentials()
 						session := helpers.CustomCF(helpers.CFEnv{
 							EnvVars: map[string]string{"CF_STARTUP_TIMEOUT": "0.1"},
-						}, "restage", appName, "--strategy", "rolling", "--max-in-flight", "3")
+						}, "restage", appName, "--strategy", "rolling")
 						Consistently(session.Err).ShouldNot(Say(`This action will cause app downtime\.`))
 						Eventually(session).Should(Say(`Restaging app %s in org %s / space %s as %s\.\.\.`, appName, orgName, spaceName, userName))
 						Eventually(session).Should(Say(`Creating deployment for app %s\.\.\.`, appName))
 						Eventually(session).Should(Say(`Waiting for app to deploy\.\.\.`))
 						Eventually(session.Err).Should(Say(`Start app timeout`))
 						Eventually(session.Err).Should(Say(`TIP: Application must be listening on the right port\.`))
+						Eventually(session).Should(Say("FAILED"))
+						Eventually(session).Should(Exit(1))
+					})
+				})
+
+				When("strategy rolling is given with max-in-flight", func() {
+					It("fails and displays the deployment failure message", func() {
+						userName, _ := helpers.GetCredentials()
+						session := helpers.CustomCF(helpers.CFEnv{
+							EnvVars: map[string]string{"CF_STARTUP_TIMEOUT": "0.1"},
+						}, "restage", appName, "--strategy", "rolling", "--max-in-flight", "3")
+						Consistently(session.Err).ShouldNot(Say(`This action will cause app downtime\.`))
+						Eventually(session).Should(Say(`Restaging app %s in org %s / space %s as %s\.\.\.`, appName, orgName, spaceName, userName))
+						Eventually(session).Should(Say(`Creating deployment for app %s\.\.\.`, appName))
+						Eventually(session).Should(Say(`Waiting for app to deploy\.\.\.`))
+						Eventually(session.Err).Should(Say(`Cannot cancel a deployment with status: FINALIZED and reason: DEPLOYED`))
 						Eventually(session).Should(Say("FAILED"))
 						Eventually(session).Should(Exit(1))
 					})
