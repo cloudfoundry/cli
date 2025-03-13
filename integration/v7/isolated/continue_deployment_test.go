6b781f06109404e245e64336181787c8a2075e3e
Sam Gunaratne <sgunaratne@vmware.com>
2025-03-13 13:46:48 -0600

    Fix canary steps tests

--- integration/v7/isolated/continue_deployment_test.go
+++ integration/v7/isolated/continue_deployment_test.go
@@ -95,8 +95,10 @@ var _ = Describe("Continue Deployment", func() {
 							helpers.CF("push", appName, "-p", appDir, "--strategy=canary", "--instance-steps 10,20,30,70", "-i 5").Wait()
 						})
 
-						session := helpers.CF("continue-deployment", appName)
+						session := helpers.CF("app", appName)
 						Eventually(session).Should(Say("canary-steps:    1/4"))
+						session = helpers.CF("continue-deployment", appName)
+						Eventually(session).Should(Say("canary-steps:    2/4"))
 						Eventually(session).Should(Exit(0))
 					})
 				})
