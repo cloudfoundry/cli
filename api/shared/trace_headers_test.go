1f3fb560bf28aadaee2db16f5c684b9572ed9bdf
Sam Gunaratne <385176+Samze@users.noreply.github.com>
2025-03-27 08:54:34 -0600

    Add B3 trace IDs to cf cli commands (#3310)
    * Add B3 trace IDs to cf cli commands
    * Auto set span headers for each request

---
+++ api/shared/trace_headers_test.go
@@ -0,0 +1,42 @@
+package shared_test
+
+import (
+	"net/http"
+
+	. "code.cloudfoundry.org/cli/api/shared"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("B3 Trace Headers", func() {
+	Describe("SetHeaders", func() {
+		Context("when there are already headers set", func() {
+			It("does not add the headers", func() {
+				traceHeaders := NewTraceHeaders("new_trace_id")
+				request := &http.Request{
+					Header: http.Header{},
+				}
+				request.Header.Set("X-B3-TraceId", "old_trace_id")
+				request.Header.Set("X-B3-SpanId", "old_span_id")
+				traceHeaders.SetHeaders(request)
+
+				Expect(request.Header.Get("X-B3-TraceId")).To(Equal("old_trace_id"))
+				Expect(request.Header.Get("X-B3-SpanId")).To(Equal("old_span_id"))
+			})
+		})
+
+		Context("when there are no headers set", func() {
+			It("adds the headers", func() {
+				traceHeaders := NewTraceHeaders("new_trace_id")
+				request := &http.Request{
+					Header: http.Header{},
+				}
+				traceHeaders.SetHeaders(request)
+
+				Expect(request.Header.Get("X-B3-TraceId")).To(Equal("new_trace_id"))
+				Expect(request.Header.Get("X-B3-SpanId")).ToNot(BeEmpty())
+			})
+		})
+	})
+})
