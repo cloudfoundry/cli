070c506e894560bc0ead7b39aec1aad49b5b1cc1
AL Berez <a-b@users.noreply.github.com>
2025-04-24 13:09:13 -0700

    Set custom version (#3491)

--- version/version.go
+++ version/version.go
@@ -1,6 +1,8 @@
 package version
 
 import (
+	"fmt"
+	"os"
 	"strings"
 
 	"github.com/blang/semver/v4"
@@ -12,9 +14,32 @@ var (
 	binaryVersion   string
 	binarySHA       string
 	binaryBuildDate string
+	customVersion   string
+
+	ExitFunc = os.Exit
 )
 
+func SetVersion(version string) {
+	if version == "" {
+		customVersion = ""
+		return
+	}
+
+	_, err := semver.Parse(version)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Invalid semantic version format: %s\n", err)
+		ExitFunc(1)
+	}
+
+	customVersion = version
+}
+
 func VersionString() string {
+	if customVersion != "" {
+		return customVersion
+	}
+
 	// Remove the "v" prefix from the binary in case it is present
 	binaryVersion = strings.TrimPrefix(binaryVersion, "v")
 	versionString, err := semver.Make(binaryVersion)
