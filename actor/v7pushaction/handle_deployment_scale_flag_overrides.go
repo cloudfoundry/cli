f4f433425621607f1b8cd905e8a8724d0470a2d1
Prem Kumar Kalle <prem.kalle@broadcom.com>
2025-07-08 22:57:31 -0700

    cf push to use deployment scale options for rolling and canary deployments

---
+++ actor/v7pushaction/handle_deployment_scale_flag_overrides.go
@@ -0,0 +1,22 @@
+package v7pushaction
+
+import (
+	"code.cloudfoundry.org/cli/util/manifestparser"
+)
+
+func (actor Actor) HandleDeploymentScaleFlagOverrides(
+	baseManifest manifestparser.Manifest,
+	flagOverrides FlagOverrides,
+) (manifestparser.Manifest, error) {
+	newManifest := baseManifest
+
+	for _, transformPlan := range actor.TransformManifestSequenceForDeployment {
+		var err error
+		newManifest, err = transformPlan(newManifest, flagOverrides)
+		if err != nil {
+			return manifestparser.Manifest{}, err
+		}
+	}
+
+	return newManifest, nil
+}
