281993a11de73349f522a6590f6919e08efc3e64
Jochen Ehret <jochen.ehret@sap.com>
2026-01-15 15:59:13 +0100

    Small fixes (contribution docu, integration tests)
    * use "command" constant for env setup tests
    * update branches in contribution guide
    * remove "units-full" goal in testing guide (is same as "units" now)

--- integration/v7/isolated/bind_service_command_test.go
+++ integration/v7/isolated/bind_service_command_test.go
@@ -132,7 +132,7 @@ var _ = Describe("bind-service command", func() {
 
 	When("the environment is not setup correctly", func() {
 		It("fails with the appropriate errors", func() {
-			helpers.CheckEnvironmentTargetedCorrectly(true, true, ReadOnlyOrg, "bind-service", "app-name", "service-name")
+			helpers.CheckEnvironmentTargetedCorrectly(true, true, ReadOnlyOrg, command, "app-name", "service-name")
 		})
 	})
 
