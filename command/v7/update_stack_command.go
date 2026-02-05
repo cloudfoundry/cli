97ae11691b1003dd2c97ca2ba1a8b06142145d0f
Simon Jones <simonjones@vmware.com>
2026-02-05 11:44:29 -0500

    Add --reason flag to update-stack command

--- command/v7/update_stack_command.go
+++ command/v7/update_stack_command.go
@@ -15,7 +15,8 @@ type UpdateStackCommand struct {
 
 	RequiredArgs    flag.StackName `positional-args:"yes"`
 	State           string         `long:"state" description:"State to transition the stack to (active, restricted, deprecated, disabled)" required:"true"`
-	usage           interface{}    `usage:"CF_NAME update-stack STACK_NAME [--state (active | restricted | deprecated | disabled)]\n\nEXAMPLES:\n   CF_NAME update-stack cflinuxfs3 --state disabled"`
+	Reason          string         `long:"reason" description:"Optional plain text describing the stack state change"`
+	usage           interface{}    `usage:"CF_NAME update-stack STACK_NAME [--state (active | restricted | deprecated | disabled)] [--reason REASON]\n\nEXAMPLES:\n   CF_NAME update-stack cflinuxfs3 --state disabled\n   CF_NAME update-stack cflinuxfs3 --state deprecated --reason 'Use cflinuxfs4 instead'"`
 	relatedCommands interface{}    `related_commands:"stack, stacks"`
 }
 
@@ -56,7 +57,7 @@ func (cmd UpdateStackCommand) Execute(args []string) error {
 	}
 
 	// Update the stack
-	updatedStack, warnings, err := cmd.Actor.UpdateStack(stack.GUID, stateValue)
+	updatedStack, warnings, err := cmd.Actor.UpdateStack(stack.GUID, stateValue, cmd.Reason)
 	cmd.UI.DisplayWarnings(warnings)
 	if err != nil {
 		return err
@@ -66,11 +67,18 @@ func (cmd UpdateStackCommand) Execute(args []string) error {
 	cmd.UI.DisplayNewline()
 
 	// Display the updated stack info
-	cmd.UI.DisplayKeyValueTable("", [][]string{
+	displayTable := [][]string{
 		{cmd.UI.TranslateText("name:"), updatedStack.Name},
 		{cmd.UI.TranslateText("description:"), updatedStack.Description},
 		{cmd.UI.TranslateText("state:"), updatedStack.State},
-	}, 3)
+	}
+
+	// Add reason if it's present
+	if updatedStack.StateReason != "" {
+		displayTable = append(displayTable, []string{cmd.UI.TranslateText("reason:"), updatedStack.StateReason})
+	}
+
+	cmd.UI.DisplayKeyValueTable("", displayTable, 3)
 
 	return nil
 }
