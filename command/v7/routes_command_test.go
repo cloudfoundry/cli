2335f734d63a4df5d66163433c737f4d24df8273
Seth Boyles <sboyles@pivotal.io>
2026-03-03 22:08:16 -0800

    Allow setting the route destination port via the CF CLI map-route command using a new app-port parameter (#3733)
    Co-authored-by: Maurice Brinkmann <Maurice.Brinkmann@mail.schwarz>

--- command/v7/routes_command_test.go
+++ command/v7/routes_command_test.go
@@ -29,7 +29,7 @@ var _ = Describe("routes Command", func() {
 		binaryName      string
 	)
 
-	const tableHeaders = `space\s+host\s+domain\s+port\s+path\s+protocol\s+app-protocol\s+apps\s+service instance`
+	const tableHeaders = `space\s+host\s+domain\s+port\s+path\s+protocol\s+app-protocol\s+app-port\s+apps\s+service instance`
 
 	BeforeEach(func() {
 		testUI = ui.NewTestUI(nil, NewBuffer(), NewBuffer())
